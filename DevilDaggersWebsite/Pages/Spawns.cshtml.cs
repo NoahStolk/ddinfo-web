f07c8d875298bb033df704bf6515a5613911bc36
Noah Stolk <noah.stolk1997@gmail.com>
2018-05-08 21:07:26 +0200

    Add spawns page.

---
+++ DevilDaggersWebsite/Pages/Spawns.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+
+namespace DevilDaggersWebsite.Pages
+{
+	public class SpawnsModel : PageModel
+    {
+		public string V3File { get; set; }
+
+		private readonly IHostingEnvironment _env;
+
+		public SpawnsModel(IHostingEnvironment env)
+		{
+			_env = env;
+		}
+
+		public void OnGet()
+        {
+			string webRoot = _env.WebRootPath;
+
+			V3File = Path.Combine(webRoot, string.Format(@"spawnsets/V3_Sorath"));
+		}
+    }
+}
