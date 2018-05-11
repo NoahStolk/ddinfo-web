1fd43ba0ade23a9074755020035844f40e5fc2ed
Noah Stolk <noah.stolk1997@gmail.com>
2018-05-11 19:48:24 +0200

    Add Enemies and Daggers pages.

---
+++ DevilDaggersWebsite/Pages/Daggers.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevilDaggersWebsite.Pages
+{
+	public class DaggersModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ DevilDaggersWebsite/Pages/Enemies.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevilDaggersWebsite.Pages
+{
+	public class EnemiesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
