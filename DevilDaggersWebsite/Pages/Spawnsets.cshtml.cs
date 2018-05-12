e200918d251b8987ad2ee5eaa66b58b9c3d1c790
Noah Stolk <noah.stolk1997@gmail.com>
2018-05-12 14:58:11 +0200

    Add Spawnsets and Spawnset pages.

---
+++ DevilDaggersWebsite/Pages/Spawnsets.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevilDaggersWebsite.Pages
+{
+	public class SpawnsetsModel : PageModel
+	{
+		public IHostingEnvironment Env { get; private set; }
+
+		public SpawnsetsModel(IHostingEnvironment env)
+		{
+			Env = env;
+		}
+    }
+}
