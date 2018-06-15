a883d48f3f5f5ff974c899e5db6d081063e64bd2
Noah Stolk <noah.stolk1997@gmail.com>
2018-06-15 23:43:02 +0200

    no message

--- DevilDaggersWebsite/Startup.cs
+++ DevilDaggersWebsite/Startup.cs
@@ -1,6 +1,7 @@
 using CoreBase;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,21 @@ namespace DevilDaggersWebsite
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			// TODO: Fix regex
+			RewriteOptions rewrite = new RewriteOptions()
+				.AddRedirect("Home", "Index")
+				.AddRedirect("Home/Index", "Index")
+				.AddRedirect("Home/Leaderboard", "Leaderboard")
+				.AddRedirect("Home/Spawns", "Spawns")
+				.AddRedirect("Home/Hands", "Upgrades")
+				.AddRedirect("Home/Enemies", "Enemies")
+				.AddRedirect("Home/Daggers", "Daggers")
+				.AddRedirect("Home/Spawnsets", "Spawnsets")
+				.AddRedirect("Home/SpawnsetsInfo", "Spawnsets")
+				.AddRedirect("Home/Donations", "Donations");
+
+			app.UseRewriter(rewrite);
+
 			base.ConfigureErrorPageAndSSL(app, env);
 
 			app.UseStaticFiles();
