6856c2491bfedbdb22af179b93097fddaf0ab468
Noah Stolk <noah.stolk1997@gmail.com>
2018-06-23 16:34:15 +0200

    no message

--- DevilDaggersWebsite/wwwroot/js-site/leaderboard.js
+++ DevilDaggersWebsite/wwwroot/js-site/leaderboard.js
@@ -5,4 +5,16 @@
 
 		$(".leaderboard-expand").not("#" + id + "-expand").removeClass('expand');
 	});
+	
+	$(document).on("click", ".sorter", function () {
+		var sorter = $(this);
+
+		var sort = $('.sort').sort(function (a, b) {
+			var contentA = parseInt($(a).attr(sorter.attr('sort')));
+			var contentB = parseInt($(b).attr(sorter.attr('sort')));
+			return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+		});
+
+		$(".leaderboard-body").html(sort);
+	});
 });
