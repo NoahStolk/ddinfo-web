8166b82086c1c1d011f0ee14b96b6877257afe8d
Noah Stolk <noah.stolk1997@gmail.com>
2018-07-04 00:10:22 +0200

    no message

--- DevilDaggersWebsite/Pages/DownloadSpawnset.cshtml.cs
+++ DevilDaggersWebsite/Pages/DownloadSpawnset.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.IO;
 using System.Net.Mime;
@@ -8,13 +7,6 @@ namespace DevilDaggersWebsite.Pages
 {
 	public class DownloadSpawnsetModel : PageModel
 	{
-		private IHostingEnvironment _env;
-
-		public DownloadSpawnsetModel(IHostingEnvironment env)
-		{
-			_env = env;
-		}
-
 		public ActionResult OnGet(string file)
 		{
 			return File(Path.Combine("spawnsets", file), MediaTypeNames.Application.Octet, file);
