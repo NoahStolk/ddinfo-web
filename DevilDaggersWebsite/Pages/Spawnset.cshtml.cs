ab118e679f1bf44b10880e1e524adf629ad12458
Noah Stolk <noah.stolk1997@gmail.com>
2018-08-09 22:21:41 +0200

    Spawnset page refactoring and fixes.

--- DevilDaggersWebsite/Pages/Spawnset.cshtml.cs
+++ DevilDaggersWebsite/Pages/Spawnset.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using CoreBase.Services;
 using DevilDaggersWebsite.Models.Spawnset;
+using DevilDaggersWebsite.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -10,11 +11,10 @@ namespace DevilDaggersWebsite.Pages
 {
 	public class SpawnsetModel : PageModel
 	{
-		public string Query { get; set; }
-
-		public SpawnsetFile SpawnsetFile { get; set; }
-
-		public string Description { get; set; }
+		public string Query { get; private set; }
+		public SpawnsetFile SpawnsetFile { get; private set; }
+		public Spawnset Spawnset { get; private set; }
+		public string Description { get; private set; }
 
 		public ICommonObjects CommonObjects { get; }
 
@@ -28,9 +28,8 @@ namespace DevilDaggersWebsite.Pages
 			try
 			{
 				Query = HttpContext.Request.Query["spawnset"];
-
 				SpawnsetFile = new SpawnsetFile(Path.Combine(CommonObjects.Env.WebRootPath, "spawnsets", Query));
-
+				Spawnset = SpawnsetParser.ParseFile(SpawnsetFile.Path);
 				Description = string.Empty;
 				foreach (string settingsPath in Directory.GetFiles(Path.Combine(CommonObjects.Env.WebRootPath, "spawnsets", "Settings")))
 				{
