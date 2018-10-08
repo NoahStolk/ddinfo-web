2c3f303631fccd8ae8123f55e4a8043c74a2a836
Noah Stolk <noah.stolk1997@gmail.com>
2018-10-08 13:25:42 +0200

    Add LeaderboardHistoryHelper.

---
+++ LeaderboardHistoryHelper/Program.cs
@@ -0,0 +1,72 @@
+﻿using DevilDaggersWebsite.Models.Leaderboard;
+using DevilDaggersWebsite.Utils;
+using NetBase.Utils;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace LeaderboardHistoryHelper
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			FileUtils.CreateText("201809020735.json", JsonConvert.SerializeObject(GetLeaderboardFromHTML()));
+		}
+
+		public static Leaderboard GetLeaderboardFromHTML()
+		{
+			Leaderboard lb = new Leaderboard
+			{
+				// TODO
+				DateTime = new DateTime(2018, 09, 02, 07, 35, 11),
+				Players = 171352,
+				TimeGlobal = 10053064700403,
+				KillsGlobal = 2599153686,
+				GemsGlobal = 293243405,
+				DeathsGlobal = 14380029,
+				ShotsHitGlobal = 2178,
+				ShotsFiredGlobal = 10000
+			};
+
+			string[] lines = FileUtils.GetContents(Path.Combine("Content", "20180902073511.html")).Split('\n');
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				lines[i] = lines[i].TrimStart('\t');
+				if (lines[i].StartsWith("<div class=\"sort\""))
+				{
+					lb.Entries.Add(new Entry
+					{
+						Rank = int.Parse(GetValue(lines[i], "rank")),
+						Username = GetValue(lines[i], "username"),
+						Time = int.Parse(GetValue(lines[i], "time")),
+						Kills = int.Parse(GetValue(lines[i], "kills")),
+						Gems = int.Parse(GetValue(lines[i], "gems")),
+						ShotsFired = 10000,
+						ShotsHit = int.Parse(GetValue(lines[i], "accuracy")),
+						DeathType = GetValue(lines[i], "death-type").ToDeathType(),
+						TimeTotal = ulong.Parse(GetValue(lines[i], "total-time")),
+						KillsTotal = ulong.Parse(GetValue(lines[i], "total-kills")),
+						GemsTotal = ulong.Parse(GetValue(lines[i], "total-gems")),
+						ShotsFiredTotal = 10000,
+						ShotsHitTotal = ulong.Parse(GetValue(lines[i], "total-accuracy")),
+						DeathsTotal = ulong.Parse(GetValue(lines[i], "total-deaths"))
+					});
+				}
+			}
+
+			return lb;
+		}
+
+		public static string GetValue(string line, string name)
+		{
+			if (!line.Contains(name))
+				return null;
+
+			int pos = line.IndexOf(name) + name.Length + 2;
+			string sub = line.Substring(pos);
+			return sub.Substring(0, sub.IndexOf('"'));
+		}
+	}
+}
