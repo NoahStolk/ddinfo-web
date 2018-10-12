27e011e936ecd902e87260316f06d14553b9d372
Noah Stolk <noah.stolk1997@gmail.com>
2018-10-12 22:17:28 +0200

    Implement LeaderboardCSVToJSON.

--- LeaderboardCSVToJSON/MainWindow.xaml.cs
+++ LeaderboardCSVToJSON/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using LeaderboardJsonCreator;
+using Microsoft.Win32;
+using NetBase.Utils;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows;
 
 namespace LeaderboardCSVToJSON
 {
@@ -11,7 +16,86 @@ namespace LeaderboardCSVToJSON
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
+			OpenFileDialog dlg = new OpenFileDialog
+			{
+				DefaultExt = ".csv",
+				InitialDirectory = @"C:\Users\NOAH\source\repos\DevilDaggersWebsite\LeaderboardCSVToJSON\Content"
+			};
 
+			bool? result = dlg.ShowDialog();
+			if (result.HasValue && result.Value)
+			{
+				Leaderboard leaderboard = new Leaderboard
+				{
+					DateTime = Utils.HistoryJsonFileNameToDateTime(Path.GetFileNameWithoutExtension(dlg.FileName))
+				};
+
+				using (StreamReader reader = new StreamReader(dlg.FileName))
+				{
+					int i = 0;
+					while (!reader.EndOfStream)
+					{
+						i++;
+
+						string line = reader.ReadLine();
+						string[] values = line.Split(',');
+
+						if (i == 1)
+							int.TryParse(values[1], out leaderboard.Players);
+						else if (i == 2)
+							ulong.TryParse(values[1], out leaderboard.TimeGlobal);
+						else if (i == 3)
+							ulong.TryParse(values[1], out leaderboard.KillsGlobal);
+						else if (i == 4)
+							ulong.TryParse(values[1], out leaderboard.GemsGlobal);
+						else if (i == 5)
+							ulong.TryParse(values[1], out leaderboard.DeathsGlobal);
+						else if (i == 6)
+						{
+							leaderboard.ShotsHitGlobal = (ulong)GetAccuracy(values[1]);
+							if (leaderboard.ShotsHitGlobal != 0)
+								leaderboard.ShotsFiredGlobal = 10000;
+						}
+
+						if (i >= 9)
+						{
+							Entry entry = new Entry
+							{
+								DeathType = -1
+							};
+
+							int.TryParse(values[0], out entry.Rank);
+							entry.Username = values[1];
+							int.TryParse(values[2].Replace(".", ""), out entry.Time);
+							int.TryParse(values[3], out entry.Kills);
+							int.TryParse(values[4], out entry.Gems);
+							entry.ShotsHit = (int)GetAccuracy(values[5]);
+							if (entry.ShotsHit != 0)
+								entry.ShotsFired = 10000;
+							entry.DeathType = values[6].ToDeathType();
+							ulong.TryParse(values[7].Replace(".", ""), out entry.TimeTotal);
+							ulong.TryParse(values[8], out entry.KillsTotal);
+							ulong.TryParse(values[9], out entry.GemsTotal);
+							entry.ShotsHitTotal = (ulong)GetAccuracy(values[10]);
+							if (entry.ShotsHitTotal != 0)
+								entry.ShotsFiredTotal = 10000;
+							ulong.TryParse(values[11], out entry.DeathsTotal);
+
+							leaderboard.Entries.Add(entry);
+						}
+					}
+				}
+
+				string json = JsonConvert.SerializeObject(leaderboard);
+
+				FileUtils.CreateText($@"C:\Users\NOAH\source\repos\DevilDaggersWebsite\DevilDaggersWebsite\wwwroot\leaderboard-history\{Path.GetFileNameWithoutExtension(dlg.FileName)}.json", json);
+			}
+		}
+
+		private static float GetAccuracy(string accuracyString)
+		{
+			float.TryParse(accuracyString.Replace("%", ""), out float percentage);
+			return percentage * 100;
 		}
 	}
 }
