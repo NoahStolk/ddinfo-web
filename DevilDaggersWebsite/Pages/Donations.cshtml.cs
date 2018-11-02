aab1c886330190c366edaece1a8c8b9710c7106c
Noah Stolk <noah.stolk1997@gmail.com>
2018-11-02 19:46:16 +0100

    Donations page refactoring.

--- DevilDaggersWebsite/Pages/Donations.cshtml.cs
+++ DevilDaggersWebsite/Pages/Donations.cshtml.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using DevilDaggersWebsite.Models;
+using DevilDaggersWebsite.Utils;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DevilDaggersWebsite.Pages
 {
 	public class DonationsModel : PageModel
 	{
+		public List<Donator> Donators { get; set; }
+
 		public void OnGet()
 		{
-
+			Donators = UserHelper.Donators
+				.OrderByDescending(d => d.Amount)
+				.ThenBy(d => d.CurrencySymbol)
+				.ThenBy(d => d.Username)
+				.ToList(); ;
 		}
 	}
 }
