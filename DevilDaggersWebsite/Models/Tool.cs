94056ef5b6e8c9de244bc2e1d54c09563c29f95e
Noah Stolk <noah.stolk1997@gmail.com>
2018-11-03 11:35:42 +0100

    Add GetToolVersions page.

--- DevilDaggersWebsite/Models/Tool.cs
+++ DevilDaggersWebsite/Models/Tool.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Html;
+using Newtonsoft.Json;
 
 namespace DevilDaggersWebsite.Models
 {
+	[JsonObject(MemberSerialization.OptIn)]
 	public class Tool
 	{
+		[JsonProperty]
 		public string Name { get; set; }
 		public string DisplayName { get; set; }
 		public HtmlString Description { get; set; }
 		public string Link { get; set; }
 		public string LinkText { get; set; }
+		[JsonProperty]
 		public string VersionNumber { get; set; }
 	}
 }
---
+++ DevilDaggersWebsite/Pages/GetToolVersions.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using CoreBase.Services;
+using DevilDaggersWebsite.Utils;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace DevilDaggersWebsite.Pages
+{
+	public class GetToolVersionsModel : PageModel
+	{
+		private readonly ICommonObjects _commonObjects;
+
+		public string JsonResult { get; set; }
+
+		public GetToolVersionsModel(ICommonObjects commonObjects)
+		{
+			_commonObjects = commonObjects;
+		}
+
+		public void OnGet()
+		{
+			JsonResult = JsonConvert.SerializeObject(ToolUtils.Tools, Formatting.Indented);
+		}
+	}
+}
