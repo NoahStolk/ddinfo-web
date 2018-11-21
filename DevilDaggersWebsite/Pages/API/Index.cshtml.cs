80a66195c26b42b5df008f17ba49a4020b698fc8
Noah Stolk <noah.stolk1997@gmail.com>
2018-11-21 21:38:17 +0100

    Add API Overview page.

---
+++ DevilDaggersWebsite/Pages/API/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using DevilDaggersWebsite.Code;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DevilDaggersWebsite.Pages.API
+{
+	public class IndexModel : PageModel
+	{
+		public List<ApiPage> ApiPages = new List<ApiPage>();
+
+		public void OnGet()
+		{
+			Assembly asm = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(a => a.FullName.Contains("DevilDaggersWebsite"))
+				.FirstOrDefault();
+
+			foreach (Type type in asm.GetTypes())
+			{
+				if (type.BaseType == typeof(PageModel) && type.Namespace.Contains("API") && !type.Name.Contains("Index"))
+				{
+					string name = type.Name.Replace("Model", "");
+
+					MethodInfo onGet = type.GetMethod("OnGet");
+					if (onGet != null)
+					{
+						ParameterInfo[] parameterInfos = onGet.GetParameters();
+
+						string[] parameters = new string[parameterInfos.Length];
+						for (int i = 0; i < parameterInfos.Length; i++)
+							parameters[i] = parameterInfos[i].Name;
+						ApiPages.Add(new ApiPage(name, parameters.ToArray()));
+					}
+					else
+					{
+						ApiPages.Add(new ApiPage(name));
+					}
+				}
+			}
+		}
+	}
+}
