c39af3de2d6f6405fd24d116cf4b594f2bb8f472
Noah Stolk <noah.stolk1997@gmail.com>
2018-12-07 20:42:40 +0100

    Add API/GetWorldRecords.

---
+++ DevilDaggersWebsite/Pages/API/GetWorldRecords.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using CoreBase.Services;
+using DevilDaggersWebsite.Models.API;
+using DevilDaggersWebsite.Models.Leaderboard;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetBase.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+
+namespace DevilDaggersWebsite.Pages.API
+{
+	[ApiFunction(Description = "Returns all the world records found in the leaderboard history section of the site.", ReturnType = MediaTypeNames.Application.Json)]
+	public class GetWorldRecordsModel : PageModel
+	{
+		private readonly ICommonObjects _commonObjects;
+
+		public GetWorldRecordsModel(ICommonObjects commonObjects)
+		{
+			_commonObjects = commonObjects;
+		}
+
+		public FileResult OnGet()
+		{
+			Dictionary<DateTime, Entry> data = new Dictionary<DateTime, Entry>();
+			int worldRecord = 0;
+
+			foreach (string leaderboardHistoryPath in Directory.GetFiles(Path.Combine(_commonObjects.Env.WebRootPath, "leaderboard-history")))
+			{
+				Models.Leaderboard.Leaderboard leaderboard = JsonConvert.DeserializeObject<Models.Leaderboard.Leaderboard>(FileUtils.GetContents(leaderboardHistoryPath));
+				if (leaderboard.Entries[0].Time != worldRecord)
+				{
+					worldRecord = leaderboard.Entries[0].Time;
+					data[leaderboard.DateTime] = leaderboard.Entries[0];
+				}
+			}
+
+			string jsonResult = JsonConvert.SerializeObject(data);
+			return File(Encoding.UTF8.GetBytes(jsonResult), MediaTypeNames.Application.Json, $"{GetType().Name.Replace("Model", "")}.json");
+		}
+	}
+}
