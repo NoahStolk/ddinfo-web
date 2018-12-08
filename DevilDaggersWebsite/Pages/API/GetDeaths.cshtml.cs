3555b50a94bf813e8b344618366052db82c69b55
Noah Stolk <noah.stolk1997@gmail.com>
2018-12-08 01:49:11 +0100

    Add API/GetDeaths.

---
+++ DevilDaggersWebsite/Pages/API/GetDeaths.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using DevilDaggersWebsite.Models.API;
+using DevilDaggersWebsite.Models.Game;
+using DevilDaggersWebsite.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace DevilDaggersWebsite.Pages.API
+{
+	[ApiFunction(Description = "Returns the Death object for the given Death Type. Returns all Death objects if no Death Type parameter was specified.", ReturnType = MediaTypeNames.Application.Json)]
+	public class GetDeathsModel : ApiPageModel
+	{
+		public FileResult OnGet(string deathType = null)
+		{
+			if (!string.IsNullOrEmpty(deathType) && int.TryParse(deathType, out int type))
+				return JsonFile(GetDeathFromDeathType(type));
+
+			List<Death> deaths = new List<Death> { GameUtils.Unknown };
+			deaths.AddRange(GameUtils.Deaths);
+			return JsonFile(deaths);
+		}
+
+		public Death GetDeathFromDeathType(int deathType)
+		{
+			try
+			{
+				return GameUtils.Deaths[deathType];
+			}
+			catch
+			{
+				return GameUtils.Unknown;
+			}
+		}
+	}
+}
