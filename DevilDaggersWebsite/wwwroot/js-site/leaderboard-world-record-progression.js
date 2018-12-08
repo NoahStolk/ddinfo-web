ed5ced55e8b54ea424df1e65e55c11c13cb4177b
Noah Stolk <noah.stolk1997@gmail.com>
2018-12-08 01:50:19 +0100

    World Record Progression WIP.

---
+++ DevilDaggersWebsite/wwwroot/js-site/leaderboard-world-record-progression.js
@@ -0,0 +1,62 @@
+﻿var deathTypes;
+$.getJSON("/API/GetDeaths", function (data) {
+	deathTypes = data;
+});
+
+$.getJSON("/API/GetWorldRecords", function (data) {
+
+	var wrs = [];
+	$.each(data, function (key, val) {
+		var accuracy = val.ShotsHit / val.ShotsFired * 100;
+
+		wrs.push([new Date(key), val.Time / 10000, val.Username, val.Gems === 0 ? "?" : val.Gems, val.Kills === 0 ? "?" : val.Kills, accuracy === 0 ? "?" : accuracy, deathTypes[val.DeathType + 1].Name]);
+	});
+
+	$.jqplot("world-record-progression-chart", [wrs], {
+		axes: {
+			xaxis: {
+				renderer: $.jqplot.DateAxisRenderer,
+				min: new Date("2016-01-01T00:00:00Z"),
+				max: Date.now() + 604800000, // Amount of milliseconds in a week
+				tickOptions: {
+					formatString: "%b %#d '%y"
+				}
+			},
+			yaxis: {
+				min: 400,
+				max: 1150,
+				tickOptions: {
+					formatString: '%.4f'
+				}
+			}
+		},
+		series: [
+			{
+				color: '#FF0000'
+			}
+		],
+		seriesDefaults: {
+			step: true,
+			markerOptions: { show: false }
+		},
+		highlighter: {
+			show: true,
+			tooltipAxes: 'xy',
+			yvalues: 6,
+			formatString: '<table class="jqplot-highlighter"> \
+          <tr><td>Date:</td><td>%s</td></tr> \
+          <tr><td>Time:</td><td>%s</td></tr> \
+          <tr><td>Username:</td><td>%s</td></tr> \
+          <tr><td>Gems:</td><td>%s</td></tr> \
+          <tr><td>Kills:</td><td>%s</td></tr> \
+          <tr><td>Accuracy:</td><td>%s</td></tr> \
+          <tr><td>Death type:</td><td>%s</td></tr></table>'
+		},
+		cursor: {
+			show: false
+		},
+		grid: {
+			backgroundColor: '#000000'
+		}
+	});
+});
