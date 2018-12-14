346077c9ec233ea2b834d585ec6cf7fb1a831da8
Noah Stolk <noah.stolk1997@gmail.com>
2018-12-14 14:49:41 +0100

    Enemies page refactoring, game version select, fixes.

--- DevilDaggersWebsite/Pages/Wiki/Enemies.cshtml.cs
+++ DevilDaggersWebsite/Pages/Wiki/Enemies.cshtml.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using DevilDaggersCore.Game;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace DevilDaggersWebsite.Pages.Wiki
 {
 	public class EnemiesModel : PageModel
 	{
-		public void OnGet()
-		{
+		public string gameVersion;
 
+		public GameVersion gameVersionObject;
+		public List<Enemy> enemies;
+		public List<Upgrade> upgrades;
+
+		public List<SelectListItem> GameVersions { get; set; }
+
+		public void OnGet(string gameVersion)
+		{
+			if (Game.TryGetGameVersionFromString(gameVersion, out gameVersionObject))
+				this.gameVersion = gameVersion;
+			else
+				this.gameVersion = Game.DEFAULT_GAME_VERSION;
+
+			enemies = Game.GetEntities<Enemy>(gameVersionObject);
+			upgrades = Game.GetEntities<Upgrade>(gameVersionObject);
+
+			GameVersions = new List<SelectListItem>();
+			for (int i = 0; i < 3; i++)
+			{
+				string gameVersionString = $"V{i + 1}";
+				GameVersions.Add(new SelectListItem(gameVersionString, gameVersionString));
+			}
 		}
 	}
 }
