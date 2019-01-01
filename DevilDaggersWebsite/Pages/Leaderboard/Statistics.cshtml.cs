6a5d251493e1ce1b960842c2545f415484ddcd83
Noah Stolk <noah.stolk1997@gmail.com>
2019-01-01 22:40:04 +0100

    Statistics page WIP.

---
+++ DevilDaggersWebsite/Pages/Leaderboard/Statistics.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using DevilDaggersCore.Game;
+using DevilDaggersWebsite.Models.Leaderboard;
+using DevilDaggersWebsite.Utils;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevilDaggersWebsite.Pages.Leaderboard
+{
+	public class StatisticsModel : PageModel
+	{
+		public async Task<Dictionary<Dagger, int>> GetDaggerStats()
+		{
+			List<int> offsets = new List<int>
+			{
+				101,
+				6201,
+				43701,
+				113001
+			};
+
+			Dictionary<Dagger, int> daggerStats = new Dictionary<Dagger, int>();
+
+			int players = 0;
+			foreach (int offset in offsets)
+			{
+				Entry entry = null;
+				int off = offset;
+				while (entry == null)
+				{
+					Models.Leaderboard.Leaderboard lb = await LeaderboardUtils.LoadLeaderboard(off);
+					players = lb.Players;
+
+					for (int i = 0; i < 99; i++)
+					{
+						if (Game.GetDaggerFromTime(lb.Entries[i].Time) != Game.GetDaggerFromTime(lb.Entries[i + 1].Time))
+						{
+							entry = lb.Entries[i - 1];
+							goto Done;
+						}
+					}
+					off += 100;
+				}
+				Done:
+
+				daggerStats.Add(Game.GetDaggerFromTime(entry.Time), entry.Rank + 1);
+			}
+			daggerStats.Add(Game.V3.Default, players);
+
+			return daggerStats;
+		}
+	}
+}
