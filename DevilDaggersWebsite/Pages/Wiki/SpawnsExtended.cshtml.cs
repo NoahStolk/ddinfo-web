dfe8d5ac74ecc8de83d22deea7bbade579caca2e
Noah Stolk <noah.stolk1997@gmail.com>
2019-02-10 02:15:58 +0100

    no message

---
+++ DevilDaggersWebsite/Pages/Wiki/SpawnsExtended.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using DevilDaggersWebsite.Models.PageModels;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace DevilDaggersWebsite.Pages.Wiki
+{
+	public class SpawnsExtendedModel : WikiPageModel
+	{
+		public string SpawnsetPath { get; set; }
+
+		private readonly IHostingEnvironment _env;
+
+		public SpawnsExtendedModel(IHostingEnvironment env)
+		{
+			_env = env;
+		}
+
+		public void OnGet(string gameVersion)
+		{
+			GetGameVersion(gameVersion);
+
+			SpawnsetPath = Path.Combine(_env.WebRootPath, "spawnsets", $"{this.gameVersion}_Sorath");
+		}
+	}
+}
