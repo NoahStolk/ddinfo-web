e392cb1f3923a802b5d3ae9a3a3400b19c29641f
Noah Stolk <noah.stolk1997@gmail.com>
2019-02-24 00:05:19 +0100

    SpawnsExtended page WIP.

--- DevilDaggersWebsite/Pages/Wiki/SpawnsExtended.cshtml.cs
+++ DevilDaggersWebsite/Pages/Wiki/SpawnsExtended.cshtml.cs
@@ -1,12 +1,17 @@
 ﻿using DevilDaggersWebsite.Models.PageModels;
+using DevilDaggersWebsite.Models.Spawnset;
 using Microsoft.AspNetCore.Hosting;
+using System;
 using System.IO;
 
 namespace DevilDaggersWebsite.Pages.Wiki
 {
 	public class SpawnsExtendedModel : WikiPageModel
 	{
-		public string SpawnsetPath { get; set; }
+		public int squidGushCount;
+		public int leviathanBeckonCount;
+
+		public SpawnEventSettings SpawnEventSettings { get; set; }
 
 		private readonly IHostingEnvironment _env;
 
@@ -15,11 +20,18 @@ namespace DevilDaggersWebsite.Pages.Wiki
 			_env = env;
 		}
 
-		public void OnGet(string gameVersion)
+		public void OnGet(string gameVersion, int squidGushCount, int leviathanBeckonCount)
 		{
-			GetGameVersion(gameVersion);
+			SetGameVersion(gameVersion);
+			this.squidGushCount = Math.Min(squidGushCount, 25);
+			this.leviathanBeckonCount = Math.Min(leviathanBeckonCount, 25);
 
-			SpawnsetPath = Path.Combine(_env.WebRootPath, "spawnsets", $"{this.gameVersion}_Sorath");
+			SpawnEventSettings = new SpawnEventSettings
+			{
+				SpawnsetPath = Path.Combine(_env.WebRootPath, "spawnsets", $"{this.gameVersion}_Sorath"),
+				SquidGushCount = this.squidGushCount,
+				LeviathanBeckonCount = this.leviathanBeckonCount
+			};
 		}
 	}
 }
