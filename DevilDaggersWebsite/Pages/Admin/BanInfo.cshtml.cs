d2d7fd70d84d9f97ec69154d6eacb4eac51c8044
Noah Stolk <noah.stolk1997@gmail.com>
2019-04-05 01:07:09 +0200

    Add Admin/BanInfo page.

---
+++ DevilDaggersWebsite/Pages/Admin/BanInfo.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreBase.Services;
+using DevilDaggersCore.Leaderboard;
+using DevilDaggersWebsite.Models.PageModels;
+using DevilDaggersWebsite.Models.User;
+using DevilDaggersWebsite.Utils;
+using DevilDaggersWebsite.Utils.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevilDaggersWebsite.Pages.Admin
+{
+	public class BanInfoModel : AdminPageModel
+	{
+		private readonly ICommonObjects _commonObjects;
+
+		public List<Tuple<Ban, string, string>> BanInfo { get; private set; } = new List<Tuple<Ban, string, string>>();
+
+		public BanInfoModel(ICommonObjects commonObjects)
+		{
+			_commonObjects = commonObjects;
+		}
+
+		public async Task<ActionResult> OnGetAsync(string password)
+		{
+			if (!Authenticate(password))
+				return RedirectToPage("/Error/404");
+
+			BanInfo = await GetBanInfo();
+
+			return null;
+		}
+
+		public async Task<List<Tuple<Ban, string, string>>> GetBanInfo()
+		{
+			List<Tuple<Ban, string, string>> list = new List<Tuple<Ban, string, string>>();
+
+			IEnumerable<Ban> bans = UserUtils.GetBans(_commonObjects);
+			foreach (Ban ban in bans)
+			{
+				Entry entry = await Hasmodai.GetUserByID(ban.ID);
+
+				if (ban.IDResponsible.HasValue)
+				{
+					Entry entryResponsible = await Hasmodai.GetUserByID(ban.IDResponsible.Value);
+					list.Add(Tuple.Create(ban, entry.Username, entryResponsible.Username));
+				}
+				else
+				{
+					list.Add(Tuple.Create(ban, entry.Username, string.Empty));
+				}
+			}
+
+			return list;
+		}
+	}
+}
