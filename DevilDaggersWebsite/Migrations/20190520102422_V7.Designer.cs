c52403a891f587bf3d534f6ff04c5135b4939c6f
Noah Stolk <noah.stolk1997@gmail.com>
2019-05-20 12:47:35 +0200

    Add DDCLClientVersion to CustomEntry.

--- DevilDaggersWebsite/Migrations/20190520102422_V7.Designer.cs
+++ DevilDaggersWebsite/Migrations/20190520102422_V7.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DevilDaggersWebsite.Code.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevilDaggersWebsite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190520102422_V7")]
+    partial class V7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
