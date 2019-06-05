0ae2958bddf8d16869e2a899af5d169ead7f3613
Noah Stolk <noah.stolk1997@gmail.com>
2019-06-05 17:50:31 +0200

    Fix user search greater than 16 characters 500 error.

--- DevilDaggersWebsite/Pages/Leaderboard/Index.cshtml.cs
+++ DevilDaggersWebsite/Pages/Leaderboard/Index.cshtml.cs
@@ -12,7 +12,19 @@ namespace DevilDaggersWebsite.Pages.Leaderboard
 		public DevilDaggersCore.Leaderboard.Leaderboard Leaderboard { get; set; } = new DevilDaggersCore.Leaderboard.Leaderboard();
 
 		public int Rank { get; set; }
-		public string Search { get; set; }
+
+		private string search = string.Empty;
+		public string Search
+		{
+			get => search;
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+					return;
+				search = value.Length >= 16 ? value.Substring(0, 16) : value;
+			}
+		}
+		
 		public bool IsUserSearch => !string.IsNullOrEmpty(Search) && Search.Length >= 3;
 
 		public async Task OnGetAsync(int rank, string search)
