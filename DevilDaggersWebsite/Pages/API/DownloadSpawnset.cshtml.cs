0b58bbede3a8f7af7ef200d771fb9acf933427d6
Noah Stolk <noah.stolk1997@gmail.com>
2019-06-08 16:31:25 +0200

    Fix error 500 when spawnset does not exist for API GetSpawnset and DownloadSpawnset.

--- DevilDaggersWebsite/Pages/API/DownloadSpawnset.cshtml.cs
+++ DevilDaggersWebsite/Pages/API/DownloadSpawnset.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using DevilDaggersWebsite.Code.API;
+﻿using CoreBase.Services;
+using DevilDaggersWebsite.Code.API;
 using DevilDaggersWebsite.Code.PageModels;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
@@ -9,16 +10,19 @@ namespace DevilDaggersWebsite.Pages.API
 	[ApiFunction(Description = "Returns the spawnset corresponding to the given file parameter. Returns to this page if the spawnset could not be found.", ReturnType = MediaTypeNames.Application.Octet, IsDeprecated = true, DeprecationMessage = "Use the GetSpawnset function instead.")]
 	public class DownloadSpawnsetModel : ApiPageModel
 	{
+		private readonly ICommonObjects _commonObjects;
+
+		public DownloadSpawnsetModel(ICommonObjects commonObjects)
+		{
+			_commonObjects = commonObjects;
+		}
+
 		public ActionResult OnGet(string file)
 		{
-			try
-			{
-				return File(Path.Combine("spawnsets", file), MediaTypeNames.Application.Octet, file);
-			}
-			catch
-			{
+			if (string.IsNullOrEmpty(file) || !System.IO.File.Exists(Path.Combine(_commonObjects.Env.WebRootPath, "spawnsets", file)))
 				return RedirectToPage("/API/Index");
-			}
+
+			return File(Path.Combine("spawnsets", file), MediaTypeNames.Application.Octet, file);
 		}
 	}
 }
--- DevilDaggersWebsite/Pages/API/GetSpawnset.cshtml.cs
+++ DevilDaggersWebsite/Pages/API/GetSpawnset.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using DevilDaggersWebsite.Code.API;
+﻿using CoreBase.Services;
+using DevilDaggersWebsite.Code.API;
 using DevilDaggersWebsite.Code.PageModels;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
@@ -9,16 +10,19 @@ namespace DevilDaggersWebsite.Pages.API
 	[ApiFunction(Description = "Returns the spawnset file corresponding to the given fileName parameter. Returns to this page if the spawnset could not be found.", ReturnType = MediaTypeNames.Application.Octet)]
 	public class GetSpawnsetModel : ApiPageModel
 	{
+		private readonly ICommonObjects _commonObjects;
+
+		public GetSpawnsetModel(ICommonObjects commonObjects)
+		{
+			_commonObjects = commonObjects;
+		}
+
 		public ActionResult OnGet(string fileName)
 		{
-			try
-			{
-				return File(Path.Combine("spawnsets", fileName), MediaTypeNames.Application.Octet, fileName);
-			}
-			catch
-			{
+			if (string.IsNullOrEmpty(fileName) || !System.IO.File.Exists(Path.Combine(_commonObjects.Env.WebRootPath, "spawnsets", fileName)))
 				return RedirectToPage("/API/Index");
-			}
+				
+			return File(Path.Combine("spawnsets", fileName), MediaTypeNames.Application.Octet, fileName);
 		}
 	}
 }
