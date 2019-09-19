320d45fc340be2066ea1d0be930e48167c84ce82
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-19 23:44:28 +0200

    no message

---
+++ DevilDaggersWebsite/Pages/Wiki/GameInternals.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevilDaggersWebsite.Pages.Wiki
+{
+	public class GameInternalsModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
+}
