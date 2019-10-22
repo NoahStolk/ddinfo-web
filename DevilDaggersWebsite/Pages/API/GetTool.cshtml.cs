8eec88faa7bb58b7cd94ef361998819896d61c9c
Noah Stolk <noah.stolk1997@gmail.com>
2019-10-22 18:02:18 +0200

    Implement API/GetTool function to get the latest zip file.

---
+++ DevilDaggersWebsite/Pages/API/GetTool.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using DevilDaggersCore.Tools;
+using DevilDaggersWebsite.Code.API;
+using DevilDaggersWebsite.Code.PageModels;
+using DevilDaggersWebsite.Code.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+
+namespace DevilDaggersWebsite.Pages.API
+{
+	[ApiFunction(Description = "Returns the latest version of the tool corresponding to the given toolName parameter as a zip file. Returns to this page if the tool could not be found.", ReturnType = MediaTypeNames.Application.Zip)]
+	public class GetToolModel : ApiPageModel
+	{
+        public ActionResult OnGet(string toolName)
+		{
+			Tool tool = ToolUtils.Tools.Where(t => t.Name == toolName).FirstOrDefault();
+			if (tool == null)
+				return RedirectToPage("/API/Index");
+
+			string fileName = $"{toolName}{tool.VersionNumber}.zip";
+			return File(Path.Combine("tools", toolName, fileName), MediaTypeNames.Application.Octet, fileName);
+		}
+    }
+}
