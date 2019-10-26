2f4fc0cc47f981f9ddb31b717ed74982bc7587a4
Noah Stolk <noah.stolk1997@gmail.com>
2019-10-26 13:15:31 +0200

    no message

--- DevilDaggersWebsite/Pages/Admin/Tasks.cshtml.cs
+++ DevilDaggersWebsite/Pages/Admin/Tasks.cshtml.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using DevilDaggersWebsite.Code.Tasks.Scheduling;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace DevilDaggersWebsite.Pages.Admin
 {
 	public class TasksModel : PageModel
     {
+		public Dictionary<string, DateTime> LastUpdatedDictionary { get; private set; } = new Dictionary<string, DateTime>();
+
         public void OnGet()
         {
+			Assembly asm = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(a => a.FullName.Contains("DevilDaggersWebsite"))
+				.FirstOrDefault();
 
-        }
+			foreach (Type type in asm.GetTypes())
+				if (typeof(IScheduledTask).IsAssignableFrom(type) && type.Namespace.Contains("Tasks"))
+					foreach (PropertyInfo pInfo in type.GetProperties())
+						if (pInfo.Name == "LastUpdated")	
+							LastUpdatedDictionary[type.Name] = (DateTime)pInfo.GetValue(null, null);
+		}
     }
 }
