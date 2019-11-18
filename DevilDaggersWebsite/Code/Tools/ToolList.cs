087cbee1238a49f17c49466d5546ca8b9721f407
Noah Stolk <noah.stolk1997@gmail.com>
2019-11-18 22:33:23 +0100

    Update DDSE and DDAE.

--- DevilDaggersWebsite/Code/Tools/ToolList.cs
+++ DevilDaggersWebsite/Code/Tools/ToolList.cs
@@ -22,10 +22,19 @@ namespace DevilDaggersWebsite.Code.Utils
 		{
 			Name = "DevilDaggersSurvivalEditor",
 			DisplayName = "Devil Daggers Survival Editor",
-			VersionNumber = new Version(2, 4, 13, 0),
+			VersionNumber = new Version(2, 4, 13, 1),
 			VersionNumberRequired = new Version(2, 4, 13, 0),
 			Changelog = new List<ChangelogEntry>
 			{
+				new ChangelogEntry
+				{
+					VersionNumber = new Version(2, 4, 13, 1),
+					Date = new DateTime(2019, 11, 18),
+					Changes = new List<Change>
+					{
+						new Change("Updated source code URL.")
+					}
+				},
 				new ChangelogEntry
 				{
 					VersionNumber = new Version(2, 4, 13, 0),
@@ -488,10 +497,19 @@ namespace DevilDaggersWebsite.Code.Utils
 		{
 			Name = "DevilDaggersAssetEditor",
 			DisplayName = "Devil Daggers Asset Editor",
-			VersionNumber = new Version(0, 7, 5, 0),
+			VersionNumber = new Version(0, 7, 5, 1),
 			VersionNumberRequired = new Version(0, 7, 5, 0),
 			Changelog = new List<ChangelogEntry>
 			{
+				new ChangelogEntry
+				{
+					VersionNumber = new Version(0, 7, 5, 1),
+					Date = new DateTime(2019, 11, 18),
+					Changes = new List<Change>
+					{
+						new Change("Update source code URL.")
+					}
+				},
 				new ChangelogEntry
 				{
 					VersionNumber = new Version(0, 7, 5, 0),
