1d6553dbaa6da38e50eff41a47b1d800b7d13b87
Noah Stolk <noah.stolk1997@gmail.com>
2020-03-13 19:15:16 +0100

    Add currency to donator list

--- DevilDaggersWebsite/Code/Donations/DonationList.cs
+++ DevilDaggersWebsite/Code/Donations/DonationList.cs
@@ -15,11 +15,11 @@ namespace DevilDaggersWebsite.Code.Donations
 
 		public static Donation[] Donations = new Donation[]
 		{
-			new Donation(137044, 100, "EUR", 100, new DateTime()),
-			new Donation(105315, 1000, "EUR", 1000, new DateTime()),
+			new Donation(137044, 100, "EUR", 100, new DateTime()), // TODO: Get date.
+			new Donation(105315, 1000, "EUR", 1000, new DateTime()), // TODO: Get date.
 			new Donation(105315, 1, "EUR", 1, new DateTime(2018, 2, 20)),
-			new Donation(94857, 77, "EUR", 39, new DateTime()),
-			new Donation(142939, 1200, "EUR", 1200, new DateTime()),
+			new Donation(94857, 77, "EUR", 39, new DateTime()), // TODO: Get date.
+			new Donation(142939, 1200, "EUR", 1200, new DateTime()), // TODO: Get date.
 			new Donation(118832, 100, "EUR", 100, new DateTime(2018, 2, 20)),
 			new Donation(118832, 150, "EUR", 150, new DateTime(2018, 9, 5)),
 			new Donation(118832, 250, "EUR", 250, new DateTime(2018, 10, 22)),
@@ -27,21 +27,21 @@ namespace DevilDaggersWebsite.Code.Donations
 			new Donation(118832, 9, "EUR", 0, new DateTime(2019, 1, 27)),
 			new Donation(118832, 2, "EUR", 0, new DateTime(2019, 1, 27)),
 			new Donation(118832, 154, "EUR", 114, new DateTime(2019, 1, 28)),
-			new Donation(113530, 500, "USD", 450, new DateTime(2018, 2, 28)), // conv not sure
-			new Donation(113530, 500, "USD", 450, new DateTime(2018, 9, 4)), // conv not sure
-			new Donation(113530, 500, "USD", 450, new DateTime(2018, 12, 8)), // conv not sure
-			new Donation(148518, 500, "USD", 450, new DateTime(2018, 3, 27)), // conv not sure
+			new Donation(113530, 500, "USD", 450, new DateTime(2018, 2, 28)), // TODO: Figure out converted amount.
+			new Donation(113530, 500, "USD", 450, new DateTime(2018, 9, 4)), // TODO: Figure out converted amount.
+			new Donation(113530, 500, "USD", 450, new DateTime(2018, 12, 8)), // TODO: Figure out converted amount.
+			new Donation(148518, 500, "USD", 450, new DateTime(2018, 3, 27)), // TODO: Figure out converted amount.
 			new Donation(148951, 750, "EUR", 750, new DateTime(2018, 6, 24)),
 			new Donation(148951, 1, "EUR", 1, new DateTime(2018, 6, 24)),
 			new Donation(148951, 749, "EUR", 749, new DateTime(2018, 10, 15)),
-			new Donation(172395, 500, "USD", 450, new DateTime(2018, 9, 21)), // conv not sure
+			new Donation(172395, 500, "USD", 450, new DateTime(2018, 9, 21)), // TODO: Figure out converted amount.
 			new Donation(115431, 500, "GBP", 559, new DateTime(2018, 9, 21)),
 			new Donation(6760, 1000, "EUR", 1000, new DateTime(2018, 9, 28)),
 			new Donation(134802, 500, "EUR", 500, new DateTime(2018, 10, 15)),
 			new Donation(65617, 2500, "EUR", 2500, new DateTime(2018, 10, 22)),
 			new Donation(121891, 500, "EUR", 500, new DateTime(2018, 11, 1)),
 			new Donation(184178, 100, "USD", 86, new DateTime(2018, 12, 19)),
-			//new Donation(106722, 11, "EUR", 11, new DateTime(2019, 1, 4)), // refunded
+			//new Donation(106722, 11, "EUR", 11, new DateTime(2019, 1, 4)), // Refunded.
 			new Donation(106722, 502, "EUR", 502, new DateTime(2019, 1, 4)),
 			new Donation(106722, 9, "EUR", 9, new DateTime(2019, 1, 27)),
 			new Donation(106722, 2, "EUR", 2, new DateTime(2019, 1, 27)),
@@ -50,15 +50,15 @@ namespace DevilDaggersWebsite.Code.Donations
 			new Donation(86805, 6666, "AUD", 4079, new DateTime(2019, 1, 21)),
 			new Donation(21059, 2000, "EUR", 2000, new DateTime(2019, 1, 22)),
 			new Donation(111007, 1000, "EUR", 1000, new DateTime(2019, 2, 17)),
-			new Donation(146931, 2500, "USD", 2000, new DateTime(2019, 6, 6)), // conv not sure
-			new Donation(146931, 2500, "USD", 2000, new DateTime(2019, 9, 11)), // conv not sure
+			new Donation(146931, 2500, "USD", 2100, new DateTime(2019, 6, 6)), // TODO: Figure out converted amount.
+			new Donation(146931, 2500, "USD", 2100, new DateTime(2019, 9, 11)), // TODO: Figure out converted amount.
 			new Donation(134740, 250, "USD", 217, new DateTime(2019, 8, 12)),
 			new Donation(171188, 1000, "GBP", 1000, new DateTime(2019, 8, 12)),
 			new Donation(116299, 2000, "EUR", 2000, new DateTime(2019, 10, 14)),
 			new Donation(109193, 1000, "GBP", 1000, new DateTime(2019, 11, 2)),
 			new Donation(197276, 40, "EUR", 40, new DateTime(2019, 12, 28)),
 			new Donation(531, 1500, "EUR", 1500, new DateTime(2020, 1, 3)),
-			new Donation(0, 666, "GBP", 728, new DateTime(2020, 3, 12)), // id not sure
+			new Donation(0, 666, "GBP", 728, new DateTime(2020, 3, 12)), // TODO: Figure out ID.
 		};
 
 		public static Donator[] Donators = new Donator[]
