b3c0310cbd01f14210eeeca7a6cb6fedc4f76efa
Noah Stolk <noah.stolk1997@gmail.com>
2020-04-04 12:29:00 +0200

    Make use of EncryptionUtils project and fix convention violations

--- DevilDaggersWebsite/Pages/CustomLeaderboards/Upload.cshtml.cs
+++ DevilDaggersWebsite/Pages/CustomLeaderboards/Upload.cshtml.cs
@@ -5,9 +5,9 @@ using DevilDaggersCore.Spawnsets.Web;
 using DevilDaggersWebsite.Code.Database;
 using DevilDaggersWebsite.Code.Database.CustomLeaderboards;
 using DevilDaggersWebsite.Code.Utils;
+using EncryptionUtils;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using NetBase.Encryption;
 using NetBase.Extensions;
 using Newtonsoft.Json;
 using System;
@@ -22,13 +22,13 @@ namespace DevilDaggersWebsite.Pages.CustomLeaderboards
 	{
 		public string JsonResult { get; set; }
 
-		private readonly ApplicationDbContext _context;
-		private readonly ICommonObjects _commonObjects;
+		private readonly ApplicationDbContext context;
+		private readonly ICommonObjects commonObjects;
 
 		public UploadModel(ApplicationDbContext context, ICommonObjects commonObjects)
 		{
-			_context = context;
-			_commonObjects = commonObjects;
+			this.context = context;
+			this.commonObjects = commonObjects;
 		}
 
 		public void OnGet(string spawnsetHash, int playerId, string username, float time, int gems, int kills, int deathType, int shotsHit, int shotsFired, int enemiesAlive, int homing, float levelUpTime2, float levelUpTime3, float levelUpTime4, string ddclClientVersion, string v)
@@ -51,7 +51,7 @@ namespace DevilDaggersWebsite.Pages.CustomLeaderboards
 				return new UploadResult(false, "You are using an unsupported and outdated version of DDCL. Please update the program.");
 
 			string spawnsetName = string.Empty;
-			foreach (string spawnsetPath in Directory.GetFiles(Path.Combine(_commonObjects.Env.WebRootPath, "spawnsets")))
+			foreach (string spawnsetPath in Directory.GetFiles(Path.Combine(commonObjects.Env.WebRootPath, "spawnsets")))
 			{
 				string hash = string.Empty;
 
@@ -84,7 +84,7 @@ namespace DevilDaggersWebsite.Pages.CustomLeaderboards
 			if (DecryptValidation(validation) != check)
 				return new UploadResult(false, "Invalid submission.");
 
-			CustomLeaderboard leaderboard = _context.CustomLeaderboards.Include(l => l.Category).FirstOrDefault(l => l.SpawnsetFileName == spawnsetName); // TODO: Multiple leaderboards
+			CustomLeaderboard leaderboard = context.CustomLeaderboards.Include(l => l.Category).FirstOrDefault(l => l.SpawnsetFileName == spawnsetName); // TODO: Multiple leaderboards
 			if (leaderboard == null)
 				return new UploadResult(false, "This spawnset doesn't have a leaderboard.");
 
@@ -103,33 +103,33 @@ namespace DevilDaggersWebsite.Pages.CustomLeaderboards
 			leaderboard.DateLastPlayed = DateTime.Now;
 
 			// Calculate the new rank.
-			List<CustomEntry> entries = _context.CustomEntries.Where(e => e.CustomLeaderboard == leaderboard).OrderByMember(leaderboard.Category.SortingPropertyName, leaderboard.Category.Ascending).ToList();
+			List<CustomEntry> entries = context.CustomEntries.Where(e => e.CustomLeaderboard == leaderboard).OrderByMember(leaderboard.Category.SortingPropertyName, leaderboard.Category.Ascending).ToList();
 			int rank = leaderboard.Category.Ascending ? entries.Where(e => e.Time < time).Count() + 1 : entries.Where(e => e.Time > time).Count() + 1; // TODO: Use reflection to use Category.SortingPropertyName.
 			int totalPlayers = entries.Count();
 
-			CustomEntry entry = _context.CustomEntries.FirstOrDefault(e => e.PlayerId == playerId && e.CustomLeaderboardId == leaderboard.Id);
+			CustomEntry entry = context.CustomEntries.FirstOrDefault(e => e.PlayerId == playerId && e.CustomLeaderboardId == leaderboard.Id);
 			if (entry == null)
 			{
 				// Add new user to this leaderboard.
-				_context.CustomEntries.Add(new CustomEntry(playerId, username, time, gems, kills, deathType, shotsHit, shotsFired, enemiesAlive, homing, levelUpTime2, levelUpTime3, levelUpTime4, DateTime.Now, clientVersion) { CustomLeaderboard = leaderboard });
+				context.CustomEntries.Add(new CustomEntry(playerId, username, time, gems, kills, deathType, shotsHit, shotsFired, enemiesAlive, homing, levelUpTime2, levelUpTime3, levelUpTime4, DateTime.Now, clientVersion) { CustomLeaderboard = leaderboard });
 
-				_context.SaveChanges();
+				context.SaveChanges();
 				return new UploadResult(true, $@"Welcome to the leaderboard for {SpawnsetFile.GetName(leaderboard.SpawnsetFileName)}.
 
-{$"Rank".PadRight(20)}{rank} / {++totalPlayers}
-{$"Score".PadRight(20)}{time.ToString("0.0000")}");
+{$"Rank",20}{rank} / {++totalPlayers}
+{$"Score",20}{time:0.0000}");
 			}
 			else
 			{
 				// Update the username.
-				foreach (CustomEntry en in _context.CustomEntries.Where(e => e.PlayerId == entry.PlayerId))
+				foreach (CustomEntry en in context.CustomEntries.Where(e => e.PlayerId == entry.PlayerId))
 					en.Username = username;
 
 				// User is already on the leaderboard, check for better score.
-				if ((leaderboard.Category.Ascending && entry.Time <= time) // TODO: Use reflection to use Category.SortingPropertyName.
-				 || (!leaderboard.Category.Ascending && entry.Time >= time))
+				if (leaderboard.Category.Ascending && entry.Time <= time // TODO: Use reflection to use Category.SortingPropertyName.
+				 || !leaderboard.Category.Ascending && entry.Time >= time)
 				{
-					_context.SaveChanges();
+					context.SaveChanges();
 					return new UploadResult(true, $"No new highscore for {SpawnsetFile.GetName(leaderboard.SpawnsetFileName)}.");
 				}
 
@@ -163,21 +163,21 @@ namespace DevilDaggersWebsite.Pages.CustomLeaderboards
 				entry.SubmitDate = DateTime.Now;
 				entry.ClientVersion = clientVersion;
 
-				_context.SaveChanges();
+				context.SaveChanges();
 
-				int width = 15;
+				const int width = 15;
 				return new UploadResult(true, $@"NEW HIGHSCORE for {SpawnsetFile.GetName(leaderboard.SpawnsetFileName)}!
 				
-{$"Rank".ToString().PadRight(width)}{$"{rank} / {totalPlayers}".PadRight(width)}({rankDiff.ToString("+0;-#")})
-{$"Time".ToString().PadRight(width)}{time.ToString("0.0000").PadRight(width)}({(timeDiff < 0 ? "" : "+")}{timeDiff.ToString("0.0000")})
-{$"Kills".ToString().PadRight(width)}{kills.ToString().PadRight(width)}({killsDiff.ToString("+0;-#")})
-{$"Gems".ToString().PadRight(width)}{gems.ToString().PadRight(width)}({gemsDiff.ToString("+0;-#")})
-{$"Accuracy".ToString().PadRight(width)}{accuracy.ToString("0.00%").PadRight(width)}({(accuracyDiff < 0 ? "" : "+")}{accuracyDiff.ToString("0.00%")})
-{$"Enemies Alive".ToString().PadRight(width)}{enemiesAlive.ToString().PadRight(width)}({enemiesAliveDiff.ToString("+0;-#")})
-{$"Homing".ToString().PadRight(width)}{homing.ToString().PadRight(width)}({homingDiff.ToString("+0;-#")})
-{$"Level 2".ToString().PadRight(width)}{levelUpTime2.ToString("0.0000").PadRight(width)}({(levelUpTime2Diff < 0 ? "" : "+")}{levelUpTime2Diff.ToString("0.0000")})
-{$"Level 3".ToString().PadRight(width)}{levelUpTime3.ToString("0.0000").PadRight(width)}({(levelUpTime3Diff < 0 ? "" : "+")}{levelUpTime3Diff.ToString("0.0000")})
-{$"Level 4".ToString().PadRight(width)}{levelUpTime4.ToString("0.0000").PadRight(width)}({(levelUpTime4Diff < 0 ? "" : "+")}{levelUpTime4Diff.ToString("0.0000")})");
+{$"Rank",width}{$"{rank} / {totalPlayers}",width}({rankDiff:+0;-#})
+{$"Time",width}{time,width:0.0000}({(timeDiff < 0 ? "" : "+")}{timeDiff:0.0000})
+{$"Kills",width}{kills,width}({killsDiff:+0;-#})
+{$"Gems",width}{gems,width}({gemsDiff:+0;-#})
+{$"Accuracy",width}{accuracy,width:0.00%}({(accuracyDiff < 0 ? "" : "+")}{accuracyDiff:0.00%})
+{$"Enemies Alive",width}{enemiesAlive,width}({enemiesAliveDiff:+0;-#})
+{$"Homing",width}{homing,width}({homingDiff:+0;-#})
+{$"Level 2",width}{levelUpTime2,width:0.0000}({(levelUpTime2Diff < 0 ? "" : "+")}{levelUpTime2Diff:0.0000})
+{$"Level 3",width}{levelUpTime3,width:0.0000}({(levelUpTime3Diff < 0 ? "" : "+")}{levelUpTime3Diff:0.0000})
+{$"Level 4",width}{levelUpTime4,width:0.0000}({(levelUpTime4Diff < 0 ? "" : "+")}{levelUpTime4Diff:0.0000})");
 			}
 		}
 
