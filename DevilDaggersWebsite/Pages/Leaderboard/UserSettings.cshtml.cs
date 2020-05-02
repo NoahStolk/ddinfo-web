9a22ce62e4238997b175ffb1b6be91b0f51db008
Noah Stolk <noah.stolk1997@gmail.com>
2020-05-02 14:13:38 +0200

    Enable top 200 for user settings

--- DevilDaggersWebsite/Pages/Leaderboard/UserSettings.cshtml.cs
+++ DevilDaggersWebsite/Pages/Leaderboard/UserSettings.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using DevilDaggersWebsite.Code.Utils.Web;
+﻿using DevilDaggersCore.Leaderboards;
+using DevilDaggersWebsite.Code.Utils.Web;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Lb = DevilDaggersCore.Leaderboards.Leaderboard;
 
@@ -11,7 +14,13 @@ namespace DevilDaggersWebsite.Pages.Leaderboard
 
 		public async Task OnGetAsync()
 		{
-			Leaderboard = await Hasmodai.GetScores(1);
+			IEnumerable<Task> tasks = Enumerable.Range(0, 2).Select(async i =>
+			{
+				Lb nextLeaderboard = await Hasmodai.GetScores(i * 100 + 1);
+				foreach (Entry entry in nextLeaderboard.Entries)
+					Leaderboard.Entries.Add(entry);
+			});
+			await Task.WhenAll(tasks);
 		}
 	}
 }
