d3573f90770c2c301cf86314bf43f3d38fde35e3
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-04 17:23:49 +0200

    Add Note property to Donation

--- DevilDaggersWebsite/Code/Donations/Donation.cs
+++ DevilDaggersWebsite/Code/Donations/Donation.cs
@@ -9,15 +9,17 @@ namespace DevilDaggersWebsite.Code.Donations
 		public Currency Currency { get; set; }
 		public int ConvertedEuroCentsReceived { get; set; }
 		public DateTime DateReceived { get; set; }
+		public string Note { get; set; }
 		public bool IsRefunded { get; set; }
 
-		public Donation(int donatorId, int amount, Currency currency, int convertedEuroCentsReceived, DateTime dateReceived, bool isRefunded = false)
+		public Donation(int donatorId, int amount, Currency currency, int convertedEuroCentsReceived, DateTime dateReceived, string note, bool isRefunded = false)
 		{
 			DonatorId = donatorId;
 			Amount = amount;
 			Currency = currency;
 			ConvertedEuroCentsReceived = convertedEuroCentsReceived;
 			DateReceived = dateReceived;
+			Note = note;
 			IsRefunded = isRefunded;
 		}
 	}
