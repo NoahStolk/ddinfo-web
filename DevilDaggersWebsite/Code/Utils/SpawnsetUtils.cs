b319b59eb79398c220fef2240fd77f9c5d2e6af7
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-06 20:01:21 +0200

    Remove unused method

--- DevilDaggersWebsite/Code/Utils/SpawnsetUtils.cs
+++ DevilDaggersWebsite/Code/Utils/SpawnsetUtils.cs
@@ -13,27 +13,6 @@ namespace DevilDaggersWebsite.Code.Utils
 		public const int DefaultMaxWaves = 28;
 		public const int MaxSpawns = 2500;
 
-		public static void SaveSpawnsetSettingsJsonFile(ICommonObjects commonObjects)
-		{
-			Dictionary<string, SpawnsetFileSettings> dict = new Dictionary<string, SpawnsetFileSettings>();
-			foreach (string path in Directory.GetFiles(Path.Combine(commonObjects.Env.WebRootPath, "spawnsets"), "*_*", SearchOption.TopDirectoryOnly))
-			{
-				string fileName = Path.GetFileName(path);
-				SpawnsetFile sf = CreateSpawnsetFileFromSettingsFile(commonObjects, path);
-				sf.settings.LastUpdated = File.GetLastWriteTime(path);
-
-				dict[fileName] = sf.settings;
-			}
-
-			JsonSerializer serializer = JsonSerializer.Create(new JsonSerializerSettings()
-			{
-				DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate
-			});
-			using StreamWriter sw = new StreamWriter(File.Create(@"C:\Users\NOAH\source\repos\DevilDaggersWebsite\DevilDaggersWebsite\wwwroot\spawnsets\Settings\Settings.json"));
-			using JsonTextWriter jtw = new JsonTextWriter(sw) { Formatting = Formatting.Indented, IndentChar = '\t', Indentation = 1 };
-			serializer.Serialize(jtw, dict);
-		}
-
 		public static SpawnsetFile CreateSpawnsetFileFromSettingsFile(ICommonObjects commonObjects, string path)
 		{
 			if (!File.Exists(path))
