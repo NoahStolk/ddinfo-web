fd90f0d1718adb16041b36641e55250a39b434f3
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-09 21:39:42 +0200

    no message

--- DevilDaggersWebsite/Pages/Api/GetTool.cshtml.cs
+++ DevilDaggersWebsite/Pages/Api/GetTool.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using DevilDaggersWebsite.Code.Api;
+﻿using CoreBase3.Services;
+using DevilDaggersWebsite.Code.Api;
 using DevilDaggersWebsite.Code.PageModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
@@ -8,9 +9,16 @@ namespace DevilDaggersWebsite.Pages.Api
 	[ApiFunction(Description = "Returns the latest version of the tool corresponding to the given toolName parameter as a zip file. Returns to this page if the tool could not be found.", ReturnType = MediaTypeNames.Application.Zip)]
 	public class GetToolModel : ApiPageModel
 	{
+		private readonly ICommonObjects commonObjects;
+
+		public GetToolModel(ICommonObjects commonObjects)
+		{
+			this.commonObjects = commonObjects;
+		}
+
 		public ActionResult OnGet(string toolName)
 		{
-			if (ApiFunctions.TryGetToolPath(toolName, out string fileName, out string path))
+			if (ApiFunctions.TryGetToolPath(commonObjects, toolName, out string fileName, out string path))
 				return File(path, MediaTypeNames.Application.Octet, fileName);
 
 			return RedirectToPage("/Api/Index");
