28cf2b67b3dad5cc9e2c1c5b55100fb199650e54
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-09 21:41:56 +0200

    Implement Admin/TestBot page and add link to both Admin Test pages to Admin/Index page

---
+++ DevilDaggersWebsite/Pages/Admin/TestBot.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using Bot = DevilDaggersDiscordBot.Program;
+
+namespace DevilDaggersWebsite.Pages.Admin
+{
+	public class TestBotModel : PageModel
+	{
+		public async Task<ActionResult> OnGetAsync()
+		{
+			await Bot.SendMessageAsyncSafe(Bot.DevChannel, "Hello, this is a test message sent from an external environment.");
+
+			return RedirectToPage("/Admin/Index");
+		}
+	}
+}
