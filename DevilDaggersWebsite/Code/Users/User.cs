652fac8303843bdbc7ddf8b1cbdaaeb61871cd2e
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-06 18:03:09 +0200

    Merge titles.json into users.json

--- DevilDaggersWebsite/Code/Users/User.cs
+++ DevilDaggersWebsite/Code/Users/User.cs
@@ -7,6 +7,7 @@
 		public int Id { get; set; }
 		public string Username { get; set; }
 		public bool IsAnonymous { get; set; }
+		public string[] Titles { get; set; }
 
 		public override string ToString()
 			=> $"{Username} ({Id})";
