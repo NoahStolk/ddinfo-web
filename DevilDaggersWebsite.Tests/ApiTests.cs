ba0ab972097a690082579cf2bbec8b0bf86c2da9
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-13 11:58:35 +0200

    Add unit test project and add API tests for daggers

---
+++ DevilDaggersWebsite.Tests/ApiTests.cs
@@ -0,0 +1,49 @@
+﻿using DevilDaggersCore.Game;
+using DevilDaggersWebsite.Code.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Tests
+{
+	[TestClass]
+	public class ApiTests
+	{
+		private readonly DaggersController daggersController = new DaggersController();
+
+		[TestMethod]
+		public void GetDaggers()
+		{
+			List<Dagger> daggers = daggersController.GetDaggers().Value;
+
+			Assert.AreEqual(5, daggers.Count);
+			Assert.IsTrue(daggers.Single(d => d.Name == "Default") != null);
+			Assert.IsTrue(daggers.Single(d => d.Name == "Bronze") != null);
+			Assert.IsTrue(daggers.Single(d => d.Name == "Silver") != null);
+			Assert.IsTrue(daggers.Single(d => d.Name == "Golden") != null);
+			Assert.IsTrue(daggers.Single(d => d.Name == "Devil") != null);
+		}
+
+		[TestMethod]
+		public void GetDaggerAtSeconds()
+		{
+			TestDaggerAtSeconds(1100, GameData.V3Devil);
+			TestDaggerAtSeconds(600, GameData.V3Devil);
+			TestDaggerAtSeconds(500, GameData.V3Devil);
+			TestDaggerAtSeconds(400, GameData.V3Golden);
+			TestDaggerAtSeconds(250, GameData.V3Golden);
+			TestDaggerAtSeconds(200, GameData.V3Silver);
+			TestDaggerAtSeconds(120, GameData.V3Silver);
+			TestDaggerAtSeconds(100, GameData.V3Bronze);
+			TestDaggerAtSeconds(60, GameData.V3Bronze);
+			TestDaggerAtSeconds(30, GameData.V3Default);
+			TestDaggerAtSeconds(0, GameData.V3Default);
+
+			void TestDaggerAtSeconds(uint seconds, Dagger expectedDagger)
+			{
+				Dagger dagger = daggersController.GetDaggerAtSeconds(seconds).Value;
+				Assert.AreEqual(expectedDagger, dagger);
+			}
+		}
+	}
+}
