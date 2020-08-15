0ee8cf9e07bdfa422d81aeefbe17d0db4841fad1
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-15 14:20:07 +0200

    Fix migration bugs

--- DevilDaggersWebsite/Pages/Admin/AdminTests/MigrateOldUserData.cshtml.cs
+++ DevilDaggersWebsite/Pages/Admin/AdminTests/MigrateOldUserData.cshtml.cs
@@ -1,10 +1,12 @@
 using DevilDaggersWebsite.Code.Database;
+using DevilDaggersWebsite.Code.External;
 using DevilDaggersWebsite.Code.Utils;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Old = DevilDaggersWebsite.Code.Users;
 
 namespace DevilDaggersWebsite.Pages.Admin.AdminTests
@@ -20,23 +22,42 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 		public IWebHostEnvironment Env { get; }
 		public ApplicationDbContext ApplicationDbContext { get; }
 
-		public void OnGet()
+		public async Task OnGetAsync()
 		{
-			ApplicationDbContext.Donations.AddRange(MigrateDonations());
-			ApplicationDbContext.Titles.AddRange(MigrateTitles());
-			ApplicationDbContext.Players.AddRange(MigratePlayers());
-			ApplicationDbContext.AssetMods.AddRange(MigrateAssetMods());
+			if (!ApplicationDbContext.Donations.Any())
+			{
+				ApplicationDbContext.Donations.AddRange(MigrateDonations());
+				ApplicationDbContext.SaveChanges();
+			}
+
+			if (!ApplicationDbContext.Titles.Any())
+			{
+				ApplicationDbContext.Titles.AddRange(MigrateTitles());
+				ApplicationDbContext.SaveChanges();
+			}
+
+			if (!ApplicationDbContext.Players.Any())
+			{
+				ApplicationDbContext.Players.AddRange(await MigratePlayers());
+				ApplicationDbContext.SaveChanges();
+			}
+
+			if (!ApplicationDbContext.AssetMods.Any())
+			{
+				ApplicationDbContext.AssetMods.AddRange(MigrateAssetMods());
+				ApplicationDbContext.SaveChanges();
+			}
 
 			foreach (Old.User user in UserUtils.GetUserObjects<Old.User>(Env))
 			{
 				Player player = ApplicationDbContext.Players.FirstOrDefault(p => p.PlayerId == user.Id);
 
-				List<Title> titles = ApplicationDbContext.Titles.Where(t => user.Titles.Contains(t.Name)).ToList();
+				List<Title> titles = ApplicationDbContext.Titles.Where(t => user.Titles != null && user.Titles.Contains(t.Name)).ToList();
 				List<PlayerTitle> playerTitles = titles.Select(title => new PlayerTitle { Title = title, TitleId = title.Id, Player = player, PlayerId = player.Id }).ToList();
 				player.PlayerTitles = playerTitles;
-				foreach (Title title in titles)
-					title.PlayerTitles = playerTitles;
 			}
+
+			ApplicationDbContext.SaveChanges();
 
 			foreach (Old.AssetMod oldMod in UserUtils.GetUserObjects<Old.AssetMod>(Env))
 			{
@@ -45,9 +66,9 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 
 				List<PlayerAssetMod> playerAssetMods = modAuthors.Select(player => new PlayerAssetMod { Player = player, PlayerId = player.Id, AssetMod = newMod, AssetModId = newMod.Id }).ToList();
 				newMod.PlayerAssetMods = playerAssetMods;
-				foreach (Player player in modAuthors)
-					player.PlayerAssetMods = playerAssetMods;
 			}
+
+			ApplicationDbContext.SaveChanges();
 		}
 
 		private List<Donation> MigrateDonations()
@@ -65,9 +86,9 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 				.ToList();
 
 		private List<Title> MigrateTitles()
-			=> UserUtils.GetUserObjects<Old.User>(Env).SelectMany(u => u.Titles).Distinct().Select(s => new Title { Name = s }).ToList();
+			=> UserUtils.GetUserObjects<Old.User>(Env).Where(u => u.Titles != null && u.Titles.Any()).SelectMany(u => u.Titles).Distinct().Select(s => new Title { Name = s }).ToList();
 
-		private List<Player> MigratePlayers()
+		private async Task<List<Player>> MigratePlayers()
 		{
 			List<Player> players = new List<Player>();
 
@@ -83,11 +104,12 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 
 			foreach (Old.PlayerSetting setting in UserUtils.GetUserObjects<Old.PlayerSetting>(Env))
 			{
-				Player? player = players.FirstOrDefault(p => p.Id == setting.Id);
+				Player? player = players.FirstOrDefault(p => p.PlayerId == setting.Id);
 				if (player == null)
 				{
 					player = new Player
 					{
+						Username = (await HasmodaiUtils.GetUserById(setting.Id)).Username,
 						PlayerId = setting.Id,
 						Dpi = setting.Dpi,
 						FlashEnabled = setting.FlashEnabled,
@@ -96,53 +118,73 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 						InGameSens = setting.InGameSens,
 						RightHanded = setting.RightHanded,
 					};
+					players.Add(player);
 				}
 				else
 				{
+					players.Remove(players.FirstOrDefault(p => p.PlayerId == setting.Id));
+
+					player.Username ??= (await HasmodaiUtils.GetUserById(player.PlayerId)).Username;
 					player.Dpi = setting.Dpi;
 					player.FlashEnabled = setting.FlashEnabled;
 					player.Fov = setting.Fov;
 					player.Gamma = setting.Gamma;
 					player.InGameSens = setting.InGameSens;
 					player.RightHanded = setting.RightHanded;
+
+					players.Add(player);
 				}
 			}
 
 			foreach (Old.Flag flag in UserUtils.GetUserObjects<Old.Flag>(Env))
 			{
-				Player? player = players.FirstOrDefault(p => p.Id == flag.Id);
+				Player? player = players.FirstOrDefault(p => p.PlayerId == flag.Id);
 				if (player == null)
 				{
 					player = new Player
 					{
+						Username = (await HasmodaiUtils.GetUserById(flag.Id)).Username,
 						PlayerId = flag.Id,
 						CountryCode = flag.CountryCode,
 					};
+					players.Add(player);
 				}
 				else
 				{
+					players.Remove(players.FirstOrDefault(p => p.PlayerId == flag.Id));
+
+					player.Username ??= (await HasmodaiUtils.GetUserById(player.PlayerId)).Username;
 					player.CountryCode = flag.CountryCode;
+
+					players.Add(player);
 				}
 			}
 
 			foreach (Old.Ban ban in UserUtils.GetUserObjects<Old.Ban>(Env))
 			{
-				Player? player = players.FirstOrDefault(p => p.Id == ban.Id);
+				Player? player = players.FirstOrDefault(p => p.PlayerId == ban.Id);
 				if (player == null)
 				{
 					player = new Player
 					{
+						Username = (await HasmodaiUtils.GetUserById(ban.Id)).Username,
 						PlayerId = ban.Id,
 						IsBanned = true,
 						BanDescription = ban.Description,
 						BanResponsibleId = ban.IdResponsible,
 					};
+					players.Add(player);
 				}
 				else
 				{
+					players.Remove(players.FirstOrDefault(p => p.PlayerId == ban.Id));
+
+					player.Username ??= (await HasmodaiUtils.GetUserById(player.PlayerId)).Username;
 					player.IsBanned = true;
 					player.BanDescription = ban.Description;
 					player.BanResponsibleId = ban.IdResponsible;
+
+					players.Add(player);
 				}
 			}
 
