30a0b79da362218a11917098ce315d7cd6d611fe
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-19 16:08:01 +0200

    Add batch file to update all API clients

---
+++ UpdateApiClients.bat
@@ -0,0 +1,8 @@
+CALL :run_nswag ../DevilDaggersSurvivalEditor/DevilDaggersSurvivalEditor/Code/Clients
+CALL :run_nswag ../DevilDaggersAssetEditor/DevilDaggersAssetEditor/Code/Clients
+CALL :run_nswag ../DevilDaggersCustomLeaderboards/DevilDaggersCustomLeaderboards/Clients
+CALL :run_nswag ../DiscordBotDdInfo/DiscordBotDdInfo/Clients
+PAUSE
+
+:run_nswag
+CALL nswag run %~1/DevilDaggersInfoApiClient.nswag /runtime:NetCore31
