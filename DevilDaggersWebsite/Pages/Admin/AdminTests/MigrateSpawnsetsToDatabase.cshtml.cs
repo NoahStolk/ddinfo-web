6033935c200bc8517da23c1c34055a666689e7d7
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-20 19:23:30 +0200

    Add file migration

--- DevilDaggersWebsite/Pages/Admin/AdminTests/MigrateSpawnsetsToDatabase.cshtml.cs
+++ DevilDaggersWebsite/Pages/Admin/AdminTests/MigrateSpawnsetsToDatabase.cshtml.cs
@@ -1,5 +1,4 @@
 using DevilDaggersWebsite.Code.Database;
-using DevilDaggersWebsite.Code.DataTransferObjects;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -46,16 +45,16 @@ namespace DevilDaggersWebsite.Pages.Admin.AdminTests
 
 		public void OnGet()
 		{
-			Dictionary<string, SpawnsetFileSettings> dict = JsonConvert.DeserializeObject<Dictionary<string, SpawnsetFileSettings>>(Io.File.ReadAllText(Path.Combine(env.WebRootPath, "spawnsets", "Settings", "Settings.json")));
+			Dictionary<string, dynamic> dict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(Io.File.ReadAllText(Path.Combine(env.WebRootPath, "spawnsets", "Settings", "Settings.json")));
 
-			List<Code.Database.SpawnsetFile> spawnsetFiles = new List<Code.Database.SpawnsetFile>();
+			List<SpawnsetFile> spawnsetFiles = new List<SpawnsetFile>();
 			int i = 1;
-			foreach (KeyValuePair<string, SpawnsetFileSettings> kvp in dict.OrderBy(kvp => kvp.Value.LastUpdated))
+			foreach (KeyValuePair<string, dynamic> kvp in dict.OrderBy(kvp => kvp.Value.LastUpdated))
 			{
 				string name = kvp.Key.Substring(0, kvp.Key.LastIndexOf('_'));
 				string author = kvp.Key.Substring(kvp.Key.LastIndexOf('_') + 1);
 
-				spawnsetFiles.Add(new Code.Database.SpawnsetFile
+				spawnsetFiles.Add(new SpawnsetFile
 				{
 					Id = i++,
 					Name = name,
