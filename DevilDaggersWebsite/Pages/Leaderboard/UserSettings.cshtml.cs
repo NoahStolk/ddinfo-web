bccd4fa57f70938d1cec216f394199900cee82b7
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-20 12:15:21 +0200

    Fix bugs related to DD servers being down

--- DevilDaggersWebsite/Pages/Leaderboard/UserSettings.cshtml.cs
+++ DevilDaggersWebsite/Pages/Leaderboard/UserSettings.cshtml.cs
@@ -10,19 +10,20 @@ namespace DevilDaggersWebsite.Pages.Leaderboard
 {
 	public class UserSettingsModel : PageModel
 	{
-		public Lb Leaderboard { get; set; } = new Lb();
+		public Lb? Leaderboard { get; set; }
 
 		public async Task OnGetAsync()
 		{
 			IEnumerable<Task> tasks = Enumerable.Range(0, 2).Select(async i =>
 			{
-				Lb nextLeaderboard = await DdHasmodaiClient.GetScores(i * 100 + 1);
-				foreach (Entry entry in nextLeaderboard.Entries)
-					Leaderboard.Entries.Add(entry);
+				Lb? nextLeaderboard = await DdHasmodaiClient.GetScores(i * 100 + 1);
+				foreach (Entry entry in nextLeaderboard?.Entries ?? new List<Entry>())
+					Leaderboard?.Entries.Add(entry);
 			});
 			await Task.WhenAll(tasks);
 
-			Leaderboard.Entries = Leaderboard.Entries.OrderBy(e => e.Rank).ToList();
+			if (Leaderboard != null)
+				Leaderboard.Entries = Leaderboard.Entries.OrderBy(e => e.Rank).ToList();
 		}
 	}
 }
