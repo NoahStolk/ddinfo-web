17ae51230f5b9937015317348b3dcc624c60db0e
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-22 17:40:53 +0200

    Add Singapore Dollar to Currency enum

--- DevilDaggersWebsite.Core/Enumerators/Currency.cs
+++ DevilDaggersWebsite.Core/Enumerators/Currency.cs
@@ -2,9 +2,29 @@
 {
 	public enum Currency
 	{
+		/// <summary>
+		/// Euro.
+		/// </summary>
 		Eur,
+
+		/// <summary>
+		/// US Dollar.
+		/// </summary>
 		Usd,
+
+		/// <summary>
+		/// Australian Dollar.
+		/// </summary>
 		Aud,
+
+		/// <summary>
+		/// Great British Pound.
+		/// </summary>
 		Gbp,
+
+		/// <summary>
+		/// Singapore Dollar.
+		/// </summary>
+		Sgd,
 	}
 }
