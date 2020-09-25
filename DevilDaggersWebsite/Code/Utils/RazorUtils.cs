2aadc959d061daa645dda433c31702361544f049
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-25 18:19:02 +0200

    Remove custom API page and Zalgo stuff

--- DevilDaggersWebsite/Code/Utils/RazorUtils.cs
+++ DevilDaggersWebsite/Code/Utils/RazorUtils.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 
 namespace DevilDaggersWebsite.Code.Utils
@@ -45,20 +44,17 @@ namespace DevilDaggersWebsite.Code.Utils
 			return new HtmlString($"Copyright &copy; {year} {name}");
 		}
 
-		public static HtmlString GetLayoutAnchor(this Enemy enemy, bool plural = false, float zalgo = 0, GameVersion? gameVersionOverride = null)
+		public static HtmlString GetLayoutAnchor(this Enemy enemy, bool plural = false, GameVersion? gameVersionOverride = null)
 		{
 			string colorCode = enemy.ColorCode;
 			if (gameVersionOverride.HasValue)
 				colorCode = GameInfo.GetEntities<Enemy>(gameVersionOverride).FirstOrDefault(e => e.Name == enemy.Name).ColorCode;
 
-			string color = zalgo == 0 ? colorCode : ZalgoUtils.InterpolateHexColor($"#FF{colorCode}", "#FFFF0000", zalgo / 100f);
-			return new HtmlString($"<a style='color: #{color};' href='/Wiki/Enemies{(gameVersionOverride == null ? string.Empty : $"?GameVersion={gameVersionOverride}")}#{enemy.Name.Replace(" ", string.Empty, StringComparison.InvariantCulture)}'>{enemy.Name.ToZalgo(zalgo / 20f)}{(plural ? "s" : string.Empty)}</a>");
+			return new HtmlString($"<a style='color: #{colorCode};' href='/Wiki/Enemies{(gameVersionOverride == null ? string.Empty : $"?GameVersion={gameVersionOverride}")}#{enemy.Name.Replace(" ", string.Empty, StringComparison.InvariantCulture)}'>{enemy.Name}{(plural ? "s" : string.Empty)}</a>");
 		}
 
 		public static HtmlString GetLayoutAnchor(this Upgrade upgrade)
-		{
-			return new HtmlString($"<a style='color: #{upgrade.ColorCode};' href='/Wiki/Upgrades?GameVersion={upgrade.GameVersion}#{upgrade.Name}'>{upgrade.Name}</a>");
-		}
+			=> new HtmlString($"<a style='color: #{upgrade.ColorCode};' href='/Wiki/Upgrades?GameVersion={upgrade.GameVersion}#{upgrade.Name}'>{upgrade.Name}</a>");
 
 		// TODO: Rewrite whole method. It's messy and not very performant.
 		public static HtmlString GetLayout(string str, GameVersion? gameVersion = null)
@@ -117,58 +113,5 @@ namespace DevilDaggersWebsite.Code.Utils
 
 		public static string S(this int value)
 			=> value == 1 ? string.Empty : "s";
-
-		public static HtmlString GetFormattedReturnType(Type type)
-		{
-			string cssClass = type.IsGenericType ? "api-generic-return-type" : "api-return-type";
-			StringBuilder sb = new StringBuilder($"<span class='{cssClass}'>{GetTypeString(type.Name)}</span>");
-			while (type.IsGenericType)
-			{
-				Type[] genericArguments = type.GetGenericArguments();
-				type = genericArguments[0];
-				cssClass = type.IsGenericType ? "api-generic-return-type" : "api-return-type";
-
-				sb.Append($"<span class='api-generic-return-type'>&lt;</span><span class='{cssClass}'>{string.Join(", ", genericArguments.Select(t => GetTypeString(t.Name)))}</span><span class='api-generic-return-type'>&gt;</span>");
-			}
-
-			return new HtmlString(sb.ToString());
-
-			static string GetTypeString(string typeName)
-			{
-				if (typeName.Contains('`', StringComparison.InvariantCulture))
-					return typeName.Substring(0, typeName.IndexOf('`', StringComparison.InvariantCulture));
-				return typeName;
-			}
-		}
-
-		public static HtmlString GetFormattedParameter(ParameterInfo parameter)
-		{
-			Type? underlyingType = Nullable.GetUnderlyingType(parameter.ParameterType);
-			Type actualType = underlyingType ?? parameter.ParameterType;
-
-			string typeSpan = $"<span class='api-parameter-type'>{actualType.Name}</span>";
-			typeSpan = underlyingType != null ? $"<span class='api-nullable'>Nullable&lt;{typeSpan}&gt;</span>" : typeSpan;
-
-			return new HtmlString(@$"{typeSpan}
-<span class='api-parameter{(parameter.IsOptional ? "-optional" : string.Empty)}'>{parameter.Name}</span>
-{(parameter.IsOptional ? $"(<span class='api-parameter-default-value'>{GetParameterFormattedDefaultValue(parameter)}</span> by default)" : string.Empty)}");
-		}
-
-		public static HtmlString GetParameterFormattedDefaultValue(ParameterInfo parameter)
-		{
-			Type? underlyingType = Nullable.GetUnderlyingType(parameter.ParameterType);
-			Type actualType = underlyingType ?? parameter.ParameterType;
-
-			if (actualType.IsValueType)
-			{
-				if (parameter.HasDefaultValue)
-					return new HtmlString(parameter.DefaultValue?.ToString() ?? "null");
-				return new HtmlString(Activator.CreateInstance(actualType)?.ToString() ?? string.Empty);
-			}
-
-			if (parameter.HasDefaultValue && !string.IsNullOrEmpty((string?)parameter.DefaultValue))
-				return new HtmlString(parameter.DefaultValue.ToString());
-			return new HtmlString("<span class='api-null'>null</span>");
-		}
 	}
 }
