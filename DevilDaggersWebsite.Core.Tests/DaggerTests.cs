4ec3cd7c8bea849438b49b76d2c73c83ac1c51f6
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-25 12:24:37 +0200

    Split unit tests in separate files

---
+++ DevilDaggersWebsite.Core.Tests/DaggerTests.cs
@@ -0,0 +1,50 @@
+﻿using DevilDaggersCore.Game;
+using DevilDaggersWebsite.Core.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Core.Tests
+{
+	[TestClass]
+	public class DaggerTests
+	{
+		private readonly DaggersController _daggersController = new DaggersController();
+
+		[TestMethod]
+		public void GetDaggers()
+		{
+			List<Dagger> daggers = _daggersController.GetDaggers().Value;
+
+			Assert.AreEqual(5, daggers.Count);
+			Assert.IsTrue(daggers.All(d => d.GameVersion == GameVersion.V3));
+			Assert.AreEqual(1, daggers.Count(d => d.Name == "Default"));
+			Assert.AreEqual(1, daggers.Count(d => d.Name == "Bronze"));
+			Assert.AreEqual(1, daggers.Count(d => d.Name == "Silver"));
+			Assert.AreEqual(1, daggers.Count(d => d.Name == "Golden"));
+			Assert.AreEqual(1, daggers.Count(d => d.Name == "Devil"));
+		}
+
+		[TestMethod]
+		public void GetDaggerAtSeconds()
+		{
+			TestDaggerAtSeconds(1100, GameInfo.V3Devil);
+			TestDaggerAtSeconds(600, GameInfo.V3Devil);
+			TestDaggerAtSeconds(500, GameInfo.V3Devil);
+			TestDaggerAtSeconds(400, GameInfo.V3Golden);
+			TestDaggerAtSeconds(250, GameInfo.V3Golden);
+			TestDaggerAtSeconds(200, GameInfo.V3Silver);
+			TestDaggerAtSeconds(120, GameInfo.V3Silver);
+			TestDaggerAtSeconds(100, GameInfo.V3Bronze);
+			TestDaggerAtSeconds(60, GameInfo.V3Bronze);
+			TestDaggerAtSeconds(30, GameInfo.V3Default);
+			TestDaggerAtSeconds(0, GameInfo.V3Default);
+
+			void TestDaggerAtSeconds(uint seconds, Dagger expectedDagger)
+			{
+				Dagger dagger = _daggersController.GetDaggerAtSeconds(seconds).Value;
+				Assert.AreEqual(expectedDagger, dagger);
+			}
+		}
+	}
+}
--- DevilDaggersWebsite.Core.Tests/DeathTests.cs
+++ DevilDaggersWebsite.Core.Tests/DeathTests.cs
@@ -7,46 +7,9 @@ using System.Linq;
 namespace DevilDaggersWebsite.Core.Tests
 {
 	[TestClass]
-	public class ApiTests
+	public class DeathTests
 	{
-		private readonly DaggersController _daggersController = new DaggersController();
 		private readonly DeathsController _deathsController = new DeathsController();
-
-		[TestMethod]
-		public void GetDaggers()
-		{
-			List<Dagger> daggers = _daggersController.GetDaggers().Value;
-
-			Assert.AreEqual(5, daggers.Count);
-			Assert.IsTrue(daggers.All(d => d.GameVersion == GameVersion.V3));
-			Assert.AreEqual(1, daggers.Count(d => d.Name == "Default"));
-			Assert.AreEqual(1, daggers.Count(d => d.Name == "Bronze"));
-			Assert.AreEqual(1, daggers.Count(d => d.Name == "Silver"));
-			Assert.AreEqual(1, daggers.Count(d => d.Name == "Golden"));
-			Assert.AreEqual(1, daggers.Count(d => d.Name == "Devil"));
-		}
-
-		[TestMethod]
-		public void GetDaggerAtSeconds()
-		{
-			TestDaggerAtSeconds(1100, GameInfo.V3Devil);
-			TestDaggerAtSeconds(600, GameInfo.V3Devil);
-			TestDaggerAtSeconds(500, GameInfo.V3Devil);
-			TestDaggerAtSeconds(400, GameInfo.V3Golden);
-			TestDaggerAtSeconds(250, GameInfo.V3Golden);
-			TestDaggerAtSeconds(200, GameInfo.V3Silver);
-			TestDaggerAtSeconds(120, GameInfo.V3Silver);
-			TestDaggerAtSeconds(100, GameInfo.V3Bronze);
-			TestDaggerAtSeconds(60, GameInfo.V3Bronze);
-			TestDaggerAtSeconds(30, GameInfo.V3Default);
-			TestDaggerAtSeconds(0, GameInfo.V3Default);
-
-			void TestDaggerAtSeconds(uint seconds, Dagger expectedDagger)
-			{
-				Dagger dagger = _daggersController.GetDaggerAtSeconds(seconds).Value;
-				Assert.AreEqual(expectedDagger, dagger);
-			}
-		}
 
 		[TestMethod]
 		public void GetDeaths()
