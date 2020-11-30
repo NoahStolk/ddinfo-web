a2b6d31696df8faa1c040df12d59c5c49f1b785d
Noah Stolk <noah.stolk1997@gmail.com>
2020-11-30 20:07:20 +0100

    Stop creating leaderboard files locally

--- DevilDaggersWebsite.Core/Tasks/CreateLeaderboardHistoryFileTask.cs
+++ DevilDaggersWebsite.Core/Tasks/CreateLeaderboardHistoryFileTask.cs
@@ -1,34 +1,43 @@
-﻿using DevilDaggersCore.Utils;
+﻿#if !DEBUG
+using DevilDaggersCore.Utils;
 using DevilDaggersWebsite.Core.Clients;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
 using System;
 using System.IO;
+#endif
 using System.Threading.Tasks;
 
 namespace DevilDaggersWebsite.Core.Tasks
 {
 	public class CreateLeaderboardHistoryFileTask : AbstractTask
 	{
+#if !DEBUG
 		private readonly IWebHostEnvironment _env;
 
 		public CreateLeaderboardHistoryFileTask(IWebHostEnvironment env)
 		{
 			_env = env;
 		}
+#endif
 
 		public override string Schedule => "0 0 * * *";
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 		protected override async Task Execute()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
 		{
+#if !DEBUG
 			if (!HistoryFileForThisDateExists(LastTriggered))
 			{
 				Dto.Leaderboard? lb = await DdHasmodaiClient.GetScores(1);
 				if (lb != null)
 					File.WriteAllText(Path.Combine(_env.WebRootPath, "leaderboard-history", $"{DateTime.UtcNow:yyyyMMddHHmm}.json"), JsonConvert.SerializeObject(lb));
 			}
+#endif
 		}
 
+#if !DEBUG
 		private bool HistoryFileForThisDateExists(DateTime dateTime)
 		{
 			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "leaderboard-history"), "*.json"))
@@ -40,5 +49,6 @@ namespace DevilDaggersWebsite.Core.Tasks
 
 			return false;
 		}
+#endif
 	}
 }
