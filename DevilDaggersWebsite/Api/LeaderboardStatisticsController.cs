ebfb04d426f392208f05faa5fda8c48a014c137e
Noah Stolk <noah.stolk1997@gmail.com>
2021-01-10 13:34:36 +0100

    Add LeaderboardStatisticsController

---
+++ DevilDaggersWebsite/Api/LeaderboardStatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using DevilDaggersWebsite.LeaderboardStatistics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Api
+{
+	[Route("api/leaderboard-statistics")]
+	[ApiController]
+	public class LeaderboardStatisticsController : ControllerBase
+	{
+		[HttpGet("daggers")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public Dictionary<string, int> GetDaggers()
+			=> StatisticsDataHolder.Instance.DaggerStats.ToDictionary(kvp => kvp.Key.Name, kvp => kvp.Value);
+
+		[HttpGet("death-types")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public Dictionary<string, int> GetDeathTypes()
+			=> StatisticsDataHolder.Instance.DeathStats.OrderBy(kvp => kvp.Key.DeathType).ToDictionary(kvp => kvp.Key.Name, kvp => kvp.Value);
+
+		[HttpGet("times")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public Dictionary<int, int> GetTimes()
+			=> StatisticsDataHolder.Instance.TimeStats;
+	}
+}
