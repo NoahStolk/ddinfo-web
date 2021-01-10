ce3dfa34ae4b2384b572b269ae5911852439869e
Noah Stolk <noah.stolk1997@gmail.com>
2021-01-10 04:13:34 +0100

    Fix user activity graph bugs

--- DevilDaggersWebsite.Razor/wwwroot/scripts/charts/history/user-activity.js
+++ DevilDaggersWebsite.Razor/wwwroot/scripts/charts/history/user-activity.js
@@ -4,35 +4,32 @@ $.getJSON("/api/leaderboard-history/user-activity?UserId=" + getUrlParameter("Us
 	const activity = [];
 	let activityIndex = 0;
 	let deathsPrevious = 0;
-	let dateStart;
 	let maxDeaths = 0;
-	$.each(data, function (key, deaths) {
-		const dateEnd = new Date(key);
-		if (!dateStart)
-			dateStart = dateEnd;
+	let i = 0;
+	$.each(data, function (_, deaths) {
+		let length = Object.keys(data).length;
+
+		const dateStart = new Date(Object.keys(data)[i]);
+		const dateEnd = i >= length - 1 ? Date.now() : new Date(Object.keys(data)[i + 1]);
+		i++;
 
 		if (activityIndex === 0)
 			deathsPrevious = deaths;
 
 		const periodLengthInMilliseconds = Math.abs(dateEnd - dateStart);
-		const periodLengthInDays = dayLengthInMilliseconds / periodLengthInMilliseconds;
+		const periodLengthInDays = Math.max(1, periodLengthInMilliseconds / dayLengthInMilliseconds);
 
 		const newDeaths = deaths - deathsPrevious;
-		if (newDeaths < 300 * periodLengthInDays) {
-			// Only apply when the numbers are realistic (less than 300 deaths per day); sometimes people switch accounts which messes up statistics.
+		const approximateDailyDeaths = periodLengthInDays === 0 ? 0 : newDeaths / periodLengthInDays;
+		activity.push([dateStart, approximateDailyDeaths, newDeaths, dateEnd]);
 
-			const approximateDailyDeaths = newDeaths * periodLengthInDays;
-			activity.push([dateEnd, approximateDailyDeaths, newDeaths, dateStart]);
-
-			if (newDeaths > maxDeaths) {
-				maxDeaths = newDeaths;
-			}
+		if (approximateDailyDeaths > maxDeaths) {
+			maxDeaths = approximateDailyDeaths;
 		}
 
 		deathsPrevious = deaths;
-		dateStart = dateEnd;
 
-		if (++activityIndex === Object.keys(data).length) {
+		if (++activityIndex === length) {
 			// Ugly way to make sure no dot is visible
 			activity.push([maxDate + 10000000000, deaths]);
 		}
@@ -44,7 +41,7 @@ $.getJSON("/api/leaderboard-history/user-activity?UserId=" + getUrlParameter("Us
 	const chartId = "#" + chartName;
 	const highlighterName = "user-activity-highlighter";
 	const highlighterId = "#" + highlighterName;
-	const chart = createChart(chartName, activity, minDate, maxDate, 0, Math.ceil(maxDeaths / 100) * 100, 20, false);
+	const chart = createChart(chartName, activity, minDate, maxDate, 0, Math.ceil(maxDeaths / 50) * 50, 11, false);
 
 	$(chartId).bind('jqplotMouseMove', function (_event, xy, _axesData, _neighbor, plot) {
 		const closestData = getDataBasedOnMouseXPosition(chart, xy, plot, minDate, maxDate);
@@ -73,15 +70,15 @@ $.getJSON("/api/leaderboard-history/user-activity?UserId=" + getUrlParameter("Us
 	function setHighlighter(data, xy) {
 		setHighlighterPosition(chart, highlighterId, data, xy, 0, maxDeaths, true);
 
-		const dateEnd = new Date(data[0]);
-		const yearEnd = dateEnd.getFullYear();
-		const monthEnd = dateEnd.getMonth();
-		const dayEnd = dateEnd.getDate();
-
-		const dateStart = new Date(data[3]);
+		const dateStart = new Date(data[0]);
 		const yearStart = dateStart.getFullYear();
 		const monthStart = dateStart.getMonth();
 		const dayStart = dateStart.getDate();
+
+		const dateEnd = new Date(data[3]);
+		const yearEnd = dateEnd.getFullYear();
+		const monthEnd = dateEnd.getMonth();
+		const dayEnd = dateEnd.getDate();
 
 		let dateString = getDateString(dayStart, monthStart, yearStart) + ' - ' + getDateString(dayEnd, monthEnd, yearEnd);
 
