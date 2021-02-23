25d19e0d73868f0f163ac1a9af26525cb3fd1310
Noah Stolk <noah.stolk@gmail.com>
2021-02-23 14:31:19 +0100

    Delete UpdateApiClients.bat

--- UpdateApiClients.bat
+++
@@ -1,8 +0,0 @@
-CALL :run_nswag ../DevilDaggersSurvivalEditor/DevilDaggersSurvivalEditor/Clients
-CALL :run_nswag ../DevilDaggersAssetEditor/DevilDaggersAssetEditor.Wpf/Clients
-CALL :run_nswag ../DevilDaggersCustomLeaderboards/DevilDaggersCustomLeaderboards/Clients
-CALL :run_nswag ../DiscordBotDdInfo/DiscordBotDdInfo/Clients
-PAUSE
-
-:run_nswag
-CALL nswag run %~1/DevilDaggersInfoApiClient.nswag /runtime:NetCore31
