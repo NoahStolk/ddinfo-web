af9796d6157c7406273a744d6d5d0055affe267e
Noah Stolk <noah.stolk@gmail.com>
2021-03-01 21:22:48 +0100

    Combine Admin tests into one page

---
+++ DevilDaggersWebsite.Razor/Pages/Admin/AdminTests/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using DevilDaggersDiscordBot.Logging;
+using DevilDaggersWebsite.LeaderboardStatistics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.AdminTests
+{
+	public class IndexModel : PageModel
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public IndexModel(IWebHostEnvironment env)
+			=> _env = env;
+
+		public async Task OnPostTestBotAsync()
+			=> await DiscordLogger.Instance.TryLog(Channel.TestMonitoring, "Hello, this is a test message sent from an external environment.");
+
+		public void OnPostTestException()
+			=> throw new("TEST EXCEPTION with 3 inner exceptions", new("Inner exception message", new("Another inner exception message", new("Big Discord embed"))));
+
+		public void OnPostUpdateStatisticsDataHolder()
+			=> StatisticsDataHolder.Instance.Update(_env);
+	}
+}
