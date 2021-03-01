1702944b00e8b1e169c9f9cf55e45cb9b6ab5041
Noah Stolk <noah.stolk@gmail.com>
2021-03-01 13:15:52 +0100

    Rename generic Admin index page

--- DevilDaggersWebsite.Razor/PageModels/AdminEntityIndexPageModel.cs
+++ DevilDaggersWebsite.Razor/PageModels/AdminEntityIndexPageModel.cs
@@ -7,30 +7,25 @@ using System.Reflection;
 
 namespace DevilDaggersWebsite.Razor.PageModels
 {
-	public class AdminEntitiesPageModel<TEntity> : PageModel
+	public class AdminEntityIndexPageModel<TEntity> : PageModel
 	   where TEntity : class
 	{
 		private readonly ApplicationDbContext _dbContext;
 
-		public AdminEntitiesPageModel(ApplicationDbContext dbContext)
+		public AdminEntityIndexPageModel(ApplicationDbContext dbContext)
 		{
 			_dbContext = dbContext;
-		}
 
-		public DbSet<TEntity> DbSet { get; private set; } = null!;
-
-		public PropertyInfo[] EntityProperties { get; private set; } = null!;
-
-		public void OnGet()
-		{
 			DbSet = ((Array.Find(typeof(ApplicationDbContext).GetProperties(), pi => pi.PropertyType == typeof(DbSet<TEntity>)) ?? throw new("Could not retrieve DbSet of TEntity.")).GetValue(_dbContext) as DbSet<TEntity>)!;
 
 			EntityProperties = typeof(TEntity).GetProperties().Where(pi => !pi.PropertyType.IsGenericType).ToArray();
 		}
 
+		public DbSet<TEntity> DbSet { get; } = null!;
+
+		public PropertyInfo[] EntityProperties { get; } = null!;
+
 		public string GetString(PropertyInfo pi, TEntity entity)
-		{
-			return pi.GetValue(entity)?.ToString() ?? string.Empty;
-		}
+			=> pi.GetValue(entity)?.ToString() ?? string.Empty;
 	}
 }
