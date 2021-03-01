26778536b3b88579a4e8a93e7b73735a627e7f94
Noah Stolk <noah.stolk@gmail.com>
2021-03-01 12:57:33 +0100

    Generate Admin table pages with reflection

---
+++ DevilDaggersWebsite.Razor/PageModels/AdminEntitiesPageModel.cs
@@ -0,0 +1,36 @@
+﻿using DevilDaggersWebsite.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DevilDaggersWebsite.Razor.PageModels
+{
+	public class AdminEntitiesPageModel<TEntity> : PageModel
+	   where TEntity : class
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public AdminEntitiesPageModel(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public DbSet<TEntity> DbSet { get; private set; } = null!;
+
+		public PropertyInfo[] EntityProperties { get; private set; } = null!;
+
+		public void OnGet()
+		{
+			DbSet = ((Array.Find(typeof(ApplicationDbContext).GetProperties(), pi => pi.PropertyType == typeof(DbSet<TEntity>)) ?? throw new("Could not retrieve DbSet of TEntity.")).GetValue(_dbContext) as DbSet<TEntity>)!;
+
+			EntityProperties = typeof(TEntity).GetProperties().Where(pi => !pi.PropertyType.IsGenericType).ToArray();
+		}
+
+		public string GetString(PropertyInfo pi, TEntity entity)
+		{
+			return pi.GetValue(entity)?.ToString() ?? string.Empty;
+		}
+	}
+}
