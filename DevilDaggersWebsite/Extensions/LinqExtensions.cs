9d9d3ea28deb79da06f9b084528ff82e7497b03c
Noah Stolk <noah.stolk@gmail.com>
2021-03-02 15:22:19 +0100

    Refactor LinqExtensions

--- DevilDaggersWebsite/Extensions/LinqExtensions.cs
+++ DevilDaggersWebsite/Extensions/LinqExtensions.cs
@@ -7,26 +7,19 @@ namespace DevilDaggersWebsite.Extensions
 	public static class LinqExtensions
 	{
 		public static IQueryable<T> OrderByMember<T>(this IQueryable<T> query, string memberName, bool ascending)
-		{
-			ParameterExpression parameter = Expression.Parameter(typeof(T), "p");
-			MemberExpression property = Expression.Property(parameter, memberName);
-			LambdaExpression expression = Expression.Lambda(property, parameter);
-			Type[] types = new[] { query.ElementType, expression.Body.Type };
-
-			string method = ascending ? "OrderBy" : "OrderByDescending";
-			MethodCallExpression mce = Expression.Call(typeof(Queryable), method, types, query.Expression, expression);
-
-			return query.Provider.CreateQuery<T>(mce);
-		}
+			=> ByMember(query, memberName, ascending, "OrderBy", "OrderByDescending");
 
 		public static IQueryable<T> ThenByMember<T>(this IQueryable<T> query, string memberName, bool ascending)
+			=> ByMember(query, memberName, ascending, "ThenBy", "ThenByDescending");
+
+		private static IQueryable<T> ByMember<T>(this IQueryable<T> query, string memberName, bool ascending, string methodNameAsc, string methodNameDesc)
 		{
 			ParameterExpression parameter = Expression.Parameter(typeof(T), "p");
 			MemberExpression property = Expression.Property(parameter, memberName);
 			LambdaExpression expression = Expression.Lambda(property, parameter);
 			Type[] types = new[] { query.ElementType, expression.Body.Type };
 
-			string method = ascending ? "ThenBy" : "ThenByDescending";
+			string method = ascending ? methodNameAsc : methodNameDesc;
 			MethodCallExpression mce = Expression.Call(typeof(Queryable), method, types, query.Expression, expression);
 
 			return query.Provider.CreateQuery<T>(mce);
