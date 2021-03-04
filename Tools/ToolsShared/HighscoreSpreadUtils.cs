70f4de203c5619a8f6038469aea6e6404fb67170
Noah Stolk <noah.stolk@gmail.com>
2021-03-04 19:56:17 +0100

    Improve HighscoreSpreadUtils functionality

--- Tools/ToolsShared/HighscoreSpreadUtils.cs
+++ Tools/ToolsShared/HighscoreSpreadUtils.cs
@@ -50,15 +50,14 @@ namespace ToolsShared
 			{
 				if (entry.Id != 0 && entry.IsEmpty())
 				{
-					Leaderboard leaderboardWithStats = leaderboards.FirstOrDefault(l => l.Entries.Any(e => e.Id == entry.Id && e.Time >= entry.Time - 1 && e.Time <= entry.Time + 1 && !e.IsEmpty())); // TODO: Get most complete data.
-					if (leaderboardWithStats == null)
+					IEnumerable<Leaderboard> leaderboardsWithStats = leaderboards.Where(l => l.Entries.Any(e => e.Id == entry.Id && e.Time >= entry.Time - 1 && e.Time <= entry.Time + 1 && !e.IsEmpty()));
+					if (!leaderboardsWithStats.Any())
 						continue;
-					Entry entryWithStats = leaderboardWithStats.Entries.FirstOrDefault(e => e.Id == entry.Id);
-					entry.Gems = entryWithStats.Gems;
-					entry.Kills = entryWithStats.Kills;
-					entry.DeathType = entryWithStats.DeathType;
-					entry.DaggersHit = entryWithStats.DaggersHit;
-					entry.DaggersFired = entryWithStats.DaggersFired;
+
+					IEnumerable<Entry> entries = leaderboardsWithStats.SelectMany(lb => lb.Entries).Where(e => e.Id == entry.Id);
+
+					Combine(entry, entries);
+
 					changes.Add(entry);
 				}
 			}
@@ -71,7 +70,7 @@ namespace ToolsShared
 					_log.AppendLine($"\tSet missing stats for {entry.Username} {entry.Time.FormatTimeInteger()}");
 					_log.AppendLine($"\t\tGems: {entry.Gems}");
 					_log.AppendLine($"\t\tKills: {entry.Kills}");
-					_log.AppendLine($"\t\tDeathType: {GameInfo.GetDeathByType(GameInfo.GetGameVersionFromDate(leaderboard.DateTime) ?? GameVersion.V1, entry.DeathType).Name}");
+					_log.AppendLine($"\t\tDeathType: {GameInfo.GetDeathByType(GameInfo.GetGameVersionFromDate(leaderboard.DateTime) ?? GameVersion.V1, entry.DeathType)?.Name ?? "Unknown"}");
 					_log.AppendLine($"\t\tAccuracy: {entry.DaggersHit / (float)entry.DaggersFired:00.00%}");
 				}
 
@@ -79,19 +78,38 @@ namespace ToolsShared
 			}
 		}
 
-		public static Entry GetEntryWithData(List<Leaderboard> leaderboards, Entry entry, int id, int time)
-		{
-			if (id == 0 || !entry.IsEmpty())
-				return null;
-
-			Leaderboard leaderboardWithStats = leaderboards.FirstOrDefault(l => l.Entries.Any(e => e.Id == id && e.Time >= time - 1 && e.Time <= time + 1 && !e.IsEmpty())); // TODO: Get most complete data.
-			if (leaderboardWithStats == null)
-				return null;
-
-			return leaderboardWithStats.Entries.FirstOrDefault(e => e.Id == id);
-		}
-
 		private static bool IsEmpty(this Entry entry)
 			=> entry.Gems == 0 && entry.Kills == 0 && entry.DeathType == -1 && entry.DaggersHit == 0 && entry.DaggersFired == 0;
+
+		private static void Combine(Entry original, IEnumerable<Entry> entries)
+		{
+			Entry? withGems = entries.FirstOrDefault(e => e.Gems != 0);
+			if (withGems != null)
+				original.Gems = withGems.Gems;
+
+			Entry? withKills = entries.FirstOrDefault(e => e.Kills != 0);
+			if (withKills != null)
+				original.Kills = withKills.Kills;
+
+			Entry? withDeathType = entries.FirstOrDefault(e => e.DeathType != -1);
+			if (withDeathType != null)
+				original.DeathType = withDeathType.DeathType;
+
+			Entry? withFullDaggerStats = entries.FirstOrDefault(e => e.DaggersFired != 0 && e.DaggersFired != 10000);
+			if (withFullDaggerStats != null)
+			{
+				original.DaggersHit = withFullDaggerStats.DaggersHit;
+				original.DaggersFired = withFullDaggerStats.DaggersFired;
+			}
+			else
+			{
+				Entry? withPartialDaggerStats = entries.FirstOrDefault(e => e.DaggersFired != 0);
+				if (withPartialDaggerStats != null)
+				{
+					original.DaggersHit = withPartialDaggerStats.DaggersHit;
+					original.DaggersFired = withPartialDaggerStats.DaggersFired;
+				}
+			}
+		}
 	}
 }
