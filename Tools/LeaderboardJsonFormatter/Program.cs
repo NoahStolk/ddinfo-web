c0ddf5d008e720c8dd2553bacaaa3f30875fad4f
Noah Stolk <noah.stolk@gmail.com>
2021-03-04 20:08:52 +0100

    Add LeaderboardJsonFormatter console app

---
+++ Tools/LeaderboardJsonFormatter/Program.cs
@@ -0,0 +1,21 @@
+﻿using DevilDaggersWebsite.Dto;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace LeaderboardJsonFormatter
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			DateTime date = new(2018, 9, 1);
+
+			foreach (string path in Directory.GetFiles(@"C:\Users\NOAH\source\repos\DevilDaggersWebsite\DevilDaggersWebsite.Razor\wwwroot\leaderboard-history", "*.json"))
+			{
+				Leaderboard leaderboard = JsonConvert.DeserializeObject<Leaderboard>(File.ReadAllText(path));
+				File.WriteAllText(path, JsonConvert.SerializeObject(leaderboard, leaderboard.DateTime > date ? Formatting.None : Formatting.Indented));
+			}
+		}
+	}
+}
