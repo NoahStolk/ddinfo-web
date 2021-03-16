0c54b409adb96c7cf9fc9acdded8f661c0b363de
Noah Stolk <noah.stolk@gmail.com>
2021-03-16 11:06:39 +0100

    Refactor _LeaderboardUserSettingsPartial.cshtml

--- DevilDaggersWebsite.Razor/Extensions/DtoExtensions.cs
+++ DevilDaggersWebsite.Razor/Extensions/DtoExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using DevilDaggersCore.Game;
 using DevilDaggersWebsite.Dto;
-using DevilDaggersWebsite.Entities;
 using Microsoft.AspNetCore.Html;
 using System.Web;
 
@@ -8,22 +7,6 @@ namespace DevilDaggersWebsite.Razor.Extensions
 {
 	public static class DtoExtensions
 	{
-		public static HtmlString ToHtmlData(this Entry entry, string flagCode, Player player)
-		{
-			return new($@"
-rank='{entry.Rank}'
-flag='{flagCode}'
-username='{HttpUtility.HtmlEncode(entry.Username)}'
-time='{entry.Time}'
-e-dpi='{player.Edpi * 1000 ?? 0}'
-dpi='{player.Dpi ?? 0}'
-in-game-sens='{player.InGameSens * 1000 ?? 0}'
-fov='{player.Fov ?? 0}'
-hand='{(!player.RightHanded.HasValue ? -1 : player.RightHanded.Value ? 1 : 0)}'
-flash='{(!player.FlashEnabled.HasValue ? -1 : player.FlashEnabled.Value ? 1 : 0)}'
-gamma='{player.Gamma * 1000 ?? 0}'");
-		}
-
 		public static HtmlString ToHtmlData(this Entry entry, string flagCode, GameVersion gameVersion)
 		{
 			ulong deathsTotal = entry.DeathsTotal == 0 ? 1 : entry.DeathsTotal;
---
+++ DevilDaggersWebsite.Razor/Models/SettingsEntryModel.cs
@@ -0,0 +1,53 @@
+﻿using DevilDaggersCore.Game;
+using DevilDaggersWebsite.Dto;
+using DevilDaggersWebsite.Entities;
+using DevilDaggersWebsite.Razor.Utils;
+using Microsoft.AspNetCore.Html;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevilDaggersWebsite.Razor.Models
+{
+	public class SettingsEntryModel
+	{
+		public SettingsEntryModel(Entry entry, Player player, IEnumerable<Donation> donations)
+		{
+			IsUnanonymousDonator = !player.IsAnonymous && donations.Any(d => d.PlayerId == entry.Id);
+
+			Titles = player.PlayerTitles.Select(pt => pt.Title.Name).ToArray();
+
+			Dagger dagger = GameInfo.GetDaggerFromTime(GameVersion.V31, entry.Time);
+			Death? death = GameInfo.GetDeathByType(GameVersion.V31, entry.DeathType);
+
+			DaggerColor = dagger.Name.ToLower();
+			DeathStyle = $"color: #{death?.ColorCode ?? "444"};";
+
+			FlagCode = player.CountryCode ?? string.Empty;
+			CountryName = UserUtils.CountryNames.ContainsKey(FlagCode) ? UserUtils.CountryNames[FlagCode] : "Invalid country code";
+
+			HtmlData = new($@"
+rank='{entry.Rank}'
+flag='{FlagCode}'
+username='{HttpUtility.HtmlEncode(entry.Username)}'
+time='{entry.Time}'
+e-dpi='{player.Edpi * 1000 ?? 0}'
+dpi='{player.Dpi ?? 0}'
+in-game-sens='{player.InGameSens * 1000 ?? 0}'
+fov='{player.Fov ?? 0}'
+hand='{(!player.RightHanded.HasValue ? -1 : player.RightHanded.Value ? 1 : 0)}'
+flash='{(!player.FlashEnabled.HasValue ? -1 : player.FlashEnabled.Value ? 1 : 0)}'
+gamma='{player.Gamma * 1000 ?? 0}'");
+		}
+
+		public bool IsUnanonymousDonator { get; }
+		public string[] Titles { get; }
+		public string DaggerColor { get; }
+		public string DeathStyle { get; }
+
+		public string FlagCode { get; }
+		public string CountryName { get; }
+
+		public HtmlString HtmlData { get; }
+	}
+}
