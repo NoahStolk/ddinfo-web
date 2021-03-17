80b7eaa19490489b738b976cfbc747ba47d3b2f3
Noah Stolk <noah.stolk@gmail.com>
2021-03-17 22:52:40 +0100

    Wrap TrackEditUpdates method in try-catch

--- DevilDaggersWebsite/Entities/IAdminUpdatableEntity.cs
+++ DevilDaggersWebsite/Entities/IAdminUpdatableEntity.cs
@@ -15,31 +15,38 @@ namespace DevilDaggersWebsite.Entities
 
 		public void TrackEditUpdates(StringBuilder auditLogger, TAdminDto adminDto, Type entityType)
 		{
-			List<(PropertyInfo? EntityProperty, PropertyInfo DtoProperty)> properties = new();
-
-			foreach (PropertyInfo dtoProperty in typeof(TAdminDto).GetProperties())
+			try
 			{
-				PropertyInfo? entityProperty = entityType.GetProperty(dtoProperty.Name);
-				properties.Add((entityProperty, dtoProperty));
-			}
+				List<(PropertyInfo? EntityProperty, PropertyInfo DtoProperty)> properties = new();
 
-			foreach ((PropertyInfo? entityProperty, PropertyInfo dtoProperty) in properties)
-			{
-				object? entityValue = entityProperty?.GetValue(this);
-				object? dtoValue = dtoProperty.GetValue(adminDto);
-
-				string? dtoValueString = dtoValue?.ToString();
-				if (dtoValue?.GetType().IsGenericType == true && dtoValue is System.Collections.IEnumerable enumerable)
+				foreach (PropertyInfo dtoProperty in typeof(TAdminDto).GetProperties())
 				{
-					StringBuilder sb = new();
-					foreach (object item in enumerable)
-						sb.Append(item).Append(", ");
-
-					dtoValueString = sb.ToString().TrimEnd(',', ' ');
+					PropertyInfo? entityProperty = entityType.GetProperty(dtoProperty.Name);
+					properties.Add((entityProperty, dtoProperty));
 				}
 
-				if (entityValue?.ToString() != dtoValueString)
-					auditLogger.AppendFormat("{0,20}", dtoProperty.Name).Append(": ").AppendFormat("{0,20}", entityValue).Append(" -> ").AppendFormat("{0,20}", dtoValueString).AppendLine();
+				foreach ((PropertyInfo? entityProperty, PropertyInfo dtoProperty) in properties)
+				{
+					object? entityValue = entityProperty?.GetValue(this);
+					object? dtoValue = dtoProperty.GetValue(adminDto);
+
+					string? dtoValueString = dtoValue?.ToString();
+					if (dtoValue?.GetType().IsGenericType == true && dtoValue is System.Collections.IEnumerable enumerable)
+					{
+						StringBuilder sb = new();
+						foreach (object item in enumerable)
+							sb.Append(item).Append(", ");
+
+						dtoValueString = sb.ToString().TrimEnd(',', ' ');
+					}
+
+					if (entityValue?.ToString() != dtoValueString)
+						auditLogger.AppendFormat("{0,20}", dtoProperty.Name).Append(": ").AppendFormat("{0,20}", entityValue).Append(" -> ").AppendFormat("{0,20}", dtoValueString).AppendLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				auditLogger.AppendLine(ex.Message);
 			}
 		}
 	}
