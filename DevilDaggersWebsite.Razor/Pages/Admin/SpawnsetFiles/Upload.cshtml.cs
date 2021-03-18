43f0041365a18f2012d1d59d87648c7b08545b05
Noah Stolk <noah.stolk@gmail.com>
2021-03-19 00:12:34 +0100

    Log exceptions for spawnset file upload

--- DevilDaggersWebsite.Razor/Pages/Admin/SpawnsetFiles/Upload.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/Admin/SpawnsetFiles/Upload.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Threading.Tasks;
 using Io = System.IO;
 
@@ -24,50 +25,58 @@ namespace DevilDaggersWebsite.Razor.Pages.Admin.SpawnsetFiles
 
 		public async Task OnPostAsync()
 		{
-			const int maxFileSize = 70 * 1024;
-			const int maxFileNameLength = 80;
 			string failedAttemptMessage = $"Failed attempt from {this.GetIdentity()} to upload new spawnset file";
 
-			if (FormFile == null)
+			try
 			{
-				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: No file.`");
-				return;
-			}
+				const int maxFileSize = 70 * 1024;
+				const int maxFileNameLength = 80;
 
-			if (FormFile.Length > maxFileSize)
+				if (FormFile == null)
+				{
+					await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: No file.`");
+					return;
+				}
+
+				if (FormFile.Length > maxFileSize)
+				{
+					await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File too large ({FormFile.Length} / max {maxFileSize} bytes).`");
+					return;
+				}
+
+				if (FormFile.FileName.Length > maxFileNameLength)
+				{
+					await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File name too long ({FormFile.FileName.Length} / max {maxFileNameLength} characters).`");
+					return;
+				}
+
+				string filePath = Io.Path.Combine(_env.WebRootPath, "spawnsets", FormFile.FileName);
+				if (Io.File.Exists(filePath))
+				{
+					await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File '{FormFile.FileName}' already exists.`");
+					return;
+				}
+
+				byte[] formFileBytes = new byte[FormFile.Length];
+				using (Io.MemoryStream ms = new())
+				{
+					FormFile.CopyTo(ms);
+					formFileBytes = ms.ToArray();
+				}
+
+				if (!Spawnset.TryParse(formFileBytes, out _))
+				{
+					await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File could not be parsed to a proper survival file.`");
+					return;
+				}
+
+				Io.File.WriteAllBytes(filePath, formFileBytes);
+				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{this.GetIdentity()} uploaded new spawnset file '{FormFile.FileName}'`");
+			}
+			catch (Exception ex)
 			{
-				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File too large ({FormFile.Length} / max {maxFileSize} bytes).`");
-				return;
+				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: Fatal error: {ex.Message}'`");
 			}
-
-			if (FormFile.FileName.Length > maxFileNameLength)
-			{
-				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File name too long ({FormFile.FileName.Length} / max {maxFileNameLength} characters).`");
-				return;
-			}
-
-			string filePath = Io.Path.Combine(_env.WebRootPath, "spawnsets", FormFile.FileName);
-			if (Io.File.Exists(filePath))
-			{
-				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File '{FormFile.FileName}' already exists.`");
-				return;
-			}
-
-			byte[] formFileBytes = new byte[FormFile.Length];
-			using (Io.MemoryStream ms = new())
-			{
-				FormFile.CopyTo(ms);
-				formFileBytes = ms.ToArray();
-			}
-
-			if (!Spawnset.TryParse(formFileBytes, out _))
-			{
-				await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{failedAttemptMessage}: File could not be parsed to a proper survival file.`");
-				return;
-			}
-
-			Io.File.WriteAllBytes(filePath, formFileBytes);
-			await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{this.GetIdentity()} uploaded new spawnset file '{FormFile.FileName}'`");
 		}
 	}
 }
