7d126722959fec3e3dd7cfd0d1fc5d26229fe50e
Noah Stolk <noah.stolk@gmail.com>
2021-03-25 22:19:28 +0100

    Remove leaderboard history completions

--- DevilDaggersWebsite.Razor/Pages/Leaderboard/History.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/Leaderboard/History.cshtml.cs
@@ -69,42 +69,39 @@ namespace DevilDaggersWebsite.Razor.Pages.Leaderboard
 			{
 				LeaderboardPrevious = JsonConvert.DeserializeObject<Lb>(Io.File.ReadAllText(Path.Combine(_env.WebRootPath, "leaderboard-history", FromNext), Encoding.UTF8));
 
-				if (LeaderboardPrevious.GetCompletionRate() > 0.999f && Leaderboard.GetCompletionRate() > 0.999f)
+				if (LeaderboardPrevious.Players != Leaderboard.Players)
+					ChangesGlobal.Add($"Total players +{Leaderboard.Players - LeaderboardPrevious.Players}");
+
+				if (LeaderboardPrevious.DeathsGlobal != Leaderboard.DeathsGlobal)
+					ChangesGlobal.Add($"Global deaths +{Leaderboard.DeathsGlobal - LeaderboardPrevious.DeathsGlobal}\n");
+
+				List<Entry> top100joins = new();
+				List<Entry> highscores = new();
+				List<Entry> plays = new();
+				foreach (Entry entry in Leaderboard.Entries)
 				{
-					if (LeaderboardPrevious.Players != Leaderboard.Players)
-						ChangesGlobal.Add($"Total players +{Leaderboard.Players - LeaderboardPrevious.Players}");
-
-					if (LeaderboardPrevious.DeathsGlobal != Leaderboard.DeathsGlobal)
-						ChangesGlobal.Add($"Global deaths +{Leaderboard.DeathsGlobal - LeaderboardPrevious.DeathsGlobal}\n");
-
-					List<Entry> top100joins = new();
-					List<Entry> highscores = new();
-					List<Entry> plays = new();
-					foreach (Entry entry in Leaderboard.Entries)
+					Entry? entryPrevious = LeaderboardPrevious.Entries.Find(e => e.Id == entry.Id);
+					if (entryPrevious == null)
 					{
-						Entry? entryPrevious = LeaderboardPrevious.Entries.Find(e => e.Id == entry.Id);
-						if (entryPrevious == null)
-						{
-							top100joins.Add(entry);
-							highscores.Add(entry);
-							plays.Add(entry);
-						}
-						else
-						{
-							if (entry.Time != entryPrevious.Time)
-								highscores.Add(entry);
-							if (entry.DeathsTotal != entryPrevious.DeathsTotal)
-								plays.Add(entry);
-						}
+						top100joins.Add(entry);
+						highscores.Add(entry);
+						plays.Add(entry);
 					}
-
-					if (top100joins.Count != 0)
-						ChangesTop100[$"{top100joins.Count} player{top100joins.Count.S()} joined top 100"] = string.Join(", ", top100joins.Select(e => e.Username));
-					if (highscores.Count != 0)
-						ChangesTop100[$"{highscores.Count} player{highscores.Count.S()} set a new highscore"] = string.Join(", ", highscores.Select(e => e.Username));
-					if (plays.Count != 0)
-						ChangesTop100[$"{plays.Count} player{plays.Count.S()} played"] = string.Join(", ", plays.Select(e => e.Username));
+					else
+					{
+						if (entry.Time != entryPrevious.Time)
+							highscores.Add(entry);
+						if (entry.DeathsTotal != entryPrevious.DeathsTotal)
+							plays.Add(entry);
+					}
 				}
+
+				if (top100joins.Count != 0)
+					ChangesTop100[$"{top100joins.Count} player{top100joins.Count.S()} joined top 100"] = string.Join(", ", top100joins.Select(e => e.Username));
+				if (highscores.Count != 0)
+					ChangesTop100[$"{highscores.Count} player{highscores.Count.S()} set a new highscore"] = string.Join(", ", highscores.Select(e => e.Username));
+				if (plays.Count != 0)
+					ChangesTop100[$"{plays.Count} player{plays.Count.S()} played"] = string.Join(", ", plays.Select(e => e.Username));
 			}
 		}
 	}
--- DevilDaggersWebsite/Dto/Leaderboard.cs
+++ DevilDaggersWebsite/Dto/Leaderboard.cs
@@ -1,10 +1,6 @@
-﻿using DevilDaggersWebsite.LeaderboardHistory;
-using DevilDaggersWebsite.Utils;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace DevilDaggersWebsite.Dto
 {
@@ -15,117 +11,23 @@ namespace DevilDaggersWebsite.Dto
 		/// </summary>
 		public DateTime DateTime { get; set; } = DateTime.UtcNow;
 
-		[CompletionProperty]
 		public int Players { get; set; }
 
-		[CompletionProperty]
 		public ulong TimeGlobal { get; set; }
 
-		[CompletionProperty]
 		public ulong KillsGlobal { get; set; }
 
-		[CompletionProperty]
 		public ulong GemsGlobal { get; set; }
 
-		[CompletionProperty]
 		public ulong DeathsGlobal { get; set; }
 
-		[CompletionProperty]
 		public ulong DaggersHitGlobal { get; set; }
 
-		[CompletionProperty]
 		public ulong DaggersFiredGlobal { get; set; }
 
 		public List<Entry> Entries { get; set; } = new();
 
 		[JsonIgnore]
 		public double AccuracyGlobal => DaggersFiredGlobal == 0 ? 0 : DaggersHitGlobal / (double)DaggersFiredGlobal;
-
-		[JsonIgnore]
-		public bool HasBlankName => Entries.Any(e => e.IsBlankName());
-
-		[JsonIgnore]
-		public Completion Completion { get; } = new();
-
-		[JsonIgnore]
-		public CompletionCombined UserCompletion { get; } = new();
-
-		public Completion GetCompletion()
-		{
-			if (Completion.Initialized)
-				return Completion;
-
-			foreach (PropertyInfo info in GetType().GetProperties())
-			{
-				object? value = info.GetValue(this);
-				if (value == null)
-					continue;
-
-				string? valueString = value.ToString();
-				if (string.IsNullOrEmpty(valueString))
-					continue;
-
-				if (!Attribute.IsDefined(info, typeof(CompletionPropertyAttribute)))
-					continue;
-
-				if (valueString == ReflectionUtils.GetDefaultValue(value.GetType())?.ToString())
-					Completion.CompletionEntries[info.Name] = CompletionEntry.Missing;
-				else
-					Completion.CompletionEntries[info.Name] = CompletionEntry.Complete;
-			}
-
-			int players = Players == 0 ? 100 : Math.Min(Players, 100);
-			if (Entries.Count != players)
-				Completion.CompletionEntries[$"{players - Entries.Count} users"] = CompletionEntry.Missing;
-
-			Completion.Initialized = true;
-			return Completion;
-		}
-
-		public CompletionCombined GetUserCompletion()
-		{
-			if (UserCompletion.Initialized)
-				return UserCompletion;
-
-			List<Completion> completions = new();
-			foreach (Entry entry in Entries)
-				completions.Add(entry.GetCompletion());
-			int total = HasBlankName ? completions.Count - 1 : completions.Count;
-
-			foreach (KeyValuePair<string, CompletionEntry> kvp in completions[0].CompletionEntries)
-			{
-				int missing = 0;
-				for (int i = 0; i < completions.Count; i++)
-				{
-					if (Entries[i].IsBlankName())
-						continue;
-
-					if (completions[i].CompletionEntries.TryGetValue(kvp.Key, out CompletionEntry ce) && ce == CompletionEntry.Missing)
-						missing++;
-				}
-
-				UserCompletion.CompletionEntries[kvp.Key] = missing == 0 ? CompletionEntryCombined.Complete : missing == total ? CompletionEntryCombined.Missing : CompletionEntryCombined.PartiallyMissing;
-			}
-
-			UserCompletion.Initialized = true;
-			return UserCompletion;
-		}
-
-		public float GetCompletionRate()
-		{
-			int total = HasBlankName ? 99 : 100;
-			int totalEntries = Players == 0 ? total : Math.Min(Players, total);
-
-			float globalCompletionRate = GetCompletion().GetCompletionRate();
-			float userCompletionRate = 0;
-			foreach (Entry entry in Entries)
-			{
-				if (entry.IsBlankName())
-					continue;
-				userCompletionRate += entry.GetCompletion().GetCompletionRate() * (1f / totalEntries);
-			}
-
-			return userCompletionRate * 0.99f + globalCompletionRate * 0.01f;
-		}
 	}
 }
