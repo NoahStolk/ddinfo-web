369cbf3341ebdfe171f7ae5c5e2d875e1e3648be
Noah Stolk <noah.stolk@gmail.com>
2021-04-05 20:45:05 +0200

    Make auto scroll faster

--- DevilDaggersWebsite.Razor/wwwroot/js-site/main.js
+++ DevilDaggersWebsite.Razor/wwwroot/js-site/main.js
@@ -19,7 +19,7 @@ $('a[href*="#"]')
 			event.preventDefault();
 			$('html, body').animate({
 				scrollTop: target.offset().top - 70 // header height
-			}, 1000, function () {
+			}, 500, function () {
 				// Callback after animation
 				// Must change focus!
 				var $target = $(target);
