095c8383d0ea7015770daa2c58320badb89261fe
Noah Stolk <noah.stolk@gmail.com>
2021-04-07 23:16:36 +0200

    Get actual current world record in WorldRecordProgression page to compare against

--- DevilDaggersWebsite.Razor/Pages/Leaderboard/WorldRecordProgression.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/Leaderboard/WorldRecordProgression.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using DevilDaggersWebsite.Dto;
+﻿using DevilDaggersWebsite.Clients;
+using DevilDaggersWebsite.Dto;
 using DevilDaggersWebsite.Razor.Utils;
 using DevilDaggersWebsite.Transients;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lb = DevilDaggersWebsite.Dto.Leaderboard;
 
 namespace DevilDaggersWebsite.Razor.Pages.Leaderboard
 {
@@ -20,9 +23,15 @@ namespace DevilDaggersWebsite.Razor.Pages.Leaderboard
 
 		public Dictionary<WorldRecord, TimeSpan> WorldRecords { get; private set; } = new();
 
-		public void OnGet()
+		public Entry? CurrentWorldRecord { get; private set; }
+
+		public async Task OnGetAsync()
 		{
 			(WorldRecordHolders, WorldRecords) = _leaderboardHistoryHelper.GetWorldRecordData();
+
+			Lb? leaderboard = await LeaderboardClient.Instance.GetScores(1);
+			if (leaderboard?.Entries.Count > 0)
+				CurrentWorldRecord = leaderboard.Entries[0];
 		}
 
 		public string GetHistoryDateString(DateTime dateTime)
