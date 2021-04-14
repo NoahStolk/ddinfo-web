de04e8de328780794c634614d8a93b8359f906d3
Noah Stolk <noah.stolk@gmail.com>
2021-04-14 21:46:58 +0200

    Implement deleting spawnset files

---
+++ DevilDaggersWebsite.Razor/Pages/Admin/SpawnsetFiles/DeleteFile.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using DevilDaggersDiscordBot.Logging;
+using DevilDaggersWebsite.Razor.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.SpawnsetFiles
+{
+	public class DeleteFileModel : PageModel
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public DeleteFileModel(IWebHostEnvironment env)
+		{
+			_env = env;
+		}
+
+		public IEnumerable<string> SpawnsetFileNames { get; private set; } = Enumerable.Empty<string>();
+
+		public void OnGet()
+		{
+			SpawnsetFileNames = Directory.GetFiles(Path.Combine(_env.WebRootPath, "spawnsets")).Select(p => Path.GetFileName(p));
+		}
+
+		public async Task<ActionResult> OnPost(string fileName)
+		{
+			System.IO.File.Delete(Path.Combine(_env.WebRootPath, "spawnsets", fileName));
+
+			await DiscordLogger.Instance.TryLog(Channel.AuditLogMonitoring, _env.EnvironmentName, $"`{this.GetIdentity()}` deleted SPAWNSET file `{fileName}`");
+
+			return RedirectToPage("Index");
+		}
+	}
+}
