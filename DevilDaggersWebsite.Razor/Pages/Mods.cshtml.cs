4794d1eec5b8f8ef467733fe85efbe818ee8537d
Noah Stolk <noah.stolk@gmail.com>
2021-04-15 22:21:34 +0200

    Add "Hosted" and "Prohibited" sorts to /Mods page

--- DevilDaggersWebsite.Razor/Pages/Mods.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/Mods.cshtml.cs
@@ -1,21 +1,40 @@
-﻿using DevilDaggersWebsite.Entities;
+﻿using DevilDaggersWebsite.Caches;
+using DevilDaggersWebsite.Entities;
 using DevilDaggersWebsite.Razor.PageModels;
 using DevilDaggersWebsite.Razor.Pagination;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Io = System.IO;
 
 namespace DevilDaggersWebsite.Razor.Pages
 {
 	public class ModsModel : PageModel, IPaginationModel
 	{
-		private readonly ApplicationDbContext _dbContext;
+		private const string _ascendingSuffix = "_asc";
 
-		public ModsModel(ApplicationDbContext dbContext)
+		public const string NameDesc = nameof(Name);
+		public const string AuthorDesc = nameof(Author);
+		public const string TypeDesc = nameof(Type);
+		public const string HostedDesc = nameof(Hosted);
+		public const string ProhibitedDesc = nameof(Prohibited);
+		public const string NameAsc = nameof(Name) + _ascendingSuffix;
+		public const string AuthorAsc = nameof(Author) + _ascendingSuffix;
+		public const string TypeAsc = nameof(Type) + _ascendingSuffix;
+		public const string HostedAsc = nameof(Hosted) + _ascendingSuffix;
+		public const string ProhibitedAsc = nameof(Prohibited) + _ascendingSuffix;
+
+		private readonly ApplicationDbContext _dbContext;
+		private readonly IWebHostEnvironment _env;
+
+		public ModsModel(ApplicationDbContext dbContext, IWebHostEnvironment env)
 		{
 			_dbContext = dbContext;
+			_env = env;
 		}
 
 		public PaginatedList<Mod> PaginatedList { get; private set; } = null!;
@@ -23,9 +42,11 @@ namespace DevilDaggersWebsite.Razor.Pages
 		public string? SearchAuthor { get; set; }
 		public string? SearchName { get; set; }
 
-		public string? NameSort { get; set; }
-		public string? AuthorSort { get; set; }
-		public string? TypeSort { get; set; }
+		public string? Name { get; set; }
+		public string? Author { get; set; }
+		public string? Type { get; set; }
+		public string? Hosted { get; set; }
+		public string? Prohibited { get; set; }
 
 		public string? SortOrder { get; set; }
 
@@ -43,33 +64,46 @@ namespace DevilDaggersWebsite.Razor.Pages
 			SortOrder = sortOrder;
 			PageIndex = pageIndex ?? 1;
 
-			NameSort = sortOrder == "Name" ? "Name_asc" : "Name";
-			AuthorSort = sortOrder == "Author_asc" ? "Author" : "Author_asc";
-			TypeSort = sortOrder == "TypeSort_asc" ? "TypeSort" : "TypeSort_asc";
+			Name = sortOrder == NameDesc ? NameAsc : NameDesc;
+			Author = sortOrder == AuthorAsc ? AuthorDesc : AuthorAsc;
+			Type = sortOrder == TypeAsc ? TypeDesc : TypeAsc;
+			Hosted = sortOrder == HostedAsc ? HostedDesc : HostedAsc;
+			Prohibited = sortOrder == ProhibitedAsc ? ProhibitedDesc : ProhibitedAsc;
 
-			List<Mod> sortedMods = new();
+			List<Mod> mods = new();
 			foreach (AssetMod assetMod in _dbContext.AssetMods.Include(am => am.PlayerAssetMods).ThenInclude(pam => pam.Player).Where(am => !am.IsHidden))
-				sortedMods.Add(new(assetMod.AssetModTypes, assetMod.Name, assetMod.PlayerAssetMods.Select(pam => pam.Player.PlayerName).OrderBy(s => s).ToList()));
+			{
+				string filePath = Path.Combine(_env.WebRootPath, "mods", $"{assetMod.Name}.zip");
+				bool isHostedOnDdInfo = Io.File.Exists(filePath);
+				bool? containsAnyProhibitedAssets = null;
+				if (isHostedOnDdInfo)
+					containsAnyProhibitedAssets = ModDataCache.Instance.GetModDataByFilePath(filePath).Any(md => md.ModAssetData.Any(mad => mad.IsProhibited));
+				mods.Add(new(assetMod.AssetModTypes, assetMod.Name, assetMod.PlayerAssetMods.Select(pam => pam.Player.PlayerName).OrderBy(s => s).ToList(), isHostedOnDdInfo, containsAnyProhibitedAssets));
+			}
 
 			if (!string.IsNullOrWhiteSpace(SearchAuthor))
-				sortedMods = sortedMods.Where(am => am.Authors.Any(a => a.Contains(SearchAuthor, StringComparison.InvariantCultureIgnoreCase))).ToList();
+				mods = mods.Where(am => am.Authors.Any(a => a.Contains(SearchAuthor, StringComparison.InvariantCultureIgnoreCase))).ToList();
 
 			if (!string.IsNullOrWhiteSpace(SearchName))
-				sortedMods = sortedMods.Where(am => am.Name.Contains(SearchName, StringComparison.InvariantCultureIgnoreCase)).ToList();
+				mods = mods.Where(am => am.Name.Contains(SearchName, StringComparison.InvariantCultureIgnoreCase)).ToList();
 
-			sortedMods = sortOrder switch
+			mods = sortOrder switch
 			{
-				"Name" => sortedMods.OrderByDescending(s => s.Name).ThenBy(s => s.Authors[0]).ToList(),
-				"Author_asc" => sortedMods.OrderBy(s => s.Authors[0]).ThenByDescending(s => s.Name).ToList(),
-				"Author" => sortedMods.OrderByDescending(s => s.Authors[0]).ThenBy(s => s.Name).ToList(),
-				"TypeSort_asc" => sortedMods.OrderBy(s => s.AssetModTypes).ThenByDescending(s => s.Name).ToList(),
-				"TypeSort" => sortedMods.OrderByDescending(s => s.AssetModTypes).ThenBy(s => s.Name).ToList(),
-				_ => sortedMods.OrderBy(s => s.Name).ThenByDescending(s => s.Authors[0]).ToList(),
+				NameDesc => mods.OrderByDescending(m => m.Name).ThenBy(m => m.Authors[0]).ToList(),
+				AuthorAsc => mods.OrderBy(m => m.Authors[0]).ThenByDescending(m => m.Name).ToList(),
+				AuthorDesc => mods.OrderByDescending(m => m.Authors[0]).ThenBy(m => m.Name).ToList(),
+				TypeAsc => mods.OrderBy(m => m.AssetModTypes).ThenByDescending(m => m.Name).ToList(),
+				TypeDesc => mods.OrderByDescending(m => m.AssetModTypes).ThenBy(m => m.Name).ToList(),
+				HostedAsc => mods.OrderBy(m => m.IsHostedOnDdInfo).ThenByDescending(m => m.Name).ToList(),
+				HostedDesc => mods.OrderByDescending(m => m.IsHostedOnDdInfo).ThenBy(m => m.Name).ToList(),
+				ProhibitedAsc => mods.OrderBy(m => m.ContainsAnyProhibitedAssets).ThenByDescending(m => m.Name).ToList(),
+				ProhibitedDesc => mods.OrderByDescending(m => m.ContainsAnyProhibitedAssets).ThenBy(m => m.Name).ToList(),
+				_ => mods.OrderBy(m => m.Name).ThenByDescending(m => m.Authors[0]).ToList(),
 			};
-			TotalResults = sortedMods.Count;
+			TotalResults = mods.Count;
 			TotalPages = Math.Max(1, (int)Math.Ceiling(TotalResults / (double)PageSize));
 			PageIndex = Math.Clamp(PageIndex, 1, TotalPages);
-			PaginatedList = new(sortedMods, PageIndex, PageSize);
+			PaginatedList = new(mods, PageIndex, PageSize);
 		}
 	}
 }
--- DevilDaggersWebsite.Razor/Pagination/Mod.cs
+++ DevilDaggersWebsite.Razor/Pagination/Mod.cs
@@ -5,15 +5,19 @@ namespace DevilDaggersWebsite.Razor.Pagination
 {
 	public class Mod
 	{
-		public Mod(AssetModTypes assetModTypes, string name, List<string> authors)
+		public Mod(AssetModTypes assetModTypes, string name, List<string> authors, bool isHostedOnDdInfo, bool? containsAnyProhibitedAssets)
 		{
 			AssetModTypes = assetModTypes;
 			Name = name;
 			Authors = authors;
+			IsHostedOnDdInfo = isHostedOnDdInfo;
+			ContainsAnyProhibitedAssets = containsAnyProhibitedAssets;
 		}
 
 		public AssetModTypes AssetModTypes { get; set; }
 		public string Name { get; set; }
 		public List<string> Authors { get; set; }
+		public bool IsHostedOnDdInfo { get; set; }
+		public bool? ContainsAnyProhibitedAssets { get; set; }
 	}
 }
