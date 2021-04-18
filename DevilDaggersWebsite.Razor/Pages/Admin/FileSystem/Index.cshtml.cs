2ba94a6a35dcb0fee3d0fe2927c6cce226899a49
Noah Stolk <noah.stolk@gmail.com>
2021-04-18 18:26:35 +0200

    Add Admin/FileSystem page

---
+++ DevilDaggersWebsite.Razor/Pages/Admin/FileSystem/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.FileSystem
+{
+	public class IndexModel : PageModel
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public IndexModel(IWebHostEnvironment env)
+		{
+			_env = env;
+		}
+
+		public Dictionary<string, long> TotalSpaceInBytesPerDrive { get; private set; } = new();
+		public Dictionary<string, long> ModFileSizes { get; private set; } = new();
+		public Dictionary<string, long> SpawnsetFileSizes { get; private set; } = new();
+		public Dictionary<string, long> LeaderboardHistoryFileSizes { get; private set; } = new();
+
+		public void OnGet()
+		{
+			foreach (DriveInfo di in DriveInfo.GetDrives())
+				TotalSpaceInBytesPerDrive.Add(di.Name, di.TotalFreeSpace);
+
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "mods")))
+				ModFileSizes.Add(Path.GetFileName(path), new FileInfo(path).Length);
+
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "spawnsets")))
+				SpawnsetFileSizes.Add(Path.GetFileName(path), new FileInfo(path).Length);
+
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "leaderboard-history")))
+				LeaderboardHistoryFileSizes.Add(Path.GetFileName(path), new FileInfo(path).Length);
+
+			TotalSpaceInBytesPerDrive = TotalSpaceInBytesPerDrive.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			ModFileSizes = ModFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			SpawnsetFileSizes = SpawnsetFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			LeaderboardHistoryFileSizes = LeaderboardHistoryFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+		}
+	}
+}
