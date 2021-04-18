dd1146731ba6b6661ff14e0c523101c94d61e81c
Noah Stolk <noah.stolk@gmail.com>
2021-04-18 19:24:49 +0200

    Throw UnauthorizedAccessException when identity could not be retrieved

--- DevilDaggersWebsite.Razor/Extensions/PageModelExtensions.cs
+++ DevilDaggersWebsite.Razor/Extensions/PageModelExtensions.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace DevilDaggersWebsite.Razor.Extensions
 {
 	public static class PageModelExtensions
 	{
 		public static string GetIdentity(this PageModel pageModel)
-			=> pageModel.User.Identity?.Name?.Substring(0, 4) ?? "UNKNOWN IDENTITY";
+			=> pageModel.User.Identity?.Name?.Substring(0, 4) ?? throw new UnauthorizedAccessException("Not allowed to access this resource.");
 	}
 }
