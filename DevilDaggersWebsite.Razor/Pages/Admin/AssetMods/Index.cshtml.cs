d5505ab96d2a1ba7e7993dedf8363923ace3c32b
Noah Stolk <noah.stolk@gmail.com>
2021-04-18 21:50:52 +0200

    Add "Dead files" section to /AssetMods and /SpawnsetFiles pages

---
+++ DevilDaggersWebsite.Razor/Pages/Admin/AssetMods/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using DevilDaggersWebsite.Entities;
+using DevilDaggersWebsite.Razor.PageModels;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.AssetMods
+{
+	public class IndexModel : AdminEntityIndexPageModel<AssetMod>
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public IndexModel(ApplicationDbContext dbContext, IWebHostEnvironment env)
+			: base(dbContext)
+		{
+			_env = env;
+		}
+
+		public List<string> DeadFiles { get; } = new();
+
+		public void OnGet()
+		{
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "mods")))
+			{
+				string fileName = Path.GetFileNameWithoutExtension(path);
+				if (!DbContext.AssetMods.Any(am => am.Name == fileName))
+					DeadFiles.Add(fileName);
+			}
+		}
+	}
+}
---
+++ DevilDaggersWebsite.Razor/Pages/Admin/SpawnsetFiles/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using DevilDaggersWebsite.Entities;
+using DevilDaggersWebsite.Razor.PageModels;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.SpawnsetFiles
+{
+	public class IndexModel : AdminEntityIndexPageModel<SpawnsetFile>
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public IndexModel(ApplicationDbContext dbContext, IWebHostEnvironment env)
+			: base(dbContext)
+		{
+			_env = env;
+		}
+
+		public List<string> DeadFiles { get; } = new();
+
+		public void OnGet()
+		{
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "spawnsets")))
+			{
+				string fileName = Path.GetFileNameWithoutExtension(path);
+				if (!DbContext.SpawnsetFiles.Any(sf => sf.Name == fileName))
+					DeadFiles.Add(fileName);
+			}
+		}
+	}
+}
