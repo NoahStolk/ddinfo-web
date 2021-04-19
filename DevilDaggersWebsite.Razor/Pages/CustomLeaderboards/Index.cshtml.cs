f3c44489b3c1f5ed1ba9594b719d7f8710df2908
Noah Stolk <noah.stolk@gmail.com>
2021-04-19 12:56:43 +0200

    Rewrite /CustomLeaderboards page

--- DevilDaggersWebsite.Razor/Pages/CustomLeaderboards/Index.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/CustomLeaderboards/Index.cshtml.cs
@@ -1,49 +1,159 @@
-﻿using DevilDaggersWebsite.Entities;
-using DevilDaggersWebsite.Enumerators;
+﻿using DevilDaggersWebsite.Enumerators;
+using DevilDaggersWebsite.Extensions;
+using DevilDaggersWebsite.Razor.PageModels;
+using DevilDaggersWebsite.Razor.Pagination;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace DevilDaggersWebsite.Razor.Pages.CustomLeaderboards
 {
-	public class IndexModel : PageModel
+	public class IndexModel : PageModel, IPaginationModel
 	{
-		private readonly ApplicationDbContext _dbContext;
+		private const string _ascendingSuffix = "_asc";
 
-		public IndexModel(ApplicationDbContext dbContext)
+		public const string NameDesc = nameof(Name);
+		public const string AuthorDesc = nameof(Author);
+		public const string CategoryDesc = nameof(Category);
+		public const string LastPlayedDesc = nameof(LastPlayed);
+		public const string CreatedDesc = nameof(Created);
+		public const string SubmitsDesc = nameof(Submits);
+		public const string PlayersDesc = nameof(Players);
+		public const string BronzeDesc = nameof(Bronze);
+		public const string SilverDesc = nameof(Silver);
+		public const string GoldenDesc = nameof(Golden);
+		public const string DevilDesc = nameof(Devil);
+		public const string LeviathanDesc = nameof(Leviathan);
+		public const string WorldRecordDesc = nameof(WorldRecord);
+		public const string NameAsc = nameof(Name) + _ascendingSuffix;
+		public const string AuthorAsc = nameof(Author) + _ascendingSuffix;
+		public const string CategoryAsc = nameof(Category) + _ascendingSuffix;
+		public const string LastPlayedAsc = nameof(LastPlayed) + _ascendingSuffix;
+		public const string CreatedAsc = nameof(Created) + _ascendingSuffix;
+		public const string SubmitsAsc = nameof(Submits) + _ascendingSuffix;
+		public const string PlayersAsc = nameof(Players) + _ascendingSuffix;
+		public const string BronzeAsc = nameof(Bronze) + _ascendingSuffix;
+		public const string SilverAsc = nameof(Silver) + _ascendingSuffix;
+		public const string GoldenAsc = nameof(Golden) + _ascendingSuffix;
+		public const string DevilAsc = nameof(Devil) + _ascendingSuffix;
+		public const string LeviathanAsc = nameof(Leviathan) + _ascendingSuffix;
+		public const string WorldRecordAsc = nameof(WorldRecord) + _ascendingSuffix;
+
+		private readonly Entities.ApplicationDbContext _dbContext;
+
+		public IndexModel(Entities.ApplicationDbContext dbContext)
 		{
 			_dbContext = dbContext;
-
-			foreach (CustomLeaderboardCategory e in (CustomLeaderboardCategory[])Enum.GetValues(typeof(CustomLeaderboardCategory)))
-			{
-				if (e == CustomLeaderboardCategory.None || e == CustomLeaderboardCategory.Challenge)
-					continue;
-
-				CategoryListItems.Add(new SelectListItem($"Category: {e}", e.ToString()));
-			}
 		}
 
-		public List<SelectListItem> CategoryListItems { get; } = new();
+		public PaginatedList<CustomLeaderboard> PaginatedList { get; private set; } = null!;
 
-		public CustomLeaderboardCategory Category { get; private set; }
-		public List<CustomLeaderboard> Leaderboards { get; private set; } = null!;
+		public string? SearchAuthor { get; set; }
+		public string? SearchName { get; set; }
 
-		public void OnGet(CustomLeaderboardCategory category = CustomLeaderboardCategory.Default)
+		public string? Name { get; set; }
+		public string? Author { get; set; }
+		public string? Category { get; set; }
+		public string? LastPlayed { get; set; }
+		public string? Created { get; set; }
+		public string? Submits { get; set; }
+		public string? Players { get; set; }
+		public string? Bronze { get; set; }
+		public string? Silver { get; set; }
+		public string? Golden { get; set; }
+		public string? Devil { get; set; }
+		public string? Leviathan { get; set; }
+		public string? WorldRecord { get; set; }
+
+		public string? SortOrder { get; set; }
+
+		public int PageSize { get; set; } = 20;
+		public int PageIndex { get; private set; }
+		public int TotalPages { get; private set; }
+		public int TotalResults { get; private set; }
+
+		public void OnGet(string searchAuthor, string searchName, string sortOrder, int? pageIndex)
 		{
-			if (category == CustomLeaderboardCategory.None || category == CustomLeaderboardCategory.Challenge)
-				category = CustomLeaderboardCategory.Default;
+			SearchAuthor = searchAuthor;
+			SearchName = searchName;
+			SortOrder = sortOrder;
+			PageIndex = pageIndex ?? 1;
 
-			Category = category;
+			Name = sortOrder == NameAsc ? NameDesc : NameAsc;
+			Author = sortOrder == AuthorAsc ? AuthorDesc : AuthorAsc;
+			Category = sortOrder == CategoryAsc ? CategoryDesc : CategoryAsc;
+			LastPlayed = sortOrder == LastPlayedAsc ? LastPlayedDesc : LastPlayedAsc;
+			Created = sortOrder == CreatedDesc ? CreatedAsc : CreatedDesc;
+			Submits = sortOrder == SubmitsDesc ? SubmitsAsc : SubmitsDesc;
+			Players = sortOrder == PlayersDesc ? PlayersAsc : PlayersDesc;
+			Bronze = sortOrder == BronzeDesc ? BronzeAsc : BronzeDesc;
+			Silver = sortOrder == SilverDesc ? SilverAsc : SilverDesc;
+			Golden = sortOrder == GoldenDesc ? GoldenAsc : GoldenDesc;
+			Devil = sortOrder == DevilDesc ? DevilAsc : DevilDesc;
+			Leviathan = sortOrder == LeviathanDesc ? LeviathanAsc : LeviathanDesc;
+			WorldRecord = sortOrder == WorldRecordDesc ? WorldRecordAsc : WorldRecordDesc;
 
-			Leaderboards = _dbContext.CustomLeaderboards
-				.Where(cl => cl.Category == category && !cl.IsArchived)
+			IIncludableQueryable<Entities.CustomLeaderboard, Entities.Player> customLeaderboardQuery = _dbContext.CustomLeaderboards
+				.Where(cl => cl.Category != CustomLeaderboardCategory.Challenge && !cl.IsArchived)
 				.Include(cl => cl.SpawnsetFile)
-					.ThenInclude(sf => sf.Player)
-				.OrderByDescending(cl => cl.DateLastPlayed)
-				.ToList();
+					.ThenInclude(sf => sf.Player);
+
+			List<CustomLeaderboard> customLeaderboards = new();
+			foreach (Entities.CustomLeaderboard cl in customLeaderboardQuery)
+			{
+				if (!string.IsNullOrWhiteSpace(SearchAuthor) && !cl.SpawnsetFile.Player.PlayerName.Contains(SearchAuthor))
+					continue;
+
+				if (!string.IsNullOrWhiteSpace(SearchName) && !cl.SpawnsetFile.Name.Contains(SearchName))
+					continue;
+
+				List<Entities.CustomEntry> entries = _dbContext.CustomEntries
+					.Where(e => e.CustomLeaderboard == cl)
+					.OrderByMember(nameof(Entities.CustomEntry.Time), cl.Category.IsAscending())
+					.ThenByMember(nameof(Entities.CustomEntry.SubmitDate), true)
+					.ToList();
+
+				int worldRecord = entries.Count == 0 ? 0 : entries[0].Time;
+				customLeaderboards.Add(new(cl.Category, cl.SpawnsetFile.Name, cl.SpawnsetFile.Player.PlayerName, cl.TimeBronze, cl.TimeSilver, cl.TimeGolden, cl.TimeDevil, cl.TimeLeviathan, worldRecord, cl.DateLastPlayed, cl.DateCreated, cl.TotalRunsSubmitted, entries.Count));
+			}
+
+			customLeaderboards = (sortOrder switch
+			{
+				NameAsc => customLeaderboards.OrderBy(cl => cl.SpawnsetName).ThenByDescending(cl => cl.AuthorName),
+				NameDesc => customLeaderboards.OrderByDescending(cl => cl.SpawnsetName).ThenBy(cl => cl.AuthorName),
+				AuthorAsc => customLeaderboards.OrderBy(cl => cl.AuthorName).ThenByDescending(cl => cl.SpawnsetName),
+				AuthorDesc => customLeaderboards.OrderByDescending(cl => cl.AuthorName).ThenBy(cl => cl.SpawnsetName),
+				CategoryAsc => customLeaderboards.OrderBy(cl => cl.Category).ThenByDescending(cl => cl.SpawnsetName),
+				CategoryDesc => customLeaderboards.OrderByDescending(cl => cl.Category).ThenBy(cl => cl.SpawnsetName),
+				LastPlayedAsc => customLeaderboards.OrderBy(cl => cl.DateLastPlayed).ThenByDescending(cl => cl.SpawnsetName),
+				CreatedAsc => customLeaderboards.OrderBy(cl => cl.DateCreated).ThenByDescending(cl => cl.SpawnsetName),
+				CreatedDesc => customLeaderboards.OrderByDescending(cl => cl.DateCreated).ThenBy(cl => cl.SpawnsetName),
+				SubmitsAsc => customLeaderboards.OrderBy(cl => cl.TotalRunsSubmitted).ThenByDescending(cl => cl.SpawnsetName),
+				SubmitsDesc => customLeaderboards.OrderByDescending(cl => cl.TotalRunsSubmitted).ThenBy(cl => cl.SpawnsetName),
+				PlayersAsc => customLeaderboards.OrderBy(cl => cl.TotalPlayers).ThenByDescending(cl => cl.SpawnsetName),
+				PlayersDesc => customLeaderboards.OrderByDescending(cl => cl.TotalPlayers).ThenBy(cl => cl.SpawnsetName),
+				BronzeAsc => customLeaderboards.OrderBy(cl => cl.TimeBronze).ThenByDescending(cl => cl.SpawnsetName),
+				BronzeDesc => customLeaderboards.OrderByDescending(cl => cl.TimeBronze).ThenBy(cl => cl.SpawnsetName),
+				SilverAsc => customLeaderboards.OrderBy(cl => cl.TimeSilver).ThenByDescending(cl => cl.SpawnsetName),
+				SilverDesc => customLeaderboards.OrderByDescending(cl => cl.TimeSilver).ThenBy(cl => cl.SpawnsetName),
+				GoldenAsc => customLeaderboards.OrderBy(cl => cl.TimeGolden).ThenByDescending(cl => cl.SpawnsetName),
+				GoldenDesc => customLeaderboards.OrderByDescending(cl => cl.TimeGolden).ThenBy(cl => cl.SpawnsetName),
+				DevilAsc => customLeaderboards.OrderBy(cl => cl.TimeDevil).ThenByDescending(cl => cl.SpawnsetName),
+				DevilDesc => customLeaderboards.OrderByDescending(cl => cl.TimeDevil).ThenBy(cl => cl.SpawnsetName),
+				LeviathanAsc => customLeaderboards.OrderBy(cl => cl.TimeLeviathan).ThenByDescending(cl => cl.SpawnsetName),
+				LeviathanDesc => customLeaderboards.OrderByDescending(cl => cl.TimeLeviathan).ThenBy(cl => cl.SpawnsetName),
+				WorldRecordAsc => customLeaderboards.OrderBy(cl => cl.WorldRecord).ThenByDescending(cl => cl.SpawnsetName),
+				WorldRecordDesc => customLeaderboards.OrderByDescending(cl => cl.WorldRecord).ThenBy(cl => cl.SpawnsetName),
+				_ => customLeaderboards.OrderByDescending(cl => cl.DateLastPlayed).ThenBy(cl => cl.SpawnsetName),
+			}).ToList();
+
+			TotalResults = customLeaderboards.Count;
+			TotalPages = Math.Max(1, (int)Math.Ceiling(TotalResults / (double)PageSize));
+			PageIndex = Math.Clamp(PageIndex, 1, TotalPages);
+			PaginatedList = new(customLeaderboards, PageIndex, PageSize);
 		}
 	}
 }
---
+++ DevilDaggersWebsite.Razor/Pagination/CustomLeaderboard.cs
@@ -0,0 +1,42 @@
+﻿using DevilDaggersWebsite.Enumerators;
+using System;
+
+namespace DevilDaggersWebsite.Razor.Pagination
+{
+	public class CustomLeaderboard
+	{
+		public CustomLeaderboard(CustomLeaderboardCategory category, string spawnsetName, string authorName, int timeBronze, int timeSilver, int timeGolden, int timeDevil, int timeLeviathan, int worldRecord, DateTime? dateLastPlayed, DateTime? dateCreated, int totalRunsSubmitted, int totalPlayers)
+		{
+			Category = category;
+			SpawnsetName = spawnsetName;
+			AuthorName = authorName;
+			TimeBronze = timeBronze;
+			TimeSilver = timeSilver;
+			TimeGolden = timeGolden;
+			TimeDevil = timeDevil;
+			TimeLeviathan = timeLeviathan;
+			WorldRecord = worldRecord;
+			DateLastPlayed = dateLastPlayed;
+			DateCreated = dateCreated;
+			TotalRunsSubmitted = totalRunsSubmitted;
+			TotalPlayers = totalPlayers;
+		}
+
+		public CustomLeaderboardCategory Category { get; set; }
+
+		public string SpawnsetName { get; set; }
+		public string AuthorName { get; set; }
+
+		public int TimeBronze { get; set; }
+		public int TimeSilver { get; set; }
+		public int TimeGolden { get; set; }
+		public int TimeDevil { get; set; }
+		public int TimeLeviathan { get; set; }
+		public int WorldRecord { get; set; }
+
+		public DateTime? DateLastPlayed { get; set; }
+		public DateTime? DateCreated { get; set; }
+		public int TotalRunsSubmitted { get; set; }
+		public int TotalPlayers { get; set; }
+	}
+}
