835c0da340fad4495b37dea6e06a5478d138111c
Noah Stolk <noah.stolk@gmail.com>
2021-04-20 22:01:55 +0200

    Add mod screenshots to /FileSystem page

--- DevilDaggersWebsite.Razor/Pages/Admin/FileSystem/Index.cshtml.cs
+++ DevilDaggersWebsite.Razor/Pages/Admin/FileSystem/Index.cshtml.cs
@@ -17,6 +17,7 @@ namespace DevilDaggersWebsite.Razor.Pages.Admin.FileSystem
 
 		public Dictionary<string, long> TotalSpaceInBytesPerDrive { get; private set; } = new();
 		public Dictionary<string, long> ModFileSizes { get; private set; } = new();
+		public Dictionary<string, long> ModScreenshotFileSizes { get; private set; } = new();
 		public Dictionary<string, long> SpawnsetFileSizes { get; private set; } = new();
 		public Dictionary<string, long> LeaderboardHistoryFileSizes { get; private set; } = new();
 
@@ -31,6 +32,12 @@ namespace DevilDaggersWebsite.Razor.Pages.Admin.FileSystem
 			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "mods")))
 				ModFileSizes.Add(Path.GetFileName(path), new FileInfo(path).Length);
 
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "mod-screenshots"), "*.png", SearchOption.AllDirectories))
+			{
+				string directoryName = new DirectoryInfo(path).Parent?.Name ?? throw new($"Invalid path '{path}' while scanning mod screenshot file sizes.");
+				ModScreenshotFileSizes.Add(Path.Combine(directoryName, Path.GetFileName(path)), new FileInfo(path).Length);
+			}
+
 			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "spawnsets")))
 				SpawnsetFileSizes.Add(Path.GetFileName(path), new FileInfo(path).Length);
 
@@ -39,6 +46,7 @@ namespace DevilDaggersWebsite.Razor.Pages.Admin.FileSystem
 
 			TotalSpaceInBytesPerDrive = TotalSpaceInBytesPerDrive.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 			ModFileSizes = ModFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+			ModScreenshotFileSizes = ModScreenshotFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 			SpawnsetFileSizes = SpawnsetFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 			LeaderboardHistoryFileSizes = LeaderboardHistoryFileSizes.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 		}
