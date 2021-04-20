f9ee99e70248254f225c128ed58f8a36ff158f42
Noah Stolk <noah.stolk@gmail.com>
2021-04-20 10:17:20 +0200

    Delete Blazor project

--- DevilDaggersWebsite.BlazorServer/Extensions/DisplayExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevilDaggersWebsite.BlazorServer.Extensions
-{
-	public static class DisplayExtensions
-	{
-		public static string Pluralize(this int value)
-			=> value == 1 ? string.Empty : "s";
-	}
-}
