4fb85306338a0ae9ef5281d17b93747f2da4c342
Noah Stolk <noah.stolk@gmail.com>
2021-04-29 12:17:42 +0200

    Refactor /SpawnsPartial to not display V1/V2 layouts in /Spawnset page (but only /Wiki)

---
+++ DevilDaggersWebsite.Razor/Models/SpawnsPartialModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DevilDaggersWebsite.Razor.Models
+{
+	public class SpawnsPartialModel
+	{
+		public SpawnsPartialModel(string spawnsetName, bool isWikiPage)
+		{
+			SpawnsetName = spawnsetName;
+			IsWikiPage = isWikiPage;
+		}
+
+		public string SpawnsetName { get; }
+
+		public bool IsWikiPage { get; }
+	}
+}
