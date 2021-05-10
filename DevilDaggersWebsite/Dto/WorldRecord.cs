16c6dad4cf06b4ac8c18872c3da538bfde050c8c
Noah Stolk <noah.stolk@gmail.com>
2021-05-10 19:40:59 +0200

    Add GameVersion to WorldRecord

--- DevilDaggersWebsite/Dto/WorldRecord.cs
+++ DevilDaggersWebsite/Dto/WorldRecord.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using DevilDaggersCore.Game;
+using System;
 
 namespace DevilDaggersWebsite.Dto
 {
 	public class WorldRecord
 	{
-		public WorldRecord(DateTime dateTime, Entry entry)
+		public WorldRecord(DateTime dateTime, Entry entry, GameVersion? gameVersion)
 		{
 			DateTime = dateTime;
 			Entry = entry;
+			GameVersion = gameVersion;
 		}
 
 		public DateTime DateTime { get; }
 		public Entry Entry { get; }
+		public GameVersion? GameVersion { get; }
 	}
 }
