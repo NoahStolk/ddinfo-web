d1bcf9bfbf96c8c526ed440ac9cc69ba0398f8d4
Noah Stolk <noah.stolk@gmail.com>
2021-05-11 10:48:33 +0200

    Add \AdminTests\BinaryNames page

---
+++ DevilDaggersWebsite.Razor/Pages/Admin/AdminTests/BinaryNames.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace DevilDaggersWebsite.Razor.Pages.Admin.AdminTests
+{
+	public class BinaryNamesModel : PageModel
+	{
+		private readonly IWebHostEnvironment _env;
+
+		public BinaryNamesModel(IWebHostEnvironment env)
+			=> _env = env;
+
+		public List<(string ModName, string BinaryName)> Names { get; set; } = new();
+
+		public void OnGet()
+		{
+			foreach (string path in Directory.GetFiles(Path.Combine(_env.WebRootPath, "mods"), "*.zip"))
+			{
+				string modName = Path.GetFileNameWithoutExtension(path);
+
+				using MemoryStream ms = new(System.IO.File.ReadAllBytes(path));
+				using ZipArchive archive = new(ms);
+				foreach (ZipArchiveEntry entry in archive.Entries)
+					Names.Add((modName, entry.Name));
+			}
+		}
+	}
+}
