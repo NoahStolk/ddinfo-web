a89b9e0213c9fe6cb5c6a2845d4d91378482de65
Noah Stolk <noah.stolk@gmail.com>
2021-06-17 13:16:14 +0200

    Rewrite HostedService to BackgroundService (WIP)

--- DevilDaggersWebsite/Tasks/HostedService.cs
+++ DevilDaggersWebsite/Tasks/HostedService.cs
@@ -1,44 +1,25 @@
 ﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace DevilDaggersWebsite.Tasks
 {
-	public abstract class HostedService : IHostedService
+	public abstract class HostedService : BackgroundService
 	{
-		// Example untested base class code kindly provided by David Fowler: https://gist.github.com/davidfowl/a7dd5064d9dcf35b6eae1a7953d615e3
-		private Task? _executingTask;
-		private CancellationTokenSource? _cts;
+		protected abstract TimeSpan Interval { get; }
 
-		public Task StartAsync(CancellationToken cancellationToken)
+		protected abstract Task ProcessDataAsync(CancellationToken stoppingToken);
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			// Create a linked token so we can trigger cancellation outside of this token's cancellation.
-			_cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				await ProcessDataAsync(stoppingToken);
 
-			// Store the task we're executing.
-			_executingTask = ExecuteAsync(_cts.Token);
-
-			// If the task is completed then return it, otherwise it's running.
-			return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+				if (Interval.TotalMilliseconds > 0)
+					await Task.Delay(Interval, stoppingToken);
+			}
 		}
-
-		public async Task StopAsync(CancellationToken cancellationToken)
-		{
-			// Stop was called without start.
-			if (_executingTask == null)
-				return;
-
-			// Signal cancellation to the executing method.
-			_cts?.Cancel();
-
-			// Wait until the task completes or the stop token triggers.
-			await Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken));
-
-			// Throw if cancellation triggered.
-			cancellationToken.ThrowIfCancellationRequested();
-		}
-
-		// Derived classes should override this and execute a long running method until cancellation is requested.
-		protected abstract Task ExecuteAsync(CancellationToken cancellationToken);
 	}
 }
