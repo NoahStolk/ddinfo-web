3f33bd6c563b6ae2a40cac28d6336853a2ae2a6e
Noah Stolk <noah.stolk@gmail.com>
2021-06-18 18:45:24 +0200

    Clean up unused extension method

--- DevilDaggersWebsite/Extensions/EnumExtensions.cs
+++ DevilDaggersWebsite/Extensions/EnumExtensions.cs
@@ -4,16 +4,6 @@ namespace DevilDaggersWebsite.Extensions
 {
 	public static class EnumExtensions
 	{
-		public static char GetChar(this Currency currency) => currency switch
-		{
-			Currency.Eur => '€',
-			Currency.Usd => '$',
-			Currency.Aud => '$',
-			Currency.Gbp => '£',
-			Currency.Sgd => '$',
-			_ => '?',
-		};
-
 		public static bool IsAscending(this CustomLeaderboardCategory category)
 			=> category == CustomLeaderboardCategory.TimeAttack || category == CustomLeaderboardCategory.Speedrun;
 	}
