fc8244589045fa0cea5a45d11cbab8aa95c5f98f
Noah Stolk <noah.stolk@gmail.com>
2021-06-21 17:05:41 +0200

    Rename ResponseLog to ResponseTimeLog

--- DevilDaggersWebsite/Singletons/ResponseTimeMonitor.cs
+++ DevilDaggersWebsite/Singletons/ResponseTimeMonitor.cs
@@ -10,16 +10,16 @@ namespace DevilDaggersWebsite.Singletons
 	{
 		private const int _maxLinesPerMessage = 10;
 
-		private readonly List<ResponseLog> _responseLogs = new();
+		private readonly List<ResponseTimeLog> _responseTimeLogs = new();
 
 		public void Add(string path, long responseTimeTicks, DateTime dateTime)
-			=> _responseLogs.Add(new(path, responseTimeTicks, dateTime));
+			=> _responseTimeLogs.Add(new(path, responseTimeTicks, dateTime));
 
 		public List<string> CreateLogs(DateTime startDateTime, DateTime endDateTime)
 		{
-			StringBuilder sb = new($"```Response times\n{startDateTime:yyyy-MM-dd HH:mm:ss}\n{endDateTime:yyyy-MM-dd HH:mm:ss}\n\n");
+			StringBuilder sb = new($"```Response times\nBetween {startDateTime:yyyy-MM-dd HH:mm:ss} and {endDateTime:yyyy-MM-dd HH:mm:ss}\n\n");
 
-			if (_responseLogs.Count == 0)
+			if (_responseTimeLogs.Count == 0)
 			{
 				sb.Append("Nobody accessed the website during this period.```");
 				return new() { sb.ToString() };
@@ -27,14 +27,14 @@ namespace DevilDaggersWebsite.Singletons
 
 			List<string> logs = new();
 
-			sb.AppendFormat("{0,-50}", nameof(ResponseLog.Path))
+			sb.AppendFormat("{0,-50}", nameof(ResponseTimeLog.Path))
 				.AppendFormat("{0,10}", "Requests")
 				.AppendFormat("{0,25}", "AverageResponseTime")
 				.AppendFormat("{0,12}", "MinTime")
 				.AppendFormat("{0,12}", "MaxTime")
 				.AppendLine();
 			int i = 0;
-			foreach (IGrouping<string, ResponseLog> group in _responseLogs.GroupBy(rl => rl.Path.ToLower()).OrderBy(rl => rl.Key))
+			foreach (IGrouping<string, ResponseTimeLog> group in _responseTimeLogs.GroupBy(rl => rl.Path.ToLower()).OrderBy(rl => rl.Key))
 			{
 				int count = group.Count();
 				double averageResponseTimeTicks = group.Average(rl => rl.ResponseTimeTicks);
@@ -64,7 +64,7 @@ namespace DevilDaggersWebsite.Singletons
 		}
 
 		public void Clear()
-			=> _responseLogs.Clear();
+			=> _responseTimeLogs.Clear();
 
 		private static string GetFormattedTime(double ticks)
 		{
@@ -88,9 +88,9 @@ namespace DevilDaggersWebsite.Singletons
 			return $"{ticks / 10f:0} μs";
 		}
 
-		private class ResponseLog
+		private class ResponseTimeLog
 		{
-			public ResponseLog(string path, long responseTimeTicks, DateTime dateTime)
+			public ResponseTimeLog(string path, long responseTimeTicks, DateTime dateTime)
 			{
 				Path = path;
 				ResponseTimeTicks = responseTimeTicks;
