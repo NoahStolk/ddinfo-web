7bdeb547c66729aa89bd1a9ee59150644a7d218f
Noah Stolk <noah.stolk@gmail.com>
2021-07-03 21:57:54 +0200

    Inject IWebHostEnvironment into SpawnsetHashCache

--- DevilDaggersWebsite/Caches/SpawnsetHash/SpawnsetHashCache.cs
+++ DevilDaggersWebsite/Caches/SpawnsetHash/SpawnsetHashCache.cs
@@ -15,24 +15,26 @@ namespace DevilDaggersWebsite.Caches.SpawnsetHash
 		private readonly ConcurrentBag<SpawnsetHashCacheData> _cache = new();
 
 		private readonly DiscordLogger _discordLogger;
+		private readonly IWebHostEnvironment _environment;
 
-		public SpawnsetHashCache(DiscordLogger discordLogger)
+		public SpawnsetHashCache(DiscordLogger discordLogger, IWebHostEnvironment environment)
 		{
 			_discordLogger = discordLogger;
+			_environment = environment;
 		}
 
-		public async Task<SpawnsetHashCacheData?> GetSpawnset(IWebHostEnvironment env, byte[] hash)
+		public async Task<SpawnsetHashCacheData?> GetSpawnset(byte[] hash)
 		{
 			SpawnsetHashCacheData? spawnsetCacheData = _cache.FirstOrDefault(scd => MatchHashes(scd.Hash, hash));
 			if (spawnsetCacheData != null)
 				return spawnsetCacheData;
 
-			foreach (string spawnsetPath in Directory.GetFiles(Path.Combine(env.WebRootPath, "spawnsets")))
+			foreach (string spawnsetPath in Directory.GetFiles(Path.Combine(_environment.WebRootPath, "spawnsets")))
 			{
 				byte[] spawnsetBytes = File.ReadAllBytes(spawnsetPath);
 				if (!Spawnset.TryParse(spawnsetBytes, out _))
 				{
-					await _discordLogger.TryLog(Channel.MonitoringError, env.EnvironmentName, $":x: Could not parse file at `{spawnsetPath}` to a spawnset. Skipping file for cache.");
+					await _discordLogger.TryLog(Channel.MonitoringError, _environment.EnvironmentName, $":x: Could not parse file at `{spawnsetPath}` to a spawnset. Skipping file for cache.");
 					continue;
 				}
 
