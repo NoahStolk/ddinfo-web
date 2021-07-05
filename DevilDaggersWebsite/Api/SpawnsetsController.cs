3430eecbd0465cd88c1e194a23f468d2aaf0fe91
Noah Stolk <noah.stolk@gmail.com>
2021-07-05 11:39:55 +0200

    Implement endpoints to upload and delete spawnset files

--- DevilDaggersWebsite/Api/SpawnsetsController.cs
+++ DevilDaggersWebsite/Api/SpawnsetsController.cs
@@ -1,6 +1,11 @@
-﻿using DevilDaggersWebsite.Api.Attributes;
+﻿using DevilDaggersCore.Spawnsets;
+using DevilDaggersWebsite.Api.Attributes;
+using DevilDaggersWebsite.Caches.SpawnsetHash;
+using DevilDaggersWebsite.Constants;
 using DevilDaggersWebsite.Dto.Spawnsets;
 using DevilDaggersWebsite.Entities;
+using DevilDaggersWebsite.HostedServices.DdInfoDiscordBot;
+using DevilDaggersWebsite.Singletons;
 using DevilDaggersWebsite.Transients;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,6 +17,8 @@ using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Io = System.IO;
 
 namespace DevilDaggersWebsite.Api
@@ -23,12 +30,16 @@ namespace DevilDaggersWebsite.Api
 		private readonly SpawnsetHelper _spawnsetHelper;
 		private readonly IWebHostEnvironment _environment;
 		private readonly ApplicationDbContext _dbContext;
+		private readonly DiscordLogger _discordLogger;
+		private readonly SpawnsetHashCache _spawnsetHashCache;
 
-		public SpawnsetsController(SpawnsetHelper spawnsetHelper, IWebHostEnvironment environment, ApplicationDbContext dbContext)
+		public SpawnsetsController(SpawnsetHelper spawnsetHelper, IWebHostEnvironment environment, ApplicationDbContext dbContext, DiscordLogger discordLogger, SpawnsetHashCache spawnsetHashCache)
 		{
 			_spawnsetHelper = spawnsetHelper;
 			_environment = environment;
 			_dbContext = dbContext;
+			_discordLogger = discordLogger;
+			_spawnsetHashCache = spawnsetHashCache;
 		}
 
 		// TODO: Re-route to /public.
@@ -140,6 +151,71 @@ namespace DevilDaggersWebsite.Api
 
 			_dbContext.SpawnsetFiles.Remove(spawnset);
 			_dbContext.SaveChanges();
+
+			return Ok();
+		}
+
+		[HttpPost("upload-file")]
+		//[Authorize(Policies.SpawnsetsPolicy)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[EndpointConsumer(EndpointConsumers.Admin)]
+		public async Task<ActionResult> UploadSpawnsetFile(IFormFile file)
+		{
+			if (file == null)
+				return BadRequest("No file.");
+
+			if (file.Length > SpawnsetFileConstants.MaxFileSize)
+				return BadRequest($"File too large (`{file.Length:n0}` / max `{SpawnsetFileConstants.MaxFileSize:n0}` bytes).");
+
+			if (file.FileName.Length > SpawnsetFileConstants.MaxFileNameLength)
+				return BadRequest($"File name too long (`{file.FileName.Length}` / max `{SpawnsetFileConstants.MaxFileNameLength}` characters).");
+
+			string filePath = Path.Combine(_environment.WebRootPath, "spawnsets", file.FileName);
+			if (Io.File.Exists(filePath))
+				return BadRequest($"File `{file.FileName}` already exists.");
+
+			byte[] formFileBytes = new byte[file.Length];
+			using (MemoryStream ms = new())
+			{
+				file.CopyTo(ms);
+				formFileBytes = ms.ToArray();
+			}
+
+			if (!Spawnset.TryParse(formFileBytes, out _))
+				return BadRequest("File could not be parsed to a proper survival file.`");
+
+			byte[] spawnsetHash = MD5.HashData(formFileBytes);
+			SpawnsetHashCacheData? existingSpawnset = await _spawnsetHashCache.GetSpawnset(spawnsetHash);
+			if (existingSpawnset != null)
+				return BadRequest($"Spawnset is exactly the same as an already existing spawnset named '{existingSpawnset.Name}'.`");
+
+			Io.File.WriteAllBytes(filePath, formFileBytes);
+			//await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: `{GetIdentity()}` uploaded new SPAWNSET file :file_folder: `{file.FileName}` (`{formFileBytes.Length:n0}` bytes).");
+			await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: Uploaded new SPAWNSET file :file_folder: `{file.FileName}` (`{formFileBytes.Length:n0}` bytes).");
+
+			return Ok();
+		}
+
+		[HttpDelete("delete-file")]
+		//[Authorize(Policies.AssetModsPolicy)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[EndpointConsumer(EndpointConsumers.Admin)]
+		public async Task<ActionResult> DeleteSpawnsetFile(string fileName)
+		{
+			string path = Path.Combine(_environment.WebRootPath, "spawnsets", fileName);
+			if (!Io.File.Exists(path))
+				return BadRequest($"File `{fileName}` does not exist.");
+
+			if (_dbContext.SpawnsetFiles.Any(s => s.Name == fileName))
+				return BadRequest($"File `{fileName}` is linked to an existing spawnset and cannot be deleted.");
+
+			Io.File.Delete(path);
+			_spawnsetHashCache.Clear();
+
+			//await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: `{GetIdentity()}` deleted SPAWNSET file :file_folder: `{fileName}`");
+			await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: Deleted SPAWNSET file :file_folder: `{fileName}`");
 
 			return Ok();
 		}
---
+++ DevilDaggersWebsite/Constants/SpawnsetFileConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace DevilDaggersWebsite.Constants
+{
+	public static class SpawnsetFileConstants
+	{
+		public const int MaxFileSize = 70 * 1024;
+		public const int MaxFileNameLength = 80;
+	}
+}
