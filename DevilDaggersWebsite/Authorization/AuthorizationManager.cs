4b8ea5d7133b65561daa2da8d451979fa26bd207
Noah Stolk <noah.stolk@gmail.com>
2021-07-05 14:37:25 +0200

    Remove FolderToPolicyMapper

--- DevilDaggersWebsite/Authorization/AuthorizationManager.cs
+++ DevilDaggersWebsite/Authorization/AuthorizationManager.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DevilDaggersWebsite.Authorization
@@ -9,15 +8,6 @@ namespace DevilDaggersWebsite.Authorization
 	public static class AuthorizationManager
 	{
 		private const string _adminUserEmail = "noah.stolk@gmail.com";
-
-		public static readonly IReadOnlyDictionary<string, string> FolderToPolicyMapper = new Dictionary<string, string>()
-		{
-			{ "/Admin/AdminTests", Policies.AdminPolicy },
-			{ "/Admin/AssetMods", Policies.AssetModsPolicy },
-			{ "/Admin/CustomLeaderboards", Policies.CustomLeaderboardsPolicy },
-			{ "/Admin/Players", Policies.PlayersPolicy },
-			{ "/Admin/SpawnsetFiles", Policies.SpawnsetsPolicy },
-		};
 
 		public static async Task CreateRolesAndAdminUser(this IServiceProvider serviceProvider)
 		{
