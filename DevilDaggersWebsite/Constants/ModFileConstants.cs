23d3563a4a5ce7e8a0c86b3d4633b87e022cefd3
Noah Stolk <noah.stolk@gmail.com>
2021-07-05 18:57:47 +0200

    Set ModFileConstants.MaxFileNameLength to 68

--- DevilDaggersWebsite/Constants/ModFileConstants.cs
+++ DevilDaggersWebsite/Constants/ModFileConstants.cs
@@ -3,7 +3,7 @@
 	public static class ModFileConstants
 	{
 		public const int MaxFileSize = 256 * 1024 * 1024;
-		public const int MaxFileNameLength = 80;
+		public const int MaxFileNameLength = 68; // 64 for mod name + ".zip".
 		public const long MaxHostingSpace = 5L * 1024 * 1024 * 1024;
 	}
 }
