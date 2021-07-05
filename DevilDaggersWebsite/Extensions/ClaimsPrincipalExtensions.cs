4bda4b1e3a67e734a3385a136b6101d2634d7fe3
Noah Stolk <noah.stolk@gmail.com>
2021-07-05 13:41:56 +0200

    Implement ClaimsPrincipalExtensions.GetShortName

---
+++ DevilDaggersWebsite/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using DevilDaggersCore.Extensions;
+using System;
+using System.Security.Claims;
+
+namespace DevilDaggersWebsite.Extensions
+{
+	public static class ClaimsPrincipalExtensions
+	{
+		public static string GetShortName(this ClaimsPrincipal claimsPrincipal)
+			=> claimsPrincipal.Identity?.Name?.TrimAfter(4) ?? throw new UnauthorizedAccessException("Not allowed to access this resource.");
+	}
+}
