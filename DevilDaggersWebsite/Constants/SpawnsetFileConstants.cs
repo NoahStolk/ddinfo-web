a8e04e02c9d48e48d27cf4fa2820879d34d48260
Noah Stolk <noah.stolk@gmail.com>
2021-07-06 13:32:52 +0200

    Move admin and client DTOs to base projects

--- DevilDaggersWebsite/Constants/SpawnsetFileConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevilDaggersWebsite.Constants
-{
-	public static class SpawnsetFileConstants
-	{
-		public const int MaxFileSize = 70 * 1024;
-		public const int MaxFileNameLength = 80;
-	}
-}
