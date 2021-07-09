4eea09069a9d0c0429340a4d1d33a94d7b402fec
Noah Stolk <noah.stolk@gmail.com>
2021-07-09 23:16:41 +0200

    Use N4 format for leaderboard global time

--- DevilDaggersWebsite.BlazorWasm/Shared/FormatUtils.cs
+++ DevilDaggersWebsite.BlazorWasm/Shared/FormatUtils.cs
@@ -1,13 +1,10 @@
-﻿using System;
-
-namespace DevilDaggersWebsite.BlazorWasm.Shared
+﻿namespace DevilDaggersWebsite.BlazorWasm.Shared
 {
 	public static class FormatUtils
 	{
-		public static readonly string LeaderboardTimeFormat = "0.0000";
-		public static readonly string LeaderboardTimeLargeFormat = "###,###,###,##0.0000";
+		public static readonly string LeaderboardGlobalTimeFormat = "N4";
 		public static readonly string LeaderboardIntFormat = "N0";
-		public static readonly string LeaderboardIntAverageFormat = "###,###,###,##0.00";
+		public static readonly string LeaderboardIntAverageFormat = "N2";
 		public static readonly string DateFormat = "yyyy-MM-dd";
 		public static readonly string DateTimeUtcFormat = "yyyy-MM-dd HH:mm UTC";
 		public static readonly string DateTimeFullFormat = "yyyy-MM-dd HH:mm:ss.fff zzz";
@@ -15,23 +12,6 @@ namespace DevilDaggersWebsite.BlazorWasm.Shared
 		public static readonly string InGameSensFormat = "0.000";
 		public static readonly string GammaFormat = "0.00";
 		public static readonly string AccuracyFormat = "0.00%";
-
-		// TODO: Improve performance.
-		public static string FormatTimeInteger<T>(this T time, bool includeThousandSeparator = false)
-			where T : struct // C# does not have a type constraint for integer types.
-		{
-			string timeStr = time.ToString() ?? "0";
-			timeStr = new string('0', Math.Max(0, 5 - timeStr.Length)) + timeStr;
-			timeStr = timeStr.Reverse().Insert(4, ".");
-
-			if (includeThousandSeparator)
-			{
-				for (int i = 8; i < timeStr.Length; i += 4)
-					timeStr = timeStr.Insert(i, ",");
-			}
-
-			return timeStr.Reverse();
-		}
 
 		public static string FormatDaggersInt32(int hit, int fired, bool isHistory = false)
 			=> FormatDaggersUInt64((ulong)hit, (ulong)fired, isHistory);
@@ -47,14 +27,6 @@ namespace DevilDaggersWebsite.BlazorWasm.Shared
 			}
 
 			return $"{hit:N0} / {fired:N0}";
-		}
-
-		private static string Reverse(this string s)
-		{
-			char[] charArray = s.ToCharArray();
-			Array.Reverse(charArray);
-
-			return new string(charArray);
 		}
 	}
 }
