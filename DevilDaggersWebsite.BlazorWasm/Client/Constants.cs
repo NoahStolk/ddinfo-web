7d71b490b95f33e8107fac014571b7fc81fcebf7
Noah Stolk <noah.stolk@gmail.com>
2021-07-09 17:37:04 +0200

    Add home page

---
+++ DevilDaggersWebsite.BlazorWasm/Client/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace DevilDaggersWebsite.BlazorWasm.Client
+{
+	public static class Constants
+	{
+#pragma warning disable S1075 // URIs should not be hardcoded
+		public const string DiscordUrl = "https://discord.gg/NF32j8S";
+#pragma warning restore S1075 // URIs should not be hardcoded
+	}
+}
