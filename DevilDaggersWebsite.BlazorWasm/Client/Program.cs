8c4b9e24e774c5156d8e65a43c8aafdaddecf95f
Noah Stolk <noah.stolk@gmail.com>
2021-07-09 10:39:39 +0200

    Improve styling

--- DevilDaggersWebsite.BlazorWasm/Client/Program.cs
+++ DevilDaggersWebsite.BlazorWasm/Client/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DevilDaggersWebsite.BlazorWasm.Client
 {
-	public class Program
+	public static class Program
 	{
 		public static async Task Main(string[] args)
 		{
