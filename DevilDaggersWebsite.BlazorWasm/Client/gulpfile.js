dbfde75bee3a66be1f9da42c2a93986b498b6d18
Noah Stolk <noah.stolk@gmail.com>
2021-07-10 18:45:08 +0200

    Install Tailwind and remove Bootstrap

---
+++ DevilDaggersWebsite.BlazorWasm/Client/gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const purgecss = require('gulp-purgecss');
+
+gulp.task('css:dev', () => {
+  return gulp.src('./Styles/site.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer')
+    ]))
+    .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('css:prod', () => {
+  return gulp.src('./Styles/site.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer')
+    ]))
+    .pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./wwwroot/css/'));
+});
---
+++ DevilDaggersWebsite.BlazorWasm/Client/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+		dropdown: '#337ab7',
+		dropdown-hover: '#23527c',
+		red: '#f00',
+		link: '#f11',
+		link-hover: '#c11',
+		dark-red: '#600',
+		dark-orange: '#630',
+		gray: '#666',
+		default-text: '#ddd',
+		gray-08: '#080808',
+		gray-0B: '#0b0b0b',
+		gray-11: '#111',
+		gray-18: '#181818',
+		gray-22: '#222',
+		nav-background: '#0D0D0D',
+		nav-background-hover: '#181818',
+		banned: '#666',
+		selection-text: '#700',
+		underline-leviathan: '#810017',
+		underline-devil: '#CB2012',
+		underline-golden: '#FCBE34',
+		underline-silver: '#9CA4AA',
+		underline-bronze: '#765A46',
+		underline-default: '#434343'
+      }
+    }
+  }
+};
