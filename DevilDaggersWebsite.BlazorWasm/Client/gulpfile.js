e5671dc0a90b0902bdf710529dcd5f4dd320c9cb
Noah Stolk <noah.stolk@gmail.com>
2021-07-10 21:50:30 +0200

    Use custom fonts

--- DevilDaggersWebsite.BlazorWasm/Client/gulpfile.js
+++ DevilDaggersWebsite.BlazorWasm/Client/gulpfile.js
@@ -5,26 +5,26 @@ const cleanCSS = require('gulp-clean-css');
 const purgecss = require('gulp-purgecss');
 
 gulp.task('css:dev', () => {
-  return gulp.src('./Styles/site.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([
-      require('precss'),
-      require('tailwindcss'),
-      require('autoprefixer')
-    ]))
-    .pipe(gulp.dest('./wwwroot/css/'));
+	return gulp.src('./Styles/site.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss([
+			require('precss'),
+			require('tailwindcss'),
+			require('autoprefixer')
+		]))
+		.pipe(gulp.dest('./wwwroot/css/'));
 });
 
 gulp.task('css:prod', () => {
-  return gulp.src('./Styles/site.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([
-      require('precss'),
-      require('tailwindcss'),
-      require('autoprefixer')
-    ]))
-    .pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
-    .pipe(cleanCSS({ level: 2 }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./wwwroot/css/'));
+	return gulp.src('./Styles/site.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss([
+			require('precss'),
+			require('tailwindcss'),
+			require('autoprefixer')
+		]))
+		.pipe(purgecss({ content: ['**/*.html', '**/*.razor'] }))
+		.pipe(cleanCSS({ level: 2 }))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./wwwroot/css/'));
 });
--- DevilDaggersWebsite.BlazorWasm/Client/tailwind.config.js
+++ DevilDaggersWebsite.BlazorWasm/Client/tailwind.config.js
@@ -1,28 +1,32 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-		red: '#f00',
-		colorlink: '#f11',
-		colorlinkhover: '#c11',
-		darkred: '#600',
-		darkorange: '#630',
-		gray: '#666',
-		colortext: '#ddd',
-		gray08: '#080808',
-		gray0B: '#0b0b0b',
-		gray11: '#111',
-		gray18: '#181818',
-		gray22: '#222',
-		colorban: '#666',
-		colorselection: '#700',
-		underlineleviathan: '#810017',
-		underlinedevil: '#CB2012',
-		underlinegolden: '#FCBE34',
-		underlinesilver: '#9CA4AA',
-		underlinebronze: '#765A46',
-		underlinedefault: '#434343'
-      }
-    }
-  }
+	theme: {
+		extend: {
+			colors: {
+				red: '#f00',
+				colorlink: '#f11',
+				colorlinkhover: '#c11',
+				darkred: '#600',
+				darkorange: '#630',
+				gray: '#666',
+				colortext: '#ddd',
+				gray08: '#080808',
+				gray0B: '#0b0b0b',
+				gray11: '#111',
+				gray18: '#181818',
+				gray22: '#222',
+				colorban: '#666',
+				colorselection: '#700',
+				underlineleviathan: '#810017',
+				underlinedevil: '#CB2012',
+				underlinegolden: '#FCBE34',
+				underlinesilver: '#9CA4AA',
+				underlinebronze: '#765A46',
+				underlinedefault: '#434343'
+			}
+		},
+		fontFamily: {
+			'roboto': ['roboto', 'sans-serif'],
+			'goethe': ['goethe']
+		}
+	}
 };
