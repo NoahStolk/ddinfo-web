0d40df15c307bd423489fd64abdcb9fadbfd274e
Noah Stolk <noah.stolk@gmail.com>
2021-07-10 19:56:12 +0200

    Fix incorrect Tailwind configuration

--- DevilDaggersWebsite.BlazorWasm/Client/tailwind.config.js
+++ DevilDaggersWebsite.BlazorWasm/Client/tailwind.config.js
@@ -2,30 +2,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-		dropdown: '#337ab7',
-		dropdown-hover: '#23527c',
 		red: '#f00',
 		link: '#f11',
-		link-hover: '#c11',
-		dark-red: '#600',
-		dark-orange: '#630',
+		linkhover: '#c11',
+		darkred: '#600',
+		darkorange: '#630',
 		gray: '#666',
-		default-text: '#ddd',
-		gray-08: '#080808',
-		gray-0B: '#0b0b0b',
-		gray-11: '#111',
-		gray-18: '#181818',
-		gray-22: '#222',
-		nav-background: '#0D0D0D',
-		nav-background-hover: '#181818',
+		text: '#ddd',
+		gray08: '#080808',
+		gray0B: '#0b0b0b',
+		gray11: '#111',
+		gray18: '#181818',
+		gray22: '#222',
+		navbackground: '#0D0D0D',
+		navbackgroundhover: '#181818',
 		banned: '#666',
-		selection-text: '#700',
-		underline-leviathan: '#810017',
-		underline-devil: '#CB2012',
-		underline-golden: '#FCBE34',
-		underline-silver: '#9CA4AA',
-		underline-bronze: '#765A46',
-		underline-default: '#434343'
+		selectiontext: '#700',
+		underlineleviathan: '#810017',
+		underlinedevil: '#CB2012',
+		underlinegolden: '#FCBE34',
+		underlinesilver: '#9CA4AA',
+		underlinebronze: '#765A46',
+		underlinedefault: '#434343'
       }
     }
   }
