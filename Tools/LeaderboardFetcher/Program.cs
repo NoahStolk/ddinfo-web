b2e21409a86ec3008aa1b00aaa36f80ae5409c21
Noah Stolk <noah.stolk@gmail.com>
2021-07-12 12:09:17 +0200

    Fetch initial amount of players for LeaderboardFetcher

--- Tools/LeaderboardFetcher/Program.cs
+++ Tools/LeaderboardFetcher/Program.cs
@@ -2,24 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-#if TASK_WHEN_ALL
-using System.Linq;
-#endif
 using System.Net.Http;
 using System.Threading.Tasks;
 
-const int _totalPages = 2424; // TODO: Fetch the leaderboard one more time on startup to retrieve total amount of players.
+byte[] init = await ExecuteRequest(1);
+
+int totalPages = BitConverter.ToInt32(init, 75) / 100 + 1;
 
 string start = DateTime.UtcNow.ToString("yyyyMMddHHmm");
 Stopwatch stopwatch = new();
 List<CompressedEntry> entries = new();
-for (int i = 0; i < _totalPages; i++)
+for (int i = 0; i < totalPages; i++)
 {
 	stopwatch.Restart();
 	entries.AddRange(await Fetch(i * 100 + 1));
-	Console.WriteLine($"Fetching page {i + 1}/{_totalPages} took {stopwatch.ElapsedMilliseconds / 1000f} seconds.");
+	Console.WriteLine($"Fetching page {i + 1}/{totalPages} took {stopwatch.ElapsedMilliseconds / 1000f} seconds.");
 
 	// Write the file after every fetch in case it crashes and all progress is lost.
 	File.WriteAllBytes($"{start}.bin", GetBytes(entries));
@@ -44,17 +42,7 @@ static byte[] GetBytes(List<CompressedEntry> entries)
 
 static async Task<List<CompressedEntry>> Fetch(int rank)
 {
-	List<KeyValuePair<string?, string?>> postValues = new()
-	{
-		new("user", "0"),
-		new("level", "survival"),
-		new("offset", (rank - 1).ToString(CultureInfo.InvariantCulture)),
-	};
-
-	using FormUrlEncodedContent content = new(postValues);
-	using HttpClient client = new();
-	HttpResponseMessage response = await client.PostAsync("http://dd.hasmodai.com/backend15/get_scores.php", content);
-	byte[] data = await response.Content.ReadAsByteArrayAsync();
+	byte[] data = await ExecuteRequest(rank);
 
 	int entryCount = BitConverter.ToInt16(data, 59);
 	int rankIterator = 0;
@@ -84,4 +72,19 @@ static async Task<List<CompressedEntry>> Fetch(int rank)
 	Console.WriteLine($"Fetched rank {rank} - {rank + 99}.");
 
 	return compressedEntries;
+}
+
+static async Task<byte[]> ExecuteRequest(int rank)
+{
+	List<KeyValuePair<string?, string?>> postValues = new()
+	{
+		new("user", "0"),
+		new("level", "survival"),
+		new("offset", (rank - 1).ToString()),
+	};
+
+	using FormUrlEncodedContent content = new(postValues);
+	using HttpClient client = new();
+	HttpResponseMessage response = await client.PostAsync("http://dd.hasmodai.com/backend15/get_scores.php", content);
+	return await response.Content.ReadAsByteArrayAsync();
 }
