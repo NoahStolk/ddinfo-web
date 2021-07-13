c3e4249b2c91c0c889ff91a45d03081f67dd071d
Noah Stolk <noah.stolk@gmail.com>
2021-07-13 21:08:16 +0200

    Remove separate UploadSpawnsetFile and DeleteSpawnsetFile endpoints

--- DevilDaggersWebsite.BlazorWasm/Server/Constants/SpawnsetFileConstants.cs
+++ DevilDaggersWebsite.BlazorWasm/Server/Constants/SpawnsetFileConstants.cs
@@ -3,6 +3,5 @@
 	public static class SpawnsetFileConstants
 	{
 		public const int MaxFileSize = 70 * 1024;
-		public const int MaxFileNameLength = 80;
 	}
 }
--- DevilDaggersWebsite.BlazorWasm/Server/Controllers/SpawnsetsAdminController.cs
+++ DevilDaggersWebsite.BlazorWasm/Server/Controllers/SpawnsetsAdminController.cs
@@ -4,7 +4,6 @@ using DevilDaggersWebsite.BlazorWasm.Server.Constants;
 using DevilDaggersWebsite.BlazorWasm.Server.Converters;
 using DevilDaggersWebsite.BlazorWasm.Server.Entities;
 using DevilDaggersWebsite.BlazorWasm.Server.Extensions;
-using DevilDaggersWebsite.BlazorWasm.Server.HostedServices.DdInfoDiscordBot;
 using DevilDaggersWebsite.BlazorWasm.Server.Singletons;
 using DevilDaggersWebsite.BlazorWasm.Shared;
 using DevilDaggersWebsite.BlazorWasm.Shared.Dto;
@@ -32,15 +31,13 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Controllers
 	{
 		private readonly IWebHostEnvironment _environment;
 		private readonly ApplicationDbContext _dbContext;
-		private readonly DiscordLogger _discordLogger;
 		private readonly SpawnsetHashCache _spawnsetHashCache;
 		private readonly AuditLogger _auditLogger;
 
-		public SpawnsetsAdminController(IWebHostEnvironment environment, ApplicationDbContext dbContext, DiscordLogger discordLogger, SpawnsetHashCache spawnsetHashCache, AuditLogger auditLogger)
+		public SpawnsetsAdminController(IWebHostEnvironment environment, ApplicationDbContext dbContext, SpawnsetHashCache spawnsetHashCache, AuditLogger auditLogger)
 		{
 			_environment = environment;
 			_dbContext = dbContext;
-			_discordLogger = discordLogger;
 			_spawnsetHashCache = spawnsetHashCache;
 			_auditLogger = auditLogger;
 		}
@@ -69,14 +66,41 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Controllers
 		[HttpPost]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public async Task<ActionResult> AddSpawnset(AddSpawnset addSpawnset)
+		public async Task<ActionResult> AddSpawnset(AddSpawnset addSpawnset, IFormFile file)
 		{
+			// File validation.
+			if (file == null)
+				return BadRequest("No file.");
+
+			if (file.Length > SpawnsetFileConstants.MaxFileSize)
+				return BadRequest($"File too large ({file.Length:n0} / max {SpawnsetFileConstants.MaxFileSize:n0} bytes).");
+
+			byte[] formFileBytes = new byte[file.Length];
+			using (MemoryStream ms = new())
+			{
+				file.CopyTo(ms);
+				formFileBytes = ms.ToArray();
+			}
+
+			if (!Spawnset.TryParse(formFileBytes, out _))
+				return BadRequest("File could not be parsed to a proper survival file.");
+
+			byte[] spawnsetHash = MD5.HashData(formFileBytes);
+			SpawnsetHashCacheData? existingSpawnset = await _spawnsetHashCache.GetSpawnset(spawnsetHash);
+			if (existingSpawnset != null)
+				return BadRequest($"Spawnset is exactly the same as an already existing spawnset named '{existingSpawnset.Name}'.");
+
+			// Entity validation.
 			if (!_dbContext.Players.Any(p => p.Id == addSpawnset.PlayerId))
 				return BadRequest($"Player with ID '{addSpawnset.PlayerId}' does not exist.");
 
 			if (_dbContext.SpawnsetFiles.Any(m => m.Name == addSpawnset.Name))
 				return BadRequest($"Spawnset with name '{addSpawnset.Name}' already exists.");
 
+			// Add file.
+			Io.File.WriteAllBytes(Path.Combine(_environment.WebRootPath, "spawnsets", addSpawnset.Name), formFileBytes);
+
+			// Add entity.
 			SpawnsetFile spawnset = new()
 			{
 				HtmlDescription = addSpawnset.HtmlDescription,
@@ -84,6 +108,7 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Controllers
 				MaxDisplayWaves = addSpawnset.MaxDisplayWaves,
 				Name = addSpawnset.Name,
 				PlayerId = addSpawnset.PlayerId,
+				LastUpdated = DateTime.UtcNow,
 			};
 			_dbContext.SpawnsetFiles.Add(spawnset);
 			_dbContext.SaveChanges();
@@ -118,7 +143,7 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Controllers
 				PlayerId = spawnset.PlayerId,
 			};
 
-			// Do not update LastUpdated. Update this value when updating the file only.
+			// Do not update LastUpdated here. This value is based only on the file which cannot be edited.
 			spawnset.HtmlDescription = editSpawnset.HtmlDescription;
 			spawnset.IsPractice = editSpawnset.IsPractice;
 			spawnset.MaxDisplayWaves = editSpawnset.MaxDisplayWaves;
@@ -144,76 +169,17 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Controllers
 			if (_dbContext.CustomLeaderboards.Any(ce => ce.SpawnsetFileId == id))
 				return BadRequest("Spawnset with custom leaderboard cannot be deleted.");
 
+			string path = Path.Combine(_environment.WebRootPath, "spawnsets", spawnset.Name);
+			if (Io.File.Exists(path))
+			{
+				Io.File.Delete(path);
+				_spawnsetHashCache.Clear();
+			}
+
 			_dbContext.SpawnsetFiles.Remove(spawnset);
 			_dbContext.SaveChanges();
 
 			await _auditLogger.LogDelete(spawnset, User, spawnset.Id);
-
-			return Ok();
-		}
-
-		[HttpPost("upload-file")]
-		[ProducesResponseType(StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public async Task<ActionResult> UploadSpawnsetFile(IFormFile file)
-		{
-			if (file == null)
-				return BadRequest("No file.");
-
-			if (file.Length > SpawnsetFileConstants.MaxFileSize)
-				return BadRequest($"File too large ({file.Length:n0} / max {SpawnsetFileConstants.MaxFileSize:n0} bytes).");
-
-			if (file.FileName.Length > SpawnsetFileConstants.MaxFileNameLength)
-				return BadRequest($"File name too long ({file.FileName.Length} / max {SpawnsetFileConstants.MaxFileNameLength} characters).");
-
-			string filePath = Path.Combine(_environment.WebRootPath, "spawnsets", file.FileName);
-			if (Io.File.Exists(filePath))
-				return BadRequest($"File '{file.FileName}' already exists.");
-
-			SpawnsetFile? spawnset = _dbContext.SpawnsetFiles.FirstOrDefault(m => m.Name == file.FileName);
-			if (spawnset == null)
-				return BadRequest($"There is no spawnset named '{file.FileName}'.");
-
-			spawnset.LastUpdated = DateTime.UtcNow;
-			_dbContext.SaveChanges();
-
-			byte[] formFileBytes = new byte[file.Length];
-			using (MemoryStream ms = new())
-			{
-				file.CopyTo(ms);
-				formFileBytes = ms.ToArray();
-			}
-
-			if (!Spawnset.TryParse(formFileBytes, out _))
-				return BadRequest("File could not be parsed to a proper survival file.");
-
-			byte[] spawnsetHash = MD5.HashData(formFileBytes);
-			SpawnsetHashCacheData? existingSpawnset = await _spawnsetHashCache.GetSpawnset(spawnsetHash);
-			if (existingSpawnset != null)
-				return BadRequest($"Spawnset is exactly the same as an already existing spawnset named '{existingSpawnset.Name}'.");
-
-			Io.File.WriteAllBytes(filePath, formFileBytes);
-			await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: `{User.GetShortName()}` uploaded new SPAWNSET file :file_folder: `{file.FileName}` (`{formFileBytes.Length:n0}` bytes).");
-
-			return Ok();
-		}
-
-		[HttpDelete("delete-file")]
-		[ProducesResponseType(StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public async Task<ActionResult> DeleteSpawnsetFile(string fileName)
-		{
-			string path = Path.Combine(_environment.WebRootPath, "spawnsets", fileName);
-			if (!Io.File.Exists(path))
-				return BadRequest($"File '{fileName}' does not exist.");
-
-			if (_dbContext.SpawnsetFiles.Any(s => s.Name == fileName))
-				return BadRequest($"File '{fileName}' is linked to an existing spawnset and cannot be deleted.");
-
-			Io.File.Delete(path);
-			_spawnsetHashCache.Clear();
-
-			await _discordLogger.TryLog(Channel.MonitoringAuditLog, $":white_check_mark: `{User.GetShortName()}` deleted SPAWNSET file :file_folder: `{fileName}`");
 
 			return Ok();
 		}
