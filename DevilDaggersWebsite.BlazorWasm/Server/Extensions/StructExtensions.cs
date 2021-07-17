86fcd5437b076979a64933b3dfa6b3b3e48ae767
Noah Stolk <noah.stolk@gmail.com>
2021-07-17 10:16:28 +0200

    Write unit test for NullIfDefault extension method

--- DevilDaggersWebsite.BlazorWasm/Server/Extensions/StructExtensions.cs
+++ DevilDaggersWebsite.BlazorWasm/Server/Extensions/StructExtensions.cs
@@ -1,9 +1,11 @@
-﻿namespace DevilDaggersWebsite.BlazorWasm.Server.Extensions
+﻿using System;
+
+namespace DevilDaggersWebsite.BlazorWasm.Server.Extensions
 {
 	public static class StructExtensions
 	{
 		public static T? NullIfDefault<T>(this T value)
-			where T : struct
+			where T : struct, IEquatable<T>
 			=> value.Equals(default) ? null : value;
 	}
 }
