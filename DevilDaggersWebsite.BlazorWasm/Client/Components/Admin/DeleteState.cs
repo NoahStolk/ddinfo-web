bd9d3b251594cd44192479282df00092bd9e57ee
Noah Stolk <noah.stolk@gmail.com>
2021-07-31 15:25:37 +0200

    Refactor

---
+++ DevilDaggersWebsite.BlazorWasm/Client/Components/Admin/DeleteState.cs
@@ -0,0 +1,11 @@
+﻿namespace DevilDaggersWebsite.BlazorWasm.Client.Components.Admin
+{
+	public enum DeleteState
+	{
+		None = 0,
+		Confirm = 1,
+		Ok = 2,
+		ValidationError = 3,
+		FatalError = 4,
+	}
+}
