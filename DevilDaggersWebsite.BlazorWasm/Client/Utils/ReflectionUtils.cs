da4c241748cc075cacf53084830c97dfcf80fee4
Noah Stolk <noah.stolk@gmail.com>
2021-08-01 16:22:12 +0200

    Update GetDtoPropertyDisplayValue to use FormatAttribute for IFormattable values

--- DevilDaggersWebsite.BlazorWasm/Client/Utils/ReflectionUtils.cs
+++ DevilDaggersWebsite.BlazorWasm/Client/Utils/ReflectionUtils.cs
@@ -1,4 +1,9 @@
-﻿using System.Linq;
+﻿using DevilDaggersWebsite.BlazorWasm.Shared;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto;
+using System;
+using System.Data;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 namespace DevilDaggersWebsite.BlazorWasm.Client.Utils
@@ -9,6 +14,24 @@ namespace DevilDaggersWebsite.BlazorWasm.Client.Utils
 			=> typeof(TDto).GetProperties().Where(pi => pi.CanWrite && (pi.PropertyType.IsValueType || pi.PropertyType == typeof(string))).ToArray();
 
 		public static string GetDtoPropertyDisplayValue<TDto>(PropertyInfo pi, TDto dto)
-			=> pi.GetValue(dto)?.ToString() ?? string.Empty;
+		{
+			Type type = pi.PropertyType;
+			object? value = pi.GetValue(dto);
+			if (value == null)
+				return string.Empty;
+
+			Type? nullableType = Nullable.GetUnderlyingType(type);
+			if (nullableType != null)
+				type = nullableType;
+
+			if (type.GetInterfaces().Any(i => i == typeof(IFormattable)))
+			{
+				FormatAttribute? fa = pi.GetCustomAttribute<FormatAttribute>();
+				if (fa != null)
+					return ((IFormattable)value).ToString(fa.Format, CultureInfo.InvariantCulture);
+			}
+
+			return pi.GetValue(dto)?.ToString() ?? string.Empty;
+		}
 	}
 }
