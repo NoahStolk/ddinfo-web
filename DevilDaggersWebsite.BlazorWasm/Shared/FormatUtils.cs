bc9a8ff4611a39716da083819f1ab6e27217508d
Noah Stolk <noah.stolk@gmail.com>
2021-08-01 14:49:37 +0200

    Make format strings in FormatUtils const

--- DevilDaggersWebsite.BlazorWasm/Shared/FormatUtils.cs
+++ DevilDaggersWebsite.BlazorWasm/Shared/FormatUtils.cs
@@ -2,16 +2,16 @@
 {
 	public static class FormatUtils
 	{
-		public static readonly string LeaderboardGlobalTimeFormat = "N4";
-		public static readonly string LeaderboardIntFormat = "N0";
-		public static readonly string LeaderboardIntAverageFormat = "N2";
-		public static readonly string DateFormat = "yyyy-MM-dd";
-		public static readonly string DateTimeUtcFormat = "yyyy-MM-dd HH:mm UTC";
-		public static readonly string DateTimeFullFormat = "yyyy-MM-dd HH:mm:ss.fff zzz";
-		public static readonly string TimeFormat = "0.0000";
-		public static readonly string InGameSensFormat = "0.000";
-		public static readonly string GammaFormat = "0.00";
-		public static readonly string AccuracyFormat = "0.00%";
+		public const string LeaderboardGlobalTimeFormat = "N4";
+		public const string LeaderboardIntFormat = "N0";
+		public const string LeaderboardIntAverageFormat = "N2";
+		public const string DateFormat = "yyyy-MM-dd";
+		public const string DateTimeUtcFormat = "yyyy-MM-dd HH:mm UTC";
+		public const string DateTimeFullFormat = "yyyy-MM-dd HH:mm:ss.fff zzz";
+		public const string TimeFormat = "0.0000";
+		public const string InGameSensFormat = "0.000";
+		public const string GammaFormat = "0.00";
+		public const string AccuracyFormat = "0.00%";
 
 		public static string FormatDaggersInt32(int hit, int fired, bool isHistory = false)
 			=> FormatDaggersUInt64((ulong)hit, (ulong)fired, isHistory);
