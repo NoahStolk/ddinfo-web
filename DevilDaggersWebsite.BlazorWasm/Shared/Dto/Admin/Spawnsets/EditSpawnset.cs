d845f0052f4e9df7dfe55f487182d795ae39afbc
Noah Stolk <noah.stolk@gmail.com>
2021-08-01 18:47:03 +0200

    Set min MaxDisplayWaves to 0

--- DevilDaggersWebsite.BlazorWasm/Shared/Dto/Admin/Spawnsets/EditSpawnset.cs
+++ DevilDaggersWebsite.BlazorWasm/Shared/Dto/Admin/Spawnsets/EditSpawnset.cs
@@ -5,17 +5,17 @@ namespace DevilDaggersWebsite.BlazorWasm.Shared.Dto.Admin.Spawnsets
 	public class EditSpawnset
 	{
 		[Required]
-		public int PlayerId { get; init; }
+		public int PlayerId { get; set; }
 
 		[StringLength(64)]
-		public string Name { get; init; } = null!;
+		public string Name { get; set; } = null!;
 
-		[Range(1, 400)]
-		public int? MaxDisplayWaves { get; init; }
+		[Range(0, 400)]
+		public int? MaxDisplayWaves { get; set; }
 
 		[StringLength(2048)]
-		public string? HtmlDescription { get; init; }
+		public string? HtmlDescription { get; set; }
 
-		public bool IsPractice { get; init; }
+		public bool IsPractice { get; set; }
 	}
 }
