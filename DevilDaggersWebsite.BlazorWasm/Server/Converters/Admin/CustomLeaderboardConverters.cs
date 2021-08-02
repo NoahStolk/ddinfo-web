cf1ed7ead50782716273a4d44d0a45fb4f993ba2
Noah Stolk <noah.stolk@gmail.com>
2021-08-02 20:29:45 +0200

    Update CustomLeaderboardConverters.cs

--- DevilDaggersWebsite.BlazorWasm/Server/Converters/Admin/CustomLeaderboardConverters.cs
+++ DevilDaggersWebsite.BlazorWasm/Server/Converters/Admin/CustomLeaderboardConverters.cs
@@ -8,7 +8,6 @@ namespace DevilDaggersWebsite.BlazorWasm.Server.Converters.Admin
 		public static GetCustomLeaderboard ToGetCustomLeaderboard(this CustomLeaderboard customLeaderboard) => new()
 		{
 			Id = customLeaderboard.Id,
-			SpawnsetAuthorName = customLeaderboard.SpawnsetFile.Player.PlayerName,
 			SpawnsetName = customLeaderboard.SpawnsetFile.Name,
 			TimeBronze = customLeaderboard.TimeBronze / 10000.0,
 			TimeSilver = customLeaderboard.TimeSilver / 10000.0,
