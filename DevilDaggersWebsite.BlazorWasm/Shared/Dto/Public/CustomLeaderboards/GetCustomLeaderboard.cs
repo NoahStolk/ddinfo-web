fb15e08ed6ccdc8dcb014eb5aef56c8248213e52
Noah Stolk <noah.stolk@gmail.com>
2021-08-08 00:43:13 +0200

    Create GetCustomLeaderboard.cs

---
+++ DevilDaggersWebsite.BlazorWasm/Shared/Dto/Public/CustomLeaderboards/GetCustomLeaderboard.cs
@@ -0,0 +1,31 @@
+﻿using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.CustomEntries;
+using System;
+using System.Collections.Generic;
+
+namespace DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.CustomLeaderboards
+{
+	public class GetCustomLeaderboard
+	{
+		public string SpawnsetName { get; init; } = null!;
+
+		public string SpawnsetAuthorName { get; init; } = null!;
+
+		public double TimeBronze { get; init; }
+
+		public double TimeSilver { get; init; }
+
+		public double TimeGolden { get; init; }
+
+		public double TimeDevil { get; init; }
+
+		public double TimeLeviathan { get; init; }
+
+		public DateTime? DateLastPlayed { get; init; }
+
+		public DateTime? DateCreated { get; init; }
+
+		public int? TotalRunsSubmitted { get; init; }
+
+		public List<GetCustomEntry> CustomEntries { get; init; } = new();
+	}
+}
