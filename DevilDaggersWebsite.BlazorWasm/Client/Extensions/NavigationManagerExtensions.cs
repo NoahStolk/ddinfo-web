da13a76b4e9137eee15b5b9dd13d809ec572ba77
Noah Stolk <noah.stolk@gmail.com>
2021-08-07 23:57:32 +0200

    Create NavigationManagerExtensions.cs

---
+++ DevilDaggersWebsite.BlazorWasm/Client/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
+
+namespace DevilDaggersWebsite.BlazorWasm.Client.Extensions
+{
+	public static class NavigationManagerExtensions
+	{
+		public static Dictionary<string, StringValues> GetQuery(this NavigationManager navigationManager)
+			=> QueryHelpers.ParseQuery(navigationManager.ToAbsoluteUri(navigationManager.Uri).Query);
+	}
+}
