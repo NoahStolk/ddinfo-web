e45f843e3dfede45e3c655c34f021e049a448ebd
Noah Stolk <noah.stolk@gmail.com>
2021-08-08 19:06:41 +0200

    Implement query parameters in CustomLeaderboards page

--- DevilDaggersWebsite.BlazorWasm/Client/Extensions/NavigationManagerExtensions.cs
+++ DevilDaggersWebsite.BlazorWasm/Client/Extensions/NavigationManagerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DevilDaggersWebsite.BlazorWasm.Client.Extensions
 {
@@ -9,5 +11,37 @@ namespace DevilDaggersWebsite.BlazorWasm.Client.Extensions
 	{
 		public static Dictionary<string, StringValues> GetQuery(this NavigationManager navigationManager)
 			=> QueryHelpers.ParseQuery(navigationManager.ToAbsoluteUri(navigationManager.Uri).Query);
+
+		public static void AddOrModifyQueryParameter(this NavigationManager navigationManager, string key, StringValues value)
+		{
+			Dictionary<string, StringValues> query = navigationManager.GetQuery();
+
+			if (!query.ContainsKey(key))
+			{
+				navigationManager.NavigateTo(QueryHelpers.AddQueryString(navigationManager.Uri, key, value));
+			}
+			else
+			{
+				string newUrl = navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
+				foreach (KeyValuePair<string, StringValues> kvp in query)
+				{
+					if (kvp.Key == key)
+						newUrl = QueryHelpers.AddQueryString(newUrl, key, value);
+					else
+						newUrl = QueryHelpers.AddQueryString(newUrl, kvp.Key, kvp.Value);
+				}
+
+				navigationManager.NavigateTo(newUrl);
+			}
+		}
+
+		public static void RemoveAllUnknownQueryParameters(this Dictionary<string, StringValues> query, params string[] knownKeys)
+		{
+			foreach (string key in query.Values)
+			{
+				if (!knownKeys.Any(s => string.Equals(s, key, StringComparison.InvariantCultureIgnoreCase)))
+					query.Remove(key);
+			}
+		}
 	}
 }
