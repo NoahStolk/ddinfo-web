dc003fcc66e3f05d9b38c27bc4a9769ca615b2a0
Noah Stolk <noah.stolk@gmail.com>
2021-08-09 18:49:50 +0200

    Use strongly-typed methods for executing public API calls

---
+++ DevilDaggersWebsite.BlazorWasm/Client/HttpClients/JsonSerializationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace DevilDaggersWebsite.BlazorWasm.Client.HttpClients
+{
+	[Serializable]
+#pragma warning disable RCS1194 // Implement exception constructors.
+	public class JsonSerializationException : Exception
+#pragma warning restore RCS1194 // Implement exception constructors.
+	{
+		public JsonSerializationException([CallerMemberName] string? methodName = null)
+			: base($"JSON deserialization error in {methodName}.")
+		{
+		}
+
+		public JsonSerializationException(string? message, Exception? innerException)
+			: base(message, innerException)
+		{
+		}
+
+		protected JsonSerializationException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
+}
--- DevilDaggersWebsite.BlazorWasm/Client/HttpClients/PublicApiHttpClient.cs
+++ DevilDaggersWebsite.BlazorWasm/Client/HttpClients/PublicApiHttpClient.cs
@@ -1,14 +1,64 @@
-﻿using System.Net.Http;
+﻿using DevilDaggersWebsite.BlazorWasm.Client.Utils;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.CustomLeaderboards;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.LeaderboardHistoryStatistics;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.Leaderboards;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.LeaderboardStatistics;
+using DevilDaggersWebsite.BlazorWasm.Shared.Dto.Public.Players;
+using DevilDaggersWebsite.BlazorWasm.Shared.Enums;
+using DevilDaggersWebsite.BlazorWasm.Shared.Enums.Sortings.Public;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace DevilDaggersWebsite.BlazorWasm.Client.HttpClients
 {
 	public class PublicApiHttpClient
 	{
+		private readonly HttpClient _client;
+
 		public PublicApiHttpClient(HttpClient client)
 		{
-			Client = client;
+			_client = client;
 		}
 
-		public HttpClient Client { get; }
+		public async Task<GetCustomLeaderboard> GetCustomLeaderboardById(int id)
+		{
+			return await _client.GetFromJsonAsync<GetCustomLeaderboard>($"api/custom-leaderboards/{id}") ?? throw new JsonSerializationException();
+		}
+
+		public async Task<Page<GetCustomLeaderboardOverview>> GetCustomLeaderboards(CustomLeaderboardCategory category, int pageIndex, int pageSize, CustomLeaderboardSorting sortBy, bool ascending)
+		{
+			Dictionary<string, object?> queryParameters = new()
+			{
+				{ nameof(category), category },
+				{ nameof(pageIndex), pageIndex },
+				{ nameof(pageSize), pageSize },
+				{ nameof(sortBy), sortBy },
+				{ nameof(ascending), ascending },
+			};
+			return await _client.GetFromJsonAsync<Page<GetCustomLeaderboardOverview>>(UrlBuilderUtils.BuildUrlWithQuery("api/custom-leaderboards", queryParameters)) ?? throw new JsonSerializationException();
+		}
+
+		public async Task<List<GetLeaderboardHistoryStatistics>> GetLeaderboardHistoryStatistics()
+		{
+			return await _client.GetFromJsonAsync<List<GetLeaderboardHistoryStatistics>>("api/leaderboard-history-statistics") ?? throw new JsonSerializationException();
+		}
+
+		public async Task<List<GetPlayerForLeaderboard>> GetPlayersForLeaderboard()
+		{
+			return await _client.GetFromJsonAsync<List<GetPlayerForLeaderboard>>("api/players/leaderboard") ?? throw new JsonSerializationException();
+		}
+
+		public async Task<GetLeaderboardStatistics> GetLeaderboardStatistics()
+		{
+			return await _client.GetFromJsonAsync<GetLeaderboardStatistics>("api/leaderboard-statistics") ?? throw new JsonSerializationException();
+		}
+
+		public async Task<GetLeaderboard> GetLeaderboard(int rankStart)
+		{
+			return await _client.GetFromJsonAsync<GetLeaderboard>(UrlBuilderUtils.BuildUrlWithQuery("api/leaderboards", new() { { nameof(rankStart), rankStart } })) ?? throw new JsonSerializationException();
+		}
 	}
 }
