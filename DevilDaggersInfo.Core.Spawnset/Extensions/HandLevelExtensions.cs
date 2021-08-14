5e4ad9a45e55afbf949a0814d72167a703e204da
Noah Stolk <noah.stolk@gmail.com>
2021-08-14 18:42:30 +0200

    Create HandLevelExtensions.cs

---
+++ DevilDaggersInfo.Core.Spawnset/Extensions/HandLevelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DevilDaggersInfo.Core.Spawnset.Enums;
+
+namespace DevilDaggersInfo.Core.Spawnset.Extensions
+{
+	public static class HandLevelExtensions
+	{
+		public static int GetStartGems(this HandLevel handLevel) => handLevel switch
+		{
+			HandLevel.Level2 => 10,
+			HandLevel.Level3 => 70,
+			HandLevel.Level4 => 220,
+			_ => 0,
+		};
+	}
+}
