8e3f86e22bc15c28e081e4b3bb72d2b9483aaea5
Noah Stolk <noah.stolk@gmail.com>
2021-08-14 18:42:26 +0200

    Create EnemyTypeExtensions.cs

---
+++ DevilDaggersInfo.Core.Spawnset/Extensions/EnemyTypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DevilDaggersInfo.Core.Spawnset.Enums;
+using System;
+
+namespace DevilDaggersInfo.Core.Spawnset.Extensions
+{
+	public static class EnemyTypeExtensions
+	{
+		public static int GetNoFarmGems(this EnemyType enemyType) => enemyType switch
+		{
+			EnemyType.Empty => 0,
+			EnemyType.Squid1 => 2,
+			EnemyType.Squid2 => 3,
+			EnemyType.Centipede => 25,
+			EnemyType.Spider1 => 1,
+			EnemyType.Leviathan => 6,
+			EnemyType.Gigapede => 50,
+			EnemyType.Squid3 => 3,
+			EnemyType.Thorn => 0,
+			EnemyType.Spider2 => 1,
+			EnemyType.Ghostpede => 10,
+			_ => throw new NotSupportedException($"{nameof(EnemyType)} '{enemyType}' is not supported."),
+		};
+	}
+}
