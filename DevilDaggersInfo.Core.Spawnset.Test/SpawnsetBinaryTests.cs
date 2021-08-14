d7f2843e4d98f822b62b97c82270754a6c33f60b
Noah Stolk <noah.stolk@gmail.com>
2021-08-14 13:49:24 +0200

    Add SpawnsetBinaryTests

---
+++ DevilDaggersInfo.Core.Spawnset.Test/SpawnsetBinaryTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace DevilDaggersInfo.Core.Spawnset.Test
+{
+	[TestClass]
+	public class SpawnsetBinaryTests
+	{
+		[TestMethod]
+		public void CompareBinaryOutput_V0()
+			=> CompareBinaryOutput("V0");
+
+		[TestMethod]
+		public void CompareBinaryOutput_V1()
+			=> CompareBinaryOutput("V1");
+
+		[TestMethod]
+		public void CompareBinaryOutput_V2()
+			=> CompareBinaryOutput("V2");
+
+		[TestMethod]
+		public void CompareBinaryOutput_V3()
+			=> CompareBinaryOutput("V3");
+
+		[TestMethod]
+		public void CompareBinaryOutput_V3_229()
+			=> CompareBinaryOutput("V3_229");
+
+		[TestMethod]
+		public void CompareBinaryOutput_V3_451()
+			=> CompareBinaryOutput("V3_451");
+
+		[TestMethod]
+		public void CompareBinaryOutput_Empty()
+			=> CompareBinaryOutput("Empty");
+
+		[TestMethod]
+		public void CompareBinaryOutput_Scanner()
+			=> CompareBinaryOutput("Scanner");
+
+		private static void CompareBinaryOutput(string fileName)
+		{
+			using FileStream fs = new(Path.Combine("Data", fileName), FileMode.Open);
+			using MemoryStream ms = new();
+			fs.CopyTo(ms);
+
+			Spawnset spawnset = Spawnset.ParseSpawnset(ms);
+
+			byte[] originalBytes = ms.ToArray();
+			byte[] bytes = spawnset.ToBytes();
+
+			Assert.AreEqual(originalBytes.Length, bytes.Length);
+			for (int i = 0; i < originalBytes.Length; i++)
+				Assert.AreEqual(originalBytes[i], bytes[i]);
+		}
+	}
+}
