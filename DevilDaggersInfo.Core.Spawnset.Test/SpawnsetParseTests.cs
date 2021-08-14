2e33e074088cd784e80ef6ec0cc852c763a729d0
Noah Stolk <noah.stolk@gmail.com>
2021-08-14 13:08:44 +0200

    Refactor SpawnsetParseTests

--- DevilDaggersInfo.Core.Spawnset.Test/SpawnsetParseTests.cs
+++ DevilDaggersInfo.Core.Spawnset.Test/SpawnsetParseTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 
 namespace DevilDaggersInfo.Core.Spawnset.Test
@@ -9,19 +10,7 @@ namespace DevilDaggersInfo.Core.Spawnset.Test
 		[TestMethod]
 		public void Parse_V0()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V0"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(4, spawnset.SpawnVersion);
-			Assert.AreEqual(8, spawnset.WorldVersion);
-			Assert.AreEqual(50, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(82, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V0", 4, 8, 50, 20, 0.025f, 60, GameMode.Default, 82, HandLevel.Level1, 0, 0);
 			Assert.AreEqual(new(EnemyType.Squid1, 3), spawnset.Spawns[0]);
 			Assert.AreEqual(new(EnemyType.Empty, 6), spawnset.Spawns[1]);
 		}
@@ -29,19 +18,7 @@ namespace DevilDaggersInfo.Core.Spawnset.Test
 		[TestMethod]
 		public void Parse_V1()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V1"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(4, spawnset.SpawnVersion);
-			Assert.AreEqual(8, spawnset.WorldVersion);
-			Assert.AreEqual(50, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(130, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V1", 4, 8, 50, 20, 0.025f, 60, GameMode.Default, 130, HandLevel.Level1, 0, 0);
 			Assert.AreEqual(new(EnemyType.Squid1, 3), spawnset.Spawns[0]);
 			Assert.AreEqual(new(EnemyType.Empty, 6), spawnset.Spawns[1]);
 		}
@@ -49,19 +26,7 @@ namespace DevilDaggersInfo.Core.Spawnset.Test
 		[TestMethod]
 		public void Parse_V2()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V2"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(4, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(50, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(87, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V2", 4, 9, 50, 20, 0.025f, 60, GameMode.Default, 87, HandLevel.Level1, 0, 0);
 			Assert.AreEqual(new(EnemyType.Squid1, 3), spawnset.Spawns[0]);
 			Assert.AreEqual(new(EnemyType.Empty, 6), spawnset.Spawns[1]);
 		}
@@ -69,114 +34,61 @@ namespace DevilDaggersInfo.Core.Spawnset.Test
 		[TestMethod]
 		public void Parse_V3()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V3"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(4, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(50, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(118, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V3", 4, 9, 50, 20, 0.025f, 60, GameMode.Default, 118, HandLevel.Level1, 0, 0);
 			Assert.AreEqual(new(EnemyType.Squid1, 3), spawnset.Spawns[0]);
 			Assert.AreEqual(new(EnemyType.Empty, 6), spawnset.Spawns[1]);
-
-			Assert.AreEqual(HandLevel.Level1, spawnset.HandLevel);
-			Assert.AreEqual(0, spawnset.AdditionalGems);
-			Assert.AreEqual(0, spawnset.TimerStart);
 		}
 
 		[TestMethod]
 		public void Parse_V3_229()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V3_229"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(6, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(44.275f, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(75, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V3_229", 6, 9, 44.275f, 20, 0.025f, 60, GameMode.Default, 75, HandLevel.Level3, 57, 229);
 			Assert.AreEqual(new(EnemyType.Squid1, 0), spawnset.Spawns[0]);
 			Assert.AreEqual(new(EnemyType.Squid2, 10), spawnset.Spawns[6]);
-
-			Assert.AreEqual(HandLevel.Level3, spawnset.HandLevel);
-			Assert.AreEqual(57, spawnset.AdditionalGems);
-			Assert.AreEqual(229, spawnset.TimerStart);
 		}
 
 		[TestMethod]
 		public void Parse_V3_451()
 		{
-			using FileStream fs = new(Path.Combine("Data", "V3_451"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(6, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(38.725f, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(18, spawnset.Spawns.Length);
-
+			Spawnset spawnset = Parse("V3_451", 6, 9, 38.725f, 20, 0.025f, 60, GameMode.Default, 18, HandLevel.Level4, 0, 451);
 			Assert.AreEqual(new(EnemyType.Empty, 5), spawnset.Spawns[0]);
-
-			Assert.AreEqual(HandLevel.Level4, spawnset.HandLevel);
-			Assert.AreEqual(0, spawnset.AdditionalGems);
-			Assert.AreEqual(451, spawnset.TimerStart);
 		}
 
 		[TestMethod]
 		public void Parse_Empty()
 		{
-			using FileStream fs = new(Path.Combine("Data", "Empty"), FileMode.Open);
-			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
-
-			Assert.AreEqual(6, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(50, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(20, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
-
-			Assert.AreEqual(0, spawnset.Spawns.Length);
-
-			Assert.AreEqual(HandLevel.Level1, spawnset.HandLevel);
-			Assert.AreEqual(0, spawnset.AdditionalGems);
-			Assert.AreEqual(0, spawnset.TimerStart);
+			Parse("Empty", 6, 9, 50, 20, 0.025f, 60, GameMode.Default, 0, HandLevel.Level1, 0, 0);
 		}
 
 		[TestMethod]
 		public void Parse_Scanner()
 		{
-			using FileStream fs = new(Path.Combine("Data", "Scanner"), FileMode.Open);
+			Spawnset spawnset = Parse("Scanner", 6, 9, 26, 15, 0.025f, 60, GameMode.Default, 125, HandLevel.Level4, 30, 0);
+			Assert.AreEqual(new(EnemyType.Squid2, 0), spawnset.Spawns[0]);
+			Assert.AreEqual(new(EnemyType.Spider1, 5), spawnset.Spawns[30]);
+		}
+
+		[AssertionMethod]
+		private static Spawnset Parse(string fileName, int expectedSpawnVersion, int expectedWorldVersion, float expectedShrinkStart, float expectedShrinkEnd, float expectedShrinkRate, float expectedBrightness, GameMode expectedGameMode, int expectedSpawnCount, HandLevel expectedHandLevel, int expectedAdditionalGems, float expectedTimerStart)
+		{
+			using FileStream fs = new(Path.Combine("Data", fileName), FileMode.Open);
 			Spawnset spawnset = Spawnset.ParseSpawnset(fs);
 
-			Assert.AreEqual(6, spawnset.SpawnVersion);
-			Assert.AreEqual(9, spawnset.WorldVersion);
-			Assert.AreEqual(26, spawnset.ShrinkStart, 0.001f);
-			Assert.AreEqual(15, spawnset.ShrinkEnd, 0.001f);
-			Assert.AreEqual(0.025f, spawnset.ShrinkRate, 0.001f);
-			Assert.AreEqual(60, spawnset.Brightness, 0.001f);
-			Assert.AreEqual(GameMode.Default, spawnset.GameMode);
+			Assert.AreEqual(expectedSpawnVersion, spawnset.SpawnVersion);
+			Assert.AreEqual(expectedWorldVersion, spawnset.WorldVersion);
+			Assert.AreEqual(expectedShrinkStart, spawnset.ShrinkStart, 0.001f);
+			Assert.AreEqual(expectedShrinkEnd, spawnset.ShrinkEnd, 0.001f);
+			Assert.AreEqual(expectedShrinkRate, spawnset.ShrinkRate, 0.001f);
+			Assert.AreEqual(expectedBrightness, spawnset.Brightness, 0.001f);
+			Assert.AreEqual(expectedGameMode, spawnset.GameMode);
 
-			Assert.AreEqual(125, spawnset.Spawns.Length);
+			Assert.AreEqual(expectedSpawnCount, spawnset.Spawns.Length);
 
-			Assert.AreEqual(HandLevel.Level4, spawnset.HandLevel);
-			Assert.AreEqual(30, spawnset.AdditionalGems);
-			Assert.AreEqual(0, spawnset.TimerStart);
+			Assert.AreEqual(expectedHandLevel, spawnset.HandLevel);
+			Assert.AreEqual(expectedAdditionalGems, spawnset.AdditionalGems);
+			Assert.AreEqual(expectedTimerStart, spawnset.TimerStart);
+
+			return spawnset;
 		}
 	}
 }
