7ef1bc012a7acd2676ab89e7981d30c449bcd7f3
Noah Stolk <noah.stolk@gmail.com>
2021-08-15 14:00:45 +0200

    Use SupplyParameterFromQuery attribute

--- DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/NavigationManagerExtensions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/NavigationManagerExtensions.cs
@@ -6,29 +6,19 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Client.Extensions;
 
 public static class NavigationManagerExtensions
 {
-	public static Dictionary<string, StringValues> GetQuery(this NavigationManager navigationManager)
-		=> QueryHelpers.ParseQuery(navigationManager.ToAbsoluteUri(navigationManager.Uri).Query);
-
 	public static void AddOrModifyQueryParameter(this NavigationManager navigationManager, string key, object value)
+		=> AddOrModifyQueryParameter(navigationManager, new KeyValuePair<string, object>(key, value));
+
+	public static void AddOrModifyQueryParameter(this NavigationManager navigationManager, params KeyValuePair<string, object>[] parameters)
 	{
-		Dictionary<string, StringValues> query = navigationManager.GetQuery();
+		Dictionary<string, StringValues> query = QueryHelpers.ParseQuery(navigationManager.ToAbsoluteUri(navigationManager.Uri).Query);
+		foreach (KeyValuePair<string, object> kvp in parameters)
+			query[kvp.Key] = kvp.Value.ToString();
 
-		if (!query.ContainsKey(key))
-		{
-			navigationManager.NavigateTo(QueryHelpers.AddQueryString(navigationManager.Uri, key, value.ToString()));
-		}
-		else
-		{
-			string newUrl = navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
-			foreach (KeyValuePair<string, StringValues> kvp in query)
-			{
-				if (kvp.Key == key)
-					newUrl = QueryHelpers.AddQueryString(newUrl, key, value.ToString());
-				else
-					newUrl = QueryHelpers.AddQueryString(newUrl, kvp.Key, kvp.Value);
-			}
+		string newUrl = navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
+		foreach (KeyValuePair<string, StringValues> kvp in query)
+			newUrl = QueryHelpers.AddQueryString(newUrl, kvp.Key, kvp.Value);
 
-			navigationManager.NavigateTo(newUrl);
-		}
+		navigationManager.NavigateTo(newUrl);
 	}
 }
