4894318845e29757734203c72596b5afc002ac16
Noah Stolk <noah.stolk@gmail.com>
2021-08-15 20:44:26 +0200

    Simplify code

--- DevilDaggersInfo.Web.BlazorWasm.Server/Caches/SpawnsetData/SpawnsetDataCache.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Caches/SpawnsetData/SpawnsetDataCache.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections.Concurrent;
-using Core = DevilDaggersCore.Spawnsets;
+using OldCore = DevilDaggersCore.Spawnsets;
 
 namespace DevilDaggersInfo.Web.BlazorWasm.Server.Caches.SpawnsetData;
 
 public class SpawnsetDataCache : IDynamicCache
 {
-	private readonly ConcurrentDictionary<string, Core.SpawnsetData> _cache = new();
+	private readonly ConcurrentDictionary<string, OldCore.SpawnsetData> _cache = new();
 
-	public Core.SpawnsetData GetSpawnsetDataByFilePath(string filePath)
+	public OldCore.SpawnsetData GetSpawnsetDataByFilePath(string filePath)
 	{
 		string name = Path.GetFileNameWithoutExtension(filePath);
 		if (_cache.ContainsKey(name))
 			return _cache[name];
 
-		if (!Core.Spawnset.TryGetSpawnsetData(File.ReadAllBytes(filePath), out Core.SpawnsetData spawnsetData))
+		if (!OldCore.Spawnset.TryGetSpawnsetData(File.ReadAllBytes(filePath), out OldCore.SpawnsetData spawnsetData))
 			throw new($"Failed to get spawn data from spawnset file: '{name}'.");
 
 		_cache.TryAdd(name, spawnsetData);
