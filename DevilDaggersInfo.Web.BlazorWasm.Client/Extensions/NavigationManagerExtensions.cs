b3662e26735ae7ad6d483b3c2a43f0f6370f1e82
Noah Stolk <noah.stolk@gmail.com>
2021-08-15 14:01:33 +0200

    Improve method name

--- DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/NavigationManagerExtensions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/NavigationManagerExtensions.cs
@@ -6,10 +6,10 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Client.Extensions;
 
 public static class NavigationManagerExtensions
 {
-	public static void AddOrModifyQueryParameter(this NavigationManager navigationManager, string key, object value)
-		=> AddOrModifyQueryParameter(navigationManager, new KeyValuePair<string, object>(key, value));
+	public static void AddOrModifyQueryParameters(this NavigationManager navigationManager, string key, object value)
+		=> AddOrModifyQueryParameters(navigationManager, new KeyValuePair<string, object>(key, value));
 
-	public static void AddOrModifyQueryParameter(this NavigationManager navigationManager, params KeyValuePair<string, object>[] parameters)
+	public static void AddOrModifyQueryParameters(this NavigationManager navigationManager, params KeyValuePair<string, object>[] parameters)
 	{
 		Dictionary<string, StringValues> query = QueryHelpers.ParseQuery(navigationManager.ToAbsoluteUri(navigationManager.Uri).Query);
 		foreach (KeyValuePair<string, object> kvp in parameters)
