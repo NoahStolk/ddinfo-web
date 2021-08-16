e5f3fa51d194831ac7b302a9d242556f44851e13
Noah Stolk <noah.stolk@gmail.com>
2021-08-16 11:49:25 +0200

    Add DaggerColors

---
+++ DevilDaggersInfo.Core.Wiki/Colors/DaggerColors.cs
@@ -0,0 +1,11 @@
+﻿namespace DevilDaggersInfo.Core.Wiki.Colors;
+
+public static class DaggerColors
+{
+	public static readonly Color Default = new(0x44, 0x44, 0x44);
+	public static readonly Color Bronze = new(0xCD, 0x7F, 0x32);
+	public static readonly Color Silver = new(0xDD, 0xDD, 0xDD);
+	public static readonly Color Golden = new(0xFF, 0xDF, 0x00);
+	public static readonly Color Devil = new(0xFF, 0x00, 0x00);
+	public static readonly Color Leviathan = new(0xA0, 0x00, 0x00);
+}
