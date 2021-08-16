f49668ae20a9920725050c2c75f01435c434cee1
Noah Stolk <noah.stolk@gmail.com>
2021-08-16 11:50:23 +0200

    Update _Imports.cs

--- DevilDaggersInfo.Core.Wiki/_Imports.cs
+++ DevilDaggersInfo.Core.Wiki/_Imports.cs
@@ -1,3 +1,4 @@
-﻿global using DevilDaggersInfo.Core.Wiki.Enums;
+﻿global using DevilDaggersInfo.Core.Wiki.Colors;
+global using DevilDaggersInfo.Core.Wiki.Enums;
 global using DevilDaggersInfo.Core.Wiki.Objects;
 global using DevilDaggersInfo.Core.Wiki.Structs;
