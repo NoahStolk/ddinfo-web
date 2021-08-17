31df3ab5874a4eeef48e1bff3ac67ca9132de222
Noah Stolk <noah.stolk@gmail.com>
2021-08-17 20:41:50 +0200

    Finish DeathTests

--- DevilDaggersInfo.Core.Wiki.Test/_Imports.cs
+++ DevilDaggersInfo.Core.Wiki.Test/_Imports.cs
@@ -1,3 +1,4 @@
-﻿global using DevilDaggersInfo.Core.Wiki.Enums;
+﻿global using DevilDaggersInfo.Core.Wiki.Colors;
+global using DevilDaggersInfo.Core.Wiki.Enums;
 global using DevilDaggersInfo.Core.Wiki.Structs;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
