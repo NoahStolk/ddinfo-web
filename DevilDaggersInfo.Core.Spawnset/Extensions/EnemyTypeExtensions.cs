5d752fdc389e4cec8b6f4049c8f1c32944d209fd
Noah Stolk <noah.stolk@gmail.com>
2021-08-27 18:00:53 +0200

    Support old enemy colors and names

--- DevilDaggersInfo.Core.Spawnset/Extensions/EnemyTypeExtensions.cs
+++ DevilDaggersInfo.Core.Spawnset/Extensions/EnemyTypeExtensions.cs
@@ -18,7 +18,7 @@ public static class EnemyTypeExtensions
 		_ => throw new NotSupportedException($"{nameof(EnemyType)} '{enemyType}' is not supported."),
 	};
 
-	public static Color GetColor(this EnemyType enemyType) => enemyType switch
+	public static Color GetColor(this EnemyType enemyType, GameVersion gameVersion) => enemyType switch
 	{
 		EnemyType.Empty => EnemyColors.Void,
 		EnemyType.Squid1 => EnemyColors.Squid1,
@@ -26,15 +26,15 @@ public static class EnemyTypeExtensions
 		EnemyType.Centipede => EnemyColors.Centipede,
 		EnemyType.Spider1 => EnemyColors.Spider1,
 		EnemyType.Leviathan => EnemyColors.Leviathan,
-		EnemyType.Gigapede => EnemyColors.Gigapede,
-		EnemyType.Squid3 => EnemyColors.Squid3,
-		EnemyType.Thorn => EnemyColors.Thorn,
-		EnemyType.Spider2 => EnemyColors.Spider2,
-		EnemyType.Ghostpede => EnemyColors.Ghostpede,
+		EnemyType.Gigapede => gameVersion is GameVersion.V3_0 or GameVersion.V3_1 ? EnemyColors.Gigapede : EnemyColors.GigapedeRed,
+		EnemyType.Squid3 => gameVersion is not GameVersion.V1_0 ? EnemyColors.Squid3 : EnemyColors.Unknown,
+		EnemyType.Thorn => gameVersion is GameVersion.V3_0 or GameVersion.V3_1 ? EnemyColors.Thorn : gameVersion is GameVersion.V2_0 ? EnemyColors.Andras : EnemyColors.Unknown,
+		EnemyType.Spider2 => gameVersion is not GameVersion.V1_0 ? EnemyColors.Spider2 : EnemyColors.Unknown,
+		EnemyType.Ghostpede => gameVersion is GameVersion.V3_0 or GameVersion.V3_1 ? EnemyColors.Ghostpede : EnemyColors.Unknown,
 		_ => throw new NotSupportedException($"{nameof(EnemyType)} '{enemyType}' is not supported."),
 	};
 
-	public static string GetName(this EnemyType enemyType) => enemyType switch
+	public static string GetName(this EnemyType enemyType, GameVersion gameVersion) => enemyType switch
 	{
 		EnemyType.Empty => "Empty",
 		EnemyType.Squid1 => EnemiesV3_1.Squid1.Name,
@@ -43,10 +43,10 @@ public static class EnemyTypeExtensions
 		EnemyType.Spider1 => EnemiesV3_1.Spider1.Name,
 		EnemyType.Leviathan => EnemiesV3_1.Leviathan.Name,
 		EnemyType.Gigapede => EnemiesV3_1.Gigapede.Name,
-		EnemyType.Squid3 => EnemiesV3_1.Squid3.Name,
-		EnemyType.Thorn => EnemiesV3_1.Thorn.Name,
-		EnemyType.Spider2 => EnemiesV3_1.Spider2.Name,
-		EnemyType.Ghostpede => EnemiesV3_1.Ghostpede.Name,
-		_ => throw new NotSupportedException($"{nameof(EnemyType)} '{enemyType}' is not supported."),
+		EnemyType.Squid3 => gameVersion is not GameVersion.V1_0 ? EnemiesV3_1.Squid3.Name : "Unsupported",
+		EnemyType.Thorn => gameVersion is GameVersion.V3_0 or GameVersion.V3_1 ? EnemiesV3_1.Thorn.Name : gameVersion is GameVersion.V2_0 ? EnemiesV2_0.Andras.Name : "Unsupported",
+		EnemyType.Spider2 => gameVersion is not GameVersion.V1_0 ? EnemiesV3_1.Spider2.Name : "Unsupported",
+		EnemyType.Ghostpede => gameVersion is GameVersion.V3_0 or GameVersion.V3_1 ? EnemiesV3_1.Ghostpede.Name : "Unsupported",
+		_ => "Unsupported",
 	};
 }
