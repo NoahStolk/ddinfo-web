139a06ea088ba80924bff737d5b82ff6c6591351
Noah Stolk <noah.stolk@gmail.com>
2021-08-27 17:11:55 +0200

    Only instantiate Color.HexCode once

--- DevilDaggersInfo.Core.Wiki/Structs/Color.cs
+++ DevilDaggersInfo.Core.Wiki/Structs/Color.cs
@@ -7,11 +7,13 @@ public struct Color
 		R = r;
 		G = g;
 		B = b;
+
+		HexCode = $"#{R:X2}{G:X2}{B:X2}";
 	}
 
 	public byte R { get; }
 	public byte G { get; }
 	public byte B { get; }
 
-	public string HexCode => $"#{R:X2}{G:X2}{B:X2}";
+	public string HexCode { get; }
 }
