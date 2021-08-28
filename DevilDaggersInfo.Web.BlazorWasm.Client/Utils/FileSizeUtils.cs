309121e84819a158f2908691aa124967520306b3
Noah Stolk <noah.stolk@gmail.com>
2021-08-28 16:26:19 +0200

    Add FileSizeUtils

---
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Utils/FileSizeUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace DevilDaggersInfo.Web.BlazorWasm.Client.Utils;
+
+public static class FileSizeUtils
+{
+	public static string Format(int fileSizeInBytes)
+	{
+		if (fileSizeInBytes >= 1_000_000_000)
+			return $"{fileSizeInBytes / 1_000_000_000f:0.##} GB";
+
+		if (fileSizeInBytes >= 1_000_000)
+			return $"{fileSizeInBytes / 1_000_000f:0.##} MB";
+
+		if (fileSizeInBytes >= 1000)
+			return $"{fileSizeInBytes / 1000f:0.##} KB";
+
+		return $"{fileSizeInBytes} bytes";
+	}
+}
