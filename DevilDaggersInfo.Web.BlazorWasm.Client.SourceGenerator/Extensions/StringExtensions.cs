b42c1870e5e28099b4e5d5b1fa62bbd84e84a7b9
Noah Stolk <noah.stolk@gmail.com>
2021-09-11 14:54:56 +0200

    Fix indentation and implement TrimStart extension method

--- DevilDaggersInfo.Web.BlazorWasm.Client.SourceGenerator/Extensions/StringExtensions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client.SourceGenerator/Extensions/StringExtensions.cs
@@ -3,5 +3,22 @@
 public static class StringExtensions
 {
 	public static string Indent(this string code, int count)
-		=> code.Replace(Environment.NewLine, new string('\t', count));
+	{
+		string indentation = new('\t', count);
+		return code.Insert(0, indentation).Replace(Environment.NewLine, Environment.NewLine + indentation);
+	}
+
+	public static string TrimStart(this string str, params string[] values)
+	{
+		if (values.Length == 0)
+			return str;
+
+		foreach (string value in values)
+		{
+			if (str.StartsWith(value))
+				return str.Substring(value.Length);
+		}
+
+		return str;
+	}
 }
