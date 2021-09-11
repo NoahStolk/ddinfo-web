5435e1a068f0b7e98c7fd2addebec722943b070c
Noah Stolk <noah.stolk@gmail.com>
2021-09-11 14:54:37 +0200

    Create TypeSyntaxExtensions.cs

---
+++ DevilDaggersInfo.Web.BlazorWasm.Client.SourceGenerator/Extensions/TypeSyntaxExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DevilDaggersInfo.Web.BlazorWasm.Client.SourceGenerator.Extensions;
+
+public static class TypeSyntaxExtensions
+{
+	public static string GetTypeStringWithoutActionResult(this TypeSyntax typeSyntax)
+	{
+		string typeString = typeSyntax.ToString();
+
+		if (!typeString.StartsWith("ActionResult<"))
+			return typeString;
+
+		string trimStart = typeString.TrimStart("ActionResult<");
+		return trimStart.Substring(0, trimStart.Length - 1);
+	}
+}
