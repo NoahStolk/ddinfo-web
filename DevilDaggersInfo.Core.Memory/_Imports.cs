2e1e2b441120555fe0d6bd75b18d7b3f089269ba
Noah Stolk <noah.stolk@gmail.com>
2021-09-13 21:23:58 +0200

    Update dependencies

--- DevilDaggersInfo.Core.Memory/_Imports.cs
+++ DevilDaggersInfo.Core.Memory/_Imports.cs
@@ -1,5 +1,5 @@
-﻿global using System.Diagnostics;
-global using DevilDaggersInfo.Core.Memory.Enums;
+﻿global using DevilDaggersInfo.Core.Memory.Enums;
 global using DevilDaggersInfo.Core.Memory.Exceptions;
 global using DevilDaggersInfo.Core.Memory.Utils;
+global using System.Diagnostics;
 global using System.Runtime.Serialization;
