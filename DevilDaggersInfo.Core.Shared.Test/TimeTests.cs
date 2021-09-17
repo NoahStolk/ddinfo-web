074bc2abc27a9cb9bdcc4a322edf6a113831e896
Noah Stolk <noah.stolk@gmail.com>
2021-09-17 21:37:13 +0200

    Create TimeTests.cs

---
+++ DevilDaggersInfo.Core.Shared.Test/TimeTests.cs
@@ -0,0 +1,13 @@
+﻿namespace DevilDaggersInfo.Core.Shared.Test;
+
+[TestClass]
+public class TimeTests
+{
+	[TestMethod]
+	public void TestTime()
+	{
+		Assert.AreEqual(10001998, 1000.1998.To10thMilliTime());
+		Assert.AreEqual(1.0, 10000.ToSecondsTime());
+		Assert.AreEqual(1234567890.1234, 12345678901234UL.ToSecondsTime());
+	}
+}
