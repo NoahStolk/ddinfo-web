57d8529cfdf3c9a2fcb8906a9f11728a901f54d8
Noah Stolk <noah.stolk@gmail.com>
2021-09-18 10:45:44 +0200

    Use parameterized tests for SpawnsetBinaryTests

--- DevilDaggersInfo.Core.Spawnset.Test/SpawnsetBinaryTests.cs
+++ DevilDaggersInfo.Core.Spawnset.Test/SpawnsetBinaryTests.cs
@@ -3,39 +3,19 @@
 [TestClass]
 public class SpawnsetBinaryTests
 {
-	[TestMethod]
-	public void CompareBinaryOutput_V0()
-		=> CompareBinaryOutput("V0");
-
-	[TestMethod]
-	public void CompareBinaryOutput_V1()
-		=> CompareBinaryOutput("V1");
-
-	[TestMethod]
-	public void CompareBinaryOutput_V2()
-		=> CompareBinaryOutput("V2");
-
-	[TestMethod]
-	public void CompareBinaryOutput_V3()
-		=> CompareBinaryOutput("V3");
-
-	[TestMethod]
-	public void CompareBinaryOutput_V3_229()
-		=> CompareBinaryOutput("V3_229");
-
-	[TestMethod]
-	public void CompareBinaryOutput_V3_451()
-		=> CompareBinaryOutput("V3_451");
-
-	[TestMethod]
-	public void CompareBinaryOutput_Empty()
-		=> CompareBinaryOutput("Empty");
-
-	[TestMethod]
-	public void CompareBinaryOutput_Scanner()
-		=> CompareBinaryOutput("Scanner");
-
-	private static void CompareBinaryOutput(string fileName)
+	[DataTestMethod]
+	[DataRow("V0")]
+	[DataRow("V1")]
+	[DataRow("V2")]
+	[DataRow("V3")]
+	[DataRow("V3_229")]
+	[DataRow("V3_451")]
+	[DataRow("Empty")]
+	[DataRow("EmptySpawn")]
+	[DataRow("NoEndLoop")]
+	[DataRow("TimeAttack")]
+	[DataRow("Scanner")]
+	public void CompareBinaryOutput(string fileName)
 	{
 		byte[] originalBytes = File.ReadAllBytes(Path.Combine("Data", fileName));
 		Spawnset spawnset = Spawnset.Parse(originalBytes);
