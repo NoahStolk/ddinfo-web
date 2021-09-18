338f7a9e99e5c59a89bbb45c0a29f051ef3ffa58
Noah Stolk <noah.stolk@gmail.com>
2021-09-18 18:29:43 +0200

    Add SpawnsViewTests (WIP)

--- DevilDaggersInfo.Core.Spawnset.Test/_Imports.cs
+++ DevilDaggersInfo.Core.Spawnset.Test/_Imports.cs
@@ -1,4 +1,6 @@
 ﻿global using DevilDaggersInfo.Core.Spawnset.Enums;
 global using DevilDaggersInfo.Core.Spawnset.Summary;
+global using DevilDaggersInfo.Core.Spawnset.View;
+global using DevilDaggersInfo.Core.Wiki.Enums;
 global using JetBrains.Annotations;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
