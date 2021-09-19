a49db8f1bc71fe746cc3a205070b7023c4dfc7ba
Noah Stolk <noah.stolk@gmail.com>
2021-09-19 20:47:07 +0200

    Refactor AssetConverter

--- DevilDaggersInfo.Core.Mod/FileHandling/AssetConverter.cs
+++ DevilDaggersInfo.Core.Mod/FileHandling/AssetConverter.cs
@@ -2,13 +2,13 @@
 
 public static class AssetConverter
 {
-	public static byte[] Compile(string inputPath, ModBinaryChunk chunk) => chunk.AssetType switch
+	public static byte[] Compile(ModBinaryChunk chunk, byte[] buffer) => chunk.AssetType switch
 	{
-		AssetType.Audio or AssetType.ModelBinding => File.ReadAllBytes(inputPath),
-		AssetType.Model => ObjFileHandler.Instance.Compile(File.ReadAllBytes(inputPath)),
-		AssetType.Shader => GlslFileHandler.Instance.Compile(File.ReadAllBytes(inputPath)),
-		AssetType.Texture => PngFileHandler.Instance.Compile(File.ReadAllBytes(inputPath)),
-		_ => throw new NotSupportedException($"Compile asset of type '{chunk.AssetType}' is not supported."),
+		AssetType.Audio or AssetType.ModelBinding => buffer,
+		AssetType.Model => ObjFileHandler.Instance.Compile(buffer),
+		AssetType.Shader => GlslFileHandler.Instance.Compile(buffer),
+		AssetType.Texture => PngFileHandler.Instance.Compile(buffer),
+		_ => throw new NotSupportedException($"Compiling asset of type '{chunk.AssetType}' is not supported."),
 	};
 
 	public static byte[] Extract(ModBinaryChunk chunk, byte[] buffer) => chunk.AssetType switch
