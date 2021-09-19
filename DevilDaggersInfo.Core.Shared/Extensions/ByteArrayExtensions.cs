88d8ab5146db3150a6019344ef9b391cb13469b5
Noah Stolk <noah.stolk@gmail.com>
2021-09-19 18:38:18 +0200

    Create NullTerminatorTests.cs

--- DevilDaggersInfo.Core.Shared/Extensions/ByteArrayExtensions.cs
+++ DevilDaggersInfo.Core.Shared/Extensions/ByteArrayExtensions.cs
@@ -7,6 +7,9 @@ public static class ByteArrayExtensions
 
 	public static string ReadNullTerminatedString(this byte[] buffer, int offset, Encoding encoding)
 	{
+		if (offset < 0 || offset >= buffer.Length)
+			throw new ArgumentOutOfRangeException(nameof(offset), "Offset was out of bounds of the array.");
+
 		int indexOfNextNullTerminator = Array.IndexOf(buffer, (byte)0, offset);
 		if (indexOfNextNullTerminator == -1)
 			throw new ArgumentOutOfRangeException(nameof(offset), $"Null terminator not observed in buffer with length '{buffer.Length}' starting from offset '{offset}'.");
