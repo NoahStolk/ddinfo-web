72ec8b1d7634cefd26081dd66bff8542fdf8c6f5
Noah Stolk <noah.stolk@gmail.com>
2021-09-21 22:42:07 +0200

    Simplify test

--- DevilDaggersInfo.Core.Mod.Test/ModBinaryTests.cs
+++ DevilDaggersInfo.Core.Mod.Test/ModBinaryTests.cs
@@ -1,4 +1,4 @@
-﻿namespace DevilDaggersInfo.Core.Mod.Test;
+namespace DevilDaggersInfo.Core.Mod.Test;
 
 [TestClass]
 public class ModBinaryTests
@@ -9,19 +9,14 @@ public class ModBinaryTests
 	[DataRow("dd-texture")]
 	public void CompareBinaryOutput(string fileName)
 	{
-		byte[] originalBytes = File.ReadAllBytes(Path.Combine("Data", fileName));
-		ModBinary modBinary = GetModBinary(fileName);
+		string filePath = Path.Combine("Data", fileName);
+		byte[] originalBytes = File.ReadAllBytes(filePath);
+		byte[] fileContents = File.ReadAllBytes(filePath);
+		ModBinary modBinary = new(fileName, fileContents, true);
 		byte[] bytes = modBinary.ToBytes();
 
 		Assert.AreEqual(originalBytes.Length, bytes.Length);
 		for (int i = 0; i < originalBytes.Length; i++)
 			Assert.AreEqual(originalBytes[i], bytes[i]);
-	}
-
-	private static ModBinary GetModBinary(string fileName)
-	{
-		string filePath = Path.Combine("Data", fileName);
-		byte[] fileContents = File.ReadAllBytes(filePath);
-		return new(fileName, fileContents, true);
 	}
 }
