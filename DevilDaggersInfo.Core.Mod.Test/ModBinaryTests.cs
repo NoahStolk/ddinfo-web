50f8d6f99df9fe89ed82c395b08980b17137bcd0
Noah Stolk <noah.stolk@gmail.com>
2021-09-22 22:34:40 +0200

    Add mod with same asset copied to unit test

--- DevilDaggersInfo.Core.Mod.Test/ModBinaryTests.cs
+++ DevilDaggersInfo.Core.Mod.Test/ModBinaryTests.cs
@@ -5,9 +5,10 @@ public class ModBinaryTests
 {
 	[DataTestMethod]
 	[DataRow("dd-model")]
-	[DataRow("dd-shader")]
-	[DataRow("dd-texture")]
 	[DataRow("dd-model-shader-texture")]
+	[DataRow("dd-shader")]
+	[DataRow("dd-skull-1-2-same-texture-copied")]
+	[DataRow("dd-texture")]
 	public void CompareBinaryOutput(string fileName)
 	{
 		string filePath = Path.Combine("Data", fileName);
