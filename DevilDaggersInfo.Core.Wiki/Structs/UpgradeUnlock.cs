1c719510f6f20cdd287fa8f583596ce5c8f975fc
Noah Stolk <noah.stolk@gmail.com>
2021-09-24 12:28:04 +0200

    Add ToString override for UpgradeUnlock

--- DevilDaggersInfo.Core.Wiki/Structs/UpgradeUnlock.cs
+++ DevilDaggersInfo.Core.Wiki/Structs/UpgradeUnlock.cs
@@ -1,3 +1,6 @@
 namespace DevilDaggersInfo.Core.Wiki.Structs;
 
-public readonly record struct UpgradeUnlock(UpgradeUnlockType UpgradeUnlockType, int Value);
+public readonly record struct UpgradeUnlock(UpgradeUnlockType UpgradeUnlockType, int Value)
+{
+	public override string ToString() => $"{Value} {(UpgradeUnlockType == UpgradeUnlockType.Gems ? "gems" : "homing daggers")}";
+}
