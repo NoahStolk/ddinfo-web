38ae5aac4f444297cf4bab457e43f3184d471fbe
Noah Stolk <noah.stolk@gmail.com>
2021-09-29 21:53:37 +0200

    Add EnumMember to correctly serialize CustomLeaderboardsClient

--- DevilDaggersWebsite/Enumerators/CustomLeaderboardsClient.cs
+++ DevilDaggersWebsite/Enumerators/CustomLeaderboardsClient.cs
@@ -1,8 +1,13 @@
-﻿namespace DevilDaggersWebsite.Enumerators
+﻿using System.Runtime.Serialization;
+
+namespace DevilDaggersWebsite.Enumerators
 {
 	public enum CustomLeaderboardsClient
 	{
+		[EnumMember(Value = "DevilDaggersCustomLeaderboards")]
 		DevilDaggersCustomLeaderboards = 0,
+
+		[EnumMember(Value = "ddstats-rust")]
 		DdstatsRust = 1,
 	}
 }
