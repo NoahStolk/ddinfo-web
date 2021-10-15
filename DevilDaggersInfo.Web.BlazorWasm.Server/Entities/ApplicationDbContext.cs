03717d7bec5aa42b4b42469a8e46b79af3652d3e
Noah Stolk <noah.stolk@gmail.com>
2021-10-15 14:26:18 +0200

    Update preview7 packages to RC2

--- DevilDaggersInfo.Web.BlazorWasm.Server/Entities/ApplicationDbContext.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Entities/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-using IdentityServer4.EntityFramework.Options; // TODO: Update to using Duende.IdentityServer.EntityFramework.Options; for RC 1.
+using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.Extensions.Options;
 
