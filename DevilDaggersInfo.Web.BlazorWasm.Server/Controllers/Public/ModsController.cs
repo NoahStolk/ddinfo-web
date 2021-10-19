153e080076e57cac338c694706e861b52ecf1326
Noah Stolk <noah.stolk@gmail.com>
2021-10-19 21:29:54 +0200

    Add GetModsFromAuthor endpoint

--- DevilDaggersInfo.Web.BlazorWasm.Server/Controllers/Public/ModsController.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Controllers/Public/ModsController.cs
@@ -200,4 +200,17 @@ public class ModsController : ControllerBase
 			bool fileExists = IoFile.Exists(filePath);
 			return (fileExists, fileExists ? filePath : null);
 		});
+
+	[HttpGet("from-author")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	public ActionResult<List<GetModName>> GetModsFromAuthor([Required] int playerId)
+	{
+		var mods = _dbContext.Mods.AsNoTracking().Include(m => m.PlayerMods).Where(s => s.PlayerMods.Any(pm => pm.PlayerId == playerId)).Select(s => new { s.Id, s.Name }).ToList();
+
+		return mods.ConvertAll(s => new GetModName
+		{
+			Id = s.Id,
+			Name = s.Name,
+		});
+	}
 }
---
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Mods/GetModName.cs
@@ -0,0 +1,8 @@
+namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Mods;
+
+public class GetModName
+{
+	public int Id { get; init; }
+
+	public string Name { get; init; } = null!;
+}
