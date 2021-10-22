6c16d7af9bac54fefbc8c3dc1e2731b9d3d949cf
Noah Stolk <noah.stolk@gmail.com>
2021-10-22 12:44:55 +0200

    Update GetSpawnsetByHash endpoint

--- DevilDaggersWebsite/Api/SpawnsetsController.cs
+++ DevilDaggersWebsite/Api/SpawnsetsController.cs
@@ -5,10 +5,12 @@ using DevilDaggersWebsite.Transients;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Linq;
 using System.Net.Mime;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -23,12 +25,14 @@ namespace DevilDaggersWebsite.Api
 		private readonly SpawnsetHelper _spawnsetHelper;
 		private readonly IWebHostEnvironment _environment;
 		private readonly SpawnsetHashCache _spawnsetHashCache;
+		private readonly Entities.ApplicationDbContext _dbContext;
 
-		public SpawnsetsController(SpawnsetHelper spawnsetHelper, IWebHostEnvironment environment, SpawnsetHashCache spawnsetHashCache)
+		public SpawnsetsController(SpawnsetHelper spawnsetHelper, IWebHostEnvironment environment, SpawnsetHashCache spawnsetHashCache, Entities.ApplicationDbContext dbContext)
 		{
 			_spawnsetHelper = spawnsetHelper;
 			_environment = environment;
 			_spawnsetHashCache = spawnsetHashCache;
+			_dbContext = dbContext;
 		}
 
 		[ApiExplorerSettings(IgnoreApi = true)]
@@ -46,17 +50,45 @@ namespace DevilDaggersWebsite.Api
 		[HttpGet("by-hash")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public async Task<ActionResult<SpawnsetFile>> GetSpawnsetByHash([FromQuery] byte[] hash)
+		public async Task<ActionResult<SpawnsetForDdcl>> GetSpawnsetByHash([FromQuery] byte[] hash)
 		{
 			SpawnsetHashCacheData? data = await _spawnsetHashCache.GetSpawnset(hash);
 			if (data == null)
 				return new NotFoundObjectResult(new ProblemDetails { Title = $"Spawnset with {nameof(hash)} '{hash.ByteArrayToHexString()}' was not found." });
 
-			SpawnsetFile? dto = _spawnsetHelper.GetSpawnset(data.Name);
-			if (dto == null)
+			Entities.SpawnsetFile? spawnset = _dbContext.SpawnsetFiles
+				.AsNoTracking()
+				.Include(s => s.Player)
+				.FirstOrDefault(s => s.Name == data.Name);
+			if (spawnset == null)
 				return new NotFoundObjectResult(new ProblemDetails { Title = $"Spawnset with {nameof(hash)} '{hash.ByteArrayToHexString()}' was not found." });
 
-			return dto;
+			Entities.CustomLeaderboard? customLeaderboard = _dbContext.CustomLeaderboards
+				.AsNoTracking()
+				.FirstOrDefault(cl => cl.SpawnsetFileId == spawnset.Id);
+
+			var customEntries = customLeaderboard == null ? null : _dbContext.CustomEntries
+				.AsNoTracking()
+				.Select(ce => new { ce.Id, ce.CustomLeaderboardId, ce.Time })
+				.Where(ce => ce.CustomLeaderboardId == customLeaderboard.Id)
+				.ToList();
+
+			return new SpawnsetForDdcl
+			{
+				AuthorName = spawnset.Player.PlayerName,
+				CustomLeaderboard = customLeaderboard == null ? null : new SpawnsetCustomLeaderboardForDdcl
+				{
+					CustomLeaderboardId = customLeaderboard.Id,
+					CustomEntries = customEntries?.ConvertAll(ce => new SpawnsetCustomEntryForDdcl
+					{
+						HasReplay = false,
+						CustomEntryId = ce.Id,
+						Time = ce.Time,
+					}) ?? new(),
+				},
+				SpawnsetId = spawnset.Id,
+				Name = spawnset.Name,
+			};
 		}
 
 		[HttpGet("hash")]
---
+++ DevilDaggersWebsite/Dto/SpawnsetCustomEntryForDdcl.cs
@@ -0,0 +1,11 @@
+﻿namespace DevilDaggersWebsite.Dto
+{
+	public class SpawnsetCustomEntryForDdcl
+	{
+		public int CustomEntryId { get; init; }
+
+		public int Time { get; init; }
+
+		public bool HasReplay { get; init; }
+	}
+}
