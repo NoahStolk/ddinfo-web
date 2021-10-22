e52c4c606123a564a8845e5500d6c9e67eff70d2
Noah Stolk <noah.stolk@gmail.com>
2021-10-22 13:43:35 +0200

    Remove DevilDaggersWebsite.Razor

--- DevilDaggersWebsite.Razor/PageModels/IDefaultLeaderboardPageModel.cs
+++
@@ -1,9 +0,0 @@
-using Lb = DevilDaggersWebsite.Clients.Leaderboard;
-
-namespace DevilDaggersWebsite.Razor.PageModels
-{
-	public interface IDefaultLeaderboardPageModel
-	{
-		public Lb? Leaderboard { get; set; }
-	}
-}
--- DevilDaggersWebsite.Razor/wwwroot/js-site/dropdown.js
+++
@@ -1,7 +0,0 @@
-$('.dropdown').on('show.bs.dropdown', function () {
-	$(this).find('.dropdown-menu').first().stop(true, true).slideDown(200);
-});
-
-$('.dropdown').on('hide.bs.dropdown', function () {
-	$(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
-});
