4a1dcc637eba7c4369736d52ef2d4f6abb950f83
Noah Stolk <noah.stolk@gmail.com>
2021-11-04 21:15:27 +0100

    Fix POST and PUT body parameter generation

--- DevilDaggersInfo.SourceGen.Web.BlazorWasm.Client/Generators/ApiHttpClient/Endpoints/PostEndpoint.cs
+++ DevilDaggersInfo.SourceGen.Web.BlazorWasm.Client/Generators/ApiHttpClient/Endpoints/PostEndpoint.cs
@@ -3,10 +3,11 @@ namespace DevilDaggersInfo.SourceGen.Web.BlazorWasm.Client.Generators.ApiHttpCli
 internal class PostEndpoint : Endpoint
 {
 	private const string _methodName = $"%{nameof(_methodName)}%";
+	private const string _bodyParameterType = $"%{nameof(_bodyParameterType)}%";
 	private const string _bodyParameter = $"%{nameof(_bodyParameter)}%";
 	private const string _apiRoute = $"%{nameof(_apiRoute)}%";
 	private const string _httpMethod = $"%{nameof(_httpMethod)}%";
-	private const string _endpointTemplate = $@"public async Task<HttpResponseMessage> {_methodName}({_bodyParameter})
+	private const string _endpointTemplate = $@"public async Task<HttpResponseMessage> {_methodName}({_bodyParameterType} {_bodyParameter})
 {{
 	return await SendRequest(new HttpMethod(""{_httpMethod}""), $""{_apiRoute}"", {_bodyParameter});
 }}
@@ -22,9 +23,14 @@ internal class PostEndpoint : Endpoint
 
 	public override string Build()
 	{
+		string bodyParameterStr = BodyParameter.ToString();
+		string bodyParameterType = bodyParameterStr.Substring(0, bodyParameterStr.IndexOf(' '));
+		string bodyParameter = bodyParameterStr.Substring(bodyParameterStr.IndexOf(' ') + 1);
+
 		return _endpointTemplate
 			.Replace(_methodName, MethodName)
-			.Replace(_bodyParameter, BodyParameter.ToString())
+			.Replace(_bodyParameterType, bodyParameterType)
+			.Replace(_bodyParameter, bodyParameter)
 			.Replace(_httpMethod, HttpMethod.ToString())
 			.Replace(_apiRoute, ApiRoute);
 	}
--- DevilDaggersInfo.SourceGen.Web.BlazorWasm.Client/Generators/ApiHttpClient/Endpoints/PutEndpoint.cs
+++ DevilDaggersInfo.SourceGen.Web.BlazorWasm.Client/Generators/ApiHttpClient/Endpoints/PutEndpoint.cs
@@ -4,10 +4,11 @@ internal class PutEndpoint : Endpoint
 {
 	private const string _methodName = $"%{nameof(_methodName)}%";
 	private const string _routeParameter = $"%{nameof(_routeParameter)}%";
+	private const string _bodyParameterType = $"%{nameof(_bodyParameterType)}%";
 	private const string _bodyParameter = $"%{nameof(_bodyParameter)}%";
 	private const string _apiRoute = $"%{nameof(_apiRoute)}%";
 	private const string _httpMethod = $"%{nameof(_httpMethod)}%";
-	private const string _endpointTemplate = $@"public async Task<HttpResponseMessage> {_methodName}({_routeParameter}, {_bodyParameter})
+	private const string _endpointTemplate = $@"public async Task<HttpResponseMessage> {_methodName}({_routeParameter}, {_bodyParameterType} {_bodyParameter})
 {{
 	return await SendRequest(new HttpMethod(""{_httpMethod}""), $""{_apiRoute}"", {_bodyParameter});
 }}
@@ -26,10 +27,15 @@ internal class PutEndpoint : Endpoint
 
 	public override string Build()
 	{
+		string bodyParameterStr = BodyParameter.ToString();
+		string bodyParameterType = bodyParameterStr.Substring(0, bodyParameterStr.IndexOf(' '));
+		string bodyParameter = bodyParameterStr.Substring(bodyParameterStr.IndexOf(' ') + 1);
+
 		return _endpointTemplate
 			.Replace(_methodName, MethodName)
 			.Replace(_routeParameter, RouteParameter.ToString())
-			.Replace(_bodyParameter, BodyParameter.ToString())
+			.Replace(_bodyParameterType, bodyParameterType)
+			.Replace(_bodyParameter, bodyParameter)
 			.Replace(_httpMethod, HttpMethod.ToString())
 			.Replace(_apiRoute, ApiRoute);
 	}
