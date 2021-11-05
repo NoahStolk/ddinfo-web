bec938040bac7ded13629d009016493605ad011d
Noah Stolk <noah.stolk@gmail.com>
2021-11-05 14:25:27 +0100

    Redirect from admin pages if not logged in (WIP)

--- DevilDaggersInfo.Web.BlazorWasm.Server/Middleware/AdminAuthenticationMiddleware.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Middleware/AdminAuthenticationMiddleware.cs
@@ -5,6 +5,11 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Server.Middleware;
 
 public class AdminAuthenticationMiddleware
 {
+	private const string _adminApiRouteStart = "api/admin/";
+	private const string _adminRouteStart = "admin/";
+	private const string _bearer = "Bearer ";
+	private const string _role = "role: ";
+
 	private static readonly Dictionary<string, string> _roleOverrides = new()
 	{
 		["custom-leaderboards"] = Roles.CustomLeaderboards,
@@ -24,44 +29,86 @@ public class AdminAuthenticationMiddleware
 
 	public Task InvokeAsync(HttpContext context)
 	{
-		const string adminRouteStart = "api/admin/";
-		const string bearer = "Bearer ";
-		const string role = "role: ";
-
 		PathString path = context.Request.Path;
 		string pathString = path.ToString();
-		if (!pathString.Contains(adminRouteStart))
-			return _next(context);
+		if (pathString.Contains(_adminApiRouteStart))
+			return HandleAdminApiCall(context, pathString);
 
+		if (pathString.Contains(_adminRouteStart))
+			return HandleAdminCall(context, pathString);
+
+		return _next(context);
+	}
+
+	private Task HandleAdminApiCall(HttpContext context, string pathString)
+	{
 		StringValues? auth = context.Request.Headers["Authorization"];
 		string? authString = auth?.ToString();
-		if (authString?.StartsWith(bearer) != true)
-			return Status(401);
+		if (authString?.StartsWith(_bearer) != true)
+		{
+			context.Response.StatusCode = 401;
+			return _next(context);
+		}
 
-		string token = authString[bearer.Length..];
+		string token = authString[_bearer.Length..];
 		ClaimsPrincipal user = token.CreateClaimsPrincipalFromJwtTokenString();
 		ClaimsIdentity? identity = user.Identities.FirstOrDefault();
 		Claim? roleClaim = identity?.Claims.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
 		string? roleClaimValueString = roleClaim?.Value?.ToString();
-		if (roleClaimValueString?.StartsWith(role) != true)
-			return Status(403);
+		if (roleClaimValueString?.StartsWith(_role) != true)
+		{
+			context.Response.StatusCode = 403;
+			return _next(context);
+		}
 
-		string[] userRoles = roleClaimValueString[role.Length..].Split(',') ?? Array.Empty<string>();
+		string[] userRoles = roleClaimValueString[_role.Length..].Split(',') ?? Array.Empty<string>();
 
-		string endpointRoute = pathString[(pathString.IndexOf(adminRouteStart) + adminRouteStart.Length)..];
+		string endpointRoute = pathString[(pathString.IndexOf(_adminApiRouteStart) + _adminApiRouteStart.Length)..];
 		if (endpointRoute.Contains('/'))
 			endpointRoute = endpointRoute[..endpointRoute.IndexOf('/')];
 
 		string requiredRole = _roleOverrides.ContainsKey(endpointRoute) ? _roleOverrides[endpointRoute] : "Admin";
 		if (!userRoles.Contains(requiredRole))
-			return Status(403);
+		{
+			context.Response.StatusCode = 403;
+			return _next(context);
+		}
 
 		return _next(context);
+	}
 
-		async Task Status(int statusCode)
+	private Task HandleAdminCall(HttpContext context, string pathString)
+	{
+		StringValues? auth = context.Request.Headers["Authorization"];
+		string? authString = auth?.ToString();
+		if (authString?.StartsWith(_bearer) != true)
 		{
-			context.Response.StatusCode = statusCode;
-			await Task.CompletedTask;
+			// TODO: Fix.
+			context.Response.Redirect("/authentication/login");
+			return _next(context);
 		}
+
+		string token = authString[_bearer.Length..];
+		ClaimsPrincipal user = token.CreateClaimsPrincipalFromJwtTokenString();
+		ClaimsIdentity? identity = user.Identities.FirstOrDefault();
+		Claim? roleClaim = identity?.Claims.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
+		string? roleClaimValueString = roleClaim?.Value?.ToString();
+		if (roleClaimValueString?.StartsWith(_role) != true)
+		{
+			context.Response.StatusCode = 403;
+			return _next(context);
+		}
+
+		string[] userRoles = roleClaimValueString[_role.Length..].Split(',') ?? Array.Empty<string>();
+
+		string endpointRoute = pathString[(pathString.IndexOf(_adminApiRouteStart) + _adminApiRouteStart.Length)..];
+		if (endpointRoute.Contains('/'))
+			endpointRoute = endpointRoute[..endpointRoute.IndexOf('/')];
+
+		string requiredRole = _roleOverrides.ContainsKey(endpointRoute) ? _roleOverrides[endpointRoute] : "Admin";
+		if (!userRoles.Contains(requiredRole))
+			context.Response.StatusCode = 403;
+
+		return _next(context);
 	}
 }
