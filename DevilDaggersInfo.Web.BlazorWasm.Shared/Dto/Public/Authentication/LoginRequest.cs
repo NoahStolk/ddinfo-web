5a6d49d984b94717ff05c68c120da9319ad47b32
Noah Stolk <noah.stolk@gmail.com>
2021-11-06 17:12:54 +0100

    Use client-side validation for required fields

--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/LoginRequest.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/LoginRequest.cs
@@ -2,7 +2,9 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Authentication;
 
 public class LoginRequest
 {
+	[Required]
 	public string Name { get; set; } = null!;
 
+	[Required]
 	public string Password { get; set; } = null!;
 }
--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/RegistrationRequest.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/RegistrationRequest.cs
@@ -2,9 +2,11 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Authentication;
 
 public class RegistrationRequest
 {
+	[Required]
 	[StringLength(32, MinimumLength = 2)]
 	public string Name { get; set; } = null!;
 
+	[Required]
 	[StringLength(128, MinimumLength = 8)]
 	public string Password { get; set; } = null!;
 }
