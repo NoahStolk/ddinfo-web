1ef5dad077da58bbdbaa8fd91199fbcac690e0fc
Noah Stolk <noah.stolk@gmail.com>
2021-11-06 17:06:10 +0100

    Use EditForm for login and register pages

--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/LoginRequest.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/LoginRequest.cs
@@ -2,7 +2,7 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Authentication;
 
 public class LoginRequest
 {
-	public string Name { get; init; } = null!;
+	public string Name { get; set; } = null!;
 
-	public string Password { get; init; } = null!;
+	public string Password { get; set; } = null!;
 }
--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/RegistrationRequest.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Authentication/RegistrationRequest.cs
@@ -2,7 +2,9 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Authentication;
 
 public class RegistrationRequest
 {
-	public string Name { get; init; } = null!;
+	[StringLength(32, MinimumLength = 2)]
+	public string Name { get; set; } = null!;
 
-	public string Password { get; init; } = null!;
+	[StringLength(128, MinimumLength = 8)]
+	public string Password { get; set; } = null!;
 }
