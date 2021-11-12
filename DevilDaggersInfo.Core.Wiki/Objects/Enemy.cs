4ff552401f98e486434d6b19ec067f15322941e0
Noah Stolk <noah.stolk@gmail.com>
2021-11-12 16:06:02 +0100

    Add EnemiesPage.razor

--- DevilDaggersInfo.Core.Wiki/Objects/Enemy.cs
+++ DevilDaggersInfo.Core.Wiki/Objects/Enemy.cs
@@ -6,4 +6,15 @@ public readonly record struct Enemy(GameVersion GameVersion, string Name, Color 
 
 	public string GetGemHpString()
 		=> $"({GemHp} x {Gems})";
+
+	public string GetImageName()
+	{
+		return Name
+			.Replace(" IV", "-4")
+			.Replace(" III", "-3")
+			.Replace(" II", "-2")
+			.Replace(" I", "-1")
+			.Replace(' ', '-')
+			.ToLower();
+	}
 }
