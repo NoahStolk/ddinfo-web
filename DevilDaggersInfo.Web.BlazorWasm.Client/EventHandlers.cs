3d243a1c8a2678a39e9952e2e38bf1fd7b690bec
Noah Stolk <noah.stolk@gmail.com>
2021-11-12 17:18:33 +0100

    Make nav collapsed by default and expand it only on mouse enter

---
+++ DevilDaggersInfo.Web.BlazorWasm.Client/EventHandlers.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace DevilDaggersInfo.Web.BlazorWasm.Client;
+
+[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+public static class EventHandlers
+{
+}
