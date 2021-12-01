4533281e00111d956cae48947b0dabd65b7701d3
Noah Stolk <noah.stolk@gmail.com>
2021-12-01 23:02:53 +0100

    Use top level program

--- DevilDaggersInfo.Web.BlazorWasm.Server/Program.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Program.cs
@@ -1,22 +1,15 @@
+using DevilDaggersInfo.Web.BlazorWasm.Server;
 using DevilDaggersInfo.Web.BlazorWasm.Server.HostedServices.DdInfoDiscordBot;
 using DevilDaggersInfo.Web.BlazorWasm.Server.Logging.Discord;
 
-namespace DevilDaggersInfo.Web.BlazorWasm.Server;
-
-public static class Program
-{
-	public static void Main(string[] args)
-		=> CreateHostBuilder(args).Build().Run();
-
-	public static IHostBuilder CreateHostBuilder(string[] args)
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+	.ConfigureLogging(builder => builder.ClearProviders().AddDiscordLogger())
+	.ConfigureWebHostDefaults(webBuilder =>
 	{
-		return Host.CreateDefaultBuilder(args)
-			.ConfigureLogging(builder => builder.ClearProviders().AddDiscordLogger())
-			.ConfigureWebHostDefaults(webBuilder =>
-			{
-				webBuilder.UseStaticWebAssets();
-				webBuilder.UseStartup<Startup>();
-			})
-			.ConfigureServices(services => services.AddHostedService<DdInfoDiscordBotService>());
-	}
-}
+		webBuilder.UseStaticWebAssets();
+		webBuilder.UseStartup<Startup>();
+	})
+	.ConfigureServices(services => services.AddHostedService<DdInfoDiscordBotService>());
+
+IHost app = builder.Build();
+app.Run();
