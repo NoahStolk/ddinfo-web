82a910ef6550cb5060575de739a2b461042e3acb
Noah Stolk <noah.stolk@gmail.com>
2021-12-10 18:55:32 +0100

    Format

--- DevilDaggersInfo.Web.BlazorWasm.Client/Utils/MarkupUtils.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Utils/MarkupUtils.cs
@@ -18,28 +18,28 @@ public static class MarkupUtils
 	private const float _lineThickness = 2;
 
 	public static readonly MarkupString NavStart = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <rect width='{_lineThickness}' height='{_size}' x='0' y='{_a}' style='fill: {_fillStyle};' />
-  <polygon points='{_a},{_m} {_m},{_a} {_m},{_b}' style='fill: {_fillStyle};' />
-  <polygon points='{_m},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
+	<rect width='{_lineThickness}' height='{_size}' x='0' y='{_a}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_m} {_m},{_a} {_m},{_b}' style='fill: {_fillStyle};' />
+	<polygon points='{_m},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
 </svg>");
 	public static readonly MarkupString NavPrevDouble = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <polygon points='{_a},{_m} {_m},{_a} {_m},{_b}' style='fill: {_fillStyle};' />
-  <polygon points='{_m},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_m} {_m},{_a} {_m},{_b}' style='fill: {_fillStyle};' />
+	<polygon points='{_m},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
 </svg>");
 	public static readonly MarkupString NavPrev = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <polygon points='{_a},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_m} {_b},{_a} {_b},{_b}' style='fill: {_fillStyle};' />
 </svg>");
 	public static readonly MarkupString NavNext = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <polygon points='{_a},{_a} {_a},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_a} {_a},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
 </svg>");
 	public static readonly MarkupString NavNextDouble = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <polygon points='{_a},{_a} {_a},{_b} {_m},{_m}' style='fill: {_fillStyle};' />
-  <polygon points='{_m},{_a} {_m},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_a} {_a},{_b} {_m},{_m}' style='fill: {_fillStyle};' />
+	<polygon points='{_m},{_a} {_m},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
 </svg>");
 	public static readonly MarkupString NavEnd = new($@"<svg style='margin: {_margin}px;' width='{_size}' height='{_size}'>
-  <polygon points='{_a},{_a} {_a},{_b} {_m},{_m}' style='fill: {_fillStyle};' />
-  <polygon points='{_m},{_a} {_m},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
-  <rect width='{_lineThickness}' height='{_size}' x='{_size - _lineThickness}' y='{_a}' style='fill: {_fillStyle};' />
+	<polygon points='{_a},{_a} {_a},{_b} {_m},{_m}' style='fill: {_fillStyle};' />
+	<polygon points='{_m},{_a} {_m},{_b} {_b},{_m}' style='fill: {_fillStyle};' />
+	<rect width='{_lineThickness}' height='{_size}' x='{_size - _lineThickness}' y='{_a}' style='fill: {_fillStyle};' />
 </svg>");
 
 	public static readonly MarkupString NoDataMarkup = new(@"<span style=""color: #666;"">N/A</span>");
