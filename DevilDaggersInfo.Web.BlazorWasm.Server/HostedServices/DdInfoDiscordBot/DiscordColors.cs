ba6b7679ca72eba9ee634234ced4e75cf932e171
Noah Stolk <noah.stolk@gmail.com>
2021-12-10 13:05:15 +0100

    Use V3.2 by default

--- DevilDaggersInfo.Web.BlazorWasm.Server/HostedServices/DdInfoDiscordBot/DiscordColors.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/HostedServices/DdInfoDiscordBot/DiscordColors.cs
@@ -4,12 +4,12 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Server.HostedServices.DdInfoDiscordBot
 
 public static class DiscordColors
 {
-	public static DiscordColor Default { get; } = ToDiscordColor(DaggersV3_1.Default.Color);
-	public static DiscordColor Bronze { get; } = ToDiscordColor(DaggersV3_1.Bronze.Color);
-	public static DiscordColor Silver { get; } = ToDiscordColor(DaggersV3_1.Silver.Color);
-	public static DiscordColor Golden { get; } = ToDiscordColor(DaggersV3_1.Golden.Color);
-	public static DiscordColor Devil { get; } = ToDiscordColor(DaggersV3_1.Devil.Color);
-	public static DiscordColor Leviathan { get; } = ToDiscordColor(DaggersV3_1.Leviathan.Color);
+	public static DiscordColor Default { get; } = ToDiscordColor(DaggersV3_2.Default.Color);
+	public static DiscordColor Bronze { get; } = ToDiscordColor(DaggersV3_2.Bronze.Color);
+	public static DiscordColor Silver { get; } = ToDiscordColor(DaggersV3_2.Silver.Color);
+	public static DiscordColor Golden { get; } = ToDiscordColor(DaggersV3_2.Golden.Color);
+	public static DiscordColor Devil { get; } = ToDiscordColor(DaggersV3_2.Devil.Color);
+	public static DiscordColor Leviathan { get; } = ToDiscordColor(DaggersV3_2.Leviathan.Color);
 
 	private static DiscordColor ToDiscordColor(Color color) => new(color.R, color.B, color.G);
 }
