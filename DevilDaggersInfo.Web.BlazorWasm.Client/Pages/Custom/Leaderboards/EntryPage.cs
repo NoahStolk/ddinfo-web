630814882fd786cd33e01dfd3e3245aeb356b0bc
Noah Stolk <noah.stolk@gmail.com>
2021-12-17 02:00:27 +0100

    Add all charts

--- DevilDaggersInfo.Web.BlazorWasm.Client/Pages/Custom/Leaderboards/EntryPage.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Pages/Custom/Leaderboards/EntryPage.cs
@@ -89,6 +89,63 @@ public partial class EntryPage
 		AddDataSet(skullsAliveSets, GetCustomEntryData.Skull3sAliveData, EnemyColors.Skull3.HexCode);
 		AddDataSet(skullsAliveSets, GetCustomEntryData.Skull4sAliveData, EnemyColors.Skull4.HexCode);
 		AddDataSets(skullsAliveSets, GetCustomEntryData.Time, "Skulls Alive", "Skull Is Alive", "Skull IIs Alive", "Skull IIIs Alive", "Skull IVs Alive");
+
+		List<LineDataSet> squidsAliveSets = new();
+		AddDataSet(squidsAliveSets, GetCustomEntryData.Squid1sAliveData, EnemyColors.Squid1.HexCode);
+		AddDataSet(squidsAliveSets, GetCustomEntryData.Squid2sAliveData, EnemyColors.Squid2.HexCode);
+		AddDataSet(squidsAliveSets, GetCustomEntryData.Squid3sAliveData, EnemyColors.Squid3.HexCode);
+		AddDataSets(squidsAliveSets, GetCustomEntryData.Time, "Squids Alive", "Squid Is Alive", "Squid IIs Alive", "Squid IIIs Alive");
+
+		List<LineDataSet> spidersAliveSets = new();
+		AddDataSet(spidersAliveSets, GetCustomEntryData.Spider1sAliveData, EnemyColors.Spider1.HexCode);
+		AddDataSet(spidersAliveSets, GetCustomEntryData.Spider2sAliveData, EnemyColors.Spider2.HexCode);
+		AddDataSet(spidersAliveSets, GetCustomEntryData.SpiderEggsAliveData, EnemyColors.SpiderEgg1.HexCode);
+		AddDataSet(spidersAliveSets, GetCustomEntryData.SpiderlingsAliveData, EnemyColors.Spiderling.HexCode);
+		AddDataSets(spidersAliveSets, GetCustomEntryData.Time, "Spiders Alive", "Spider Is Alive", "Spider IIs Alive", "Spider Eggs Alive", "Spiderlings Alive");
+
+		List<LineDataSet> pedesAliveSets = new();
+		AddDataSet(pedesAliveSets, GetCustomEntryData.CentipedesAliveData, EnemyColors.Centipede.HexCode);
+		AddDataSet(pedesAliveSets, GetCustomEntryData.GigapedesAliveData, EnemyColors.Gigapede.HexCode);
+		AddDataSet(pedesAliveSets, GetCustomEntryData.GhostpedesAliveData, EnemyColors.Ghostpede.HexCode);
+		AddDataSets(pedesAliveSets, GetCustomEntryData.Time, "Pedes Alive", "Centipedes Alive", "Gigapedes Alive", "Ghostpedes Alive");
+
+		List<LineDataSet> otherEnemiesAliveSets = new();
+		AddDataSet(otherEnemiesAliveSets, GetCustomEntryData.ThornsAliveData, EnemyColors.Thorn.HexCode);
+		AddDataSet(otherEnemiesAliveSets, GetCustomEntryData.LeviathansAliveData, EnemyColors.Leviathan.HexCode);
+		AddDataSet(otherEnemiesAliveSets, GetCustomEntryData.OrbsAliveData, EnemyColors.TheOrb.HexCode);
+		AddDataSets(otherEnemiesAliveSets, GetCustomEntryData.Time, "Other Enemies Alive", "Thorns Alive", "Leviathans Alive", "Orbs Alive");
+
+		List<LineDataSet> skullsKilledSets = new();
+		AddDataSet(skullsKilledSets, GetCustomEntryData.Skull1sKilledData, EnemyColors.Skull1.HexCode);
+		AddDataSet(skullsKilledSets, GetCustomEntryData.Skull2sKilledData, EnemyColors.Skull2.HexCode);
+		AddDataSet(skullsKilledSets, GetCustomEntryData.Skull3sKilledData, EnemyColors.Skull3.HexCode);
+		AddDataSet(skullsKilledSets, GetCustomEntryData.Skull4sKilledData, EnemyColors.Skull4.HexCode);
+		AddDataSets(skullsKilledSets, GetCustomEntryData.Time, "Skulls Killed", "Skull Is Killed", "Skull IIs Killed", "Skull IIIs Killed", "Skull IVs Killed");
+
+		List<LineDataSet> squidsKilledSets = new();
+		AddDataSet(squidsKilledSets, GetCustomEntryData.Squid1sKilledData, EnemyColors.Squid1.HexCode);
+		AddDataSet(squidsKilledSets, GetCustomEntryData.Squid2sKilledData, EnemyColors.Squid2.HexCode);
+		AddDataSet(squidsKilledSets, GetCustomEntryData.Squid3sKilledData, EnemyColors.Squid3.HexCode);
+		AddDataSets(squidsKilledSets, GetCustomEntryData.Time, "Squids Killed", "Squid Is Killed", "Squid IIs Killed", "Squid IIIs Killed");
+
+		List<LineDataSet> spidersKilledSets = new();
+		AddDataSet(spidersKilledSets, GetCustomEntryData.Spider1sKilledData, EnemyColors.Spider1.HexCode);
+		AddDataSet(spidersKilledSets, GetCustomEntryData.Spider2sKilledData, EnemyColors.Spider2.HexCode);
+		AddDataSet(spidersKilledSets, GetCustomEntryData.SpiderEggsKilledData, EnemyColors.SpiderEgg1.HexCode);
+		AddDataSet(spidersKilledSets, GetCustomEntryData.SpiderlingsKilledData, EnemyColors.Spiderling.HexCode);
+		AddDataSets(spidersKilledSets, GetCustomEntryData.Time, "Spiders Killed", "Spider Is Killed", "Spider IIs Killed", "Spider Eggs Killed", "Spiderlings Killed");
+
+		List<LineDataSet> pedesKilledSets = new();
+		AddDataSet(pedesKilledSets, GetCustomEntryData.CentipedesKilledData, EnemyColors.Centipede.HexCode);
+		AddDataSet(pedesKilledSets, GetCustomEntryData.GigapedesKilledData, EnemyColors.Gigapede.HexCode);
+		AddDataSet(pedesKilledSets, GetCustomEntryData.GhostpedesKilledData, EnemyColors.Ghostpede.HexCode);
+		AddDataSets(pedesKilledSets, GetCustomEntryData.Time, "Pedes Killed", "Centipedes Killed", "Gigapedes Killed", "Ghostpedes Killed");
+
+		List<LineDataSet> otherEnemiesKilledSets = new();
+		AddDataSet(otherEnemiesKilledSets, GetCustomEntryData.ThornsKilledData, EnemyColors.Thorn.HexCode);
+		AddDataSet(otherEnemiesKilledSets, GetCustomEntryData.LeviathansKilledData, EnemyColors.Leviathan.HexCode);
+		AddDataSet(otherEnemiesKilledSets, GetCustomEntryData.OrbsKilledData, EnemyColors.TheOrb.HexCode);
+		AddDataSets(otherEnemiesKilledSets, GetCustomEntryData.Time, "Other Enemies Killed", "Thorns Killed", "Leviathans Killed", "Orbs Killed");
 	}
 
 	protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -96,49 +153,4 @@ public partial class EntryPage
 		if (firstRender)
 			await JsRuntime.InvokeAsync<object>("init");
 	}
-	//await AddLabelsDatasets(_skullsAliveChart,
-	//	new("Skull Is Alive", GetCustomEntryData.Skull1sAliveData, EnemyColors.Skull1.HexCode, EnemyColors.Skull1.HexCode),
-	//	new("Skull IIs Alive", GetCustomEntryData.Skull2sAliveData, EnemyColors.Skull2.HexCode, EnemyColors.Skull2.HexCode),
-	//	new("Skull IIIs Alive", GetCustomEntryData.Skull3sAliveData, EnemyColors.Skull3.HexCode, EnemyColors.Skull3.HexCode),
-	//	new("Skull IVs Alive", GetCustomEntryData.Skull4sAliveData, EnemyColors.Skull4.HexCode, EnemyColors.Skull4.HexCode));
-	//await AddLabelsDatasets(_squidsAliveChart,
-	//	new("Squid Is Alive", GetCustomEntryData.Squid1sAliveData, EnemyColors.Squid1.HexCode, EnemyColors.Squid1.HexCode),
-	//	new("Squid IIs Alive", GetCustomEntryData.Squid2sAliveData, EnemyColors.Squid2.HexCode, EnemyColors.Squid2.HexCode),
-	//	new("Squid IIIs Alive", GetCustomEntryData.Squid3sAliveData, EnemyColors.Squid3.HexCode, EnemyColors.Squid3.HexCode));
-	//await AddLabelsDatasets(_spidersAliveChart,
-	//	new("Spider Is Alive", GetCustomEntryData.Spider1sAliveData, EnemyColors.Spider1.HexCode, EnemyColors.Spider1.HexCode),
-	//	new("Spider IIs Alive", GetCustomEntryData.Spider2sAliveData, EnemyColors.Spider2.HexCode, EnemyColors.Spider2.HexCode),
-	//	new("Spider Eggs Alive", GetCustomEntryData.SpiderEggsAliveData, EnemyColors.SpiderEgg1.HexCode, EnemyColors.SpiderEgg1.HexCode),
-	//	new("Spiderlings Alive", GetCustomEntryData.SpiderlingsAliveData, EnemyColors.Spiderling.HexCode, EnemyColors.Spiderling.HexCode));
-	//await AddLabelsDatasets(_pedesAliveChart,
-	//	new("Centipedes Alive", GetCustomEntryData.CentipedesAliveData, EnemyColors.Centipede.HexCode, EnemyColors.Centipede.HexCode),
-	//	new("Gigapedes Alive", GetCustomEntryData.GigapedesAliveData, EnemyColors.Gigapede.HexCode, EnemyColors.Gigapede.HexCode),
-	//	new("Ghostpedes Alive", GetCustomEntryData.GhostpedesAliveData, EnemyColors.Ghostpede.HexCode, EnemyColors.Ghostpede.HexCode));
-	//await AddLabelsDatasets(_otherEnemiesAliveChart,
-	//	new("Thorns Alive", GetCustomEntryData.ThornsAliveData, EnemyColors.Thorn.HexCode, EnemyColors.Thorn.HexCode),
-	//	new("Leviathans Alive", GetCustomEntryData.LeviathansAliveData, EnemyColors.Leviathan.HexCode, EnemyColors.Leviathan.HexCode),
-	//	new("Orbs Alive", GetCustomEntryData.OrbsAliveData, EnemyColors.TheOrb.HexCode, EnemyColors.TheOrb.HexCode));
-
-	//await AddLabelsDatasets(_skullsKilledChart,
-	//	new("Skull Is Killed", GetCustomEntryData.Skull1sKilledData, EnemyColors.Skull1.HexCode, EnemyColors.Skull1.HexCode),
-	//	new("Skull IIs Killed", GetCustomEntryData.Skull2sKilledData, EnemyColors.Skull2.HexCode, EnemyColors.Skull2.HexCode),
-	//	new("Skull IIIs Killed", GetCustomEntryData.Skull3sKilledData, EnemyColors.Skull3.HexCode, EnemyColors.Skull3.HexCode),
-	//	new("Skull IVs Killed", GetCustomEntryData.Skull4sKilledData, EnemyColors.Skull4.HexCode, EnemyColors.Skull4.HexCode));
-	//await AddLabelsDatasets(_squidsKilledChart,
-	//	new("Squid Is Killed", GetCustomEntryData.Squid1sKilledData, EnemyColors.Squid1.HexCode, EnemyColors.Squid1.HexCode),
-	//	new("Squid IIs Killed", GetCustomEntryData.Squid2sKilledData, EnemyColors.Squid2.HexCode, EnemyColors.Squid2.HexCode),
-	//	new("Squid IIIs Killed", GetCustomEntryData.Squid3sKilledData, EnemyColors.Squid3.HexCode, EnemyColors.Squid3.HexCode));
-	//await AddLabelsDatasets(_spidersKilledChart,
-	//	new("Spider Is Killed", GetCustomEntryData.Spider1sKilledData, EnemyColors.Spider1.HexCode, EnemyColors.Spider1.HexCode),
-	//	new("Spider IIs Killed", GetCustomEntryData.Spider2sKilledData, EnemyColors.Spider2.HexCode, EnemyColors.Spider2.HexCode),
-	//	new("Spider Eggs Killed", GetCustomEntryData.SpiderEggsKilledData, EnemyColors.SpiderEgg1.HexCode, EnemyColors.SpiderEgg1.HexCode),
-	//	new("Spiderlings Killed", GetCustomEntryData.SpiderlingsKilledData, EnemyColors.Spiderling.HexCode, EnemyColors.Spiderling.HexCode));
-	//await AddLabelsDatasets(_pedesKilledChart,
-	//	new("Centipedes Killed", GetCustomEntryData.CentipedesKilledData, EnemyColors.Centipede.HexCode, EnemyColors.Centipede.HexCode),
-	//	new("Gigapedes Killed", GetCustomEntryData.GigapedesKilledData, EnemyColors.Gigapede.HexCode, EnemyColors.Gigapede.HexCode),
-	//	new("Ghostpedes Killed", GetCustomEntryData.GhostpedesKilledData, EnemyColors.Ghostpede.HexCode, EnemyColors.Ghostpede.HexCode));
-	//await AddLabelsDatasets(_otherEnemiesKilledChart,
-	//	new("Thorns Killed", GetCustomEntryData.ThornsKilledData, EnemyColors.Thorn.HexCode, EnemyColors.Thorn.HexCode),
-	//	new("Leviathans Killed", GetCustomEntryData.LeviathansKilledData, EnemyColors.Leviathan.HexCode, EnemyColors.Leviathan.HexCode),
-	//	new("Orbs Killed", GetCustomEntryData.OrbsKilledData, EnemyColors.TheOrb.HexCode, EnemyColors.TheOrb.HexCode));
 }
