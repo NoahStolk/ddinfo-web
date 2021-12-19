97647f34a5cdee1d4c5d6835a3a262edd60ce4cd
Noah Stolk <noah.stolk@gmail.com>
2021-12-19 16:30:06 +0100

    Improve sorting for world records

--- DevilDaggersInfo.Web.BlazorWasm.Client.Core.CanvasChart/Options/LineChart/LineChartOptions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client.Core.CanvasChart/Options/LineChart/LineChartOptions.cs
@@ -2,8 +2,8 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Client.Core.CanvasChart.Options.LineCh
 
 public class LineChartOptions
 {
-	public string CanvasBackgroundColor { get; set; } = "#080808";
-	public string ChartBackgroundColor { get; set; } = "#0b0b0b";
+	public string CanvasBackgroundColor { get; set; } = "#040404";
+	public string ChartBackgroundColor { get; set; } = "#080808";
 
 	public double ChartMarginXInPx { get; set; } = 40;
 	public double ChartMarginYInPx { get; set; } = 40;
