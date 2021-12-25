2ae37a96526f2dc38887dd3cc584f9eda3c43da2
Noah Stolk <noah.stolk@gmail.com>
2021-12-25 11:55:01 +0100

    Delete ModScreenshotsController

--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Admin/ModScreenshots/AddModScreenshot.cs
+++
@@ -1,9 +0,0 @@
-namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Admin.ModScreenshots;
-
-public class AddModScreenshot
-{
-	public string ModName { get; set; } = null!;
-
-	[MaxLength(ModScreenshotConstants.MaxFileSize, ErrorMessage = ModScreenshotConstants.MaxFileSizeErrorMessage)]
-	public byte[] FileContents { get; set; } = null!;
-}
--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Admin/ModScreenshots/DeleteModScreenshot.cs
+++
@@ -1,8 +0,0 @@
-namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Admin.ModScreenshots;
-
-public class DeleteModScreenshot
-{
-	public string ModName { get; set; } = null!;
-
-	public string ScreenshotName { get; set; } = null!;
-}
