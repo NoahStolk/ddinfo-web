99d54d7999a701546abc7329f1884b1ef34b49f8
Noah Stolk <noah.stolk@gmail.com>
2022-01-01 23:30:27 +0100

    Apply Leviathan background offset to times

--- DevilDaggersInfo.Web.BlazorWasm.Client/Utils/LeviathanAnimationUtils.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Utils/LeviathanAnimationUtils.cs
@@ -2,7 +2,9 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Client.Utils;
 
 public static class LeviathanAnimationUtils
 {
-	public static int GetOffset(int total, int index) => (total - index) * 300;
-
-	public static string GetStyle(int offset) => $"animation-delay: -{offset}ms;";
+	public static string? GetStyle(int total, int index)
+	{
+		int offset = (total - index) * 300;
+		return offset == 0 ? null : $"animation-delay: -{offset}ms;";
+	}
 }
