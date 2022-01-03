3a7af47a36d615ca63fbfe9978492ba6cd14b57d
Noah Stolk <noah.stolk@gmail.com>
2022-01-03 22:51:53 +0100

    Remove unused redirects

--- DevilDaggersInfo.Web.BlazorWasm.Server/Startup.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Startup.cs
@@ -153,8 +153,6 @@ public class Startup
 			.AddRedirect("Tools/DevilDaggersAssetEditor", "tools/asset-editor")
 			.AddRedirect("Tools/DevilDaggersCustomLeaderboards", "tools/custom-leaderboards")
 			.AddRedirect("Tools/DevilDaggersSurvivalEditor", "tools/survival-editor")
-			.AddRedirect("Wiki/Guides/SpawnsetGuide$", "guides/survival-editor")
-			.AddRedirect("Wiki/Guides/AssetGuide$", "guides/asset-editor")
 			.AddRedirect("Wiki/Guides/SurvivalEditor$", "guides/survival-editor")
 			.AddRedirect("Wiki/Guides/AssetEditor$", "guides/asset-editor")
 
