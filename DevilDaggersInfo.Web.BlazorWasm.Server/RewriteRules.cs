d9a52178ba84a5fb1e38979f0a18c4fa15ec6b7d
Noah Stolk <noah.stolk@gmail.com>
2022-01-03 22:37:14 +0100

    Use absolute header location

--- DevilDaggersInfo.Web.BlazorWasm.Server/RewriteRules.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/RewriteRules.cs
@@ -16,7 +16,9 @@ public class RewriteRules : IRule
 			request.Path.Value.EndsWith(".css") ||
 			request.Path.Value.EndsWith(".json") ||
 			request.Path.Value.EndsWith(".png") ||
-			request.Path.Value.EndsWith(".ttf"))
+			request.Path.Value.EndsWith(".ttf") ||
+			request.Path.Value.EndsWith(".ico") ||
+			request.Path.Value.EndsWith(".dll"))
 		{
 			return;
 		}
@@ -31,10 +33,9 @@ public class RewriteRules : IRule
 		if (!int.TryParse(request.QueryString.Value.TrimStart(queryStringKey), out int id))
 			return;
 
-		HttpResponse response = context.HttpContext.Response;
-		//response.StatusCode = StatusCodes.Status301MovedPermanently;
-		 context.Result = RuleResult.EndResponse;
+		context.Result = RuleResult.EndResponse;
 
-		response.Headers[HeaderNames.Location] = $"./leaderboard/player/{id}";
+		context.HttpContext.Response.StatusCode = StatusCodes.Status301MovedPermanently;
+		context.HttpContext.Response.Headers[HeaderNames.Location] = $"{request.Scheme}://{request.Host}/leaderboard/player/{id}";
 	}
 }
