96e5229d90733839f760ec8766eeafa9069b5168
Noah Stolk <noah.stolk@gmail.com>
2022-01-19 22:36:59 +0100

    Add highlighter for all bar charts

--- DevilDaggersInfo.Web.BlazorWasm.Client/Pages/Leaderboard/StatisticsPage.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Pages/Leaderboard/StatisticsPage.cs
@@ -9,8 +9,13 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Client.Pages.Leaderboard;
 
 public partial class StatisticsPage
 {
-	private static BarChartOptions _barChartOptions = new() { ChartMarginXInPx = 60, HighlighterKeys = new() { "Score range", "Players" }, HighlighterWidth = 320 };
-	private static BarChartOptions _textBarChartOptions = new() { ChartMarginXInPx = 60, ChartMarginYInPx = 80, HighlighterKeys = new() { "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _scoreBarChartOptions = new() { ChartMarginXInPx = 60, HighlighterKeys = new() { "Score range", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _killsBarChartOptions = new() { ChartMarginXInPx = 60, HighlighterKeys = new() { "Kills range", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _gemsBarChartOptions = new() { ChartMarginXInPx = 60, HighlighterKeys = new() { "Gems range", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _upgradesBarChartOptions = new() { ChartMarginXInPx = 60, ChartMarginYInPx = 80, HighlighterKeys = new() { "Upgrade", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _daggersBarChartOptions = new() { ChartMarginXInPx = 60, ChartMarginYInPx = 80, HighlighterKeys = new() { "Dagger", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _deathsBarChartOptions = new() { ChartMarginXInPx = 60, ChartMarginYInPx = 80, HighlighterKeys = new() { "Death type", "Players" }, HighlighterWidth = 320 };
+	private static BarChartOptions _enemiesBarChartOptions = new() { ChartMarginXInPx = 60, ChartMarginYInPx = 80, HighlighterKeys = new() { "Enemy", "Players" }, HighlighterWidth = 320 };
 
 	private static readonly List<string> _daggers = Daggers.GetDaggers(GameConstants.CurrentVersion).ConvertAll(d => d.Name);
 	private static readonly List<string> _deathTypes = Deaths.GetDeaths(GameConstants.CurrentVersion).ConvertAll(d => d.Name);
@@ -69,7 +74,7 @@ public partial class StatisticsPage
 				return new()
 				{
 					new($"<span style='text-align: right;'>{start.ToString(FormatUtils.TimeFormat)} - {(start + 9.9999).ToString(FormatUtils.TimeFormat)}</span>"),
-					new($"<span style='color: {barData.Color}; text-align: right;'>{barData.Y.ToString("0")}</span>"),
+					new($"<span style='text-align: right;'>{barData.Y.ToString("0")}</span>"),
 				};
 			});
 		}
@@ -77,17 +82,29 @@ public partial class StatisticsPage
 		List<BarData> killsSet = _statistics.KillsStatistics.Where(kvp => kvp.Key < 1000).Select(kvp => new BarData("#880", kvp.Value, kvp)).ToList();
 		const double killsScale = 2500.0;
 		_killsDataOptions = new(0, killsScale, Math.Ceiling(_statistics.KillsStatistics.Max(kvp => kvp.Value) / killsScale) * killsScale);
-		_killsData = new(killsSet, (ds, d) =>
+		_killsData = new(killsSet, (ds, i) =>
 		{
-			return new();
+			BarData barData = ds.Data[i];
+			int start = _statistics.KillsStatistics.ElementAt(i).Key;
+			return new()
+			{
+				new($"<span style='text-align: right;'>{start.ToString("0")} - {(start + 9).ToString("0")}</span>"),
+				new($"<span style='text-align: right;'>{barData.Y.ToString("0")}</span>"),
+			};
 		});
 
 		List<BarData> gemsSet = _statistics.GemsStatistics.Where(kvp => kvp.Key < 500).Select(kvp => new BarData("#f00", kvp.Value, kvp)).ToList();
 		const double gemsScale = 20000.0;
 		_gemsDataOptions = new(0, gemsScale, Math.Ceiling(_statistics.GemsStatistics.Max(kvp => kvp.Value) / gemsScale) * gemsScale);
-		_gemsData = new(gemsSet, (ds, d) =>
+		_gemsData = new(gemsSet, (ds, i) =>
 		{
-			return new();
+			BarData barData = ds.Data[i];
+			int start = _statistics.GemsStatistics.ElementAt(i).Key;
+			return new()
+			{
+				new($"<span style='text-align: right;'>{start.ToString("0")} - {(start + 9).ToString("0")}</span>"),
+				new($"<span style='text-align: right;'>{barData.Y.ToString("0")}</span>"),
+			};
 		});
 
 		List<BarData> upgradesSet = new()
@@ -98,33 +115,49 @@ public partial class StatisticsPage
 		};
 		const double upgradesScale = 50000.0;
 		_upgradesDataOptions = new(0, upgradesScale, Math.Ceiling(new int[] { _statistics.PlayersWithLevel1, _statistics.PlayersWithLevel2, _statistics.PlayersWithLevel3Or4 }.Max() / upgradesScale) * upgradesScale);
-		_upgradesData = new(upgradesSet, (ds, d) =>
+		_upgradesData = new(upgradesSet, (ds, i) =>
 		{
-			return new();
+			return new()
+			{
+				new($"<span style='text-align: right;'>{_upgrades[i]}</span>"),
+				new($"<span style='text-align: right;'>{ds.Data[i].Y.ToString("0")}</span>"),
+			};
 		});
 
 		List<BarData> daggersSet = _statistics.DaggersStatistics.Select(kvp => new BarData(Daggers.GetDaggerByName(GameConstants.CurrentVersion, kvp.Key)?.Color.HexCode ?? "#444", kvp.Value, kvp)).ToList();
 		const double daggersScale = 20000.0;
 		_daggersDataOptions = new(0, daggersScale, Math.Ceiling(_statistics.DaggersStatistics.Max(kvp => kvp.Value) / daggersScale) * daggersScale);
-		_daggersData = new(daggersSet, (ds, d) =>
+		_daggersData = new(daggersSet, (ds, i) =>
 		{
-			return new();
+			return new()
+			{
+				new($"<span style='text-align: right;'>{_daggers[i]}</span>"),
+				new($"<span style='text-align: right;'>{ds.Data[i].Y.ToString("0")}</span>"),
+			};
 		});
 
 		List<BarData> deathsSet = _statistics.DeathsStatistics.Select(kvp => new BarData(Deaths.GetDeathByName(GameConstants.CurrentVersion, kvp.Key)?.Color.HexCode ?? "#444", kvp.Value, kvp)).ToList();
 		const double deathsScale = 20000.0;
 		_deathsDataOptions = new(0, deathsScale, Math.Ceiling(_statistics.DeathsStatistics.Max(kvp => kvp.Value) / deathsScale) * deathsScale);
-		_deathsData = new(deathsSet, (ds, d) =>
+		_deathsData = new(deathsSet, (ds, i) =>
 		{
-			return new();
+			return new()
+			{
+				new($"<span style='text-align: right;'>{_deathTypes[i]}</span>"),
+				new($"<span style='text-align: right;'>{ds.Data[i].Y.ToString("0")}</span>"),
+			};
 		});
 
 		List<BarData> enemiesSet = _statistics.EnemiesStatistics.Select(kvp => new BarData(Enemies.GetEnemyByName(GameConstants.CurrentVersion, kvp.Key)?.Color.HexCode ?? "#444", kvp.Value, kvp)).ToList();
 		const double enemiesScale = 50000.0;
 		_enemiesDataOptions = new(0, enemiesScale, Math.Ceiling(_statistics.EnemiesStatistics.Max(kvp => kvp.Value) / enemiesScale) * enemiesScale);
-		_enemiesData = new(enemiesSet, (ds, d) =>
+		_enemiesData = new(enemiesSet, (ds, i) =>
 		{
-			return new();
+			return new()
+			{
+				new($"<span style='text-align: right;'>{_enemies[i]}</span>"),
+				new($"<span style='text-align: right;'>{ds.Data[i].Y.ToString("0")}</span>"),
+			};
 		});
 	}
 
