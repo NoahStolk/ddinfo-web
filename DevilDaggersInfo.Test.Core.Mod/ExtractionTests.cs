0a7c094d13f4116a45ffab09da200720c4c86046
Noah Stolk <noah.stolk@gmail.com>
2022-02-08 22:21:57 +0100

    Expand ExtractTextureAndCompareToSourcePng unit test

--- DevilDaggersInfo.Test.Core.Mod/ExtractionTests.cs
+++ DevilDaggersInfo.Test.Core.Mod/ExtractionTests.cs
@@ -3,16 +3,18 @@ namespace DevilDaggersInfo.Test.Core.Mod;
 [TestClass]
 public class ExtractionTests
 {
-	[TestMethod]
-	public void ExtractTexture()
+	[DataTestMethod]
+	[DataRow("dd-texture", "pedeblackbody", "pedeblackbody.png")]
+	[DataRow("dd-iconmaskhoming", "iconmaskhoming", "iconmaskhoming.png")]
+	public void ExtractTextureAndCompareToSourcePng(string modFileName, string assetName, string sourcePngFileName)
 	{
-		const string modFileName = "dd-texture";
 		string filePath = Path.Combine(TestUtils.ResourcePath, modFileName);
 		ModBinary modBinary = new(modFileName, File.ReadAllBytes(filePath), BinaryReadComprehensiveness.All);
-		KeyValuePair<ModBinaryChunk, AssetData> asset = modBinary.AssetMap!.First(kvp => kvp.Key.Name == "pedeblackbody");
+		Assert.IsNotNull(modBinary.AssetMap);
+		KeyValuePair<ModBinaryChunk, AssetData> asset = modBinary.AssetMap.First(kvp => kvp.Key.Name == assetName);
 
 		byte[] extractedPng = AssetConverter.Extract(asset.Key.AssetType, asset.Value);
-		byte[] pngContents = File.ReadAllBytes(Path.Combine(TestUtils.ResourcePath, "pedeblackbody.png"));
+		byte[] pngContents = File.ReadAllBytes(Path.Combine(TestUtils.ResourcePath, sourcePngFileName));
 
 		TestUtils.AssertArrayContentsEqual(extractedPng, pngContents);
 	}
