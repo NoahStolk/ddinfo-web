e0fe70e5a8b7df26dbd0e8ecc0a840545beeda02
Noah Stolk <noah.stolk@gmail.com>
2022-02-09 21:18:51 +0100

    Add ValidateTocSingleAsset unit test

--- DevilDaggersInfo.Test.Core.Mod/ModBinaryTests.cs
+++ DevilDaggersInfo.Test.Core.Mod/ModBinaryTests.cs
@@ -18,4 +18,18 @@ public class ModBinaryTests
 
 		TestUtils.AssertArrayContentsEqual(originalBytes, bytes);
 	}
+
+	[TestMethod]
+	public void ValidateTocSingleAsset()
+	{
+		const string fileName = "dd-maken-main";
+		string filePath = Path.Combine(TestUtils.ResourcePath, fileName);
+		byte[] originalBytes = File.ReadAllBytes(filePath);
+		ModBinary modBinary = new(fileName, originalBytes, BinaryReadComprehensiveness.TocOnly);
+
+		ModBinaryChunk chunk = modBinary.Chunks[0];
+		Assert.AreEqual("dagger6", chunk.Name);
+		Assert.AreEqual(AssetType.Texture, chunk.AssetType);
+		Assert.AreEqual(21855, chunk.Size);
+	}
 }
