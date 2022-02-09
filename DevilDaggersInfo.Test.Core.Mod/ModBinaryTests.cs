a4b2c9263e9b9e6cc77b2a267e678ce6291711ef
Noah Stolk <noah.stolk@gmail.com>
2022-02-09 21:31:37 +0100

    Add ValidateTocMultipleAssets unit test

--- DevilDaggersInfo.Test.Core.Mod/ModBinaryTests.cs
+++ DevilDaggersInfo.Test.Core.Mod/ModBinaryTests.cs
@@ -22,14 +22,36 @@ public class ModBinaryTests
 	[TestMethod]
 	public void ValidateTocSingleAsset()
 	{
-		const string fileName = "dd-maken-main";
+		const string fileName = "dd-maken";
 		string filePath = Path.Combine(TestUtils.ResourcePath, fileName);
 		byte[] originalBytes = File.ReadAllBytes(filePath);
 		ModBinary modBinary = new(fileName, originalBytes, BinaryReadComprehensiveness.TocOnly);
 
+		Assert.AreEqual(1, modBinary.Chunks.Count);
 		ModBinaryChunk chunk = modBinary.Chunks[0];
 		Assert.AreEqual("dagger6", chunk.Name);
 		Assert.AreEqual(AssetType.Texture, chunk.AssetType);
 		Assert.AreEqual(21855, chunk.Size);
+	}
+
+	[TestMethod]
+	public void ValidateTocMultipleAssets()
+	{
+		const string fileName = "dd-nohand";
+		string filePath = Path.Combine(TestUtils.ResourcePath, fileName);
+		byte[] originalBytes = File.ReadAllBytes(filePath);
+		ModBinary modBinary = new(fileName, originalBytes, BinaryReadComprehensiveness.TocOnly);
+
+		Assert.AreEqual(8, modBinary.Chunks.Count);
+
+		string[] names = new[] { "hand", "hand2", "hand2left", "hand3", "hand3left", "hand4", "hand4left", "handleft" };
+		int[] sizes = new[] { 166, 166, 198, 166, 198, 262, 390, 198 };
+		for (int i = 0; i < 8; i++)
+		{
+			ModBinaryChunk chunk = modBinary.Chunks[i];
+			Assert.AreEqual(names[i], chunk.Name);
+			Assert.AreEqual(AssetType.Model, chunk.AssetType);
+			Assert.AreEqual(sizes[i], chunk.Size);
+		}
 	}
 }
