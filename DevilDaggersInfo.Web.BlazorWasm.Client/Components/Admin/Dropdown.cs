5495b2728ab45c35b2e8b433faefeff264d17db2
Noah Stolk <noah.stolk@gmail.com>
2022-02-25 21:44:21 +0100

    Implement Dropdown component

---
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Components/Admin/Dropdown.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DevilDaggersInfo.Web.BlazorWasm.Client.Components.Admin;
+
+public partial class Dropdown<TKey>
+	where TKey : notnull
+{
+	private string SelectedDisplayValue => CurrentValue == null || Values?.ContainsKey(CurrentValue) != true ? "<None selected>" : DisplayValue(CurrentValue);
+
+	[Parameter, EditorRequired] public Dictionary<TKey, string>? Values { get; set; }
+	[Parameter, EditorRequired] public Func<string?, TKey> Converter { get; set; } = null!;
+	[Parameter] public bool ShowDisplayValue { get; set; } = true;
+
+	private bool _show = false;
+
+	public void HandleSelect(TKey? item)
+	{
+		CurrentValue = item;
+		_show = false;
+		StateHasChanged();
+	}
+
+	protected override bool TryParseValueFromString(string? value, out TKey? result, out string validationMessage)
+	{
+		validationMessage = string.Empty;
+		result = Converter(value);
+		return true;
+	}
+
+	private string DisplayValue(TKey? key)
+	{
+		if (key == null || Values == null)
+			return string.Empty;
+
+		if (!ShowDisplayValue)
+			return key.ToString() ?? string.Empty;
+
+		return $"{key} ({Values[key]})";
+	}
+
+	private void OnClickOutside()
+	{
+		_show = false;
+		StateHasChanged();
+	}
+}
