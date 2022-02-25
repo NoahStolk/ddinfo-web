0ccb66d80f37426cf7103cc87abeedbf20077244
Noah Stolk <noah.stolk@gmail.com>
2022-02-25 21:11:39 +0100

    Fix binding for EditPlayerProfile DTO

--- DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Players/EditPlayerProfile.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Shared/Dto/Public/Players/EditPlayerProfile.cs
@@ -3,35 +3,35 @@ namespace DevilDaggersInfo.Web.BlazorWasm.Shared.Dto.Public.Players;
 public class EditPlayerProfile
 {
 	[StringLength(2)]
-	public string? CountryCode { get; init; }
+	public string? CountryCode { get; set; }
 
 	[Range(10, 20000)]
-	public int? Dpi { get; init; }
+	public int? Dpi { get; set; }
 
 	[Range(0.01f, 2)]
-	public float? InGameSens { get; init; }
+	public float? InGameSens { get; set; }
 
 	[Range(85, 120)]
-	public int? Fov { get; init; }
+	public int? Fov { get; set; }
 
-	public bool? IsRightHanded { get; init; }
+	public bool? IsRightHanded { get; set; }
 
-	public bool? HasFlashHandEnabled { get; init; }
+	public bool? HasFlashHandEnabled { get; set; }
 
 	[Range(1.5f, 2.2f)]
-	public float? Gamma { get; init; }
+	public float? Gamma { get; set; }
 
-	public bool? UsesLegacyAudio { get; init; }
+	public bool? UsesLegacyAudio { get; set; }
 
-	public bool? UsesHrtf { get; init; }
+	public bool? UsesHrtf { get; set; }
 
 	public bool? UsesInvertY { get; set; }
 
 	public VerticalSync VerticalSync { get; set; }
 
-	public bool HideSettings { get; init; }
+	public bool HideSettings { get; set; }
 
-	public bool HideDonations { get; init; }
+	public bool HideDonations { get; set; }
 
-	public bool HidePastUsernames { get; init; }
+	public bool HidePastUsernames { get; set; }
 }
