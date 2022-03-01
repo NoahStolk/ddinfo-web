dc02184b3850885c062d9c754713df7515911e05
Noah Stolk <noah.stolk@gmail.com>
2022-03-01 17:04:01 +0100

    Rewrite survival editor guide

--- DevilDaggersInfo.Web.BlazorWasm.Server/Startup.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Startup.cs
@@ -152,8 +152,8 @@ public class Startup
 			.AddRedirect("^Upgrades$", "wiki/upgrades")
 
 			// V4 outdated
-			.AddRedirect("^Wiki/SpawnsetGuide$", "guides/survival-editor")
 			.AddRedirect("^Wiki/AssetGuide$", "guides/asset-editor")
+			.AddRedirect("^Wiki/SpawnsetGuide$", "guides/creating-spawnsets")
 			.AddRedirect("^Leaderboard/UserSettings", "leaderboard/player-settings")
 
 			// V4
@@ -165,8 +165,9 @@ public class Startup
 			.AddRedirect("^Tools/DevilDaggersAssetEditor", "tools/asset-editor")
 			.AddRedirect("^Tools/DevilDaggersCustomLeaderboards", "tools/custom-leaderboards")
 			.AddRedirect("^Tools/DevilDaggersSurvivalEditor", "tools/survival-editor")
-			.AddRedirect("^Wiki/Guides/SurvivalEditor$", "guides/survival-editor")
 			.AddRedirect("^Wiki/Guides/AssetEditor$", "guides/asset-editor")
+			.AddRedirect("^Wiki/Guides/SurvivalEditor$", "guides/creating-spawnsets")
+			.AddRedirect("^guides/survival-editor", "guides/creating-spawnsets")
 
 			.Add(new CustomLeaderboardPageRewriteRules())
 			.Add(new ModPageRewriteRules())
