d05222f05cb7db6f420f14af505f3d9f0d4c72eb
Noah Stolk <noah.stolk@gmail.com>
2022-03-20 17:26:07 +0100

    Transform modes for dagger and accuracy data

--- DevilDaggersInfo.Web.BlazorWasm.Server/Caches/LeaderboardStatistics/ArrayStatistic.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Caches/LeaderboardStatistics/ArrayStatistic.cs
@@ -6,14 +6,14 @@ public class ArrayStatistic
 	public double Median { get; private set; }
 	public double Mode { get; private set; }
 
-	public void Populate(IEnumerable<double> data)
+	public void Populate(IEnumerable<double> data, Func<double, double> modeTransformer)
 	{
 		data = data.OrderBy(n => n);
 
 		Average = data.Average();
 		Median = data.ElementAt(data.Count() / 2);
 		Mode = data
-			.GroupBy(n => n)
+			.GroupBy(n => modeTransformer(n))
 			.OrderByDescending(g => g.Count())
 			.Select(g => g.Key)
 			.FirstOrDefault();
--- DevilDaggersInfo.Web.BlazorWasm.Server/Caches/LeaderboardStatistics/ArrayStatistics.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Server/Caches/LeaderboardStatistics/ArrayStatistics.cs
@@ -7,16 +7,18 @@ public class ArrayStatistics
 	public ArrayStatistic Gems { get; } = new();
 	public ArrayStatistic DaggersFired { get; } = new();
 	public ArrayStatistic DaggersHit { get; } = new();
+	public ArrayStatistic Accuracy { get; } = new();
 
 	public void Populate(List<CompressedEntry> entries, int? limit = null)
 	{
 		if (limit.HasValue)
 			entries = entries.Take(limit.Value).ToList();
 
-		Times.Populate(entries.Select(e => e.Time.ToSecondsTime()));
-		Kills.Populate(entries.Select(e => (double)e.Kills));
-		Gems.Populate(entries.Select(e => (double)e.Gems));
-		DaggersFired.Populate(entries.Select(e => (double)e.DaggersFired));
-		DaggersHit.Populate(entries.Select(e => (double)e.DaggersHit));
+		Times.Populate(entries.Select(e => e.Time.ToSecondsTime()), d => d);
+		Kills.Populate(entries.Select(e => (double)e.Kills), d => d);
+		Gems.Populate(entries.Select(e => (double)e.Gems), d => d);
+		DaggersFired.Populate(entries.Select(e => (double)e.DaggersFired), d => Math.Round(d / 100) * 100);
+		DaggersHit.Populate(entries.Select(e => (double)e.DaggersHit), d => Math.Round(d / 100) * 100);
+		Accuracy.Populate(entries.Select(e => e.DaggersFired == 0 ? 0 : e.DaggersHit / (double)e.DaggersFired * 100), d => Math.Floor(d));
 	}
 }
