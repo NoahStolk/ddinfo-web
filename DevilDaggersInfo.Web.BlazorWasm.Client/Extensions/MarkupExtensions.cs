e2761ce088725c1c3ed6418deed9a08d5bcf156e
Noah Stolk <noah.stolk@gmail.com>
2022-03-21 19:39:04 +0100

    Refactor

--- DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/MarkupExtensions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/MarkupExtensions.cs
@@ -1,3 +1,4 @@
+using DevilDaggersInfo.Web.BlazorWasm.Shared.Enums;
 using Microsoft.AspNetCore.Components;
 
 namespace DevilDaggersInfo.Web.BlazorWasm.Client.Utils;
@@ -9,4 +10,7 @@ public static class MarkupExtensions
 
 	public static MarkupString ToNoBreakString(this string? str)
 		=> new(str?.Replace(" ", "&nbsp;") ?? string.Empty);
+
+	public static string ToCssClass(this CustomLeaderboardDagger customLeaderboardDagger)
+		=> customLeaderboardDagger.ToString().ToLower();
 }
