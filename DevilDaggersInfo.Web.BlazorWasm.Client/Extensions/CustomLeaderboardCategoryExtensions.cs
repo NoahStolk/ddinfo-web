6a1765abd4f58601b79b3ae859e7c2537e74b775
Noah Stolk <noah.stolk@gmail.com>
2022-03-25 09:47:11 +0100

    Display "Time Attack" rather than "TimeAttack"

--- DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/CustomLeaderboardCategoryExtensions.cs
+++ DevilDaggersInfo.Web.BlazorWasm.Client/Extensions/CustomLeaderboardCategoryExtensions.cs
@@ -9,8 +9,18 @@ public static class CustomLeaderboardCategoryExtensions
 		CustomLeaderboardCategory.Survival => "Survive as long as you can.",
 		CustomLeaderboardCategory.TimeAttack => "Kill all enemies as quickly as possible.",
 		CustomLeaderboardCategory.Speedrun => "Jump into the void as quickly as possible. Note that this category has been superseded by the Race category; new Speedrun leaderboards will not be added.",
-		CustomLeaderboardCategory.Pacifist => "Survive as long as you can, but without damaging or killing any enemies.",
 		CustomLeaderboardCategory.Race => "Reach the dagger as quickly as possible.",
+		CustomLeaderboardCategory.Pacifist => "Survive as long as you can, but without damaging or killing any enemies.",
+		_ => throw new NotSupportedException($"{nameof(CustomLeaderboardCategory)} '{category}' is not supported."),
+	};
+
+	public static string ToDisplayString(this CustomLeaderboardCategory category) => category switch
+	{
+		CustomLeaderboardCategory.Survival => nameof(CustomLeaderboardCategory.Survival),
+		CustomLeaderboardCategory.TimeAttack => "Time Attack",
+		CustomLeaderboardCategory.Speedrun => nameof(CustomLeaderboardCategory.Speedrun),
+		CustomLeaderboardCategory.Race => nameof(CustomLeaderboardCategory.Race),
+		CustomLeaderboardCategory.Pacifist => nameof(CustomLeaderboardCategory.Pacifist),
 		_ => throw new NotSupportedException($"{nameof(CustomLeaderboardCategory)} '{category}' is not supported."),
 	};
 }
