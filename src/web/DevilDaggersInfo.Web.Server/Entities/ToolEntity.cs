3edda4b4563113bfc7d3e900413b49a15ca0e6c7
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-04-29 00:09:53 +0200

    Improve styling

--- src/web/DevilDaggersInfo.Web.Server/Entities/ToolEntity.cs
+++ src/web/DevilDaggersInfo.Web.Server/Entities/ToolEntity.cs
@@ -5,14 +5,14 @@ public class ToolEntity
 {
 	[Key]
 	[StringLength(64)]
-	public string Name { get; set; } = string.Empty;
+	public string Name { get; init; } = string.Empty;
 
 	[StringLength(64)]
-	public string DisplayName { get; set; } = string.Empty;
+	public string DisplayName { get; init; } = string.Empty;
 
 	[StringLength(16)]
-	public string CurrentVersionNumber { get; set; } = string.Empty;
+	public string CurrentVersionNumber { get; init; } = string.Empty;
 
 	[StringLength(16)]
-	public string RequiredVersionNumber { get; set; } = string.Empty;
+	public string RequiredVersionNumber { get; init; } = string.Empty;
 }
