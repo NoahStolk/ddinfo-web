1120f1347782257a42be42a1737ee87549cae077
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-05-14 14:52:18 +0200

    Clean up unused Discord channel and message IDs

--- src/web/DevilDaggersInfo.Web.Server/HostedServices/DdInfoDiscordBot/Channel.cs
+++ src/web/DevilDaggersInfo.Web.Server/HostedServices/DdInfoDiscordBot/Channel.cs
@@ -3,12 +3,8 @@ namespace DevilDaggersInfo.Web.Server.HostedServices.DdInfoDiscordBot;
 public enum Channel
 {
 	MaintainersAuditLog,
-	MonitoringBackgroundService,
-	MonitoringCache,
 	MonitoringCustomLeaderboardValid,
 	MonitoringCustomLeaderboardInvalid,
-	MonitoringDatabase,
-	MonitoringFile,
 	MonitoringLog,
 	MonitoringTest,
 	CustomLeaderboards,
--- src/web/DevilDaggersInfo.Web.Server/HostedServices/DdInfoDiscordBot/DiscordServerConstants.cs
+++ src/web/DevilDaggersInfo.Web.Server/HostedServices/DdInfoDiscordBot/DiscordServerConstants.cs
@@ -5,11 +5,6 @@ namespace DevilDaggersInfo.Web.Server.HostedServices.DdInfoDiscordBot;
 
 public static class DiscordServerConstants
 {
-	private const ulong _backgroundServiceMessageId = 856557628816490537;
-	private const ulong _cacheMessageId = 856151636368031785;
-	private const ulong _databaseMessageId = 856491602675236894;
-	private const ulong _fileMessageId = 856265036486148146;
-
 	public const long TestChannelId = 813508325705515008;
 
 	public const ulong BotUserId = 645209987949395969;
@@ -17,21 +12,12 @@ public static class DiscordServerConstants
 	private static readonly Dictionary<Channel, ChannelWrapper> _channels = new()
 	{
 		{ Channel.MaintainersAuditLog, new(821489129615130684) },
-		{ Channel.MonitoringBackgroundService, new(856553635206266911) },
-		{ Channel.MonitoringCache, new(831981757200859206) },
 		{ Channel.MonitoringCustomLeaderboardValid, new(813506112670007306) },
 		{ Channel.MonitoringCustomLeaderboardInvalid, new(952210186859339816) },
-		{ Channel.MonitoringDatabase, new(856490882371289098) },
-		{ Channel.MonitoringFile, new(856263861372321823) },
 		{ Channel.MonitoringLog, new(727227801664618607) },
 		{ Channel.MonitoringTest, new(TestChannelId) },
 		{ Channel.CustomLeaderboards, new(578316107836817418) },
 	};
-
-	public static DiscordMessage? BackgroundServiceMessage { get; private set; }
-	public static DiscordMessage? CacheMessage { get; private set; }
-	public static DiscordMessage? DatabaseMessage { get; private set; }
-	public static DiscordMessage? FileMessage { get; private set; }
 
 	/// <summary>
 	/// Returns the channel based on the <paramref name="channel"/> enum. Always returns the channel for <see cref="Channel.MonitoringTest"/> when running in development.
@@ -51,22 +37,6 @@ public static class DiscordServerConstants
 			if (wrapper.DiscordChannel == null)
 				wrapper.DiscordChannel = await client.GetChannelAsync(wrapper.ChannelId);
 		}
-
-		DiscordChannel? backgroundServiceChannel = _channels[Channel.MonitoringBackgroundService].DiscordChannel;
-		if (backgroundServiceChannel != null)
-			BackgroundServiceMessage = await backgroundServiceChannel.GetMessageAsync(_backgroundServiceMessageId);
-
-		DiscordChannel? cacheChannel = _channels[Channel.MonitoringCache].DiscordChannel;
-		if (cacheChannel != null)
-			CacheMessage = await cacheChannel.GetMessageAsync(_cacheMessageId);
-
-		DiscordChannel? databaseChannel = _channels[Channel.MonitoringDatabase].DiscordChannel;
-		if (databaseChannel != null)
-			DatabaseMessage = await databaseChannel.GetMessageAsync(_databaseMessageId);
-
-		DiscordChannel? fileChannel = _channels[Channel.MonitoringFile].DiscordChannel;
-		if (fileChannel != null)
-			FileMessage = await fileChannel.GetMessageAsync(_fileMessageId);
 	}
 
 	private sealed class ChannelWrapper
