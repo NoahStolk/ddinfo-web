d703939e51eb811a251b8bb538e0885bd4e17acc
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-05-24 20:24:15 +0200

    Implement sorting world record data and display more data

--- src/web/DevilDaggersInfo.Web.Client/Pages/Leaderboard/WorldRecordProgressionPage.cs
+++ src/web/DevilDaggersInfo.Web.Client/Pages/Leaderboard/WorldRecordProgressionPage.cs
@@ -1,3 +1,4 @@
+using DevilDaggersInfo.Common.Extensions;
 using DevilDaggersInfo.Core.Wiki.Extensions;
 using DevilDaggersInfo.Razor.Core.CanvasChart.Data;
 using DevilDaggersInfo.Razor.Core.CanvasChart.Options.LineChart;
@@ -7,6 +8,7 @@ using DevilDaggersInfo.Web.Shared.Dto.Public.WorldRecords;
 using DevilDaggersInfo.Web.Shared.Utils;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
 
 namespace DevilDaggersInfo.Web.Client.Pages.Leaderboard;
 
@@ -30,7 +32,12 @@ public partial class WorldRecordProgressionPage
 	private TimeSpan _totalTimeSinceFirstRecord;
 
 	private LineChartDataOptions _dataOptions = LineChartDataOptions.Default;
-	private GetWorldRecordDataContainer? _data;
+
+	private List<GetWorldRecordHolder>? _worldRecordHolders;
+	private List<GetWorldRecord>? _worldRecords;
+
+	private Dictionary<string, bool> _worldRecordHoldersSortings = new();
+	private Dictionary<string, bool> _worldRecordsSortings = new();
 
 	[Inject]
 	public PublicApiHttpClient Http { get; set; } = null!;
@@ -40,25 +47,25 @@ public partial class WorldRecordProgressionPage
 
 	protected override async Task OnInitializedAsync()
 	{
-		_data = await Http.GetWorldRecordData();
-		GetWorldRecord currentWr = _data.WorldRecords.MaxBy(wr => wr.Entry.Time)!;
+		GetWorldRecordDataContainer data = await Http.GetWorldRecordData();
+		GetWorldRecord currentWr = data.WorldRecords.MaxBy(wr => wr.Entry.Time)!;
 		_currentWorldRecord = currentWr.Entry.Time;
 		_currentWorldRecordHolderId = currentWr.Entry.Id;
 
-		_totalTimeSinceFirstRecord = DateTime.UtcNow - _data.WorldRecords.OrderBy(wr => wr.DateTime).First().DateTime;
+		_totalTimeSinceFirstRecord = DateTime.UtcNow - data.WorldRecords.OrderBy(wr => wr.DateTime).First().DateTime;
 
 		DateTime minX = new(2016, 1, 1);
 		DateTime maxX = DateTime.UtcNow;
-		GetWorldRecord firstWr = _data.WorldRecords[0];
-		GetWorldRecord lastWr = _data.WorldRecords[^1];
+		GetWorldRecord firstWr = data.WorldRecords[0];
+		GetWorldRecord lastWr = data.WorldRecords[^1];
 		double minY = Math.Floor(firstWr.Entry.Time / 100.0) * 100;
 		double maxY = Math.Ceiling(lastWr.Entry.Time / 100.0) * 100;
 
-		List<LineData> set = _data.WorldRecords.Select((wr, i) => new LineData(wr.DateTime.Ticks, wr.Entry.Time, i)).ToList();
+		List<LineData> set = data.WorldRecords.Select((wr, i) => new LineData(wr.DateTime.Ticks, wr.Entry.Time, i)).ToList();
 		_dataOptions = new(minX.Ticks, null, maxX.Ticks, minY, 100, maxY);
 		_lineDataSets.Add(new("#f00", true, true, true, set, (ds, d) =>
 		{
-			GetWorldRecord? wr = _data.WorldRecords.Count <= d.Index ? null : _data.WorldRecords[d.Index];
+			GetWorldRecord? wr = data.WorldRecords.Count <= d.Index ? null : data.WorldRecords[d.Index];
 			if (wr == null)
 				return new();
 
@@ -76,13 +83,30 @@ public partial class WorldRecordProgressionPage
 				new($"<span style='text-align: right;'>{gameVersion.GetGameVersionString()}</span>"),
 			};
 		}));
+
+		_worldRecordHolders = data.WorldRecordHolders.OrderByDescending(wrh => wrh.LastHeld).ToList();
+		_worldRecords = data.WorldRecords.OrderByDescending(wr => wr.DateTime).ToList();
 	}
 
 	private string GetHistoryDateString(DateTime dateTime)
 	{
 		int daysAgo = (int)Math.Round((DateTime.UtcNow - dateTime).TotalDays);
-		return $"{dateTime:MMM dd} '{dateTime:yy} ({daysAgo} day{S(daysAgo)} ago)";
+		return $"{dateTime:MMM dd} '{dateTime:yy} ({daysAgo:N0} day{S(daysAgo)} ago)";
 	}
 
-	private static string S(int value) => value == 1 ? string.Empty : "s";
+	private static string S(int value)
+		=> value == 1 ? string.Empty : "s";
+
+	private void Sort<TSource, TKey>(ref List<TSource> source, Dictionary<string, bool> sortings, Func<TSource, TKey> sorting, [CallerArgumentExpression("sorting")] string sortingExpression = "")
+	{
+		bool sortDirection = false;
+		if (sortings.ContainsKey(sortingExpression))
+			sortDirection = sortings[sortingExpression];
+		else
+			sortings.Add(sortingExpression, false);
+
+		source = source.OrderBy(sorting, sortDirection).ToList();
+
+		sortings[sortingExpression] = !sortDirection;
+	}
 }
