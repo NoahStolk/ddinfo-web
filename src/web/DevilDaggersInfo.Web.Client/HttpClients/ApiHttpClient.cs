52d12a244b299626ec5e2860a564453875a019cc
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-05-27 15:23:04 +0200

    Remove duplicate implementations

--- src/web/DevilDaggersInfo.Web.Client/HttpClients/ApiHttpClient.cs
+++ src/web/DevilDaggersInfo.Web.Client/HttpClients/ApiHttpClient.cs
@@ -1,11 +1,37 @@
+using Blazored.LocalStorage;
+using DevilDaggersInfo.Web.Client.Authentication;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace DevilDaggersInfo.Web.Client.HttpClients;
 
 public abstract class ApiHttpClient
 {
-	// TODO: Implementations are the same, so just implement this without abstract.
-	protected abstract Task<HttpResponseMessage> SendRequest(HttpMethod httpMethod, string url, JsonContent? body = null);
+	private readonly HttpClient _client;
+	private readonly ILocalStorageService _localStorageService;
+
+	protected ApiHttpClient(HttpClient client, ILocalStorageService localStorageService)
+	{
+		_client = client;
+		_localStorageService = localStorageService;
+	}
+
+	public HttpClient Client => _client;
+
+	protected async Task<HttpResponseMessage> SendRequest(HttpMethod httpMethod, string url, JsonContent? body = null)
+	{
+		HttpRequestMessage request = new()
+		{
+			RequestUri = new Uri(url, UriKind.Relative),
+			Method = httpMethod,
+			Content = body,
+		};
+		string? token = await _localStorageService.GetItemAsStringAsync(AdminAuthenticationStateProvider.LocalStorageAuthKey);
+		if (token != null)
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+		return await _client.SendAsync(request);
+	}
 
 	protected async Task<T> SendGetRequest<T>(string url)
 	{
