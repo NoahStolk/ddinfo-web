cebd37c3ac7f8ce0465e0d7f82568c13b23de582
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-05-28 12:54:10 +0200

    Move SubmitScoreForDdcl to DDCL API

--- src/web/DevilDaggersInfo.Web.Server/Controllers/Public/CustomEntriesController.cs
+++ src/web/DevilDaggersInfo.Web.Server/Controllers/Public/CustomEntriesController.cs
@@ -9,18 +9,14 @@ namespace DevilDaggersInfo.Web.Server.Controllers.Public;
 public class CustomEntriesController : ControllerBase
 {
 	private readonly ApplicationDbContext _dbContext;
-	private readonly ILogger<CustomEntriesController> _logger;
 	private readonly SpawnsetSummaryCache _spawnsetSummaryCache;
 	private readonly IFileSystemService _fileSystemService;
-	private readonly CustomEntryProcessor _customEntryProcessor;
 
-	public CustomEntriesController(ApplicationDbContext dbContext, ILogger<CustomEntriesController> logger, SpawnsetSummaryCache spawnsetSummaryCache, IFileSystemService fileSystemService, CustomEntryProcessor customEntryProcessor)
+	public CustomEntriesController(ApplicationDbContext dbContext, SpawnsetSummaryCache spawnsetSummaryCache, IFileSystemService fileSystemService)
 	{
 		_dbContext = dbContext;
-		_logger = logger;
 		_spawnsetSummaryCache = spawnsetSummaryCache;
 		_fileSystemService = fileSystemService;
-		_customEntryProcessor = customEntryProcessor;
 	}
 
 	[HttpGet("{id}/replay-buffer")]
@@ -37,6 +33,7 @@ public class CustomEntriesController : ControllerBase
 	}
 
 	// FORBIDDEN: Used by DDLIVE.
+	// FORBIDDEN: Used by DDCL 1.8.3.0.
 	[HttpGet("{id}/replay")]
 	[ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -86,28 +83,5 @@ public class CustomEntriesController : ControllerBase
 		SpawnsetSummary ss = _spawnsetSummaryCache.GetSpawnsetSummaryByFilePath(Path.Combine(_fileSystemService.GetPath(DataSubDirectory.Spawnsets), customEntry.CustomLeaderboard.Spawnset.Name));
 		EffectivePlayerSettings eps = SpawnsetBinary.GetEffectivePlayerSettings(ss.HandLevel, ss.AdditionalGems);
 		return customEntry.ToGetCustomEntryData(customEntryData, eps.HandLevel, IoFile.Exists(Path.Combine(_fileSystemService.GetPath(DataSubDirectory.CustomEntryReplays), $"{id}.ddreplay")));
-	}
-
-	[HttpPost("submit")]
-	[ProducesResponseType(StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status400BadRequest)]
-	public async Task<ActionResult<GetUploadSuccess>> SubmitScoreForDdcl([FromBody] AddUploadRequest uploadRequest)
-	{
-		try
-		{
-			return await _customEntryProcessor.ProcessUploadRequestAsync(uploadRequest);
-		}
-		catch (CustomEntryValidationException ex)
-		{
-			return new BadRequestObjectResult(new ProblemDetails { Title = ex.Message });
-		}
-		catch (Exception ex)
-		{
-			ex.Data[nameof(uploadRequest.ClientVersion)] = uploadRequest.ClientVersion;
-			ex.Data[nameof(uploadRequest.OperatingSystem)] = uploadRequest.OperatingSystem;
-			ex.Data[nameof(uploadRequest.BuildMode)] = uploadRequest.BuildMode;
-			_logger.LogError(ex, "Upload failed for user `{playerName}` (`{playerId}`) for `{spawnset}`.", uploadRequest.PlayerName, uploadRequest.PlayerId, uploadRequest.SurvivalHashMd5.ByteArrayToHexString());
-			throw;
-		}
 	}
 }
