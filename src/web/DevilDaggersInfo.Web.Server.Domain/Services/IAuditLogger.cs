969e87740e3dbbdaa46cf55c2a158399762188e1
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-04 20:43:14 +0200

    Fix missing CallerMemberName

--- src/web/DevilDaggersInfo.Web.Server.Domain/Services/IAuditLogger.cs
+++ src/web/DevilDaggersInfo.Web.Server.Domain/Services/IAuditLogger.cs
@@ -1,15 +1,16 @@
 using DevilDaggersInfo.Web.Server.Domain.Models.FileSystem;
+using System.Runtime.CompilerServices;
 using System.Security.Claims;
 
 namespace DevilDaggersInfo.Web.Server.Domain.Services;
 
 public interface IAuditLogger
 {
-	void LogAdd(Dictionary<string, string> dtoLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, string endpointName = "");
+	void LogAdd(Dictionary<string, string> dtoLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, [CallerMemberName] string endpointName = "");
 
-	void LogDelete(Dictionary<string, string> entityLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, string endpointName = "");
+	void LogDelete(Dictionary<string, string> entityLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, [CallerMemberName] string endpointName = "");
 
-	void LogEdit(Dictionary<string, string> oldDtoLog, Dictionary<string, string> dtoLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, string endpointName = "");
+	void LogEdit(Dictionary<string, string> oldDtoLog, Dictionary<string, string> dtoLog, ClaimsPrincipal claimsPrincipal, int id, List<FileSystemInformation>? fileSystemInformation = null, [CallerMemberName] string endpointName = "");
 
 	void LogPlayerUpdates(string caller, List<(int PlayerId, string OldValue, string NewValue)> logs);
 
