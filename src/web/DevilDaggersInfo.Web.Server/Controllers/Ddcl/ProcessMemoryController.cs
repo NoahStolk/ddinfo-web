8bf56345a1cf81530a490647775ba0c3cb01a43c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-05 00:58:15 +0200

    Implement new marker endpoint

--- src/web/DevilDaggersInfo.Web.Server/Controllers/Ddcl/ProcessMemoryController.cs
+++ src/web/DevilDaggersInfo.Web.Server/Controllers/Ddcl/ProcessMemoryController.cs
@@ -14,20 +14,31 @@ public class ProcessMemoryController : ControllerBase
 	}
 
 	// FORBIDDEN: Used by ddstats-rust.
+	[Obsolete("Use the new route instead.")]
 	[HttpGet("/api/process-memory/marker")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public ActionResult<Marker> GetMarkerObsolete(SupportedOperatingSystem operatingSystem)
+	{
+		return GetMarkerRepo(operatingSystem);
+	}
+
+	[HttpGet("marker")]
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	public ActionResult<Marker> GetMarker(SupportedOperatingSystem operatingSystem)
 	{
-		string? name = operatingSystem switch
+		return GetMarkerRepo(operatingSystem);
+	}
+
+	private ActionResult<Marker> GetMarkerRepo(SupportedOperatingSystem operatingSystem)
+	{
+		string name = operatingSystem switch
 		{
 			SupportedOperatingSystem.Windows => "WindowsSteam",
 			SupportedOperatingSystem.Linux => "LinuxSteam",
-			_ => null,
+			_ => throw new UnsupportedOperatingSystemException($"Operating system '{operatingSystem}' is not supported."),
 		};
-
-		if (name == null)
-			return BadRequest($"Operating system '{operatingSystem}' is not supported.");
 
 		MarkerEntity? marker = _dbContext.Markers.FirstOrDefault(m => m.Name == name);
 		if (marker == null)
