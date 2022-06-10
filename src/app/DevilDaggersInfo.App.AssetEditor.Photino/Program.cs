13a4025964967c6b61d77174eaa3324159887646
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-10 14:40:24 +0200

    Add Photino app for DDAE

---
+++ src/app/DevilDaggersInfo.App.AssetEditor.Photino/Program.cs
@@ -0,0 +1,36 @@
+using DevilDaggersInfo.App.AssetEditor.Photino.Services;
+using DevilDaggersInfo.Common.Utils;
+using DevilDaggersInfo.Core.NativeInterface;
+using DevilDaggersInfo.Razor.Core.AssetEditor.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace DevilDaggersInfo.App.AssetEditor.Photino;
+
+public static class Program
+{
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		PhotinoBlazorAppBuilder appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+		appBuilder.Services.AddLogging();
+
+		appBuilder.Services.AddSingleton<INativeErrorReporter, NativeErrorReporter>();
+		appBuilder.Services.AddSingleton<INativeFileSystemService, NativeFileSystemService>();
+		appBuilder.Services.AddSingleton<IAssetEditorFileFilterService, AssetEditorFileFilterService>();
+		appBuilder.Services.AddSingleton<BinaryState>();
+
+		// register root component
+		appBuilder.RootComponents.Add<Razor.Core.AssetEditor.App>("app");
+
+		PhotinoBlazorApp app = appBuilder.Build();
+
+		app.MainWindow
+			.SetIconFile("Icon.ico")
+			.SetTitle($"Devil Daggers Asset Editor {VersionUtils.EntryAssemblyVersion}");
+
+		AppDomain.CurrentDomain.UnhandledException += (sender, error) => app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+
+		app.Run();
+	}
+}
---
+++ src/app/DevilDaggersInfo.App.AssetEditor.Photino/Services/AssetEditorFileFilterService.cs
@@ -0,0 +1,36 @@
+using DevilDaggersInfo.Common.Exceptions;
+using DevilDaggersInfo.Core.Asset.Enums;
+using DevilDaggersInfo.Core.Asset.Extensions;
+using DevilDaggersInfo.Razor.Core.AssetEditor.Services;
+
+namespace DevilDaggersInfo.App.AssetEditor.Photino.Services;
+
+public class AssetEditorFileFilterService : IAssetEditorFileFilterService
+{
+	public string GetAssetTypeFilter(AssetType assetType)
+	{
+		if (assetType == AssetType.Shader)
+			throw new NotSupportedException($"Asset type '{AssetType.Shader}' has multiple file extensions.");
+
+		string fileTypeName = assetType switch
+		{
+			AssetType.Audio => "Audio",
+			AssetType.Mesh => "Mesh",
+			AssetType.ObjectBinding => "Text",
+			AssetType.Texture => "Texture",
+			_ => throw new InvalidEnumConversionException(assetType),
+		};
+
+		return BuildFilter(fileTypeName, false, $"*{assetType.GetFileExtension()}");
+	}
+
+	public string GetVertexShaderFilter() => BuildFilter("Vertex shader", true, "*.vert", "*.glsl");
+
+	public string GetFragmentShaderFilter() => BuildFilter("Fragment shader", true, "*.frag", "*.glsl");
+
+	private static string BuildFilter(string fileTypeName, bool includeAllFiles, params string[] fileExtensionPatterns)
+	{
+		string patterns = string.Join(";", fileExtensionPatterns);
+		return $"{fileTypeName} files ({patterns})|{patterns}{(includeAllFiles ? "|All files (*.*)|*.*" : null)}";
+	}
+}
