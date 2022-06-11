7519b51a6e608720734f488bcfe654d5c222c7dd
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-11 14:23:08 +0200

    Implement GetSpawnsetBufferById endpoint for DDCL

---
+++ src/api/DevilDaggersInfo.Api.Ddcl/Spawnsets/GetSpawnsetBuffer.cs
@@ -0,0 +1,6 @@
+namespace DevilDaggersInfo.Api.Ddcl.CustomLeaderboards;
+
+public record GetSpawnsetBuffer
+{
+	public byte[] Data { get; init; } = null!;
+}
---
+++ src/web/DevilDaggersInfo.Web.Server/Controllers/Ddcl/SpawnsetsController.cs
@@ -0,0 +1,42 @@
+using DevilDaggersInfo.Api.Ddcl.CustomLeaderboards;
+using DevilDaggersInfo.Web.Server.Domain.Models.FileSystem;
+using DevilDaggersInfo.Web.Server.Domain.Services;
+
+namespace DevilDaggersInfo.Web.Server.Controllers.Ddcl;
+
+[Route("api/ddcl/spawnsets")]
+[ApiController]
+public class SpawnsetsController : ControllerBase
+{
+	private readonly ApplicationDbContext _dbContext;
+	private readonly IFileSystemService _fileSystemService;
+
+	public SpawnsetsController(ApplicationDbContext dbContext, IFileSystemService fileSystemService)
+	{
+		_dbContext = dbContext;
+		_fileSystemService = fileSystemService;
+	}
+
+	[HttpGet("{id}/buffer")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<ActionResult<GetSpawnsetBuffer>> GetSpawnsetBufferById([Required] int id)
+	{
+		var spawnset = await _dbContext.Spawnsets
+			.AsNoTracking()
+			.Select(s => new { s.Id, s.Name })
+			.FirstOrDefaultAsync(s => s.Id == id);
+		if (spawnset == null)
+			throw new NotFoundException();
+
+		string path = Path.Combine(_fileSystemService.GetPath(DataSubDirectory.Spawnsets), spawnset.Name);
+		if (!IoFile.Exists(path))
+			throw new NotFoundException();
+
+		return new GetSpawnsetBuffer
+		{
+			Data = IoFile.ReadAllBytes(path),
+		};
+	}
+}
