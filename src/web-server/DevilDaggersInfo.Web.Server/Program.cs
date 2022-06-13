d8c08e95e72ae516336284bec29ed6aa9ead0eea
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-13 21:32:14 +0200

    Remove Discord logging

--- src/web-server/DevilDaggersInfo.Web.Server/Program.cs
+++ src/web-server/DevilDaggersInfo.Web.Server/Program.cs
@@ -1,9 +1,7 @@
 using DevilDaggersInfo.Web.Server;
 using DevilDaggersInfo.Web.Server.HostedServices.DdInfoDiscordBot;
-using DevilDaggersInfo.Web.Server.Logging.Discord;
 
 IHostBuilder builder = Host.CreateDefaultBuilder(args)
-	.ConfigureLogging(builder => builder.ClearProviders().AddDiscordLogger())
 	.ConfigureWebHostDefaults(webBuilder =>
 	{
 		webBuilder.UseSentry(o => o.TracesSampleRate = 1.0);
