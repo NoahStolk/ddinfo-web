8c52758b15fce1f7e76903d4ef6d48031dba9331
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-13 21:18:32 +0200

    Enable Sentry

--- src/web-server/DevilDaggersInfo.Web.Server/Program.cs
+++ src/web-server/DevilDaggersInfo.Web.Server/Program.cs
@@ -6,6 +6,8 @@ IHostBuilder builder = Host.CreateDefaultBuilder(args)
 	.ConfigureLogging(builder => builder.ClearProviders().AddDiscordLogger())
 	.ConfigureWebHostDefaults(webBuilder =>
 	{
+		webBuilder.UseSentry(o => o.TracesSampleRate = 1.0);
+
 		webBuilder.UseStaticWebAssets();
 		webBuilder.UseStartup<Startup>();
 	})
