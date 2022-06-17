2fc46724bdcb7b2f55ef190668cdc775837ab2ce
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-17 15:57:20 +0200

    Implement ToDisplayString for GameMode enums

--- src/web/DevilDaggersInfo.Web.Client/Extensions/EnumDisplayStringExtensions.cs
+++ src/web/DevilDaggersInfo.Web.Client/Extensions/EnumDisplayStringExtensions.cs
@@ -1,5 +1,7 @@
 using DevilDaggersInfo.Api.Main.CustomLeaderboards;
 using DevilDaggersInfo.Common.Exceptions;
+using DevilDaggersInfo.Core.Spawnset.Enums;
+using MainApi = DevilDaggersInfo.Api.Main;
 
 namespace DevilDaggersInfo.Web.Client.Extensions;
 
@@ -20,5 +22,21 @@ public static class EnumDisplayStringExtensions
 		CustomLeaderboardCategory.Race => nameof(CustomLeaderboardCategory.Race),
 		CustomLeaderboardCategory.Pacifist => nameof(CustomLeaderboardCategory.Pacifist),
 		_ => throw new InvalidEnumConversionException(category),
+	};
+
+	public static string ToDisplayString(this GameMode gameMode) => gameMode switch
+	{
+		GameMode.Survival => nameof(GameMode.Survival),
+		GameMode.TimeAttack => "Time Attack",
+		GameMode.Race => nameof(GameMode.Race),
+		_ => throw new InvalidEnumConversionException(gameMode),
+	};
+
+	public static string ToDisplayString(this MainApi.Spawnsets.GameMode gameMode) => gameMode switch
+	{
+		MainApi.Spawnsets.GameMode.Survival => nameof(MainApi.Spawnsets.GameMode.Survival),
+		MainApi.Spawnsets.GameMode.TimeAttack => "Time Attack",
+		MainApi.Spawnsets.GameMode.Race => nameof(MainApi.Spawnsets.GameMode.Race),
+		_ => throw new InvalidEnumConversionException(gameMode),
 	};
 }
