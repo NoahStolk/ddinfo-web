bb1951676208de53c3fd62e226d887a399bca309
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-07-22 16:17:02 +0200

    Add AboutPage

---
+++ src/razor/DevilDaggersInfo.Razor.ReplayEditor/Utils/RazorAssemblyUtils.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+namespace DevilDaggersInfo.Razor.ReplayEditor.Utils;
+
+internal static class RazorAssemblyUtils
+{
+	static RazorAssemblyUtils()
+	{
+		Version = Assembly.GetExecutingAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+	}
+
+	public static string? Version { get; set; }
+}
