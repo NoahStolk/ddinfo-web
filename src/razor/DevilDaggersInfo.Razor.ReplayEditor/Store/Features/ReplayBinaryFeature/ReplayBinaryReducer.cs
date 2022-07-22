28f3c128a7abaab8aa3388d4bf1282c33f254ab8
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-07-22 16:46:09 +0200

    Refactor main component

--- src/razor/DevilDaggersInfo.Razor.ReplayEditor/Store/Features/ReplayBinaryFeature/ReplayBinaryReducer.cs
+++ src/razor/DevilDaggersInfo.Razor.ReplayEditor/Store/Features/ReplayBinaryFeature/ReplayBinaryReducer.cs
@@ -1,5 +1,4 @@
 using DevilDaggersInfo.Core.Replay;
-using DevilDaggersInfo.Core.Spawnset;
 using DevilDaggersInfo.Razor.ReplayEditor.Store.Features.ReplayBinaryFeature.Actions;
 using DevilDaggersInfo.Razor.ReplayEditor.Store.State;
 using Fluxor;
@@ -17,11 +16,9 @@ public static class ReplayBinaryReducer
 	[ReducerMethod]
 	public static ReplayBinaryState ReduceOpenLeaderboardBinaryAction(ReplayBinaryState state, OpenLeaderboardReplayAction action)
 	{
-		// TODO: Get data from leaderboard.
-		// TODO: Get V3 spawnset instead of default (empty).
-		LocalReplayBinaryHeader header = new(0, 0, 0, 0, 0, 0, 0, 0, 0, action.PlayerId, action.ReplayBinary.Header.Username, SpawnsetBinary.CreateDefault().ToBytes());
+		LocalReplayBinaryHeader header = new(0, 0, 0, 0, 0, -1, 0, 0, 0, 0, string.Empty, Array.Empty<byte>());
 		ReplayBinary<LocalReplayBinaryHeader> localReplay = new(header, action.ReplayBinary.EventsPerTick);
 
-		return new(localReplay, $"Leaderboard replay from {action.ReplayBinary.Header.Username} ({action.PlayerId})");
+		return new(localReplay, $"Leaderboard replay from {action.ReplayBinary.Header.Username} ({action.PlayerId}) - Converted to local replay");
 	}
 }
