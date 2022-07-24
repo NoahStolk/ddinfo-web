bc33d2a0f4f731372ed529990951566203ee1bb0
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-07-24 12:59:28 +0200

    Clear ReplayStatisticsBuilder on rebuild

--- src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Statistics/ReplayStatisticsBuilder.cs
+++ src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Statistics/ReplayStatisticsBuilder.cs
@@ -4,6 +4,8 @@ namespace DevilDaggersInfo.Core.Replay.PostProcessing.Statistics;
 
 public class ReplayStatisticsBuilder
 {
+	#region Fields
+
 	private int _gemsCollected;
 	private int _enemiesKilled;
 	private int _daggersFired;
@@ -49,51 +51,7 @@ public class ReplayStatisticsBuilder
 	private ushort _ghostpedesKilled;
 	private ushort _spiderEggsKilled;
 
-	private ReplayStatisticsEntry FlushCurrentState() => new(
-		GemsCollected: _gemsCollected,
-		EnemiesKilled: _enemiesKilled,
-		DaggersFired: _daggersFired,
-		DaggersHit: _daggersHit,
-		EnemiesAlive: _enemiesAlive,
-		HomingStored: _homingStored,
-		HomingEaten: _homingEaten,
-		GemsDespawned: _gemsDespawned,
-		GemsEaten: _gemsEaten,
-		GemsTotal: _gemsTotal,
-		Skull1sAlive: _skull1sAlive,
-		Skull2sAlive: _skull2sAlive,
-		Skull3sAlive: _skull3sAlive,
-		SpiderlingsAlive: _spiderlingsAlive,
-		Skull4sAlive: _skull4sAlive,
-		Squid1sAlive: _squid1sAlive,
-		Squid2sAlive: _squid2sAlive,
-		Squid3sAlive: _squid3sAlive,
-		CentipedesAlive: _centipedesAlive,
-		GigapedesAlive: _gigapedesAlive,
-		Spider1sAlive: _spider1sAlive,
-		Spider2sAlive: _spider2sAlive,
-		LeviathansAlive: _leviathansAlive,
-		OrbsAlive: _orbsAlive,
-		ThornsAlive: _thornsAlive,
-		GhostpedesAlive: _ghostpedesAlive,
-		SpiderEggsAlive: _spiderEggsAlive,
-		Skull1sKilled: _skull1sKilled,
-		Skull2sKilled: _skull2sKilled,
-		Skull3sKilled: _skull3sKilled,
-		SpiderlingsKilled: _spiderlingsKilled,
-		Skull4sKilled: _skull4sKilled,
-		Squid1sKilled: _squid1sKilled,
-		Squid2sKilled: _squid2sKilled,
-		Squid3sKilled: _squid3sKilled,
-		CentipedesKilled: _centipedesKilled,
-		GigapedesKilled: _gigapedesKilled,
-		Spider1sKilled: _spider1sKilled,
-		Spider2sKilled: _spider2sKilled,
-		LeviathansKilled: _leviathansKilled,
-		OrbsKilled: _orbsKilled,
-		ThornsKilled: _thornsKilled,
-		GhostpedesKilled: _ghostpedesKilled,
-		SpiderEggsKilled: _spiderEggsKilled);
+	#endregion Fields
 
 	/// <summary>
 	/// Builds statistics in the same manner as the "stats" memory block in game memory, but based purely on replay events.
@@ -101,6 +59,7 @@ public class ReplayStatisticsBuilder
 	/// </summary>
 	public List<ReplayStatisticsEntry> Build(List<IEvent> events)
 	{
+		Clear();
 		int currentTick = 0;
 
 		List<ReplayStatisticsEntry> entries = new() { FlushCurrentState() };
@@ -188,4 +147,102 @@ public class ReplayStatisticsBuilder
 
 		return entries;
 	}
+
+	#region Utils
+
+	private ReplayStatisticsEntry FlushCurrentState() => new(
+		GemsCollected: _gemsCollected,
+		EnemiesKilled: _enemiesKilled,
+		DaggersFired: _daggersFired,
+		DaggersHit: _daggersHit,
+		EnemiesAlive: _enemiesAlive,
+		HomingStored: _homingStored,
+		HomingEaten: _homingEaten,
+		GemsDespawned: _gemsDespawned,
+		GemsEaten: _gemsEaten,
+		GemsTotal: _gemsTotal,
+		Skull1sAlive: _skull1sAlive,
+		Skull2sAlive: _skull2sAlive,
+		Skull3sAlive: _skull3sAlive,
+		SpiderlingsAlive: _spiderlingsAlive,
+		Skull4sAlive: _skull4sAlive,
+		Squid1sAlive: _squid1sAlive,
+		Squid2sAlive: _squid2sAlive,
+		Squid3sAlive: _squid3sAlive,
+		CentipedesAlive: _centipedesAlive,
+		GigapedesAlive: _gigapedesAlive,
+		Spider1sAlive: _spider1sAlive,
+		Spider2sAlive: _spider2sAlive,
+		LeviathansAlive: _leviathansAlive,
+		OrbsAlive: _orbsAlive,
+		ThornsAlive: _thornsAlive,
+		GhostpedesAlive: _ghostpedesAlive,
+		SpiderEggsAlive: _spiderEggsAlive,
+		Skull1sKilled: _skull1sKilled,
+		Skull2sKilled: _skull2sKilled,
+		Skull3sKilled: _skull3sKilled,
+		SpiderlingsKilled: _spiderlingsKilled,
+		Skull4sKilled: _skull4sKilled,
+		Squid1sKilled: _squid1sKilled,
+		Squid2sKilled: _squid2sKilled,
+		Squid3sKilled: _squid3sKilled,
+		CentipedesKilled: _centipedesKilled,
+		GigapedesKilled: _gigapedesKilled,
+		Spider1sKilled: _spider1sKilled,
+		Spider2sKilled: _spider2sKilled,
+		LeviathansKilled: _leviathansKilled,
+		OrbsKilled: _orbsKilled,
+		ThornsKilled: _thornsKilled,
+		GhostpedesKilled: _ghostpedesKilled,
+		SpiderEggsKilled: _spiderEggsKilled);
+
+	private void Clear()
+	{
+		_gemsCollected = 0;
+		_enemiesKilled = 0;
+		_daggersFired = 0;
+		_daggersHit = 0;
+		_enemiesAlive = 0;
+		_homingStored = 0;
+		_homingEaten = 0;
+		_gemsDespawned = 0;
+		_gemsEaten = 0;
+		_gemsTotal = 0;
+		_skull1sAlive = 0;
+		_skull2sAlive = 0;
+		_skull3sAlive = 0;
+		_spiderlingsAlive = 0;
+		_skull4sAlive = 0;
+		_squid1sAlive = 0;
+		_squid2sAlive = 0;
+		_squid3sAlive = 0;
+		_centipedesAlive = 0;
+		_gigapedesAlive = 0;
+		_spider1sAlive = 0;
+		_spider2sAlive = 0;
+		_leviathansAlive = 0;
+		_orbsAlive = 0;
+		_thornsAlive = 0;
+		_ghostpedesAlive = 0;
+		_spiderEggsAlive = 0;
+		_skull1sKilled = 0;
+		_skull2sKilled = 0;
+		_skull3sKilled = 0;
+		_spiderlingsKilled = 0;
+		_skull4sKilled = 0;
+		_squid1sKilled = 0;
+		_squid2sKilled = 0;
+		_squid3sKilled = 0;
+		_centipedesKilled = 0;
+		_gigapedesKilled = 0;
+		_spider1sKilled = 0;
+		_spider2sKilled = 0;
+		_leviathansKilled = 0;
+		_orbsKilled = 0;
+		_thornsKilled = 0;
+		_ghostpedesKilled = 0;
+		_spiderEggsKilled = 0;
+	}
+
+	#endregion Utils
 }
