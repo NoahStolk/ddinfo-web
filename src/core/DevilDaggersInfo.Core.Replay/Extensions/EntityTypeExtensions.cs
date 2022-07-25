c6dc35013f74dd7f5b99d37f59c88caa32c1682d
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-07-25 20:17:58 +0200

    Update comment

--- src/core/DevilDaggersInfo.Core.Replay/Extensions/EntityTypeExtensions.cs
+++ src/core/DevilDaggersInfo.Core.Replay/Extensions/EntityTypeExtensions.cs
@@ -39,7 +39,7 @@ public static class EntityTypeExtensions
 		_ => throw new InvalidOperationException($"{nameof(EntityType)} '{entityType}' cannot be transmuted."),
 	};
 
-	// TODO: Take enemy parts into account.
+	// TODO: Take enemy parts into account? May not be needed.
 	public static int GetDamage(this EntityType enemyType, EntityType daggerType)
 	{
 		if (!enemyType.IsEnemy())
