4737a7b5d5bc3ad19bb3c0d46e7efd008b9c734e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-07-31 14:06:22 +0200

    Update WebViewRuntimeWrapper.cs

--- src/razor-core/DevilDaggersInfo.Razor.Core.Canvas/JSRuntime/WebViewRuntimeWrapper.cs
+++ src/razor-core/DevilDaggersInfo.Razor.Core.Canvas/JSRuntime/WebViewRuntimeWrapper.cs
@@ -21,17 +21,14 @@ public class WebViewRuntimeWrapper : IJSRuntimeWrapper
 	public void Invoke<T0, T1, T2, T3, T4, T5, T6>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6) => InvokeReturn<T0, T1, T2, T3, T4, T5, T6, object>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6);
 	public void Invoke<T0, T1, T2, T3, T4, T5, T6, T7>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7) => InvokeReturn<T0, T1, T2, T3, T4, T5, T6, T7, object>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
 
-#pragma warning disable S5034 // "ValueTask" should be consumed correctly
-#pragma warning disable CA2012 // Use ValueTasks correctly
-	public TResult InvokeReturn<TResult>(string identifier, string elementId) => _runtime.InvokeAsync<TResult>(identifier, elementId).Result;
-	public TResult InvokeReturn<T0, TResult>(string identifier, string elementId, T0 arg0) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0).Result;
-	public TResult InvokeReturn<T0, T1, TResult>(string identifier, string elementId, T0 arg0, T1 arg1) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1).Result;
-	public TResult InvokeReturn<T0, T1, T2, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2).Result;
-	public TResult InvokeReturn<T0, T1, T2, T3, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3).Result;
-	public TResult InvokeReturn<T0, T1, T2, T3, T4, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4).Result;
-	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5).Result;
-	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, T6, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6).Result;
-	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, T6, T7, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7).Result;
-#pragma warning restore CA2012 // Use ValueTasks correctly
-#pragma warning restore S5034 // "ValueTask" should be consumed correctly
+	// TODO: Find a proper fix for this.
+	public TResult InvokeReturn<TResult>(string identifier, string elementId) => _runtime.InvokeAsync<TResult>(identifier, elementId).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, TResult>(string identifier, string elementId, T0 arg0) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, TResult>(string identifier, string elementId, T0 arg0, T1 arg1) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, T3, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, T3, T4, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, T6, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6).ConfigureAwait(false).GetAwaiter().GetResult();
+	public TResult InvokeReturn<T0, T1, T2, T3, T4, T5, T6, T7, TResult>(string identifier, string elementId, T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7) => _runtime.InvokeAsync<TResult>(identifier, elementId, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7).ConfigureAwait(false).GetAwaiter().GetResult();
 }
