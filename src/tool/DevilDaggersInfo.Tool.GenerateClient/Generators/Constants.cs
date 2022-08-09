79a8c5966a88b20d40f80b8620af1d9d7a4bbf60
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-09 19:33:01 +0200

    Update path for client generator

--- src/tool/DevilDaggersInfo.Tool.GenerateClient/Generators/Constants.cs
+++ src/tool/DevilDaggersInfo.Tool.GenerateClient/Generators/Constants.cs
@@ -4,9 +4,9 @@ internal static class Constants
 {
 	private const string _devRoot = @"C:\Users\NOAH\source\repos\DevilDaggersInfo\src";
 
-	public static readonly string ServerProjectPath = Path.Combine(_devRoot, "web", "DevilDaggersInfo.Web.Server");
+	public static readonly string ServerProjectPath = Path.Combine(_devRoot, "web-server", "DevilDaggersInfo.Web.Server");
 
-	public static readonly string ClientProjectPath = Path.Combine(_devRoot, "web", "DevilDaggersInfo.Web.Client");
+	public static readonly string ClientProjectPath = Path.Combine(_devRoot, "web-client", "DevilDaggersInfo.Web.Client");
 	public static readonly string RazorIamProjectPath = Path.Combine(_devRoot, "razor", "DevilDaggersInfo.Razor.AppManager");
 	public static readonly string RazorClProjectPath = Path.Combine(_devRoot, "razor", "DevilDaggersInfo.Razor.CustomLeaderboard");
 	public static readonly string RazorReProjectPath = Path.Combine(_devRoot, "razor", "DevilDaggersInfo.Razor.ReplayEditor");
