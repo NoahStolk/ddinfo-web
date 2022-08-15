b319bd59b4f00b76b08a47eb0397ae2a7ee28a53
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-15 17:34:50 +0200

    Add comment to CustomLeaderboardCategory

--- src/types/DevilDaggersInfo.Types.Web/CustomLeaderboardCategory.cs
+++ src/types/DevilDaggersInfo.Types.Web/CustomLeaderboardCategory.cs
@@ -1,5 +1,6 @@
 namespace DevilDaggersInfo.Types.Web;
 
+// Note; do not use values 0, 5, and 6, as they have been removed. Re-using these might result in unexpected behavior when this type is exposed to clients using the API.
 public enum CustomLeaderboardCategory
 {
 	Survival = 1,
