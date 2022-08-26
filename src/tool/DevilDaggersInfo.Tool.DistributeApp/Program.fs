b4800c0334bd55260aea8e4dabf0a95e55d3ed9f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-26 17:54:49 +0200

    Finish DistributeApp tool

--- src/tool/DevilDaggersInfo.Tool.DistributeApp/Program.fs
+++ src/tool/DevilDaggersInfo.Tool.DistributeApp/Program.fs
@@ -17,42 +17,49 @@ let main argv =
     let publishDirectoryName = "_temp-release"
     AppBuilder.build projectFilePath publishDirectoryName
 
-    let proj = ProjectReader.readProjectFile projectFilePath
     let publishDirectoryPath = Path.Combine(Path.GetDirectoryName(projectFilePath), publishDirectoryName)
+    let proj = ProjectReader.readProjectFile projectFilePath
     let outputZipFilePath = Path.Combine(zipOutputDirectory, $"{proj.Name}-{proj.Version}-WindowsPhotino-SelfContained.zip")
 
     ZipWriter.zip outputZipFilePath publishDirectoryPath
 
     let client = new HttpClient() 
-    
+
     // Authenticate
-    let loginRequest = new HttpRequestMessage(HttpMethod.Post, "https://devildaggers.info/api/authentication/login")
-    loginRequest.Content = JsonContent.Create(JsonSerializer.Deserialize<LoginRequest>(File.ReadAllText("appsettings.json"))) |> ignore
-    
+    let loginRequest = new HttpRequestMessage(
+        HttpMethod.Post,
+        "https://devildaggers.info/api/authentication/login",
+        Content = JsonContent.Create(JsonSerializer.Deserialize<LoginRequest>(File.ReadAllText("appsettings.json"))))
     let loginAsync = async {
         let! response = client.SendAsync(loginRequest) |> Async.AwaitTask
         match response.StatusCode with
         | HttpStatusCode.OK -> ()
         | _ -> failwith $"Unsuccessful status code from login '{response.StatusCode}'"
-        
+
         let! loginResponse = response.Content.ReadFromJsonAsync<LoginResponse>() |> Async.AwaitTask
         return loginResponse
     }
     let loginResponse = loginAsync |> Async.RunSynchronously
-    
+
     // Upload
-    let uploadRequest = new HttpRequestMessage(HttpMethod.Post, "https://devildaggers.info/api/admin/tools")
-    uploadRequest.Content = JsonContent.Create(AddDistribution(Name = proj.Name, Version = proj.Version, BuildType = ToolBuildType.WindowsPhotino, PublishMethod = ToolPublishMethod.SelfContained, ZipFileContents = File.ReadAllBytes(outputZipFilePath), UpdateVersion = true, UpdateRequiredVersion = false)) |> ignore
-    uploadRequest.Headers.Authorization = AuthenticationHeaderValue("Bearer", loginResponse.Token) |> ignore
-    
+    let jsonContent = JsonContent.Create(AddDistribution(Name = proj.Name, Version = proj.Version, BuildType = ToolBuildType.WindowsPhotino, PublishMethod = ToolPublishMethod.SelfContained, ZipFileContents = File.ReadAllBytes(outputZipFilePath), UpdateVersion = true, UpdateRequiredVersion = false))
+    let mutable uploadRequest = new HttpRequestMessage(
+        HttpMethod.Post,
+        "https://devildaggers.info/api/admin/tools",
+        Content = jsonContent)
+    uploadRequest.Headers.Authorization <- AuthenticationHeaderValue("Bearer", loginResponse.Token)
+
     let uploadAsync = async {
         let! response = client.SendAsync(uploadRequest) |> Async.AwaitTask
         return response
     }
     let uploadResponse = uploadAsync |> Async.RunSynchronously
-    
+
     match uploadResponse.StatusCode with
     | HttpStatusCode.OK -> ()
     | _ -> failwith $"Unsuccessful status code from upload '{uploadResponse.StatusCode}'"
-    
+
+    printfn $"Deleting zip file '{outputZipFilePath}'"
+    File.Delete(outputZipFilePath)
+
     0
