0ebc4d66880999a2888744d3da76274e291c7e7f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-26 12:33:44 +0200

    Refactor F# script

--- src/tool/DevilDaggersInfo.Tool.DistributeApp/AppBuilder.fs
+++ src/tool/DevilDaggersInfo.Tool.DistributeApp/AppBuilder.fs
@@ -2,12 +2,35 @@ module DevilDaggersInfo.Tool.DistributeApp.AppBuilder
 
 open System
 open System.Diagnostics
+open System.Text
+
+let getPublishCommandProperties publishDirectoryName : Map<string, string> =
+    Map.empty
+        .Add("PublishSingleFile", "True")
+        .Add("PublishTrimmed", "True")
+        .Add("EnableCompressionInSingleFile", "True")
+        .Add("PublishReadyToRun", "False")
+        .Add("PublishProtocol", "FileSystem")
+        .Add("SelfContained", "true")
+        .Add("TargetFramework", "net6.0")
+        .Add("RuntimeIdentifier", "win-x64")
+        .Add("Platform", "x64")
+        .Add("Configuration", "Release")
+        .Add("PublishMethod", "SELF_CONTAINED")
+        .Add("PublishDir", publishDirectoryName)
+
+let getPublishCommand projectFilePath publishDirectoryName : string =
+    let sb = StringBuilder()
+    getPublishCommandProperties publishDirectoryName |> Map.iter (fun key value -> sb.Append($" -p:{key}={value}") |> ignore)
+
+    let propsStr = sb.ToString()
+    $"publish {projectFilePath}{propsStr}"
 
 let build projectFilePath publishDirectoryName =
-    let dotnetPublish = ProcessStartInfo(FileName = "dotnet", Arguments = $"publish {projectFilePath} -p:PublishSingleFile=True -p:PublishTrimmed=True -p:EnableCompressionInSingleFile=True -p:PublishReadyToRun=False -p:PublishProtocol=FileSystem -p:SelfContained=true -p:TargetFramework=net6.0 -p:RuntimeIdentifier=win-x64 -p:Platform=x64 -p:Configuration=Release -p:PublishMethod=SELF_CONTAINED -p:PublishDir={publishDirectoryName}")
+    let dotnetPublish = ProcessStartInfo(FileName = "dotnet", Arguments = getPublishCommand projectFilePath publishDirectoryName)
     let dotnetPublishProc = Process.Start(dotnetPublish)
     dotnetPublishProc.WaitForExit()
-    
+
     match dotnetPublishProc.ExitCode with
     | 0 -> ()
     | _ -> Environment.Exit(dotnetPublishProc.ExitCode)
