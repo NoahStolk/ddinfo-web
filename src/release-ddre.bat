40e9866b5557a70f0954a27e509a7db4b4220a61
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-28 22:01:57 +0200

    Update release-ddre.bat

--- src/release-ddre.bat
+++ src/release-ddre.bat
@@ -1,6 +1,3 @@
-@echo off
 set root=%cd%\
-set app_project_file=app\DevilDaggersInfo.App.ReplayEditor.Photino\DevilDaggersInfo.App.ReplayEditor.Photino.csproj
-
 cd tool\DevilDaggersInfo.Tool.DistributeApp\
-dotnet run -- %root%%app_project_file% %root%
+dotnet run -- %root%app\DevilDaggersInfo.App.ReplayEditor.Photino\DevilDaggersInfo.App.ReplayEditor.Photino.csproj %root%
