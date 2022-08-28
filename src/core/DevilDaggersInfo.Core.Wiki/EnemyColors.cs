c9bb41cde1d9cce95a8a94c41a9b20973924c53e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-08-28 21:37:15 +0200

    Refactor colors in Core.Wiki

---
+++ src/core/DevilDaggersInfo.Core.Wiki/EnemyColors.cs
@@ -0,0 +1,31 @@
+namespace DevilDaggersInfo.Core.Wiki;
+
+public static class EnemyColors
+{
+	public static readonly Color Skull1 = new(0x5A, 0x42, 0x1B);
+	public static readonly Color Skull2 = new(0x69, 0x4D, 0x20);
+	public static readonly Color Skull3 = new(0x78, 0x58, 0x24);
+	public static readonly Color Skull4 = new(0x97, 0x6E, 0x2E);
+	public static readonly Color TransmutedSkull1 = new(0x5A, 0x22, 0x1B);
+	public static readonly Color TransmutedSkull2 = new(0x69, 0x28, 0x20);
+	public static readonly Color TransmutedSkull3 = new(0x78, 0x2E, 0x24);
+	public static readonly Color TransmutedSkull4 = new(0x97, 0x3A, 0x2E);
+	public static readonly Color Squid1 = new(0x69, 0x40, 0x00);
+	public static readonly Color Squid2 = new(0x8C, 0x55, 0x00);
+	public static readonly Color Squid3 = new(0xAF, 0x6B, 0x00);
+	public static readonly Color Spiderling = new(0xDC, 0xCB, 0x00);
+	public static readonly Color Centipede = new(0x83, 0x7E, 0x75);
+	public static readonly Color GigapedeRed = new(0x7B, 0x51, 0x57);
+	public static readonly Color Gigapede = new(0x47, 0x8B, 0x41);
+	public static readonly Color Ghostpede = new(0xC8, 0xA2, 0xC8);
+	public static readonly Color Leviathan = new(0xFF, 0x00, 0x00);
+	public static readonly Color TheOrb = new(0xFF, 0x31, 0x31);
+	public static readonly Color Spider1 = new(0x09, 0x7A, 0x00);
+	public static readonly Color Spider2 = new(0x13, 0xFF, 0x00);
+	public static readonly Color SpiderEgg1 = new(0x99, 0xA1, 0x00);
+	public static readonly Color SpiderEgg2 = new(0x65, 0x7A, 0x00);
+	public static readonly Color Thorn = new(0x77, 0x1D, 0x00);
+	public static readonly Color Andras = new(0xFF, 0x3C, 0x00);
+	public static readonly Color Unknown = new(0x66, 0x66, 0x66);
+	public static readonly Color Void = new(0xDD, 0xDD, 0xDD);
+}
