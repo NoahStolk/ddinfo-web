4e26741211682642f99befc834d139fae1b2d0fe
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-09-12 20:38:50 +0200

    Display time criteria differently

--- src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
+++ src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
@@ -1,9 +1,13 @@
+using DevilDaggersInfo.Common.Extensions;
+using DevilDaggersInfo.Types.Web;
+
 namespace DevilDaggersInfo.Web.Core.CriteriaExpression.Parts;
 
 public record ExpressionValue(int Value) : IExpressionPart
 {
-	public override string ToString()
+	public string ToDisplayString(CustomLeaderboardCriteriaType criteriaType)
 	{
-		return Value.ToString();
+		bool isTime = criteriaType is CustomLeaderboardCriteriaType.Time or CustomLeaderboardCriteriaType.LevelUpTime2 or CustomLeaderboardCriteriaType.LevelUpTime3 or CustomLeaderboardCriteriaType.LevelUpTime4;
+		return isTime ? Value.ToSecondsTime().ToString() : Value.ToString();
 	}
 }
