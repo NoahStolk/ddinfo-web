58493887fb4e266e9782c758f33ce4ed0a164043
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-09-12 20:45:35 +0200

    Re-add old ToString behavior

--- src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
+++ src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
@@ -5,6 +5,11 @@ namespace DevilDaggersInfo.Web.Core.CriteriaExpression.Parts;
 
 public record ExpressionValue(int Value) : IExpressionPart
 {
+	public override string ToString()
+	{
+		return Value.ToString();
+	}
+
 	public string ToDisplayString(CustomLeaderboardCriteriaType criteriaType)
 	{
 		bool isTime = criteriaType is CustomLeaderboardCriteriaType.Time or CustomLeaderboardCriteriaType.LevelUpTime2 or CustomLeaderboardCriteriaType.LevelUpTime3 or CustomLeaderboardCriteriaType.LevelUpTime4;
