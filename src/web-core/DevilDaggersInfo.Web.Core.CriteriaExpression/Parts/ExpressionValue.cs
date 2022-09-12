9e06bd21b79335cf5d7ab77c685663e26a9ce1c1
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-09-12 21:35:24 +0200

    Display death name in ExpressionValue.ToDisplayString

--- src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
+++ src/web-core/DevilDaggersInfo.Web.Core.CriteriaExpression/Parts/ExpressionValue.cs
@@ -1,4 +1,7 @@
+using DevilDaggersInfo.Common;
 using DevilDaggersInfo.Common.Extensions;
+using DevilDaggersInfo.Core.Wiki;
+using DevilDaggersInfo.Core.Wiki.Objects;
 using DevilDaggersInfo.Types.Web;
 
 namespace DevilDaggersInfo.Web.Core.CriteriaExpression.Parts;
@@ -12,7 +15,13 @@ public record ExpressionValue(int Value) : IExpressionPart
 
 	public string ToDisplayString(CustomLeaderboardCriteriaType criteriaType)
 	{
+		if (criteriaType == CustomLeaderboardCriteriaType.DeathType)
+		{
+			Death? death = Deaths.GetDeathByLeaderboardType(GameConstants.CurrentVersion, (byte)Value);
+			return death.HasValue ? death.Value.Name : "???";
+		}
+
 		bool isTime = criteriaType is CustomLeaderboardCriteriaType.Time or CustomLeaderboardCriteriaType.LevelUpTime2 or CustomLeaderboardCriteriaType.LevelUpTime3 or CustomLeaderboardCriteriaType.LevelUpTime4;
-		return isTime ? Value.ToSecondsTime().ToString() : Value.ToString();
+		return isTime ? Value.ToSecondsTime().ToString(StringFormats.TimeFormat) : Value.ToString();
 	}
 }
