41f02f9ea0efc1373ceeb6f4cb1b5af3b675aa1f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-09-30 19:06:16 +0200

    Stop showing history hashes

--- src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetHistory/History.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetHistory/History.cs
@@ -8,7 +8,7 @@ namespace DevilDaggersInfo.App.Ui.SurvivalEditor.Components.SpawnsetHistory;
 
 public class History : ScrollContent<History, HistoryWrapper>
 {
-	public const int HistoryEntryHeight = 16;
+	private const int _historyEntryHeight = 16;
 
 	private readonly List<AbstractComponent> _historyComponents = new();
 
@@ -17,7 +17,7 @@ public class History : ScrollContent<History, HistoryWrapper>
 	{
 	}
 
-	public override int ContentHeightInPixels => _historyComponents.Count * HistoryEntryHeight;
+	public override int ContentHeightInPixels => _historyComponents.Count * _historyEntryHeight;
 
 	public void SetHistory()
 	{
@@ -32,16 +32,11 @@ public class History : ScrollContent<History, HistoryWrapper>
 		{
 			States.SpawnsetHistory history = SpawnsetHistoryManager.History[i];
 			bool isActive = i == SpawnsetHistoryManager.Index;
-			Button button = new(Rectangle.At(0, i * HistoryEntryHeight, Metric.Size.X, HistoryEntryHeight), () => {}, isActive ? new(0, 127, 63, 255) : new(0, 63, 0, 255), Color.Black, isActive ? new(0, 191, 127, 255) : new(0, 127, 0, 255), Color.White, $"{Convert(history.Hash.Take(4))} {history.Change}", TextAlign.Left, 2, FontSize.F8X8);
+			Button button = new(Rectangle.At(0, i * _historyEntryHeight, Metric.Size.X, _historyEntryHeight), () => {}, isActive ? new(0, 127, 63, 255) : new(0, 63, 0, 255), Color.Black, isActive ? new(0, 191, 127, 255) : new(0, 127, 0, 255), Color.White, history.Change, TextAlign.Left, 2, FontSize.F8X8);
 			_historyComponents.Add(button);
 		}
 
 		foreach (AbstractComponent component in _historyComponents)
 			NestingContext.Add(component);
-
-		string Convert(IEnumerable<byte> a)
-		{
-			return string.Join("", a.Select(b => $"{b:X2}"));
-		}
 	}
 }
