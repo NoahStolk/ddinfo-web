1e01d56ba991eb0c5bbff828d5e2b0e0cf062b6c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-10-08 00:50:37 +0200

    Stop using label components for SpawnEntry

--- src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetSpawns/SpawnEntry.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetSpawns/SpawnEntry.cs
@@ -1,4 +1,3 @@
-using DevilDaggersInfo.App.Ui.Base.Components;
 using DevilDaggersInfo.App.Ui.Base.Enums;
 using DevilDaggersInfo.App.Ui.Base.Extensions;
 using DevilDaggersInfo.App.Ui.Base.Rendering;
@@ -13,12 +12,15 @@ namespace DevilDaggersInfo.App.Ui.SurvivalEditor.Components.SpawnsetSpawns;
 
 public class SpawnEntry : AbstractComponent
 {
+	private readonly SpawnUiEntry _spawnUiEntry;
+	private readonly Enemy? _enemy;
+	private readonly Color _enemyColor;
+
 	public SpawnEntry(Rectangle metric, SpawnUiEntry spawnUiEntry)
 		: base(metric)
 	{
-		Index = spawnUiEntry.Index;
-
-		Enemy? enemy = spawnUiEntry.EnemyType switch
+		_spawnUiEntry = spawnUiEntry;
+		_enemy = spawnUiEntry.EnemyType switch
 		{
 			EnemyType.Squid1 => EnemiesV3_2.Squid1,
 			EnemyType.Squid2 => EnemiesV3_2.Squid2,
@@ -32,22 +34,9 @@ public class SpawnEntry : AbstractComponent
 			EnemyType.Ghostpede => EnemiesV3_2.Ghostpede,
 			_ => null,
 		};
-		Color backgroundColor = enemy == null ? Color.Gray(0.75f) : new(enemy.Color.R, enemy.Color.G, enemy.Color.B, byte.MaxValue);
+		_enemyColor = _enemy == null ? Color.Gray(0.75f) : new(_enemy.Color.R, _enemy.Color.G, _enemy.Color.B, byte.MaxValue);
 
-		NestingContext.Add(new Label(Rectangle.At(0, 0, 96, Spawns.SpawnEntryHeight), backgroundColor, enemy?.Name ?? "Empty", TextAlign.Left, FontSize.F8X8));
-		NestingContext.Add(new Label(Rectangle.At(96, 0, 96, Spawns.SpawnEntryHeight), Color.White, spawnUiEntry.Delay.ToString("0.0000"), TextAlign.Right, FontSize.F8X8));
-		NestingContext.Add(new Label(Rectangle.At(192, 0, 96, Spawns.SpawnEntryHeight), Color.White, spawnUiEntry.Seconds.ToString("0.0000"), TextAlign.Right, FontSize.F8X8));
-		NestingContext.Add(new Label(Rectangle.At(288, 0, 48, Spawns.SpawnEntryHeight), Color.White, NoFarmGemsString(spawnUiEntry.NoFarmGems), TextAlign.Right, FontSize.F8X8));
-		NestingContext.Add(new Label(Rectangle.At(336, 0, 48, Spawns.SpawnEntryHeight), GetColorFromHand(spawnUiEntry.GemState.HandLevel), spawnUiEntry.GemState.Value.ToString(), TextAlign.Right, FontSize.F8X8));
-
-		Color GetColorFromHand(HandLevel handLevel) => handLevel switch
-		{
-			HandLevel.Level3 => UpgradeColors.Level3.ToWarpColor(),
-			HandLevel.Level4 => UpgradeColors.Level4.ToWarpColor(),
-			_ => Color.Red,
-		};
-
-		static string NoFarmGemsString(int gems) => gems == 0 ? "-" : $"+{gems}";
+		Index = spawnUiEntry.Index;
 	}
 
 	public bool IsSelected { get; set; }
@@ -74,5 +63,34 @@ public class SpawnEntry : AbstractComponent
 		};
 		if (background != Color.Transparent)
 			RenderBatchCollector.RenderRectangleTopLeft(Metric.Size, parentPosition + Metric.TopLeft, Depth, background);
+
+		RenderText(Rectangle.At(Metric.X1, Metric.Y1, 96, Spawns.SpawnEntryHeight), _enemyColor, _enemy?.Name ?? "Empty", TextAlign.Left, FontSize.F8X8);
+		RenderText(Rectangle.At(Metric.X1 + 96, Metric.Y1, 96, Spawns.SpawnEntryHeight), Color.White, _spawnUiEntry.Delay.ToString("0.0000"), TextAlign.Right, FontSize.F8X8);
+		RenderText(Rectangle.At(Metric.X1 + 192, Metric.Y1, 96, Spawns.SpawnEntryHeight), Color.White, _spawnUiEntry.Seconds.ToString("0.0000"), TextAlign.Right, FontSize.F8X8);
+		RenderText(Rectangle.At(Metric.X1 + 288, Metric.Y1, 48, Spawns.SpawnEntryHeight), Color.White, NoFarmGemsString(_spawnUiEntry.NoFarmGems), TextAlign.Right, FontSize.F8X8);
+		RenderText(Rectangle.At(Metric.X1 + 336, Metric.Y1, 48, Spawns.SpawnEntryHeight), GetColorFromHand(_spawnUiEntry.GemState.HandLevel), _spawnUiEntry.GemState.Value.ToString(), TextAlign.Right, FontSize.F8X8);
+
+		void RenderText(Rectangle metric, Color textColor, string text, TextAlign textAlign, FontSize fontSize)
+		{
+			int padding = (int)MathF.Round((metric.Y2 - metric.Y1) / 4f);
+			Vector2i<int> textPosition = textAlign switch
+			{
+				TextAlign.Middle => new Vector2i<int>(metric.X1 + metric.X2, metric.Y1 + metric.Y2) / 2,
+				TextAlign.Left => new(metric.X1 + padding, metric.Y1 + padding),
+				TextAlign.Right => new(metric.X2 - padding, metric.Y1 + padding),
+				_ => throw new InvalidOperationException("Invalid text align."),
+			};
+
+			RenderBatchCollector.RenderMonoSpaceText(fontSize, Vector2i<int>.One, parentPosition + textPosition, Depth + 2, textColor, text, textAlign);
+		}
+
+		Color GetColorFromHand(HandLevel handLevel) => handLevel switch
+		{
+			HandLevel.Level3 => UpgradeColors.Level3.ToWarpColor(),
+			HandLevel.Level4 => UpgradeColors.Level4.ToWarpColor(),
+			_ => Color.Red,
+		};
+
+		static string NoFarmGemsString(int gems) => gems == 0 ? "-" : $"+{gems}";
 	}
 }
