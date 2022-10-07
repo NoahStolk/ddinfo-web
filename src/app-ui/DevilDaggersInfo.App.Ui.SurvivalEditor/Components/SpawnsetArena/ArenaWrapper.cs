dc38779a4e4c70da637c2b1efe87b4886361c958
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-10-07 09:17:07 +0200

    Move height buttons to separate component

--- src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetArena/ArenaWrapper.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetArena/ArenaWrapper.cs
@@ -8,8 +8,6 @@ namespace DevilDaggersInfo.App.Ui.SurvivalEditor.Components.SpawnsetArena;
 
 public class ArenaWrapper : AbstractComponent
 {
-	private const int _arenaButtonSize = 20;
-
 	private readonly ShrinkSlider _shrinkSlider;
 	private readonly Arena _arena;
 
@@ -19,39 +17,15 @@ public class ArenaWrapper : AbstractComponent
 		const int titleHeight = 48;
 
 		_arena = new(new(0, titleHeight), 6);
-		NestingContext.Add(_arena);
 
 		Label title = new(Rectangle.At(0, 0, _arena.Metric.Size.X, titleHeight), Color.White, "Arena", TextAlign.Middle, FontSize.F12X12);
-		NestingContext.Add(title);
-
-		int buttonsOffsetX = _arena.Metric.Size.X + 8;
-
-		Span<float> heights = stackalloc float[] { -1000, -1.1f, -1.01f, -1, -0.8f, -0.6f, -0.4f, -0.2f };
-		for (int i = 0; i < heights.Length; i++)
-		{
-			float height = heights[i];
-			int offsetX = i % 4 * _arenaButtonSize;
-			int offsetY = i / 4 * _arenaButtonSize;
-			AddHeightButton(height, buttonsOffsetX + offsetX, titleHeight + offsetY);
-		}
-
-		for (int i = 0; i < 32; i++)
-		{
-			int offsetX = i % 4 * _arenaButtonSize;
-			int offsetY = i / 4 * _arenaButtonSize;
-
-			float height = i < 16 ? i : 16 + (i - 16) * 2;
-			AddHeightButton(height, buttonsOffsetX + offsetX, titleHeight + offsetY + _arenaButtonSize * 2);
-		}
-
+		ArenaHeightButtons arenaHeightButtons = new(Rectangle.At(_arena.Metric.Size.X + 8, titleHeight, 80, 320));
 		_shrinkSlider = new(Rectangle.At(0, _arena.Metric.TopLeft.Y + _arena.Metric.Size.Y + 8, _arena.Metric.Size.X, 16), _arena.SetShrinkCurrent, true, 0, StateManager.SpawnsetState.Spawnset.GetSliderMaxSeconds(), 0.001f, 0, 2, Color.White);
-		NestingContext.Add(_shrinkSlider);
-	}
 
-	private void AddHeightButton(float height, int offsetX, int offsetY)
-	{
-		HeightButton button = new(Rectangle.At(offsetX, offsetY, _arenaButtonSize, _arenaButtonSize), () => StateManager.SetArenaSelectedHeight(height), height);
-		NestingContext.Add(button);
+		NestingContext.Add(title);
+		NestingContext.Add(_arena);
+		NestingContext.Add(arenaHeightButtons);
+		NestingContext.Add(_shrinkSlider);
 	}
 
 	public void SetSpawnset()
