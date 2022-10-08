b4d85558852e5d131410728846a1b801dc84bdff
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-10-08 15:58:04 +0200

    Add delay text input

--- src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetSpawns/SpawnEditor.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.SurvivalEditor/Components/SpawnsetSpawns/SpawnEditor.cs
@@ -1,3 +1,4 @@
+using DevilDaggersInfo.App.Ui.Base;
 using DevilDaggersInfo.App.Ui.Base.Components;
 using DevilDaggersInfo.App.Ui.Base.Enums;
 using DevilDaggersInfo.App.Ui.Base.Extensions;
@@ -11,21 +12,49 @@ namespace DevilDaggersInfo.App.Ui.SurvivalEditor.Components.SpawnsetSpawns;
 
 public class SpawnEditor : AbstractComponent
 {
+	private readonly Dictionary<EnemyType, Button> _enemyTypeButtons = new();
+
+	private EnemyType _selectedEnemyType;
+	private float _selectedDelay;
+
 	public SpawnEditor(Rectangle metric)
 		: base(metric)
 	{
 		const int width = 80;
 		const int height = 16;
 
-		int i = 0;
-		foreach (EnemyType enemyType in Enum.GetValues<EnemyType>())
-		{
-			Enemy? enemy = enemyType.GetEnemy();
-			Color enemyColor = enemy?.Color.ToWarpColor() ?? Color.Gray(0.75f);
-			Button button = new(Rectangle.At(i % 3 * width, i / 3 * height, width, height), () => {}, enemyColor, Color.White, Color.Invert(enemyColor), enemyColor.ReadableColorForBrightness(), enemy?.Name ?? "Empty", TextAlign.Left, 2, FontSize.F8X8);
-			NestingContext.Add(button);
+		AddEnemyButton(0, 0, EnemyType.Empty, Color.Gray(0.75f), "Empty");
 
-			i++;
+		for (int i = 0; i < 10; i++)
+		{
+			EnemyType enemyType = (EnemyType)i;
+			Enemy? enemy = enemyType.GetEnemy();
+			if (enemy != null)
+				AddEnemyButton(i % 2 * width, height + i / 2 * height, enemyType, enemy.Color.ToWarpColor(), enemy.Name);
 		}
+
+		void OnDelayChange(string s)
+		{
+			ParseUtils.TryParseAndExecute<float>(s, 0, f => _selectedDelay = f);
+		}
+
+		TextInput delayTextInput = ComponentBuilder.CreateTextInput(Rectangle.At(width * 2, 0, 64, 16), true, OnDelayChange, OnDelayChange, OnDelayChange);
+		NestingContext.Add(delayTextInput);
+
+		void AddEnemyButton(int x, int y, EnemyType enemyType, Color enemyColor, string enemyName)
+		{
+			Color borderColor = _selectedEnemyType == enemyType ? Color.White : Color.Black;
+			Button button = new(Rectangle.At(x, y, width, height), () => SetSelectedEnemyType(enemyType), enemyColor, borderColor, Color.Invert(enemyColor), enemyColor.ReadableColorForBrightness(), enemyName, TextAlign.Left, 2, FontSize.F8X8);
+			_enemyTypeButtons.Add(enemyType, button);
+			NestingContext.Add(button);
+		}
+	}
+
+	private void SetSelectedEnemyType(EnemyType enemyType)
+	{
+		_selectedEnemyType = enemyType;
+
+		foreach (KeyValuePair<EnemyType, Button> kvp in _enemyTypeButtons)
+			kvp.Value.BorderColor = _selectedEnemyType == kvp.Key ? Color.White : Color.Black;
 	}
 }
