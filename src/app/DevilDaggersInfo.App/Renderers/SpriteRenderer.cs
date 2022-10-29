50e3092f6395493cddf3a01e18fcff5fcb34741d
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-10-29 20:35:02 +0200

    Fix face order in SpriteRenderer

--- src/app/DevilDaggersInfo.App/Renderers/SpriteRenderer.cs
+++ src/app/DevilDaggersInfo.App/Renderers/SpriteRenderer.cs
@@ -21,12 +21,12 @@ public class SpriteRenderer
 		float[] vertices =
 		{
 			-0.5f, +0.5f, +0.0f, +1.0f, // top left
-			+0.5f, +0.5f, +1.0f, +1.0f, // top right
 			-0.5f, -0.5f, +0.0f, +0.0f, // bottom left
+			+0.5f, +0.5f, +1.0f, +1.0f, // top right
 
 			+0.5f, +0.5f, +1.0f, +1.0f, // top right
-			+0.5f, -0.5f, +1.0f, +0.0f, // bottom right
 			-0.5f, -0.5f, +0.0f, +0.0f, // bottom left
+			+0.5f, -0.5f, +1.0f, +0.0f, // bottom right
 		};
 
 		fixed (float* v = &vertices[0])
