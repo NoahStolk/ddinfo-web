19fb5942afaa6a30ff29d90c05f93be5ee64a7c1
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-01 20:54:33 +0100

    Update to SDK 7.0.100-rc.2.22477.23

--- src/app-ui/DevilDaggersInfo.App.Ui.Base/ParseUtils.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.Base/ParseUtils.cs
@@ -7,14 +7,14 @@ public static class ParseUtils
 	public static void TryParseAndExecute<T>(string input, Action<T> action)
 		where T : IParsable<T>
 	{
-		if (T.TryParse(input, null, out T v))
+		if (T.TryParse(input, null, out T? v))
 			action(v);
 	}
 
 	public static void TryParseAndExecute<T>(string input, T min, Action<T> action)
 		where T : IParsable<T>, INumber<T>
 	{
-		if (T.TryParse(input, null, out T v) && v >= min)
+		if (T.TryParse(input, null, out T? v) && v >= min)
 			action(v);
 	}
 }
