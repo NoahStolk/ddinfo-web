536b95b4992d99fb4d491c5d67fc32965a5f741f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-02 21:22:31 +0100

    Add edit state objects for mods, players, and spawnsets

--- src/web-client/DevilDaggersInfo.Web.Client/Pages/Admin/Mods/EditPage.cs
+++ src/web-client/DevilDaggersInfo.Web.Client/Pages/Admin/Mods/EditPage.cs
@@ -4,6 +4,7 @@ using DevilDaggersInfo.Api.Admin.Players;
 using DevilDaggersInfo.Common.Extensions;
 using DevilDaggersInfo.Web.Client.Components.Admin;
 using DevilDaggersInfo.Web.Client.Enums;
+using DevilDaggersInfo.Web.Client.StateObjects.Admin.Mods;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
@@ -13,7 +14,7 @@ namespace DevilDaggersInfo.Web.Client.Pages.Admin.Mods;
 public partial class EditPage
 {
 	private Dictionary<int, string>? _playerNames;
-	private readonly EditMod _editMod = new();
+	private readonly EditModState _editModState = new();
 	private readonly List<string> _binaryFileSizeErrors = new();
 	private readonly List<string> _screenshotFileSizeErrors = new();
 
@@ -22,7 +23,7 @@ public partial class EditPage
 	private IReadOnlyList<string>? _binaryNames;
 	private IReadOnlyList<string>? _screenshotNames;
 
-	private async Task PopulateAsync(AdminEdit<EditMod> editComponent)
+	private async Task PopulateAsync(AdminEdit<EditModState, EditMod> editComponent)
 	{
 		try
 		{
@@ -30,13 +31,13 @@ public partial class EditPage
 			_playerNames = getPlayerNames.ToDictionary(m => m.Id, m => m.PlayerName);
 
 			GetMod getMod = await Http.GetModById(Id);
-			_editMod.ModTypes = getMod.ModTypes.AsEnumerable().ToList();
-			_editMod.HtmlDescription = getMod.HtmlDescription;
-			_editMod.IsHidden = getMod.IsHidden;
-			_editMod.Name = getMod.Name;
-			_editMod.PlayerIds = getMod.PlayerIds;
-			_editMod.TrailerUrl = getMod.TrailerUrl;
-			_editMod.Url = getMod.Url;
+			_editModState.ModTypes = getMod.ModTypes.AsEnumerable().ToList();
+			_editModState.HtmlDescription = getMod.HtmlDescription;
+			_editModState.IsHidden = getMod.IsHidden;
+			_editModState.Name = getMod.Name;
+			_editModState.PlayerIds = getMod.PlayerIds;
+			_editModState.TrailerUrl = getMod.TrailerUrl;
+			_editModState.Url = getMod.Url;
 
 			_binaryNames = getMod.BinaryNames;
 			_screenshotNames = getMod.ScreenshotNames;
@@ -58,11 +59,11 @@ public partial class EditPage
 	private async Task LoadBinaries(InputFileChangeEventArgs e)
 	{
 		Dictionary<string, byte[]> files = await GetFiles(e, ModConstants.BinaryMaxFiles, ModConstants.BinaryMaxFileSize, _binaryFileSizeErrors);
-		_editMod.Binaries = files.Select(kvp => new BinaryData { Name = kvp.Key, Data = kvp.Value }).ToList();
+		_editModState.Binaries = files.Select(kvp => new BinaryData { Name = kvp.Key, Data = kvp.Value }).ToList();
 	}
 
 	private async Task LoadScreenshots(InputFileChangeEventArgs e)
 	{
-		_editMod.Screenshots = await GetFiles(e, ModConstants.ScreenshotMaxFiles, ModConstants.ScreenshotMaxFileSize, _screenshotFileSizeErrors);
+		_editModState.Screenshots = await GetFiles(e, ModConstants.ScreenshotMaxFiles, ModConstants.ScreenshotMaxFileSize, _screenshotFileSizeErrors);
 	}
 }
---
+++ src/web-client/DevilDaggersInfo.Web.Client/StateObjects/Admin/Mods/EditModState.cs
@@ -0,0 +1,51 @@
+using DevilDaggersInfo.Api.Admin;
+using DevilDaggersInfo.Api.Admin.Mods;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevilDaggersInfo.Web.Client.StateObjects.Admin.Mods;
+
+public class EditModState : IStateObject<EditMod>
+{
+	[StringLength(64)]
+	public string Name { get; set; } = string.Empty;
+
+	public bool IsHidden { get; set; }
+
+	[StringLength(64)]
+	public string? TrailerUrl { get; set; }
+
+	[StringLength(2048)]
+	public string? HtmlDescription { get; set; }
+
+	public List<int>? ModTypes { get; set; }
+
+	[StringLength(128)]
+	public string? Url { get; set; }
+
+	public List<int>? PlayerIds { get; set; }
+
+	public List<string> BinariesToDelete { get; set; } = new();
+
+	[MaxLength(ModConstants.BinaryMaxFiles, ErrorMessage = ModConstants.BinaryMaxFilesErrorMessage)]
+	public List<BinaryData> Binaries { get; set; } = new();
+
+	public List<string> ScreenshotsToDelete { get; set; } = new();
+
+	[MaxLength(ModConstants.ScreenshotMaxFiles, ErrorMessage = ModConstants.ScreenshotMaxFilesErrorMessage)]
+	public Dictionary<string, byte[]> Screenshots { get; set; } = new();
+
+	public EditMod ToModel() => new()
+	{
+		Name = Name,
+		IsHidden = IsHidden,
+		TrailerUrl = TrailerUrl,
+		HtmlDescription = HtmlDescription,
+		ModTypes = ModTypes,
+		Url = Url,
+		PlayerIds = PlayerIds,
+		BinariesToDelete = BinariesToDelete,
+		Binaries = Binaries,
+		ScreenshotsToDelete = ScreenshotsToDelete,
+		Screenshots = Screenshots,
+	};
+}
