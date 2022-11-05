9bcff599e915d8da961ce54ae7eabf2771ad4c50
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-11-05 20:01:24 +0100

    Start CL list implementation

--- src/app-ui/DevilDaggersInfo.App.Ui.CustomLeaderboardsRecorder/Components/LeaderboardList.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.CustomLeaderboardsRecorder/Components/LeaderboardList.cs
@@ -1,4 +1,11 @@
+using DevilDaggersInfo.Api.Ddcl;
+using DevilDaggersInfo.Api.Ddcl.CustomLeaderboards;
+using DevilDaggersInfo.App.Core.ApiClient;
+using DevilDaggersInfo.App.Core.ApiClient.TaskHandlers;
+using DevilDaggersInfo.App.Ui.Base;
+using DevilDaggersInfo.App.Ui.Base.Components;
 using DevilDaggersInfo.App.Ui.Base.Rendering;
+using DevilDaggersInfo.Types.Web;
 using Warp.Numerics;
 using Warp.Ui;
 using Warp.Ui.Components;
@@ -7,9 +14,40 @@ namespace DevilDaggersInfo.App.Ui.CustomLeaderboardsRecorder.Components;
 
 public class LeaderboardList : AbstractComponent
 {
+	private readonly List<TextButton> _leaderboardComponents = new(); // TODO: Custom component.
+
 	public LeaderboardList(Rectangle metric)
 		: base(metric)
 	{
+		Button button = new(Rectangle.At(0, 0, 16, 16), Load, GlobalStyles.DefaultButtonStyle);
+		NestingContext.Add(button);
+	}
+
+	public void Load() // TODO: Load on initialize.
+	{
+		AsyncHandler.Run(Populate, () => FetchCustomLeaderboards.HandleAsync(CustomLeaderboardCategory.Survival, 0, 10, 1, false));
+
+		void Populate(Page<GetCustomLeaderboardForOverview>? cls)
+		{
+			foreach (TextButton leaderboardComponent in _leaderboardComponents)
+				NestingContext.Remove(leaderboardComponent);
+
+			_leaderboardComponents.Clear();
+
+			if (cls == null)
+				return;
+
+			int y = 0;
+			foreach (GetCustomLeaderboardForOverview cl in cls.Results)
+			{
+				const int height = 16;
+				_leaderboardComponents.Add(new TextButton(Rectangle.At(0, y, 256, height), () => {}, GlobalStyles.DefaultButtonStyle, GlobalStyles.DefaultLeft, cl.SpawnsetName));
+				y += height;
+			}
+
+			foreach (TextButton leaderboardComponent in _leaderboardComponents)
+				NestingContext.Add(leaderboardComponent);
+		}
 	}
 
 	public override void Render(Vector2i<int> parentPosition)
