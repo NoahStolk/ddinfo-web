18a349f5d77a0fc4289a12da741de6097a49d69c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-12-16 17:01:49 +0100

    Make GetCommonName immutable

--- src/api/DevilDaggersInfo.Api.DdLive/Players/GetCommonName.cs
+++ src/api/DevilDaggersInfo.Api.DdLive/Players/GetCommonName.cs
@@ -2,7 +2,7 @@ namespace DevilDaggersInfo.Api.DdLive.Players;
 
 public record GetCommonName
 {
-	public required int Id { get; set; }
+	public required int Id { get; init; }
 
-	public required string CommonName { get; set; }
+	public required string CommonName { get; init; }
 }
