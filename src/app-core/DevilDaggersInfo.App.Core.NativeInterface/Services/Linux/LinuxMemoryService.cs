c7192e76521741e55789f203d2bdeec1c9a5e278
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-12-22 23:46:23 +0100

    Remove NotImplementedException from LinuxMemoryService

--- src/app-core/DevilDaggersInfo.App.Core.NativeInterface/Services/Linux/LinuxMemoryService.cs
+++ src/app-core/DevilDaggersInfo.App.Core.NativeInterface/Services/Linux/LinuxMemoryService.cs
@@ -2,20 +2,19 @@ using System.Diagnostics;
 
 namespace DevilDaggersInfo.App.Core.NativeInterface.Services.Linux;
 
+// TODO: Implement.
 public class LinuxMemoryService : INativeMemoryService
 {
 	public void WriteMemory(Process process, long address, byte[] bytes, int offset, int size)
 	{
-		throw new NotImplementedException();
 	}
 
 	public void ReadMemory(Process process, long address, byte[] bytes, int offset, int size)
 	{
-		throw new NotImplementedException();
 	}
 
 	public Process? GetDevilDaggersProcess()
 	{
-		throw new NotImplementedException();
+		return null;
 	}
 }
