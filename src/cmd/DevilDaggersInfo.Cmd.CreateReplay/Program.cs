ad6a93879b1154198ac1197f394f244a08925b05
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-12-27 23:47:17 +0100

    Test replay dagger jump heights

--- src/cmd/DevilDaggersInfo.Cmd.CreateReplay/Program.cs
+++ src/cmd/DevilDaggersInfo.Cmd.CreateReplay/Program.cs
@@ -1,6 +1,36 @@
-using DevilDaggersInfo.Cmd.CreateReplay;
+using DevilDaggersInfo.Core.Replay.Events.Interfaces;
+using DevilDaggersInfo.Types.Core.Replays;
 
 // File.WriteAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\TEST.ddreplay", new RandomReplayWriter().Write().Compile());
 // File.WriteAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\skull-2-analysis-done_113.72-xvlv-0be4d3b8[2d05a83a].ddreplay", new Skull2TargetVisualizerReplayWriter().Write().Compile());
 // File.WriteAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\deaths_1.00-xvlv-0be4d3b8[2d05a83a].ddreplay", new DeathsReplayWriter().Write().Compile());
-File.WriteAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\111-exposed-ninja-skull2_335.77-Derkan-0c0e40f3.ddreplay", new Skull2TargetVisualizerReplayWriter().Write().Compile());
+// File.WriteAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\DJUMPMODIFIED.ddreplay", new DaggerDirectionModifier().Write().Compile());
+
+ReplayBinary<LocalReplayBinaryHeader> original = new(File.ReadAllBytes(@"C:\Users\NOAH\AppData\Roaming\DevilDaggers\replays\PSY_DJUMP.ddreplay"));
+
+int tick = 0;
+int consecutiveHoldCount = 0;
+int consecutiveNoneCount = 0;
+foreach (IInputsEvent e in original.EventsData.Events.OfType<IInputsEvent>())
+{
+	if (e.Shoot == ShootType.None)
+	{
+		consecutiveNoneCount++;
+	}
+	else if (e.Shoot == ShootType.Hold)
+	{
+		consecutiveHoldCount++;
+	}
+	else if (e.Shoot == ShootType.Release)
+	{
+		Console.WriteLine($"Not using LMB for {consecutiveNoneCount} ticks");
+		Console.WriteLine($"Holding LMB for   {consecutiveHoldCount} ticks");
+		Console.WriteLine($"Released at       {tick / 60f:00.0000} seconds");
+		Console.WriteLine("===");
+
+		consecutiveNoneCount = 0;
+		consecutiveHoldCount = 0;
+	}
+
+	tick++;
+}
--- src/core/DevilDaggersInfo.Core.Replay/Numerics/Int16Mat3x3.cs
+++ src/core/DevilDaggersInfo.Core.Replay/Numerics/Int16Mat3x3.cs
@@ -1,8 +1,24 @@
+// ReSharper disable InconsistentNaming
 namespace DevilDaggersInfo.Core.Replay.Numerics;
 
 public readonly record struct Int16Mat3x3(short M11, short M12, short M13, short M21, short M22, short M23, short M31, short M32, short M33)
 {
 	public static Int16Mat3x3 Identity { get; } = new(1, 0, 0, 0, 1, 0, 0, 0, 1);
+
+	// TODO: Untested.
+	public static Int16Mat3x3 FromMatrix4x4(Matrix4x4 matrix4x4)
+	{
+		return new(
+			(short)(matrix4x4.M11 * short.MaxValue),
+			(short)(matrix4x4.M12 * short.MaxValue),
+			(short)(matrix4x4.M13 * short.MaxValue),
+			(short)(matrix4x4.M21 * short.MaxValue),
+			(short)(matrix4x4.M22 * short.MaxValue),
+			(short)(matrix4x4.M23 * short.MaxValue),
+			(short)(matrix4x4.M31 * short.MaxValue),
+			(short)(matrix4x4.M32 * short.MaxValue),
+			(short)(matrix4x4.M33 * short.MaxValue));
+	}
 
 	public override string ToString()
 		=> $"<{M11}, {M12}, {M13}> <{M21}, {M22}, {M23}> <{M31}, {M32}, {M33}>";
