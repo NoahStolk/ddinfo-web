5f17bdf2b74766e8e2068ca2282698ba0f3112fd
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-12-27 21:39:04 +0100

    Allow multiple actions of the same type at the same time
    This can now safely be done, because the event handlers will only execute once regardless.

--- src/app-ui/DevilDaggersInfo.App.Ui.Base/StateManagement/StateManager.cs
+++ src/app-ui/DevilDaggersInfo.App.Ui.Base/StateManagement/StateManager.cs
@@ -1,13 +1,12 @@
 using DevilDaggersInfo.App.Ui.Base.StateManagement.Base.States;
 using DevilDaggersInfo.App.Ui.Base.StateManagement.CustomLeaderboardsRecorder.States;
 using DevilDaggersInfo.App.Ui.Base.StateManagement.SurvivalEditor.States;
-using Warp.NET.Debugging;
 
 namespace DevilDaggersInfo.App.Ui.Base.StateManagement;
 
 public static class StateManager
 {
-	private static readonly Dictionary<Type, IAction> _actionsToDispatch = new();
+	private static readonly Dictionary<Type, List<IAction>> _actionsToDispatch = new();
 	private static readonly Dictionary<Type, List<Action>> _eventHandlers = new();
 
 	// TODO: These should only be able to be set by actions.
@@ -28,6 +27,22 @@ public static class StateManager
 	public static SpawnsetHistoryState SpawnsetHistoryState { get; set; } = SpawnsetHistoryState.GetDefault();
 	public static SpawnsetState SpawnsetState { get; set; } = SpawnsetState.GetDefault();
 
+	/// <summary>
+	/// <para>
+	/// Subscribes this <paramref name="eventHandler"/> to the specified <typeparamref name="TAction"/>.
+	/// </para>
+	/// <para>
+	/// When one or more instances of this action type are dispatched, the event handler will be called <b>once</b> when all the actions are reduced.
+	/// </para>
+	/// <para>
+	/// If this event handler is subscribed to multiple action types which are reduced at the same time, the event handler will still only be called once.
+	/// </para>
+	/// <para>
+	/// This is done to prevent the event handler from executing twice, which can lead to corrupted components or crashes. Note that <see cref="Warp.NET.Ui.NestingContext"/> also does not allow the same components to be added or removed more than once per update.
+	/// </para>
+	/// </summary>
+	/// <param name="eventHandler">The event handler.</param>
+	/// <typeparam name="TAction">The action type to subscribe to.</typeparam>
 	public static void Subscribe<TAction>(Action eventHandler)
 		where TAction : class, IAction
 	{
@@ -38,36 +53,32 @@ public static class StateManager
 	}
 
 	/// <summary>
-	/// Dispatches an action, if it already exists for this action type, overwrite it.
+	/// Dispatches an action.
 	/// </summary>
 	/// <param name="action">The instance of the action.</param>
 	/// <typeparam name="TAction">The type of the action.</typeparam>
 	public static void Dispatch<TAction>(TAction action)
 		where TAction : class, IAction
 	{
-		if (_actionsToDispatch.ContainsKey(typeof(TAction)))
-		{
-			DebugStack.Add($"Overwriting action {typeof(TAction)} {_actionsToDispatch[typeof(TAction)]} with {action}.", 5, "WARN");
-			_actionsToDispatch[typeof(TAction)] = action;
-		}
-		else
-		{
-			_actionsToDispatch.Add(typeof(TAction), action);
-		}
+		if (!_actionsToDispatch.ContainsKey(typeof(TAction)))
+			_actionsToDispatch.Add(typeof(TAction), new());
+
+		_actionsToDispatch[typeof(TAction)].Add(action);
 	}
 
 	public static void ReduceAll()
 	{
 		// Copy the actions, because new actions can be dispatched while reducing.
-		List<KeyValuePair<Type, IAction>> copy = _actionsToDispatch.ToList();
+		List<KeyValuePair<Type, List<IAction>>> copy = _actionsToDispatch.ToList();
 		_actionsToDispatch.Clear();
 
 		// Collect all the event handlers.
 		List<Action> allEventHandlers = new();
-		foreach (KeyValuePair<Type, IAction> kvp in copy)
+		foreach (KeyValuePair<Type, List<IAction>> kvp in copy)
 		{
-			// Reduce the action first.
-			kvp.Value.Reduce();
+			// Reduce the actions first.
+			foreach (IAction action in kvp.Value)
+				action.Reduce();
 
 			if (_eventHandlers.TryGetValue(kvp.Key, out List<Action>? eventHandlers))
 			{
