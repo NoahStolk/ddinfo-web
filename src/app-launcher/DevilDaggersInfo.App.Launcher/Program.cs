63daf443435692ba6e9491fc1bd9a1d077143201
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-02-06 21:19:23 +0100

    Refactor launcher and get current version from installation

--- src/app-launcher/DevilDaggersInfo.App.Launcher/Program.cs
+++ src/app-launcher/DevilDaggersInfo.App.Launcher/Program.cs
@@ -1,83 +1,17 @@
-﻿using DevilDaggersInfo.Api.App.Updates;
-using DevilDaggersInfo.App.Core.ApiClient;
-using DevilDaggersInfo.Core.Versioning;
-using DevilDaggersInfo.Types.Web;
-using System.Diagnostics;
-using System.IO.Compression;
+using DevilDaggersInfo.App.Launcher;
 
 // TODO: Check launcher version and close if outdated, telling the user to update the launcher itself first.
-string installationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "DDINFO");
 
-#if WINDOWS
-const ToolBuildType toolBuildType = ToolBuildType.WindowsWarp;
-#elif LINUX
-const ToolBuildType toolBuildType = ToolBuildType.LinuxWarp;
-#endif
+UpdateLogic updateLogic = new();
 
-if (FindExecutableFileName() == null)
+if (!updateLogic.IsInstalled())
 {
-	// App is not installed, so install it.
-	await DownloadAndInstall();
+	await updateLogic.DownloadAndInstall();
 }
-else if (await ShouldUpdate())
+else if (await updateLogic.ShouldUpdate())
 {
-	// App is outdated, clean the old files and install the new version.
-	foreach (string file in Directory.GetFiles(installationDirectory))
-		File.Delete(file);
-
-	await DownloadAndInstall();
+	updateLogic.CleanOldInstallation();
+	await updateLogic.DownloadAndInstall();
 }
 
-// Always launch the app after checking for updates.
-Launch();
-
-async Task<bool> ShouldUpdate()
-{
-	Console.WriteLine("Checking for updates...");
-	GetLatestVersion latestVersion = await AsyncHandler.Client.GetLatestVersion(ToolPublishMethod.SelfContained, toolBuildType);
-	if (!AppVersion.TryParse(latestVersion.VersionNumber, out AppVersion? onlineVersion))
-	{
-		Console.WriteLine($"Could not parse '{latestVersion.VersionNumber}' as version number.");
-		return false;
-	}
-
-	// TODO: Get version from installed app.
-	AppVersion currentVersion = new(0, 0, 0);
-	if (onlineVersion > currentVersion)
-	{
-		Console.WriteLine($"New version available: {onlineVersion}");
-		return true;
-	}
-
-	Console.WriteLine("No new version available.");
-	return false;
-}
-
-async Task DownloadAndInstall()
-{
-	GetLatestVersionFile file = await AsyncHandler.Client.GetLatestVersionFile(ToolPublishMethod.SelfContained, toolBuildType);
-	using MemoryStream ms = new(file.ZipFileContents);
-	using ZipArchive archive = new(ms);
-	archive.ExtractToDirectory(installationDirectory, true);
-}
-
-void Launch()
-{
-	string? executableFileName = FindExecutableFileName();
-	if (executableFileName == null)
-	{
-		Console.WriteLine($"Could not launch new version. Please launch it manually. It is installed at {installationDirectory}.");
-		return;
-	}
-
-	Process process = new();
-	process.StartInfo.FileName = executableFileName;
-	process.StartInfo.WorkingDirectory = installationDirectory;
-	process.Start();
-}
-
-string? FindExecutableFileName()
-{
-	// TODO: Use something else on Linux.
-	return Array.Find(Directory.GetFiles(installationDirectory), f => Path.GetExtension(f) == ".exe");
-}
+updateLogic.Launch();
---
+++ src/app-launcher/DevilDaggersInfo.App.Launcher/UpdateLogic.cs
@@ -0,0 +1,99 @@
+using DevilDaggersInfo.Api.App.Updates;
+using DevilDaggersInfo.App.Core.ApiClient;
+using DevilDaggersInfo.Core.Versioning;
+using DevilDaggersInfo.Types.Web;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace DevilDaggersInfo.App.Launcher;
+
+public class UpdateLogic
+{
+#if WINDOWS
+	private const ToolBuildType _toolBuildType = ToolBuildType.WindowsWarp;
+#elif LINUX
+	private const ToolBuildType _toolBuildType = ToolBuildType.LinuxWarp;
+#endif
+
+	private readonly string _installationDirectory;
+
+	public UpdateLogic()
+	{
+		_installationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "DDINFO");
+	}
+
+	public async Task<bool> ShouldUpdate()
+	{
+		Cmd.WriteLine(ConsoleColor.Yellow, "Checking for updates...");
+		GetLatestVersion latestVersion = await AsyncHandler.Client.GetLatestVersion(ToolPublishMethod.SelfContained, _toolBuildType);
+		if (!AppVersion.TryParse(latestVersion.VersionNumber, out AppVersion? onlineVersion))
+		{
+			Cmd.WriteLine(ConsoleColor.Red, $"Could not parse response from API as a valid version number: '{latestVersion.VersionNumber}'");
+			return false;
+		}
+
+		string executableFileName = FindExecutableFileName() ?? throw new InvalidOperationException("ShouldUpdate can only be called when the app is installed.");
+		FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(executableFileName);
+		if (versionInfo.FileVersion == null)
+		{
+			Cmd.WriteLine(ConsoleColor.Red, "Could not get file version from installed executable.");
+			return true;
+		}
+
+		AppVersion currentVersion = AppVersion.Parse(versionInfo.FileVersion);
+		if (onlineVersion > currentVersion)
+		{
+			Cmd.WriteLine(ConsoleColor.Green, $"New version available: {onlineVersion}");
+			return true;
+		}
+
+		Cmd.WriteLine(ConsoleColor.Green, "No new version available.");
+		return false;
+	}
+
+	public void CleanOldInstallation()
+	{
+		if (!Directory.Exists(_installationDirectory))
+			return;
+
+		foreach (string existingFile in Directory.GetFiles(_installationDirectory))
+			File.Delete(existingFile);
+	}
+
+	public async Task DownloadAndInstall()
+	{
+		Cmd.WriteLine(ConsoleColor.Yellow, "Downloading new version...");
+		GetLatestVersionFile latestInstallation = await AsyncHandler.Client.GetLatestVersionFile(ToolPublishMethod.SelfContained, _toolBuildType);
+		using MemoryStream ms = new(latestInstallation.ZipFileContents);
+		using ZipArchive archive = new(ms);
+		archive.ExtractToDirectory(_installationDirectory, true);
+
+		Cmd.WriteLine(ConsoleColor.Green, "Update installed.");
+	}
+
+	public void Launch()
+	{
+		string? executableFileName = FindExecutableFileName();
+		if (executableFileName == null)
+		{
+			Cmd.WriteLine(ConsoleColor.Red, $"Could not launch executable. It should be installed at '{_installationDirectory}'.");
+			return;
+		}
+
+		Process process = new();
+		process.StartInfo.FileName = executableFileName;
+		process.StartInfo.WorkingDirectory = _installationDirectory;
+		process.Start();
+	}
+
+	private string? FindExecutableFileName()
+	{
+		// TODO: Use something else on Linux.
+		return Array.Find(Directory.GetFiles(_installationDirectory), f => Path.GetExtension(f) == ".exe");
+	}
+
+	public bool IsInstalled()
+	{
+		return FindExecutableFileName() != null;
+	}
+}
