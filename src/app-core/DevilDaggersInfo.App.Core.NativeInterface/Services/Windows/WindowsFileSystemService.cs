9839e23453a0751e6be96a768cb91ec48fde9de1
Noah Stolk <noah.stolk@gmail.com>
2023-02-11 13:00:56 +0100

    Use NativeFileDialog library for file dialogs (#229)

--- src/app-core/DevilDaggersInfo.App.Core.NativeInterface/Services/Windows/WindowsFileSystemService.cs
+++ src/app-core/DevilDaggersInfo.App.Core.NativeInterface/Services/Windows/WindowsFileSystemService.cs
@@ -1,89 +1,24 @@
-using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.System.Com;
-using Windows.Win32.UI.Shell;
+using NativeFileDialogSharp;
 
 namespace DevilDaggersInfo.App.Core.NativeInterface.Services.Windows;
 
 /// <summary>
 /// Platform-specific code for interacting with the Windows file system.
 /// </summary>
-[SupportedOSPlatform("windows6.0.6000")]
 public class WindowsFileSystemService : INativeFileSystemService
 {
-	public unsafe string? CreateOpenFileDialog(string dialogTitle, string? extensionFilter)
+	public string? CreateOpenFileDialog(string dialogTitle, string? extensionFilter)
 	{
-		PInvoke.CoCreateInstance(
-			typeof(FileOpenDialog).GUID,
-			null,
-			CLSCTX.CLSCTX_INPROC_SERVER,
-			out IFileOpenDialog openDialog).ThrowOnFailure();
-
-		try
-		{
-			openDialog.Show(default);
-		}
-		catch (COMException)
-		{
-			// Return null when the user cancels the dialog.
-			return null;
-		}
-
-		openDialog.GetResult(out IShellItem item);
-		item.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, out PWSTR name);
-		Marshal.FreeCoTaskMem(new(name.Value));
-		return name.ToString();
+		return Dialog.FileOpen().Path;
 	}
 
-	public unsafe string? CreateSaveFileDialog(string dialogTitle, string? extensionFilter)
+	public string? CreateSaveFileDialog(string dialogTitle, string? extensionFilter)
 	{
-		PInvoke.CoCreateInstance(
-			typeof(FileSaveDialog).GUID,
-			null,
-			CLSCTX.CLSCTX_INPROC_SERVER,
-			out IFileSaveDialog saveDialog).ThrowOnFailure();
-
-		try
-		{
-			saveDialog.Show(default);
-		}
-		catch (COMException)
-		{
-			// Return null when the user cancels the dialog.
-			return null;
-		}
-
-		saveDialog.GetResult(out IShellItem item);
-		item.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, out PWSTR name);
-		Marshal.FreeCoTaskMem(new(name.Value));
-		return name.ToString();
+		return Dialog.FileSave().Path;
 	}
 
-	public unsafe string? SelectDirectory()
+	public string? SelectDirectory()
 	{
-		PInvoke.CoCreateInstance(
-			typeof(FileOpenDialog).GUID,
-			null,
-			CLSCTX.CLSCTX_INPROC_SERVER,
-			out IFileOpenDialog openDialog).ThrowOnFailure();
-
-		openDialog.SetOptions(FILEOPENDIALOGOPTIONS.FOS_PICKFOLDERS);
-
-		try
-		{
-			openDialog.Show(default);
-		}
-		catch (COMException)
-		{
-			// Return null when the user cancels the dialog.
-			return null;
-		}
-
-		openDialog.GetResult(out IShellItem item);
-		item.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, out PWSTR name);
-		Marshal.FreeCoTaskMem(new(name.Value));
-		return name.ToString();
+		return Dialog.FolderPicker().Path;
 	}
 }
