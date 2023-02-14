9f517507c89cf0ac753a06dedaf9c73317598205
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-02-14 20:30:32 +0100

    Make required

--- src/web-server/DevilDaggersInfo.Web.Server.Domain/Models/CustomLeaderboards/UploadCriteriaRejection.cs
+++ src/web-server/DevilDaggersInfo.Web.Server.Domain/Models/CustomLeaderboards/UploadCriteriaRejection.cs
@@ -6,9 +6,9 @@ public record UploadCriteriaRejection
 {
 	public required string CriteriaName { get; init; }
 
-	public CustomLeaderboardCriteriaOperator CriteriaOperator { get; init; }
+	public required CustomLeaderboardCriteriaOperator CriteriaOperator { get; init; }
 
-	public int ExpectedValue { get; init; }
+	public required int ExpectedValue { get; init; }
 
-	public int ActualValue { get; init; }
+	public required int ActualValue { get; init; }
 }
