f2f3a8e909cd05d148005a7eeeb793ff43e13fe6
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-03-04 17:36:41 +0100

    Fix scissor bug occurring when rendering empty label

--- src/app/DevilDaggersInfo.App.Ui.Base/Components/Label.cs
+++ src/app/DevilDaggersInfo.App.Ui.Base/Components/Label.cs
@@ -27,19 +27,19 @@ public class Label : AbstractLabel
 
 		base.Render(scrollOffset);
 
-		if (Text.Length == 0)
-			return;
-
-		int padding = (int)MathF.Round((Bounds.Y2 - Bounds.Y1) / 4f);
-		Vector2i<int> textPosition = LabelStyle.TextAlign switch
+		if (Text.Length > 0)
 		{
-			TextAlign.Middle => new Vector2i<int>(Bounds.X1 + Bounds.X2, Bounds.Y1 + Bounds.Y2) / 2,
-			TextAlign.Left => new(Bounds.X1 + padding, Bounds.Y1 + padding),
-			TextAlign.Right => new(Bounds.X2 - padding, Bounds.Y1 + padding),
-			_ => throw new InvalidOperationException("Invalid text align."),
-		};
+			int padding = (int)MathF.Round((Bounds.Y2 - Bounds.Y1) / 4f);
+			Vector2i<int> textPosition = LabelStyle.TextAlign switch
+			{
+				TextAlign.Middle => new Vector2i<int>(Bounds.X1 + Bounds.X2, Bounds.Y1 + Bounds.Y2) / 2,
+				TextAlign.Left => new(Bounds.X1 + padding, Bounds.Y1 + padding),
+				TextAlign.Right => new(Bounds.X2 - padding, Bounds.Y1 + padding),
+				_ => throw new InvalidOperationException("Invalid text align."),
+			};
 
-		Root.Game.GetFontRenderer(LabelStyle.FontSize).Schedule(Vector2i<int>.One, scrollOffset + textPosition, Depth, LabelStyle.TextColor, Text, LabelStyle.TextAlign);
+			Root.Game.GetFontRenderer(LabelStyle.FontSize).Schedule(Vector2i<int>.One, scrollOffset + textPosition, Depth, LabelStyle.TextColor, Text, LabelStyle.TextAlign);
+		}
 
 		if (!RenderOverflow)
 			ScissorScheduler.PopScissor();
