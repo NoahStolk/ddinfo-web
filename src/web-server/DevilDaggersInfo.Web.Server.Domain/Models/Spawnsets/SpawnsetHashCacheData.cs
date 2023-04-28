fc11d9d79f88c5abd5637d589c4de5bc0098ba2a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-04-28 17:24:55 +0200

    Delete SpawnsetHashCacheData.cs

--- src/web-server/DevilDaggersInfo.Web.Server.Domain/Models/Spawnsets/SpawnsetHashCacheData.cs
+++
@@ -1,8 +0,0 @@
-namespace DevilDaggersInfo.Web.Server.Domain.Models.Spawnsets;
-
-public class SpawnsetHashCacheData
-{
-	public required string Name { get; init; }
-
-	public required byte[] Hash { get; init; }
-}
