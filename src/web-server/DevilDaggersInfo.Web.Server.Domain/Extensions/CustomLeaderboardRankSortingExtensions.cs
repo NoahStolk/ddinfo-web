f180bf413d59fc47447b5cc8f62654ff02fd0f91
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-04-30 01:33:13 +0200

    Make IsAscending explicit

--- src/web-server/DevilDaggersInfo.Web.Server.Domain/Extensions/CustomLeaderboardRankSortingExtensions.cs
+++ src/web-server/DevilDaggersInfo.Web.Server.Domain/Extensions/CustomLeaderboardRankSortingExtensions.cs
@@ -5,5 +5,12 @@ namespace DevilDaggersInfo.Web.Server.Domain.Extensions;
 public static class CustomLeaderboardRankSortingExtensions
 {
 	public static bool IsAscending(this CustomLeaderboardRankSorting rankSorting)
-		=> rankSorting is CustomLeaderboardRankSorting.TimeAsc;
+	{
+		return rankSorting switch
+		{
+			CustomLeaderboardRankSorting.TimeAsc => true,
+			CustomLeaderboardRankSorting.TimeDesc => false,
+			_ => throw new InvalidOperationException($"Rank sorting '{rankSorting}' is not supported."),
+		};
+	}
 }
