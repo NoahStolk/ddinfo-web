3be334baf91f584313f268844e14bb7adfef86eb
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-06 00:29:27 +0200

    Clean up

--- src/app/DevilDaggersInfo.App.Ui.SurvivalEditor/_Imports.cs
+++
@@ -1,6 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Numerics;
-global using static DevilDaggersInfo.App.Engine.Graphics;
-global using Texture = DevilDaggersInfo.App.Engine.Content.TextureContent;
