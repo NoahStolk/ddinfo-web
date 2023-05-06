7d6989eb5173cc8094bb6aee3dfbf8a4e6fab210
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-06 19:11:34 +0200

    Clean up

--- src/app/DevilDaggersInfo.App.Ui.CustomLeaderboardsRecorder/Layouts/CustomLeaderboardsRecorderMainLayout.cs
+++ src/app/DevilDaggersInfo.App.Ui.CustomLeaderboardsRecorder/Layouts/CustomLeaderboardsRecorderMainLayout.cs
@@ -9,8 +9,6 @@ public class CustomLeaderboardsRecorderMainLayout : Layout, IExtendedLayout
 {
 	private const int _headerHeight = 24;
 
-	private readonly StateWrapper _stateWrapper;
-	private readonly RecordingScrollArea _recordingScrollArea;
 	private readonly RecordingResultScrollArea _recordingResultScrollArea;
 
 	public CustomLeaderboardsRecorderMainLayout()
@@ -18,16 +16,10 @@ public class CustomLeaderboardsRecorderMainLayout : Layout, IExtendedLayout
 		const int stateWrapperBottom = 96;
 		const int recordingScrollAreaBottom = 444;
 
-		MainLayoutBackButton backButton = new(new PixelBounds(0, 0, 24, _headerHeight), () => StateManager.Dispatch(new SetLayout(Root.Dependencies.MainLayout)));
-		_stateWrapper = new(new PixelBounds(0, _headerHeight, 256, stateWrapperBottom - _headerHeight));
-		_recordingScrollArea = new(new PixelBounds(0, stateWrapperBottom, 256, recordingScrollAreaBottom - stateWrapperBottom));
 		_recordingResultScrollArea = new(new PixelBounds(0, stateWrapperBottom, 256, recordingScrollAreaBottom - stateWrapperBottom));
 		LeaderboardListWrapper leaderboardListWrapper = new(new PixelBounds(256, _headerHeight, 768, recordingScrollAreaBottom - _headerHeight));
 		LeaderboardWrapper leaderboardWrapper = new(new PixelBounds(0, recordingScrollAreaBottom, 1024, 768 - recordingScrollAreaBottom));
 
-		NestingContext.Add(backButton);
-		NestingContext.Add(_stateWrapper);
-		NestingContext.Add(_recordingScrollArea);
 		NestingContext.Add(_recordingResultScrollArea);
 		NestingContext.Add(leaderboardListWrapper);
 		NestingContext.Add(leaderboardWrapper);
@@ -37,11 +29,6 @@ public class CustomLeaderboardsRecorderMainLayout : Layout, IExtendedLayout
 
 	private void Initialize()
 	{
-		if (StateManager.LayoutState.CurrentLayout != Root.Dependencies.CustomLeaderboardsRecorderMainLayout)
-			return;
-
-		_stateWrapper.Initialize();
-
 		StateManager.Dispatch(new LoadLeaderboardList());
 	}
 }
