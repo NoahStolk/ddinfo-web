d7a89764eb5af0af6d39d15e04689ba6ba8e845f
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-20 10:49:52 +0200

    Fix double enumeration in EnemyTimeline

--- src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Timeline/EnemyTimeline.cs
+++ src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Timeline/EnemyTimeline.cs
@@ -4,9 +4,10 @@ public record EnemyTimeline(int EntityId, IReadOnlyList<EnemyTimelineEvent> Even
 {
 	public int? GetHp(int tick)
 	{
-		IEnumerable<EnemyTimelineEvent> events = Events.Where(e => e.Tick < tick).OrderBy(e => e.Tick);
-		if (events.Any())
-			return events.Last().Hp;
+		// TODO: Optimize this.
+		List<EnemyTimelineEvent> events = Events.Where(e => e.Tick < tick).OrderBy(e => e.Tick).ToList();
+		if (events.Count > 0)
+			return events[^1].Hp;
 
 		return null;
 	}
