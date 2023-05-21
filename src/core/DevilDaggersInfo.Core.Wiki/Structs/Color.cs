76f0a9c7c6ce9506820b52abaad39017380be5ca
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-21 20:03:01 +0200

    Fix allocating heap memory when creating Color struct

--- src/core/DevilDaggersInfo.Core.Wiki/Structs/Color.cs
+++ src/core/DevilDaggersInfo.Core.Wiki/Structs/Color.cs
@@ -2,22 +2,9 @@ using System.Numerics;
 
 namespace DevilDaggersInfo.Core.Wiki.Structs;
 
-public readonly record struct Color
+public readonly record struct Color(byte R, byte G, byte B)
 {
-	public Color(byte r, byte g, byte b)
-	{
-		R = r;
-		G = g;
-		B = b;
-
-		HexCode = $"#{R:X2}{G:X2}{B:X2}";
-	}
-
-	public byte R { get; }
-	public byte G { get; }
-	public byte B { get; }
-
-	public string HexCode { get; }
+	public string HexCode => $"#{R:X2}{G:X2}{B:X2}";
 
 	public static implicit operator Vector4(Color c)
 	{
