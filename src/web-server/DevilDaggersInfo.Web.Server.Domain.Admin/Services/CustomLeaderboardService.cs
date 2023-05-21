bc2b512c3cfe5ae1be2f39d0c65f4f2b78593ab8
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-21 15:34:22 +0200

    Use SequenceEqual

--- src/web-server/DevilDaggersInfo.Web.Server.Domain.Admin/Services/CustomLeaderboardService.cs
+++ src/web-server/DevilDaggersInfo.Web.Server.Domain.Admin/Services/CustomLeaderboardService.cs
@@ -529,15 +529,6 @@ public class CustomLeaderboardService
 		if (a == null || b == null)
 			return false;
 
-		if (a.Length != b.Length)
-			return false;
-
-		for (int i = 0; i < a.Length; i++)
-		{
-			if (a[i] != b[i])
-				return false;
-		}
-
-		return true;
+		return a.SequenceEqual(b);
 	}
 }
