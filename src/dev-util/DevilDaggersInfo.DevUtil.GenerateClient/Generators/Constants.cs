2114f15141234395924f93d0a632509a1cac73dd
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-29 19:06:03 +0200

    Update generator paths

--- src/dev-util/DevilDaggersInfo.DevUtil.GenerateClient/Generators/Constants.cs
+++ src/dev-util/DevilDaggersInfo.DevUtil.GenerateClient/Generators/Constants.cs
@@ -4,9 +4,9 @@ internal static class Constants
 {
 	private const string _devRoot = @"C:\Users\NOAH\source\repos\DevilDaggersInfo\src";
 
-	public static readonly string ServerProjectPath = Path.Combine(_devRoot, "web-server", "DevilDaggersInfo.Web.Server");
+	public static readonly string ServerProjectPath = Path.Combine(_devRoot, "web", "DevilDaggersInfo.Web.Server");
 
-	public static readonly string ClientProjectPath = Path.Combine(_devRoot, "web-client", "DevilDaggersInfo.Web.Client");
+	public static readonly string ClientProjectPath = Path.Combine(_devRoot, "web", "DevilDaggersInfo.Web.Client");
 
 	public static readonly string ToolsProjectPath = Path.Combine(_devRoot, "app", "DevilDaggersInfo.App");
 }
