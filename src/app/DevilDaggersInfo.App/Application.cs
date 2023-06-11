9978d754a0fc2b4542a4a5627579e5a0d2304eff
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-06-11 17:20:57 +0200

    Add FontGoetheBold30 font

--- src/app/DevilDaggersInfo.App/Application.cs
+++ src/app/DevilDaggersInfo.App/Application.cs
@@ -74,6 +74,7 @@ public class Application
 
 			// Add the default font first so it is actually used by default.
 			io.Fonts.AddFontDefault();
+			Root.FontGoetheBold30 = io.Fonts.AddFontFromFileTTF("goethebold.ttf", 30);
 			Root.FontGoetheBold60 = io.Fonts.AddFontFromFileTTF("goethebold.ttf", 60);
 		});
 
