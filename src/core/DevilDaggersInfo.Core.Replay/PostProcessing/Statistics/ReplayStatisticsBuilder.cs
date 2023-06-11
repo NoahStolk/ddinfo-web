5f56ad422ad8d795d23bf03a72d68a65e9675f78
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-06-11 20:20:34 +0200

    Fix more naming

--- src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Statistics/ReplayStatisticsBuilder.cs
+++ src/core/DevilDaggersInfo.Core.Replay/PostProcessing/Statistics/ReplayStatisticsBuilder.cs
@@ -17,40 +17,40 @@ public class ReplayStatisticsBuilder
 	private int _gemsDespawned;
 	private int _gemsEaten;
 	private int _gemsTotal;
-	private ushort _skull1sAlive;
-	private ushort _skull2sAlive;
-	private ushort _skull3sAlive;
-	private ushort _spiderlingsAlive;
-	private ushort _skull4sAlive;
-	private ushort _squid1sAlive;
-	private ushort _squid2sAlive;
-	private ushort _squid3sAlive;
-	private ushort _centipedesAlive;
-	private ushort _gigapedesAlive;
-	private ushort _spider1sAlive;
-	private ushort _spider2sAlive;
-	private ushort _leviathansAlive;
-	private ushort _orbsAlive;
-	private ushort _thornsAlive;
-	private ushort _ghostpedesAlive;
-	private ushort _spiderEggsAlive;
-	private ushort _skull1sKilled;
-	private ushort _skull2sKilled;
-	private ushort _skull3sKilled;
-	private ushort _spiderlingsKilled;
-	private ushort _skull4sKilled;
-	private ushort _squid1sKilled;
-	private ushort _squid2sKilled;
-	private ushort _squid3sKilled;
-	private ushort _centipedesKilled;
-	private ushort _gigapedesKilled;
-	private ushort _spider1sKilled;
-	private ushort _spider2sKilled;
-	private ushort _leviathansKilled;
-	private ushort _orbsKilled;
-	private ushort _thornsKilled;
-	private ushort _ghostpedesKilled;
-	private ushort _spiderEggsKilled;
+	private ushort _skull1AliveCount;
+	private ushort _skull2AliveCount;
+	private ushort _skull3AliveCount;
+	private ushort _spiderlingAliveCount;
+	private ushort _skull4AliveCount;
+	private ushort _squid1AliveCount;
+	private ushort _squid2AliveCount;
+	private ushort _squid3AliveCount;
+	private ushort _centipedeAliveCount;
+	private ushort _gigapedeAliveCount;
+	private ushort _spider1AliveCount;
+	private ushort _spider2AliveCount;
+	private ushort _leviathanAliveCount;
+	private ushort _orbAliveCount;
+	private ushort _thornAliveCount;
+	private ushort _ghostpedeAliveCount;
+	private ushort _spiderEggAliveCount;
+	private ushort _skull1KillCount;
+	private ushort _skull2KillCount;
+	private ushort _skull3KillCount;
+	private ushort _spiderlingKillCount;
+	private ushort _skull4KillCount;
+	private ushort _squid1KillCount;
+	private ushort _squid2KillCount;
+	private ushort _squid3KillCount;
+	private ushort _centipedeKillCount;
+	private ushort _gigapedeKillCount;
+	private ushort _spider1KillCount;
+	private ushort _spider2KillCount;
+	private ushort _leviathanKillCount;
+	private ushort _orbKillCount;
+	private ushort _thornKillCount;
+	private ushort _ghostpedeKillCount;
+	private ushort _spiderEggKillCount;
 
 	#endregion Fields
 
@@ -72,11 +72,11 @@ public class ReplayStatisticsBuilder
 					_enemiesAlive++;
 					switch (boid.BoidType)
 					{
-						case BoidType.Skull1: _skull1sAlive++; break;
-						case BoidType.Skull2: _skull2sAlive++; break;
-						case BoidType.Skull3: _skull3sAlive++; break;
-						case BoidType.Spiderling: _spiderlingsAlive++; break;
-						case BoidType.Skull4: _skull4sAlive++; break;
+						case BoidType.Skull1: _skull1AliveCount++; break;
+						case BoidType.Skull2: _skull2AliveCount++; break;
+						case BoidType.Skull3: _skull3AliveCount++; break;
+						case BoidType.Spiderling: _spiderlingAliveCount++; break;
+						case BoidType.Skull4: _skull4AliveCount++; break;
 					}
 
 					break;
@@ -91,28 +91,28 @@ public class ReplayStatisticsBuilder
 					break;
 				case LeviathanSpawnEvent:
 					_enemiesAlive++;
-					_leviathansAlive++;
+					_leviathanAliveCount++;
 					break;
 				case PedeSpawnEvent pede:
 					_enemiesAlive++;
 					switch (pede.PedeType)
 					{
-						case PedeType.Centipede: _centipedesAlive++; break;
-						case PedeType.Gigapede: _gigapedesAlive++; break;
-						case PedeType.Ghostpede: _ghostpedesAlive++; break;
+						case PedeType.Centipede: _centipedeAliveCount++; break;
+						case PedeType.Gigapede: _gigapedeAliveCount++; break;
+						case PedeType.Ghostpede: _ghostpedeAliveCount++; break;
 					}
 
 					break;
 				case SpiderEggSpawnEvent:
 					_enemiesAlive++;
-					_spiderEggsAlive++;
+					_spiderEggAliveCount++;
 					break;
 				case SpiderSpawnEvent spider:
 					_enemiesAlive++;
 					switch (spider.SpiderType)
 					{
-						case SpiderType.Spider1: _spider1sAlive++; break;
-						case SpiderType.Spider2: _spider2sAlive++; break;
+						case SpiderType.Spider1: _spider1AliveCount++; break;
+						case SpiderType.Spider2: _spider2AliveCount++; break;
 					}
 
 					break;
@@ -120,23 +120,21 @@ public class ReplayStatisticsBuilder
 					_enemiesAlive++;
 					switch (squid.SquidType)
 					{
-						case SquidType.Squid1: _squid1sAlive++; break;
-						case SquidType.Squid2: _squid2sAlive++; break;
-						case SquidType.Squid3: _squid3sAlive++; break;
+						case SquidType.Squid1: _squid1AliveCount++; break;
+						case SquidType.Squid2: _squid2AliveCount++; break;
+						case SquidType.Squid3: _squid3AliveCount++; break;
 					}
 
 					break;
 				case ThornSpawnEvent:
 					_enemiesAlive++;
-					_thornsAlive++;
+					_thornAliveCount++;
 					break;
 				case IInputsEvent:
-				{
 					currentTick++;
 					if (currentTick % 60 == 0)
 						entries.Add(FlushCurrentState());
 					break;
-				}
 				case EndEvent:
 					entries.Add(FlushCurrentState());
 					break;
@@ -159,40 +157,40 @@ public class ReplayStatisticsBuilder
 		GemsDespawned: _gemsDespawned,
 		GemsEaten: _gemsEaten,
 		GemsTotal: _gemsTotal,
-		Skull1sAlive: _skull1sAlive,
-		Skull2sAlive: _skull2sAlive,
-		Skull3sAlive: _skull3sAlive,
-		SpiderlingsAlive: _spiderlingsAlive,
-		Skull4sAlive: _skull4sAlive,
-		Squid1sAlive: _squid1sAlive,
-		Squid2sAlive: _squid2sAlive,
-		Squid3sAlive: _squid3sAlive,
-		CentipedesAlive: _centipedesAlive,
-		GigapedesAlive: _gigapedesAlive,
-		Spider1sAlive: _spider1sAlive,
-		Spider2sAlive: _spider2sAlive,
-		LeviathansAlive: _leviathansAlive,
-		OrbsAlive: _orbsAlive,
-		ThornsAlive: _thornsAlive,
-		GhostpedesAlive: _ghostpedesAlive,
-		SpiderEggsAlive: _spiderEggsAlive,
-		Skull1sKilled: _skull1sKilled,
-		Skull2sKilled: _skull2sKilled,
-		Skull3sKilled: _skull3sKilled,
-		SpiderlingsKilled: _spiderlingsKilled,
-		Skull4sKilled: _skull4sKilled,
-		Squid1sKilled: _squid1sKilled,
-		Squid2sKilled: _squid2sKilled,
-		Squid3sKilled: _squid3sKilled,
-		CentipedesKilled: _centipedesKilled,
-		GigapedesKilled: _gigapedesKilled,
-		Spider1sKilled: _spider1sKilled,
-		Spider2sKilled: _spider2sKilled,
-		LeviathansKilled: _leviathansKilled,
-		OrbsKilled: _orbsKilled,
-		ThornsKilled: _thornsKilled,
-		GhostpedesKilled: _ghostpedesKilled,
-		SpiderEggsKilled: _spiderEggsKilled);
+		Skull1AliveCount: _skull1AliveCount,
+		Skull2AliveCount: _skull2AliveCount,
+		Skull3AliveCount: _skull3AliveCount,
+		SpiderlingAliveCount: _spiderlingAliveCount,
+		Skull4AliveCount: _skull4AliveCount,
+		Squid1AliveCount: _squid1AliveCount,
+		Squid2AliveCount: _squid2AliveCount,
+		Squid3AliveCount: _squid3AliveCount,
+		CentipedeAliveCount: _centipedeAliveCount,
+		GigapedeAliveCount: _gigapedeAliveCount,
+		Spider1AliveCount: _spider1AliveCount,
+		Spider2AliveCount: _spider2AliveCount,
+		LeviathanAliveCount: _leviathanAliveCount,
+		OrbAliveCount: _orbAliveCount,
+		ThornAliveCount: _thornAliveCount,
+		GhostpedeAliveCount: _ghostpedeAliveCount,
+		SpiderEggAliveCount: _spiderEggAliveCount,
+		Skull1KillCount: _skull1KillCount,
+		Skull2KillCount: _skull2KillCount,
+		Skull3KillCount: _skull3KillCount,
+		SpiderlingKillCount: _spiderlingKillCount,
+		Skull4KillCount: _skull4KillCount,
+		Squid1KillCount: _squid1KillCount,
+		Squid2KillCount: _squid2KillCount,
+		Squid3KillCount: _squid3KillCount,
+		CentipedeKillCount: _centipedeKillCount,
+		GigapedeKillCount: _gigapedeKillCount,
+		Spider1KillCount: _spider1KillCount,
+		Spider2KillCount: _spider2KillCount,
+		LeviathanKillCount: _leviathanKillCount,
+		OrbKillCount: _orbKillCount,
+		ThornKillCount: _thornKillCount,
+		GhostpedeKillCount: _ghostpedeKillCount,
+		SpiderEggKillCount: _spiderEggKillCount);
 
 	private void Clear()
 	{
@@ -206,40 +204,40 @@ public class ReplayStatisticsBuilder
 		_gemsDespawned = 0;
 		_gemsEaten = 0;
 		_gemsTotal = 0;
-		_skull1sAlive = 0;
-		_skull2sAlive = 0;
-		_skull3sAlive = 0;
-		_spiderlingsAlive = 0;
-		_skull4sAlive = 0;
-		_squid1sAlive = 0;
-		_squid2sAlive = 0;
-		_squid3sAlive = 0;
-		_centipedesAlive = 0;
-		_gigapedesAlive = 0;
-		_spider1sAlive = 0;
-		_spider2sAlive = 0;
-		_leviathansAlive = 0;
-		_orbsAlive = 0;
-		_thornsAlive = 0;
-		_ghostpedesAlive = 0;
-		_spiderEggsAlive = 0;
-		_skull1sKilled = 0;
-		_skull2sKilled = 0;
-		_skull3sKilled = 0;
-		_spiderlingsKilled = 0;
-		_skull4sKilled = 0;
-		_squid1sKilled = 0;
-		_squid2sKilled = 0;
-		_squid3sKilled = 0;
-		_centipedesKilled = 0;
-		_gigapedesKilled = 0;
-		_spider1sKilled = 0;
-		_spider2sKilled = 0;
-		_leviathansKilled = 0;
-		_orbsKilled = 0;
-		_thornsKilled = 0;
-		_ghostpedesKilled = 0;
-		_spiderEggsKilled = 0;
+		_skull1AliveCount = 0;
+		_skull2AliveCount = 0;
+		_skull3AliveCount = 0;
+		_spiderlingAliveCount = 0;
+		_skull4AliveCount = 0;
+		_squid1AliveCount = 0;
+		_squid2AliveCount = 0;
+		_squid3AliveCount = 0;
+		_centipedeAliveCount = 0;
+		_gigapedeAliveCount = 0;
+		_spider1AliveCount = 0;
+		_spider2AliveCount = 0;
+		_leviathanAliveCount = 0;
+		_orbAliveCount = 0;
+		_thornAliveCount = 0;
+		_ghostpedeAliveCount = 0;
+		_spiderEggAliveCount = 0;
+		_skull1KillCount = 0;
+		_skull2KillCount = 0;
+		_skull3KillCount = 0;
+		_spiderlingKillCount = 0;
+		_skull4KillCount = 0;
+		_squid1KillCount = 0;
+		_squid2KillCount = 0;
+		_squid3KillCount = 0;
+		_centipedeKillCount = 0;
+		_gigapedeKillCount = 0;
+		_spider1KillCount = 0;
+		_spider2KillCount = 0;
+		_leviathanKillCount = 0;
+		_orbKillCount = 0;
+		_thornKillCount = 0;
+		_ghostpedeKillCount = 0;
+		_spiderEggKillCount = 0;
 	}
 
 	#endregion Utils
