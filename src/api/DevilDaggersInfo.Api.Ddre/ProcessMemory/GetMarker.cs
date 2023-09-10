b29b5fafd6e601852f53b3c6fda4860137e51a53
Noah Stolk <noah.stolk@gmail.com>
2023-09-10 11:53:00 +0200

    Deprecate replay editor (#457)
    * Remove DDRE API
    
    * Update communication.md
    
    * Remove downloads from DDRE page

--- src/api/DevilDaggersInfo.Api.Ddre/ProcessMemory/GetMarker.cs
+++
@@ -1,6 +0,0 @@
-namespace DevilDaggersInfo.Api.Ddre.ProcessMemory;
-
-public record GetMarker
-{
-	public required long Value { get; init; }
-}
--- src/api/DevilDaggersInfo.Api.Ddre/ProcessMemory/SupportedOperatingSystem.cs
+++
@@ -1,7 +0,0 @@
-namespace DevilDaggersInfo.Api.Ddre.ProcessMemory;
-
-public enum SupportedOperatingSystem
-{
-	Windows = 1,
-	Linux = 2,
-}
