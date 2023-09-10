47fee8c5a39672ca810a2c579adb1e7121f189a8
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-09-10 11:41:58 +0200

    Improve home page and separate new tools from old ones in nav menu

--- src/web/DevilDaggersInfo.Web.Client/Pages/Index.cs
+++ src/web/DevilDaggersInfo.Web.Client/Pages/Index.cs
@@ -8,14 +8,14 @@ public partial class Index
 {
 	private readonly Section[] _sections =
 	{
-		new("View the official leaderboard", "home/leaderboard.png", "/leaderboard"),
-		new("Check out mods made by the community", "home/mod.png", "/custom/mods"),
-		new("Check out spawnsets made by the community", "home/spawnset.png", "/custom/spawnsets"),
-		new("Check out the asset editor which can be used to mod the game", "tools/ddae.png", "/tools/asset-editor"),
-		new("Check out the survival editor which can be used to create spawnsets and practice sections of the game", "tools/ddse.png", "/tools/survival-editor"),
-		new("Participate in custom leaderboards for spawnsets", "home/custom-leaderboard.png", "/custom/leaderboards"),
-		new("Check out the world record progression and other statistics", "home/graph.png", "/leaderboard/world-record-progression"),
-		new("Check out various wiki pages about the game, such as spawn times, enemy types, or hand upgrades", "home/enemies.png", "/wiki/enemies"),
+		new("View the official leaderboard", "home/leaderboard.png", "/leaderboard", "Leaderboard"),
+		new("Check out mods made by the community", "home/mod.png", "/custom/mods", "Mods"),
+		new("Check out spawnsets made by the community", "home/spawnset.png", "/custom/spawnsets", "Spawnsets"),
+		new("Check out the asset editor which can be used to mod the game", "tools/ddae.png", "/tools/asset-editor", "Asset Editor"),
+		new("Check out the survival editor which can be used to create spawnsets and practice sections of the game", "tools/ddse.png", "/tools/survival-editor", "Spawnset Editor"),
+		new("Participate in custom leaderboards for spawnsets", "home/custom-leaderboard.png", "/custom/leaderboards", "Custom Leaderboards"),
+		new("Check out the world record progression and other statistics", "home/graph.png", "/leaderboard/world-record-progression", "World Record Progression"),
+		new("Check out various wiki pages about the game, such as spawn times, enemy types, or hand upgrades", "home/enemies.png", "/wiki/enemies", "Enemies Wiki"),
 	};
 
 	[Inject]
@@ -35,5 +35,5 @@ public partial class Index
 		LatestNewsContent = new(html);
 	}
 
-	private record Section(string Text, string ImageUrl, string Url);
+	private sealed record Section(string Text, string ImageUrl, string Url, string Alt);
 }
