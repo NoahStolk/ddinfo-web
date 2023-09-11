3c76277d87f1e655b875720a58f7bbfb1dd0cc00
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-09-11 21:01:16 +0200

    Refactor Shortcuts class

--- src/app/DevilDaggersInfo.App/Shortcuts.cs
+++ src/app/DevilDaggersInfo.App/Shortcuts.cs
@@ -30,29 +30,31 @@ public static class Shortcuts
 			return;
 		}
 
-		if (ctrl && !shift)
+		if (ctrl)
 		{
-			Action? action = key switch
+			if (!shift)
 			{
-				Key.N => SpawnsetEditorMenu.NewSpawnset,
-				Key.O => SpawnsetEditorMenu.OpenSpawnset,
-				Key.S => SpawnsetEditorMenu.SaveSpawnset,
-				Key.R => SpawnsetEditorMenu.ReplaceCurrentSpawnset,
-				Key.D => SpawnsetEditorMenu.DeleteCurrentSpawnset,
-				_ => null,
-			};
-			action?.Invoke();
-		}
-
-		if (ctrl && shift)
-		{
-			Action? action = key switch
+				Action? action = key switch
+				{
+					Key.N => SpawnsetEditorMenu.NewSpawnset,
+					Key.O => SpawnsetEditorMenu.OpenSpawnset,
+					Key.S => SpawnsetEditorMenu.SaveSpawnset,
+					Key.R => SpawnsetEditorMenu.ReplaceCurrentSpawnset,
+					Key.D => SpawnsetEditorMenu.DeleteCurrentSpawnset,
+					_ => null,
+				};
+				action?.Invoke();
+			}
+			else
 			{
-				Key.O => SpawnsetEditorMenu.OpenCurrentSpawnset,
-				Key.D => SpawnsetEditorMenu.OpenDefaultSpawnset,
-				_ => null,
-			};
-			action?.Invoke();
+				Action? action = key switch
+				{
+					Key.O => SpawnsetEditorMenu.OpenCurrentSpawnset,
+					Key.D => SpawnsetEditorMenu.OpenDefaultSpawnset,
+					_ => null,
+				};
+				action?.Invoke();
+			}
 		}
 	}
 
@@ -61,28 +63,30 @@ public static class Shortcuts
 		if (key == Key.Escape)
 			ReplayEditorMenu.Close();
 
-		if (ctrl && !shift)
+		if (ctrl)
 		{
-			Action? action = key switch
+			if (!shift)
 			{
-				Key.N => ReplayEditorMenu.NewReplay,
-				Key.O => ReplayEditorMenu.OpenReplay,
-				Key.S => ReplayEditorMenu.SaveReplay,
-				Key.I => ReplayEditorMenu.InjectReplay,
-				Key.G => ReplayEditorMenu.OpenReplayFromGameMemory,
-				_ => null,
-			};
-			action?.Invoke();
-		}
-
-		if (ctrl && shift)
-		{
-			Action? action = key switch
+				Action? action = key switch
+				{
+					Key.N => ReplayEditorMenu.NewReplay,
+					Key.O => ReplayEditorMenu.OpenReplay,
+					Key.S => ReplayEditorMenu.SaveReplay,
+					Key.I => ReplayEditorMenu.InjectReplay,
+					Key.G => ReplayEditorMenu.OpenReplayFromGameMemory,
+					_ => null,
+				};
+				action?.Invoke();
+			}
+			else
 			{
-				Key.O => ReplayEditorMenu.OpenLeaderboardReplay,
-				_ => null,
-			};
-			action?.Invoke();
+				Action? action = key switch
+				{
+					Key.O => ReplayEditorMenu.OpenLeaderboardReplay,
+					_ => null,
+				};
+				action?.Invoke();
+			}
 		}
 	}
 }
