4a9abb1f4d69218442fc1be6433cd158f198056e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-10-07 20:57:02 +0200

    Remove API client generator

--- src/DevilDaggersInfo.DevUtil.GenerateClient/Program.cs
+++
@@ -1,5 +0,0 @@
-using DevilDaggersInfo.DevUtil.GenerateClient.Generators;
-
-new HttpClientSourceGenerator("Main", "DevilDaggersInfo.Web.Client.HttpClients", "MainApiHttpClient", Path.Combine(Constants.ClientProjectPath, "HttpClients", "MainApiHttpClientGenerated.cs")).Execute();
-new HttpClientSourceGenerator("Admin", "DevilDaggersInfo.Web.Client.HttpClients", "AdminApiHttpClient", Path.Combine(Constants.ClientProjectPath, "HttpClients", "AdminApiHttpClientGenerated.cs")).Execute();
-new HttpClientSourceGenerator("App", "DevilDaggersInfo.App.Networking", "AppApiHttpClient", Path.Combine(Constants.ToolsProjectPath, "Networking", "AppApiHttpClientGenerated.cs")).Execute();
