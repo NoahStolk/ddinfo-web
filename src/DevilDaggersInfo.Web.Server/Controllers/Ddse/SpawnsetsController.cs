eab42ef6000e107690b53a8e79e45642f35a749b
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-17 21:05:46 +0100

    Remove old DDSE endpoints

--- src/DevilDaggersInfo.Web.Server/Controllers/Ddse/SpawnsetsController.cs
+++ src/DevilDaggersInfo.Web.Server/Controllers/Ddse/SpawnsetsController.cs
@@ -23,32 +23,6 @@ public class SpawnsetsController : ControllerBase
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	public async Task<List<GetSpawnsetDdse>> GetSpawnsets(string? authorFilter = null, string? nameFilter = null)
 	{
-		return await GetSpawnsetsImpl(authorFilter, nameFilter);
-	}
-
-	[Obsolete("Support for DDSE 2.46.1 will be dropped in the future.")]
-	[HttpGet("/api/spawnsets/ddse")]
-	[ProducesResponseType(StatusCodes.Status200OK)]
-	public async Task<List<GetSpawnsetDdse>> GetSpawnsetsObsolete(string? authorFilter = null, string? nameFilter = null)
-	{
-		return await GetSpawnsetsImpl(authorFilter, nameFilter);
-	}
-
-	[HttpGet("{spawnsetId}/file")]
-	[ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status400BadRequest)]
-	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	public async Task<ActionResult> GetSpawnsetFile([Required] int spawnsetId)
-	{
-		var spawnset = await _dbContext.Spawnsets.AsNoTracking().Select(s => new { s.Id, s.Name, s.File }).FirstOrDefaultAsync(s => s.Id == spawnsetId);
-		if (spawnset == null)
-			return NotFound();
-
-		return File(spawnset.File, MediaTypeNames.Application.Octet, spawnset.Name);
-	}
-
-	private async Task<List<GetSpawnsetDdse>> GetSpawnsetsImpl(string? authorFilter, string? nameFilter)
-	{
 		IQueryable<SpawnsetEntity> spawnsetsQuery = _dbContext.Spawnsets.AsNoTracking().Include(sf => sf.Player);
 
 		if (!string.IsNullOrWhiteSpace(authorFilter))
@@ -67,5 +41,18 @@ public class SpawnsetsController : ControllerBase
 		List<SpawnsetEntity> spawnsets = await spawnsetsQuery.ToListAsync();
 
 		return spawnsets.ConvertAll(s => s.ToDdseApi(spawnsetsWithCustomLeaderboardIds.Contains(s.Id)));
+	}
+
+	[HttpGet("{spawnsetId}/file")]
+	[ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<ActionResult> GetSpawnsetFile([Required] int spawnsetId)
+	{
+		var spawnset = await _dbContext.Spawnsets.AsNoTracking().Select(s => new { s.Id, s.Name, s.File }).FirstOrDefaultAsync(s => s.Id == spawnsetId);
+		if (spawnset == null)
+			return NotFound();
+
+		return File(spawnset.File, MediaTypeNames.Application.Octet, spawnset.Name);
 	}
 }
