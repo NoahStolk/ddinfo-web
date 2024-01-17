5002cfd482e4169603937b99d0c3dc5227838a7a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-17 21:26:56 +0100

    Remove more dead code

--- src/DevilDaggersInfo.Web.ApiSpec.Main/Tools/GetToolDownloadCount.cs
+++
@@ -1,8 +0,0 @@
-namespace DevilDaggersInfo.Web.ApiSpec.Main.Tools;
-
-public record GetToolDownloadCount
-{
-	public required string Version { get; init; }
-
-	public required int Count { get; init; }
-}
--- src/DevilDaggersInfo.Web.ApiSpec.Main/Tools/ToolPublishMethod.cs
+++
@@ -1,8 +0,0 @@
-namespace DevilDaggersInfo.Web.ApiSpec.Main.Tools;
-
-public enum ToolPublishMethod
-{
-	SelfContained = 0,
-	Default = 1,
-	Aot = 2,
-}
