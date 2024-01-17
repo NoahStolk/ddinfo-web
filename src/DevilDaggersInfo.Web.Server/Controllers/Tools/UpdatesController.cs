f73a26b8f9402dd76104f21d3edc21c760b6343e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-17 11:34:12 +0100

    Remove ddinfo-tool downloads from website

--- src/DevilDaggersInfo.Web.Server/Controllers/Tools/UpdatesController.cs
+++ src/DevilDaggersInfo.Web.Server/Controllers/Tools/UpdatesController.cs
@@ -1,67 +1,36 @@
 using DevilDaggersInfo.Web.ApiSpec.Tools;
 using DevilDaggersInfo.Web.ApiSpec.Tools.Updates;
-using DevilDaggersInfo.Web.Server.Converters.DomainToApi.Tools;
-using DevilDaggersInfo.Web.Server.Domain.Exceptions;
-using DevilDaggersInfo.Web.Server.Domain.Models.Tools;
-using DevilDaggersInfo.Web.Server.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace DevilDaggersInfo.Web.Server.Controllers.Tools;
 
+[Obsolete("This is only here for backwards compatibility with ddinfo-tools 0.10.3.0 and older.")]
 [Route("api/app/updates")]
 [ApiController]
 public class UpdatesController : ControllerBase
 {
-	private const string _toolName = "ddinfo-tools";
-
-	private readonly ToolRepository _toolRepository;
-
-	public UpdatesController(ToolRepository toolRepository)
-	{
-		_toolRepository = toolRepository;
-	}
-
+	[Obsolete("This is only here for backwards compatibility with ddinfo-tools 0.10.3.0 and older.")]
 	[HttpGet("latest")]
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<ActionResult<GetLatestVersion>> GetLatest([Required] AppOperatingSystem appOperatingSystem)
 	{
-		Domain.Entities.Enums.ToolBuildType toolBuildType = GetToolBuildType(appOperatingSystem);
-		ToolDistribution? distribution = await _toolRepository.GetLatestToolDistributionAsync(_toolName, Domain.Entities.Enums.ToolPublishMethod.SelfContained, toolBuildType);
-		if (distribution == null)
-			return NotFound();
-
-		return distribution.ToAppApi();
+		return new GetLatestVersion
+		{
+			FileSize = 0,
+			VersionNumber = "0.0.0.0",
+		};
 	}
 
+	[Obsolete("This is only here for backwards compatibility with ddinfo-tools 0.10.3.0 and older.")]
 	[HttpGet("latest-file")]
 	[ProducesResponseType(typeof(FileStreamResult), StatusCodes.Status200OK)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	[ProducesResponseType(StatusCodes.Status404NotFound)]
 	public async Task<ActionResult> GetLatestFile([Required] AppOperatingSystem appOperatingSystem)
 	{
-		Domain.Entities.Enums.ToolBuildType toolBuildType = GetToolBuildType(appOperatingSystem);
-		ToolDistribution? distribution = await _toolRepository.GetLatestToolDistributionAsync(_toolName, Domain.Entities.Enums.ToolPublishMethod.SelfContained, toolBuildType);
-		if (distribution == null)
-			return NotFound();
-
-		byte[] bytes = await _toolRepository.GetToolDistributionFileAsync(_toolName, Domain.Entities.Enums.ToolPublishMethod.SelfContained, toolBuildType, distribution.VersionNumber);
-
-		await _toolRepository.UpdateToolDistributionStatisticsAsync(_toolName, Domain.Entities.Enums.ToolPublishMethod.SelfContained, toolBuildType, distribution.VersionNumber);
-
-		MemoryStream ms = new(bytes);
-		return new FileStreamResult(ms, "application/octet-stream");
-	}
-
-	private static Domain.Entities.Enums.ToolBuildType GetToolBuildType(AppOperatingSystem appOperatingSystem)
-	{
-		return appOperatingSystem switch
-		{
-			AppOperatingSystem.Windows => Domain.Entities.Enums.ToolBuildType.WindowsWarp,
-			AppOperatingSystem.Linux => Domain.Entities.Enums.ToolBuildType.LinuxWarp,
-			_ => throw new NotFoundException($"Operator system {appOperatingSystem} does not exist."),
-		};
+		return NotFound();
 	}
 }
