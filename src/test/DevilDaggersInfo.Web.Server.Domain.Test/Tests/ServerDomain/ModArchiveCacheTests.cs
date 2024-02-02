d6ac5240290f36048fa4349b3c6508b7129121ba
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-02-02 16:54:15 +0100

    Add test (WIP)

---
+++ src/test/DevilDaggersInfo.Web.Server.Domain.Test/Tests/ServerDomain/ModArchiveCacheTests.cs
@@ -0,0 +1,15 @@
+using DevilDaggersInfo.Web.Server.Domain.Models.ModArchives;
+
+namespace DevilDaggersInfo.Web.Server.Domain.Test.Tests.ServerDomain;
+
+[TestClass]
+public class ModArchiveCacheTests
+{
+	[TestMethod]
+	public void GetModArchiveCacheData()
+	{
+		const string binaryName = "maken";
+
+
+	}
+}
