631e4bc8c83ed4750597433c54c9fded4424de7e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-10-14 18:44:41 +0200

    Rewrite script

--- src/DevilDaggersInfo.DevUtil.HistoryCsvDump/Program.cs
+++ src/DevilDaggersInfo.DevUtil.HistoryCsvDump/Program.cs
@@ -1,89 +1,71 @@
 ﻿using DevilDaggersInfo.Web.Server.Domain.Models.LeaderboardHistory;
 using System.Text;
 
-string baseDirectory = args[0];
+const string baseDirectory = """C:\Users\NOAH\source\repos\ddinfo-web\src\DevilDaggersInfo.Web.Server\Data\LeaderboardHistory""";
 
+// Find all players that have ever been in the top 10.
 string[] files = Directory.GetFiles(baseDirectory, "*.bin", SearchOption.TopDirectoryOnly).Order().ToArray();
+List<LeaderboardHistory> leaderboardHistories = files.Select(file => LeaderboardHistory.CreateFromFile(File.ReadAllBytes(file))).ToList();
+List<int> top10Players = [];
+foreach (LeaderboardHistory leaderboardHistory in leaderboardHistories)
+	top10Players.AddRange(leaderboardHistory.Entries.Where(e => e.Rank <= 10).Select(e => e.Id));
+top10Players = top10Players.Distinct().Order().ToList();
 
+// For each date; find the player's score and keep track of it.
+Dictionary<int, int> currentBestTimes = new();
 List<DayEntry> dayEntries = [];
-
 DateOnly releaseDate = new(2016, 2, 18);
 DateOnly currentDate = releaseDate;
 while (currentDate < DateOnly.FromDateTime(DateTime.UtcNow))
 {
-	string dateString = currentDate.ToString("yyyyMMdd");
-	string? filePath = Array.Find(files, f => Path.GetFileNameWithoutExtension(f).StartsWith(dateString));
-	if (filePath == null)
+	LeaderboardHistory? leaderboardHistory = leaderboardHistories.OrderByDescending(lh => lh.DateTime).FirstOrDefault(lh => lh.DateTime <= new DateTime(currentDate, TimeOnly.MinValue, DateTimeKind.Utc));
+	if (leaderboardHistory != null)
 	{
-		Console.WriteLine($"Missing file for {currentDate}.");
-
-		// TODO: Add DayEntry with Completeness = Incomplete and the entries copied from the previous day.
-	}
-	else
-	{
-		LeaderboardHistory leaderboardHistory = LeaderboardHistory.CreateFromFile(await File.ReadAllBytesAsync(filePath));
-
-		bool isCompleteTop10 = leaderboardHistory.Entries.DistinctBy(e => e.Rank).Count(e => e.Rank <= 10) == 10;
-		if (isCompleteTop10)
+		// Some history files might not contain all players, so we need to keep track of the best time for each player.
+		foreach (EntryHistory entryHistory in leaderboardHistory.Entries)
 		{
-			dayEntries.Add(new DayEntry
-			{
-				DaysSinceRelease = DateOnly.FromDateTime(leaderboardHistory.DateTime).DayNumber - releaseDate.DayNumber,
-				Completeness = Completeness.Complete,
-				ScoreEntries = leaderboardHistory.Entries
-					.Where(e => e.Rank <= 10)
-					.Select(scoreEntry => new ScoreEntry
-					{
-						PlayerId = scoreEntry.Id,
-						Rank = scoreEntry.Rank,
-						Time = scoreEntry.Time,
-					})
-					.ToList(),
-			});
-		}
-		else
-		{
-			Console.WriteLine($"Incomplete top 10 for {currentDate}.");
-
-			// TODO: Add DayEntry with Completeness = Partial and the missing entries copied from the previous day.
+			if (currentBestTimes.TryGetValue(entryHistory.Id, out int value))
+				currentBestTimes[entryHistory.Id] = Math.Max(value, entryHistory.Time);
+			else
+				currentBestTimes.Add(entryHistory.Id, entryHistory.Time);
 		}
 	}
+
+	// Always add a day entry, even if there's no history for that day.
+	dayEntries.Add(new DayEntry
+	{
+		DaysSinceRelease = currentDate.DayNumber - releaseDate.DayNumber,
+		ScoreEntries = top10Players.ConvertAll(p => new ScoreEntry
+		{
+			PlayerId = p,
+			Time = currentBestTimes.GetValueOrDefault(p, 0),
+		}),
+	});
 
 	currentDate = currentDate.AddDays(1);
 }
 
 StringBuilder csvBuilder = new();
-int[] playerIds = dayEntries.SelectMany(d => d.ScoreEntries.Select(s => s.PlayerId)).Distinct().Order().ToArray();
-csvBuilder.AppendLine(";;" + string.Join(";", playerIds.Select(p => p.ToString())));
+csvBuilder.AppendLine("Days since release;" + string.Join(";", top10Players.Select(p => p.ToString())));
 foreach (DayEntry dayEntry in dayEntries)
 {
-	csvBuilder.AppendLine($"{dayEntry.Completeness};{dayEntry.DaysSinceRelease};" + string.Join(";", playerIds.Select(p =>
+	csvBuilder.AppendLine($"{dayEntry.DaysSinceRelease};" + string.Join(";", top10Players.Select(p =>
 	{
-		// TODO: Need to keep track of everyone's score for each day, even if they're not present in the top 10.
 		ScoreEntry? scoreEntry = dayEntry.ScoreEntries.Find(s => s.PlayerId == p);
-		return scoreEntry == null ? string.Empty : (scoreEntry.Time / 10_000f).ToString("0.0000");
+		return scoreEntry == null ? "0.0000" : (scoreEntry.Time / 10_000f).ToString("0.0000");
 	})));
 }
 
 await File.WriteAllTextAsync(Path.Combine(baseDirectory, "history.csv"), csvBuilder.ToString());
 
-internal enum Completeness
-{
-	Complete,
-	Partial,
-	Incomplete,
-}
-
 internal sealed record DayEntry
 {
 	public required int DaysSinceRelease { get; init; }
-	public required Completeness Completeness { get; init; }
 	public required List<ScoreEntry> ScoreEntries { get; init; }
 }
 
 internal sealed record ScoreEntry
 {
 	public required int PlayerId { get; init; }
-	public required int Rank { get; init; }
 	public required int Time { get; init; }
 }
